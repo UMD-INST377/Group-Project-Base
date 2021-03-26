18dd4bcff107fc7a228f09b8da1ab09c9ec8db4e
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-03-26 11:12:15 -0400

    updated server

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.use(express.json());
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 
-app.use('/public', public);
-
 async function bootServer() {
   try {
     const mysql = await db.sequelizeDB;

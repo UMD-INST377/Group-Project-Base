7fed356127a4429c19d1f96ee635fd0a5368071d
JacobVanderVat <jacobvandervat@gmail.com>
2021-03-29 21:36:12 -0400

    Setup

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
 app.use('/api', apiRoutes);
 

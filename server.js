5bd5d51265f25ffde9695448cec17888a623a738
Saamia Bashir <sbashir1@terpmail.umd.edu>
2021-03-30 14:16:39 -0400

    Added staticFolder code in server.js

--- server.js
+++ server.js
@@ -6,9 +6,11 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
 
 app.use('/api', apiRoutes);
 

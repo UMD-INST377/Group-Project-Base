01d3d0d37cfc0df4ce21e9b644f1505b74230480
erhinton <65618575+erhinton@users.noreply.github.com>
2021-03-30 10:41:05 -0400

    Added static folder and live-reload

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 /* eslint-disable no-console */
+import path from 'path';
 import express from 'express';
+import reload from 'livereload';
+import connectReload from 'connect-livereload';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
 
+const __dirname = path.resolve();
+
 const app = express();
 
+
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
+
+const liveReloadServer = reload.createServer();
+liveReloadServer.watch(path.join(__dirname, staticFolder));
+
+app.use(connectReload());
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(staticFolder));
+
 
 app.use('/api', apiRoutes);
 
@@ -22,6 +37,13 @@ async function bootServer() {
   } catch (err) {
     console.error(err);
   }
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('/');
+    }, 100);
+  });
 }
+
+
 
 bootServer();

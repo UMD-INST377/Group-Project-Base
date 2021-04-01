4e93595a87ca36e35e5f504043a7063214c4405e
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-04-01 17:51:44 -0400

    added ada table

---
+++ models/ada_compliance.js
@@ -0,0 +1,18 @@
+export default (sequelize, DataTypes) => {
+    const adaCompliance = sequelize.define(
+        'ada_compliance',
+        {
+            ada_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                unique: true
+            },
+            ada_type: {
+                type: DataTypes.STRING,
+            }
+        },
+        {freezeTableName: true, timestamps: false}
+    );
+    return visitorInfo;
+};

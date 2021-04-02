75dc27db90d795a68f145c031ccabb1f1f853465
dbongiov <61100159+dbongiov@users.noreply.github.com>
2021-04-02 11:19:54 -0400

    visits table

---
+++ models/museum_visits.js
@@ -0,0 +1,35 @@
+export default (sequelize, DataTypes) => {
+    const visitorVisits = sequelize.define(
+        'museum_visits',
+        {
+            visitor_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+            },
+            museum_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+            },
+            visit_date: {
+                type: DataTypes.DATETIME,
+                allowNull: false,
+                primaryKey: true,
+            },
+            member_status: {
+                type: DataTypes.TINYINT,
+                allowNull: true,
+            },
+            fk_visitors_has_Museum_info_Museum_info1: {
+                type: DataTypes.STRING,
+                foreignKey: true,
+            },
+            fk_visitors_has_Museum_info_visitors1 {
+                type: DataTypes.STRING,
+                foreignKey: true,
+            },
+        {freezeTableName: true, timestamps: false}
+    );
+    return visitorInfo;
+};

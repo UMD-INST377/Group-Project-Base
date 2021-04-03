d2a7a07419be3cbea80bc12c0beb9ff34856d392
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-03 14:49:01 -0400

    update formating with ES lint

--- models/visitors.js
+++ models/visitors.js
@@ -1,27 +1,27 @@
 export default (sequelize, DataTypes) => {
-    const visitorInfo = sequelize.define(
-        'visitor_info',
-        {
-            visitor_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-                unique: true
-            },
-            visitor_fn: {
-                type: DataTypes.STRING,
-            },
-            visitor_ln: {
-                type: DataTypes.STRING,
-            },
-            visitor_phone_num: {
-                type: DataTypes.STRING,
-            },
-            visitor_email: {
-                type: DataTypes.STRING,
-            }
-        },
-        {freezeTableName: true, timestamps: false}
-    );
-    return visitorInfo;
+  const visitorInfo = sequelize.define(
+    'visitor_info',
+    {
+      visitor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        unique: true
+      },
+      visitor_fn: {
+        type: DataTypes.STRING
+      },
+      visitor_ln: {
+        type: DataTypes.STRING
+      },
+      visitor_phone_num: {
+        type: DataTypes.STRING
+      },
+      visitor_email: {
+        type: DataTypes.STRING
+      }
+    },
+    {freezeTableName: true, timestamps: false}
+  );
+  return visitorInfo;
 };

0167feeebe1f55e01c74e60e84fbf40b116633e6
nisaputri <pnisa1911@gmail.com>
2021-04-05 16:18:14 -0400

    UPDATE- UPDATE the index file

--- models/index.js
+++ models/index.js
@@ -1,16 +1,19 @@
-import ArtisticMovement from './artistic_movement.js';
+import ArtisticMovement from './ArtisticMovement.js';
 import Authors from './authors.js';
 import Macros from './description.js';
-import Meals from './genre_has_popular_book.js';
+import genre_has_popular_books from './genre_has_popular_book.js';
 import Meals from './genre.js';
 import Meals from './popular_books.js';
-import Meals from './publisher.js';
-import Meals from './retailers.js';
+import Publishers from './publisher.js';
+import Retailers from './retailers.js';
 
 
 export default {
     ArtisticMovement,
     Authors,
   Macros,
-  Meals
+  genre_has_popular_books,
+  Meals,
+  Publishers,
+  Retailers
 };

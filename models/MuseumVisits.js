e4a8508f465d2e0e6cf58c0bbebc49c3baf33577
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-06 09:32:35 -0400

    remove extra commas and format

--- models/MuseumVisits.js
+++ models/MuseumVisits.js
@@ -1,34 +1,34 @@
 export default (sequelize, DataTypes) => {
-    const MuseumVisits = sequelize.define(
-        'museum_visits',
-        {
-            visitor_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-            },
-            museum_id: {
-                type: DataTypes.INTEGER,
-                allowNull: false,
-                primaryKey: true,
-            },
-            visit_date: {
-                type: DataTypes.DATETIME,
-                allowNull: false,
-                primaryKey: true,
-            },
-            member_status: {
-                type: DataTypes.BOOLEAN,
-                allowNull: true,
-            },
-            fk_visitors_has_Museum_info_Museum_info1: {
-                type: DataTypes.STRING,
-            },
-            fk_visitors_has_Museum_info_visitors1: {
-                type: DataTypes.STRING,
-            }
-        },
-        {freezeTableName: true, timestamps: false}
-    );
-    return MuseumVisits;
+  const MuseumVisits = sequelize.define(
+    'museum_visits',
+    {
+      visitor_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      museum_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      visit_date: {
+        type: DataTypes.DATETIME,
+        allowNull: false,
+        primaryKey: true
+      },
+      member_status: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true
+      },
+      fk_visitors_has_Museum_info_Museum_info1: {
+        type: DataTypes.STRING
+      },
+      fk_visitors_has_Museum_info_visitors1: {
+        type: DataTypes.STRING
+      }
+    },
+    {freezeTableName: true, timestamps: false}
+  );
+  return MuseumVisits;
 };

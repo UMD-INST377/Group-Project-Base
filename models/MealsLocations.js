b5ea52292940a0e697b4918eb37518a26cb72049
YaredDemissie <demissieyared2@gmail.com>
2021-04-06 08:35:35 -0400

    Update MealsLocations.js

--- models/MealsLocations.js
+++ models/MealsLocations.js
@@ -1,6 +1,6 @@
 export default (sequelize, DataTypes) => {
-  const Meals = sequelize.define(
-    "Meals_Locations",
+  const Meals_Locations = sequelize.define(
+    "meals_locations",
     {
       hall_id: {
         type: DataTypes.INTEGER,
@@ -13,5 +13,5 @@ export default (sequelize, DataTypes) => {
     },
     { freezeTableName: true, timestamps: false }
   );
-  return Meals;
+  return Meals_Locations;
 };

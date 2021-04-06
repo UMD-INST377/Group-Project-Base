3d7223ee8034276c0773767736b93d6d48b70191
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-05 21:06:16 -0400

    add semicolon line 5

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import MuseumInfo from './MuseumInfo.js';
 import MuseumStaff from './MuseumStaff.js';
 import StaffRole from './StaffRole.js';
 import Visitors from './visitors.js';
-import Visitor_Transactions from './visitor_transactions.js'
+import Visitor_Transactions from './visitor_transactions.js';
 
 export default {
   MuseumInfo,

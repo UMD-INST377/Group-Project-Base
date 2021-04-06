32f389a9479e85e2cbfebd0c18624cabfdd07465
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-06 09:32:03 -0400

    update name formate

--- models/MuseumAmenityLink.js
+++ models/MuseumAmenityLink.js
@@ -1,17 +1,17 @@
 export default (sequelize, DataTypes) => {
-    const Museum_Amenity_Link = sequelize.define(
-      "museum_amenity_link",
-      {
-        amenity_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        museum_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
+  const MuseumAmenityLink = sequelize.define(
+    'museum_amenity_link',
+    {
+      amenity_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return Museum_Amenity_Link;
-  };
+      museum_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return MuseumAmenityLink;
+};

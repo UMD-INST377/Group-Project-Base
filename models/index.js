41f9b8ba8e820037191e488bcbe30053e14a1867
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-04-06 09:31:16 -0400

    delete unused models from index and update name formatting

--- models/index.js
+++ models/index.js
@@ -1,11 +1,6 @@
 import AdaCompliance from './AdaCompliance.js';
-import DietaryRestrictions from './DietaryRestrictions.js';
-import DiningHall from './DiningHall.js';
-import Macros from './Macros.js';
-import Meals from './Meals.js';
-import Meals_Locations from './MealsLocations.js'
-import Museum_Amenity from './MuseumAmenity.js';
-import Museum_Amenity_Link from './MuseumAmenityLink.js';
+import MuseumAmenity from './MuseumAmenity.js';
+import MuseumAmenityLink from './MuseumAmenityLink.js';
 import MuseumInfo from './MuseumInfo.js';
 import MuseumStaff from './MuseumStaff.js';
 import MuseumVisits from './MuseumVisits.js';
@@ -13,16 +8,10 @@ import StaffRole from './StaffRole.js';
 import VisitorTransactions from './visitor_transactions.js';
 import Visitors from './visitors.js';
 
-
 export default {
   AdaCompliance,
-  DietaryRestrictions,
-  DiningHall,
-  Macros,
-  Meals,
-  Meals_Locations,
-  Museum_Amenity,
-  Museum_Amenity_Link,
+  MuseumAmenity,
+  MuseumAmenityLink,
   MuseumInfo,
   MuseumStaff,
   MuseumVisits,

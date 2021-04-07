a2227b4fcdb155aac78ebcdb113370bd5cc23bca
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-07 14:38:17 -0400

    add models to index file

--- models/index.js
+++ models/index.js
@@ -2,10 +2,17 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
+import AnimalType from './AnimalTypes.js';
+import Animals from './Animals.js';
+import Applicants from './Applicants.js';
+
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
-  Meals
+  Meals,
+  AnimalType,
+  Animals,
+  Applicants
 };

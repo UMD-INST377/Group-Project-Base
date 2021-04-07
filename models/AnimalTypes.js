761efe0d7915cc4b98999236be2d77c89b877387
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-07 14:37:57 -0400

    add and set up model js files

---
+++ models/AnimalTypes.js
@@ -0,0 +1,18 @@
+export default (database, DataTypes) => {
+    const AnimalType = database.define(
+      'animal_type',
+      {
+        species_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        species_name: {
+          type: DataTypes.STRING
+        },
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return AnimalType;
+  };
+  
---
+++ models/Applicants.js
@@ -0,0 +1,27 @@
+export default (database, DataTypes) => {
+    const Applicants = database.define(
+      'applicants',
+      {
+        applicant_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        last_name: {
+          type: DataTypes.STRING
+        },
+        first_name: {
+          type: DataTypes.STRING
+        },
+        phone_number: {
+          type: DataTypes.STRING
+        },
+        age: {
+          type: DataTypes.INTEGER
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return Applicants;
+  };
+  

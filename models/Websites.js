fa3242ee7938c75980e95086ec92bd4643131d2b
Ethan Salcedo <53275250+esalcedo1@users.noreply.github.com>
2021-04-07 18:26:47 -0400

    added shelter models, modified animal models

---
+++ models/Websites.js
@@ -0,0 +1,21 @@
+export default (database, DataTypes) => {
+  const Websites = database.define(
+    'Websites',
+  {
+    website_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    Shelters_shelter_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    website_name: {
+      type: DataTypes.STRING
+    }
+  },
+  { freezeTableName: true, timestamps: false }
+  );
+  return Websites;
+};

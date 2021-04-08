016f9874afaa75128327d4fec7d098cedfa63d2f
Chris Ash <christine.ash7@gmail.com>
2021-04-07 20:03:20 -0400

    Muting models/js files that are empty to test if that is source of errors

--- models/index.js
+++ models/index.js
@@ -1,10 +1,10 @@
-import listings from './listings.js';
-import calendar from './calendar.js';
+// import listings from './listings.js';
+// import calendar from './calendar.js';
 import neighborhoods from './neighborhoods.js';
 import scores from './scores.js';
-import hosts from './hosts.js';
+// import hosts from './hosts.js';
 import reviews from './reviews.js';
-import properties from './properties.js';
+// import properties from './properties.js';
 
 export default {
   reviews,

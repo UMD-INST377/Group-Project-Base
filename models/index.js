d816a91854d26db112a76a168109b204448e6e7a
Yazeedz91 <yazeedz91@hotmail.com>
2021-04-14 13:13:46 -0400

    Added Clubs model

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
+import Clubs from './Clubs.js';
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
-  Meals
+  Meals,
+  Clubs
 };

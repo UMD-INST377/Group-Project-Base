8cc67b1a1fec3761cd6c41996413eabcfdc5d101
ajghelman <78034606+ajghelman@users.noreply.github.com>
2021-04-15 16:36:00 -0400

    Updated index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 import Artists from './Artists.js'; // Anna Kafrune
 import Genres from './Genres.js'; // Anna Kafrune
+import songchar from './Song_Characteristics.js'; //Alex Ghelman
 // import Macros from './Macros.js';
 // import Meals from './Meals.js';
 
 export default {
   Artists, // Anna Kafrune
   Genres, // Anna Kafrune
+  songchar, // Song Characteristics - Alex Ghelman
   // Macros,
   // Meals
 };

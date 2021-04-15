add9904c72523b46d3098e95ad26299112f751e4
ajghelman <78034606+ajghelman@users.noreply.github.com>
2021-04-15 15:33:35 -0400

    created js for my table

---
+++ models/Song_Characteristics.js
@@ -0,0 +1,25 @@
+//Alex Ghelman
+
+export default (sequelize, DataTypes) => {
+    const songchar = sequelize.define(
+      'genres',
+      {
+        SONG_CHARACTERISTICS_ID: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true
+        },
+        SONG_ID: {
+          type: DataTypes.INTEGER
+        },
+        CHARACTERISTICS_ID: {
+          type: DataTypes.INTEGER
+        },
+        SONG_CHARACTERISTICS_VALUE: {
+          type: DataTypes.INTEGER  
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return songchar;
+  };

a435a5095a5ed44253466f63460b420d1737add4
LeToucanDev <34908969+LeToucanDev@users.noreply.github.com>
2021-04-16 16:07:15 -0700

    Update index.js
    Updating using Baldric's branch

--- models/index.js
+++ models/index.js
@@ -1,9 +1,10 @@
 import Artists from './Artists.js';
 import Albums from './Albums.js';
 import Macros from './Macros.js';
+import Lyrics from './Lyrics.js';
 
 export default {
   Artists,
   Albums,
-  Macros
+  Lyrics
 };

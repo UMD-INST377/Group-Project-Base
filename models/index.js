b2d88f686648cda7ceb5af3be92e225c3cb9b000
JaredAveryCaplan <78220609+JaredAveryCaplan@users.noreply.github.com>
2021-04-16 18:16:30 -0400

    index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,13 @@
 import Artists from './Artists.js'; // Anna Kafrune
 import Genres from './Genres.js'; // Anna Kafrune
+import Characteristics from './Characteristics.js';//Jared Caplan
 // import Macros from './Macros.js';
 // import Meals from './Meals.js';
 
 export default {
   Artists, // Anna Kafrune
   Genres, // Anna Kafrune
+  Characteristics, //Jared
   // Macros,
   // Meals
 };

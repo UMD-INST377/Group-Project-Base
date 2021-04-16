c4f4fa4cf7c941108c212504634539c24e72d450
Hamid-00 <78131168+Hamid-00@users.noreply.github.com>
2021-04-16 09:56:32 -0400

    hosts.js update

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@
 // import calendar from './calendar.js';
 import neighborhoods from './neighborhoods.js';
 import scores from './scores.js';
-// import hosts from './hosts.js';
+import hosts from './hosts.js';
 import reviews from './reviews.js';
 // import properties from './properties.js';
 

3a68a81cf3e2c8844496e12f50f59e35c27e2994
Segen Tirfe <tirfesegen@gmail.com>
2021-04-16 19:13:56 -0400

    added Genre js file

---
+++ models/Genre.js
@@ -0,0 +1,17 @@
+export default (database, DataTypes) => {
+    const Genre = database.define(
+      'Genre',
+      {
+        Genre_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+        },
+        Genre_title: {
+          type: DataTypes.STRING
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return Genre;
+  };

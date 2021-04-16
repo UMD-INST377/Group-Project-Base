6bd98396c6bc1740acfeea6ebb603bcfffab1175
qhollis <58087502+qhollis@users.noreply.github.com>
2021-04-15 20:35:13 -0400

    social media / staidum info js

--- models/SocialMedia.js
+++ models/SocialMedia.js
@@ -1,3 +1,4 @@
+
 export default (sequelize, DataTypes) => {
   const SocialMedia = sequelize.define(
     'SocialMedia',
--- models/StadiumInfo.js
+++ models/StadiumInfo.js
@@ -1,3 +1,4 @@
+
 export default (sequelize, DataTypes) => {
   const StadiumInfo = sequelize.define(
     'StadiumInfo',

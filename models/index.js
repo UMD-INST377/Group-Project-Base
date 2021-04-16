36c04cc866367df0657d680e481bfdc1b94a45a8
j13krause <51761305+j13krause@users.noreply.github.com>
2021-04-16 17:47:36 -0400

    Took out unused models and added table to see if everything is working

--- models/index.js
+++ models/index.js
@@ -1,12 +1,9 @@
-import DietaryRestrictions from './DietaryRestrictions.js';
 import movies from './movies.js';
 import Director from './Director.js';
-import Meals from './Meals.js';
+
 
 
 export default {
-  DietaryRestrictions,
   movies,
-  Director,
-  Meals
+  Director
 };

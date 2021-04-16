409dc5fa808647a3dd81c282ed7bce02ce81497d
alechacon99 <77997217+alechacon99@users.noreply.github.com>
2021-04-16 18:38:36 -0400

    Update Lyrics.js

--- models/Lyrics.js
+++ models/Lyrics.js
@@ -1,23 +1,23 @@
 export default (sequelize, DataTypes) => {
-    const Lyrics = sequelize.define(
-      'Lyrics',
-      {
-        lyrics_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          unique: true,
-          primaryKey: true
-        },
-        lyrics: {
-          type: DataTypes.STRING
-        },
-        song_id: {
-          type: DataTypes.INTEGER,
-          allownull: false,
-          unique: true
-        }
+  const Lyrics = sequelize.define(
+    'Lyrics',
+    {
+      lyrics_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return Lyrics;
-  };
+      lyrics: {
+        type: DataTypes.STRING
+      },
+      song_id: {
+        type: DataTypes.INTEGER,
+        allownull: false,
+        unique: true
+      }
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return Lyrics;
+};

aa3019ffe3bb8f75d6871a07729a0555a6640a15
xiaoma521 <57551433+xiaoma521@users.noreply.github.com>
2021-04-17 00:10:21 -0400

    Create config.js

--- database/config.js
+++ database/config.js
@@ -2,23 +2,22 @@ export default {
   development: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'group14MovieDB',
+    database: 'mydb',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   test: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'group14MovieDB',
+    database: 'mydb',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   production: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'group14MovieDB',
+    database: 'mydb',
     host: '3.236.243.212',
     dialect: 'mysql'
   }
 };
-

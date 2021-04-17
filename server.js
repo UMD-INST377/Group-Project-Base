d7394cf35689a721c7b35cfd31044061dfc2a70c
HaoLiangCQ <78054019+HaoLiangCQ@users.noreply.github.com>
2021-04-16 23:42:34 -0400

    port change in server.js
    3000 => 5000

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const staticFolder = "public";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

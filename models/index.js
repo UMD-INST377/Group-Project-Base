cbd7b07e4f3c485ff775d27090585ce1d14c6860
Lauren Eng <73508837+littledumpleng@users.noreply.github.com>
2021-04-17 15:58:23 -0400

    Renamed apiRoutes.js and Roles.js, customized index.js, reformatted server.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,25 @@
-import DietaryRestrictions from './DietaryRestrictions.js';
-import DiningHall from './DiningHall.js';
-import Macros from './Macros.js';
-import Meals from './Meals.js';
+import Media from './Media.js';
+import Creators from './DiningHall.js';
+import Backgrounds from './Macros.js';
+import Genres from './Meals.js';
+import Roles from './Meals.js';
+import Themes from './Meals.js';
+import MediaBackgroundLinks from './Meals.js';
+import   MediaCreatorLinks from './Meals.js';
+import MediaGenreLinks from './Meals.js';
+import MediaThemeLinks from './Meals.js';
+import CreatorRoleLinks from './Meals.js';
 
 export default {
-  DietaryRestrictions,
-  DiningHall,
-  Macros,
-  Meals
+  Media,
+  Creators,
+  Backgrounds,
+  Genres,
+  Roles,
+  Themes,
+  MediaBackgroundLinks,
+  MediaCreatorLinks,
+  MediaGenreLinks,
+  MediaThemeLinks,
+  CreatorRoleLinks,
 };

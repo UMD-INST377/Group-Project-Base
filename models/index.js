f7e8251527bb3e8fed43a63ef1c5855962e99ae9
mlima <mlima@umd.edu>
2021-04-16 20:09:49 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,11 @@
 import ArtistInfo from './ArtistInfo.js';
 import Playlist from './Playlists.js';
-import Macros from './Macros.js';
 import PlaylistDetails from './PlaylistDetails';
 import SongDetails from './SongDetails.js';
 
 export default {
   ArtistInfo,
   Playlist,
-  Macros,
   PlaylistDetails,
   SongDetails
 };

c1f1c56478171515b56e8bac4900df01766ad859
Emily Zheng <69724738+emzheng1@users.noreply.github.com>
2021-04-17 02:11:02 -0400

    Update Artists.js

--- models/Artists.js
+++ models/Artists.js
@@ -1,26 +1,26 @@
 export default (sequelize, DataTypes) => {
-    const Artists = sequelize.define(
-      'artist',
-      {
-        artist_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true
-        },
-        artist_name: {
-          type: DataTypes.STRING,
-          allowNull: false
-        },
-        verified: {
-          type: DataTypes.BOOLEAN,
-          allowNull: false
-        },
-        monthly_listeners: {
-          type: DataTypes.INTEGER,
-          allowNull: true
-        }
+  const Artists = sequelize.define(
+    'artist',
+    {
+      artist_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return Artists;
+      artist_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      verified: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+      },
+      monthly_listeners: {
+        type: DataTypes.INTEGER,
+        allowNull: true
+      }
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return Artists;
 };

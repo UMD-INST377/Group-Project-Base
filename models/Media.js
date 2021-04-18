cd23a0e96e3d4e8a9708b7e207421dd655f728c7
carcamo1 <carcamo1@users.noreply.github.com>
2021-04-18 19:39:07 -0400

    medias

--- models/Media.js
+++ models/Media.js
@@ -1,5 +1,41 @@
 export default (database, DataTypes) => {
     const media = database.define(
-        'Media',
+     'Media',
+        {
+            media_id: {
+                type: DataTypes.INTEGER,
+                allowNull = false, 
+                primaryKey = true
+            },
+            media_title: {
+                type: DataTypes.STRING
+            },
+            media_type: {
+                type: DataTypes.STRING
+            },
+            media_release_year: {
+                type: DataTypes.INTEGER
+            },
+            media_description: {
+                type: DataTypes.STRING
+            },
+            media_duration: {
+                type: DateTypes.STRING
+            },
+            albums_songs_number : {
+                type: DataTypes.INTEGER
+            },
+            television_seasons_number: {
+                type: DataTypes.INTEGER
+            },
+            audience_rating :{
+                type: DataTypes.DECIMAL
+            },
+            show_still_airing: {
+                type: DataTypes.STRING
+            }
+        },
+        { freezeTableName: true, timestamps: false }
     )
+    return media;
 }

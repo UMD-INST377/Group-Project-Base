a75788b04ea8f4eb9dfedbe208fab776f34728d6
Katherine Vo <katherinevo09@gmail.com>
2021-04-20 10:51:29 -0400

    fixed class name to populate global charts table

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -182,6 +182,34 @@ router.route('/us/:us_top50_rank')
 /// /////////////////////////////////
 /// ////GlobalTop50 Endpoints////////////
 /// /////////////////////////////////
+router.route('/wholeGlobalChart')
+  .get(async (req, res) => {
+    try {
+      const ranks = await db.GlobalTop50.findAll();
+      const artists = await db.Artists.findAll();
+      const songs = await db.Songs.findAll();
+      // map is like a for each
+      const wholeGlobalChart = ranks.map((rank) => {
+        const songEntry = songs.find((song) => song.song_id === rank.song_id);
+        const artistEntry = artists.find((artist) => artist.artist_id === rank.artist_id);
+        // console.log('stream', stream.dataValues);
+        // console.log('songEntry', songEntry.dataValues);
+        // console.log('artistEntry', artistEntry.dataValues);
+
+        return {
+          // spread operator
+          ...rank.dataValues,
+          ...songEntry.dataValues,
+          ...artistEntry.dataValues
+        }
+      })
+      res.json({data: wholeGlobalChart});
+
+    } catch (err) {
+      console.error(err);
+      res.json({message: "Something went wrong on the server"});
+    }
+});
 
 router.route('/global')
   .get(async (req, res) => {

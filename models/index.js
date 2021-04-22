37205849d149a9e87a5b2bc4e3b069dd8a8ec5f8
xiaoma521 <1273114582@qq.com>
2021-04-21 23:31:55 -0400

    added to index

--- models/index.js
+++ models/index.js
@@ -1,9 +1,15 @@
 import movies from './movies.js';
 import Director from './Director.js';
+import Actors from './Actors.js';
+import productionCompany from './ProductionCompany.js';
+
+
 
 
 
 export default {
   movies,
-  Director
+  Director,
+  Actors,
+  productionCompany
 };

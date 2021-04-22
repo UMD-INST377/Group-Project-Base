a760a089389b37aca81dfe3e9035e2a50801d15c
aferguson9806 <aferguson9806@gmail.com>
2021-04-22 19:57:16 -0400

    finished carousel of images in js

--- public/scripts.js
+++ public/scripts.js
@@ -16,25 +16,33 @@ async function loadMyData() {
     return newMyData;
 }
 
-data = loadMyData();
-console.log(data);
-
 function leftShift() {
     const one = data.shift();
     data.push(one);
-    display = data.slice(0);
+    display = data[0];
+    console.log(display);
     myDisplay.setAttribute('src', display);
-    console.log(data);
 }
 
 function rightShift() {
     const last = data.pop();
     data.unshift(last);
-    display = data.slice(0);
+    display = data[0];
+    console.log(display);
     myDisplay.setAttribute('src', display);
-    console.log(data);
 }
 
-left.addEventListener('click', leftShift);
 
-right.addEventListener('click', rightShift);
+
+
+
+async function windowActions() {
+    data = await loadMyData();
+
+    left.addEventListener('click', leftShift);
+
+    right.addEventListener('click', rightShift);
+}
+
+window.onload = windowActions()
+

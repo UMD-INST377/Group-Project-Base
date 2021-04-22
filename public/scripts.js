c056b2b806a06f7696513aed7d628a828cc307a0
aferguson9806 <aferguson9806@gmail.com>
2021-04-22 19:42:24 -0400

    working on front page carousel

--- public/scripts.js
+++ public/scripts.js
@@ -1,38 +1,40 @@
-const mybutton = document.getElementById('randomizer');
-const myLabel = document.getElementById('myFurnLabel')
+const myDisplay = document.getElementsByClassName('displayMe')[0];
+const left = document.getElementsByClassName('leftarrow')[0];
+const right = document.getElementsByClassName('rightarrow')[0];
 
-function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+async function loadMyData() {
 
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    myData = []
+    const products = await fetch('/api/products/');
+    const jsonProducts = await products.json();
+
+    jsonProducts["data"].forEach((product) => {
+        myData.push(product.image_link);
+    });
+
+    newMyData = await myData;
+    return newMyData;
 }
 
-async function loadData() {
-    randVal = getRandomIntInclusive(1, 16);
-    const requestProducts = await fetch(`/api/products/${randVal}`);
-    dataProducts = await requestProducts.json();
+data = loadMyData();
+console.log(data);
 
-    const newSrc = dataProducts[0].image_link;
-
-    document.getElementsByClassName('testimg')[0].setAttribute('src', newSrc)
-
-    const fam_id = dataProducts[0].family_id;
-    const cat_id = dataProducts[0].category_id;
-
-
-    const requestFamilies = await fetch (`/api/productFamilies/${fam_id}`);
-    dataFamilies = await requestFamilies.json();
-    
-
-    const requestCategories = await fetch (`/api/productCategories/${cat_id}`);
-    dataCategories = await requestCategories.json();
-
-    const fam_name = dataFamilies[0].family_name;
-    const cat_name = dataCategories[0].category_name;
-
-    myLabel.innerHTML = fam_name + ' ' + cat_name;
+function leftShift() {
+    const one = data.shift();
+    data.push(one);
+    display = data.slice(0);
+    myDisplay.setAttribute('src', display);
+    console.log(data);
 }
 
+function rightShift() {
+    const last = data.pop();
+    data.unshift(last);
+    display = data.slice(0);
+    myDisplay.setAttribute('src', display);
+    console.log(data);
+}
 
-mybutton.addEventListener('click', loadData);
+left.addEventListener('click', leftShift);
+
+right.addEventListener('click', rightShift);

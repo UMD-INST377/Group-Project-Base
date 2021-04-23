9eba8116f3b1b3926817b4a37b1216ee9bc13715
Kati Ilieva <78005557+ks-ilieva@users.noreply.github.com>
2021-04-22 23:23:22 -0400

    link to logo and admin icon, preference form

--- public/scripts.js
+++ public/scripts.js
@@ -3,46 +3,40 @@ const left = document.getElementsByClassName('leftarrow')[0];
 const right = document.getElementsByClassName('rightarrow')[0];
 
 async function loadMyData() {
+  myData = [];
+  const products = await fetch('/api/products/');
+  const jsonProducts = await products.json();
 
-    myData = []
-    const products = await fetch('/api/products/');
-    const jsonProducts = await products.json();
+  jsonProducts["data"].forEach((product) => {
+    myData.push(product.image_link);
+  });
 
-    jsonProducts["data"].forEach((product) => {
-        myData.push(product.image_link);
-    });
-
-    newMyData = await myData;
-    return newMyData;
+  newMyData = await myData;
+  return newMyData;
 }
 
 function leftShift() {
-    const one = data.shift();
-    data.push(one);
-    display = data[0];
-    console.log(display);
-    myDisplay.setAttribute('src', display);
+  const one = data.shift();
+  data.push(one);
+  display = data[0];
+  console.log(display);
+  myDisplay.setAttribute('src', display);
 }
 
 function rightShift() {
-    const last = data.pop();
-    data.unshift(last);
-    display = data[0];
-    console.log(display);
-    myDisplay.setAttribute('src', display);
+  const last = data.pop();
+  data.unshift(last);
+  display = data[0];
+  console.log(display);
+  myDisplay.setAttribute('src', display);
 }
-
-
-
-
 
 async function windowActions() {
-    data = await loadMyData();
+  data = await loadMyData();
 
-    left.addEventListener('click', leftShift);
+  left.addEventListener('click', leftShift);
 
-    right.addEventListener('click', rightShift);
+  right.addEventListener('click', rightShift);
 }
 
-window.onload = windowActions()
-
+window.onload = windowActions();

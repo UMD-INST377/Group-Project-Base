017a736c9a3f0788a0b0cd09cc5002e5b7d83540
accunniff <accunniff@gmail.com>
2021-04-22 20:32:27 -0400

    new

--- public/scripts.js
+++ public/scripts.js
@@ -1,7 +1,19 @@
-const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
 
 // Option 1: Passing a connection URI
-const sequelize = new Sequelize('AAPI_Art_Corner_377', 'student', 'INST377@UMD', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
+const sequelize = new Sequelize(
+  "AAPI_Art_Corner_377",
+  "student",
+  "INST377@UMD",
+  {
+    host: "localhost",
+    dialect: "mysql"
+  }
+);
+
+try {
+  await sequelize.authenticate();
+  console.log("Connection has been established successfully.");
+} catch (error) {
+  console.error("Unable to connect to the database:", error);
+}

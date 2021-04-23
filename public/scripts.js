11e17b6fa88f1f9f3159d092136c1b0bd634dc2e
gHenriquez99 <henriquez.gus99@gmail.com>
2021-04-23 12:34:14 -0400

    progress with preference form. TODO: figure out clear button using js

--- public/scripts.js
+++ public/scripts.js
@@ -1,13 +1,25 @@
 const myDisplay = document.getElementsByClassName('displayMe')[0];
 const left = document.getElementsByClassName('leftarrow')[0];
 const right = document.getElementsByClassName('rightarrow')[0];
+const fpClear = document.querySelector('#fpClear');
+
+function clearButtons() {
+  fpClear.addEventListener('click', () => {
+    console.log('Clicked');
+    // document.getElementById('Chairs').checked = false;
+    // document.getElementById('Sofas').checked = false;
+    // document.getElementById('Beds').checked = false;
+    // document.getElementById('Drawers').checked = false;
+    // document.getElementById('Rugs').checked = false;
+  });
+}
 
 async function loadMyData() {
   myData = [];
   const products = await fetch('/api/products/');
   const jsonProducts = await products.json();
 
-  jsonProducts["data"].forEach((product) => {
+  jsonProducts.data.forEach((product) => {
     myData.push(product.image_link);
   });
 
@@ -18,7 +30,7 @@ async function loadMyData() {
 function leftShift() {
   const one = data.shift();
   data.push(one);
-  display = data[0];
+  const {display} = data[0];
   console.log(display);
   myDisplay.setAttribute('src', display);
 }
@@ -26,7 +38,7 @@ function leftShift() {
 function rightShift() {
   const last = data.pop();
   data.unshift(last);
-  display = data[0];
+  const {display} = data[0];
   console.log(display);
   myDisplay.setAttribute('src', display);
 }
@@ -37,6 +49,8 @@ async function windowActions() {
   left.addEventListener('click', leftShift);
 
   right.addEventListener('click', rightShift);
+
+  clearButtons();
 }
 
 window.onload = windowActions();

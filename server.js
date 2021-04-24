0745014eb008c086b18920356134a8d2b1a47c0f
LeToucanDev <34908969+LeToucanDev@users.noreply.github.com>
2021-04-23 17:12:20 -0700

    My attempt to fixing thingies. Not as Good as AH-MANDA

--- server.js
+++ server.js
@@ -1,24 +1,37 @@
 /* eslint-disable no-console */
 import express from 'express';
+import path from 'path';
+
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
 
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 const app = express();
 const staticFolder = 'public';
+
 
 const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static(staticFolder));
+
 
 app.use('/api', apiRoutes);
 app.get('/', () => {
   res.send('index.html');
 });
 
+
+
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/about', (req, res) => {
   res.render('about');

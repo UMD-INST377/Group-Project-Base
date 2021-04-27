4a945cca121f4c1466e4ec47b786697747d33b47
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-27 18:45:01 -0400

    fix table classes and populate functions in script

--- public/script.js
+++ public/script.js
@@ -6,16 +6,16 @@ async function populateAnimals() {
   const animalTable = document.querySelector('.animaltable');
 
   animalData.forEach((animal) => {
-    const appendItem = document.createElement('tr');
-    appendItem.innerHTML = `
+    const appendAnimal = document.createElement('tr');
+    appendAnimal.innerHTML = `
     <td>${animal.animal_id}</td>
     <td>${animal.name}</td>
     <td>${animal.status}</td>
     <td>${animal.gender}</td>
     <td>${animal.Animal_type_species_id}</td>
     `;
-
-    animalTable.append(appendItem);
+    animalTable.append(appendAnimal);
+    console.table(animalData);
   });
 }
 
@@ -23,22 +23,50 @@ async function populateApplicants() {
   console.log('data request');
   const applicantRequest = await fetch('/api/Applicants');
   const applicantData = await applicantRequest.json();
-  //   return applicantData;
-  console.table(applicantData);
+
+  const applicantsTable = document.querySelector('.applicantstable');
+
+  applicantData.forEach((applicant) => {
+    const appendApplicant = document.createElement('tr');
+    appendApplicant.innerHTML = `
+    <td>${applicant.applicant_id}</td>
+    <td>${applicant.last_name}</td>
+    <td>${applicant.first_name}</td>
+    <td>${applicant.phone_number}</td>
+    <td>${applicant.age}</td>
+    `;
+
+    applicantsTable.append(appendApplicant);
+    console.table(applicantData);
+  });
 }
 
 async function populateShelters() {
   console.log('data request');
-  const shelterRequest = await fetch('/api/Shelters');
+  const shelterRequest = await fetch('/api/shelters');
   const shelterData = await shelterRequest.json();
-  //   return shelterData;
-  console.table(shelterData);
+
+  const sheltersTable = document.querySelector('.shelterstable');
+
+  shelterData.forEach((shelter) => {
+    const appendShelter = document.createElement('tr');
+    appendShelter.innerHTML = `
+    <td>${shelter.shelter_id}</td>
+    <td>${shelter.shelter_name}</td>
+    <td>${shelter.shelter_address}</td>
+    <td>${shelter.phone_number}</td>
+    <td>${shelter.num_employees}</td>
+    `;
+
+    sheltersTable.append(appendShelter);
+    console.table(shelterData);
+  });
 }
 
 async function windowActions() {
-  populateAnimals();
-  populateShelters();
-  populateApplicants();
+  await populateAnimals();
+  await populateApplicants();
+  await populateShelters();
 }
 
 window.onload = windowActions;

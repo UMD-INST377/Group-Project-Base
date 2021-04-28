2bc57c360830364910fe70cbc03d12313f27b859
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-28 10:18:38 -0400

    add missing data for websites table

--- public/Websites_async.js
+++ public/Websites_async.js
@@ -2,7 +2,7 @@
 // eslint-disable-next-line no-unused-vars
 async function populateWebsites() {
   console.log('data request');
-  const websiteRequest = await fetch('/api/websites');
+  const websiteRequest = await fetch('/api/Websites');
   const websiteData = await websiteRequest.json();
 
   const websiteTable = document.querySelector('.websitestable');
@@ -11,6 +11,9 @@ async function populateWebsites() {
     const appendWebsite = document.createElement('tr');
     appendWebsite.innerHTML = `
       <td>${website.website_id}</td>
+      <td>${website.shelter_id}</td>
+      <td>${website.website_name}</td>
+
       `;
 
     websiteTable.append(appendWebsite);

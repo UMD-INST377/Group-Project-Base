9ee24a5a0913a1523ecfadc85f4b9aa9977e7aa1
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-04-28 22:19:06 +0800

    earnings

--- models/earnings.js
+++ models/earnings.js
@@ -1,23 +1,22 @@
 export default (sequelize, DataTypes) => {
-    const earnings = sequelize.define(
-      'earnings',
-      {
-        movie_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-        },
-        earnings_gross: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true
-        },
-        budget: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
+  const earnings = sequelize.define(
+    'earnings',
+    {
+      movie_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return earnings;
-  };
-  
+      earnings_gross: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
+      },
+      budget: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return earnings;
+};
--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 import actor from './actor.js';
 import actorHasMovies from './actor_has_movies.js';
-import earnings from './earnings';
+import earnings from './earnings.js';
 import genre from './genre.js';
 import movies from './movies.js';
 import movieHasGenre from './movie_has_genre';

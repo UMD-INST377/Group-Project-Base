4983b38ad5bf70365c4b43ed7ade7fd06e6d9c42
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-04-28 10:14:28 -0400

    add missing data for employees table

--- public/Employee_async.js
+++ public/Employee_async.js
@@ -2,17 +2,23 @@
 async function populateEmployees() {
   console.log('data request');
 
-  const employeesRequest = await fetch('/api/employees');
+  const employeesRequest = await fetch('/api/Employees');
   const employeesData = await employeesRequest.json();
   const employeeTable = document.querySelector('.employeetable');
+  console.table(employeesData);
 
   employeesData.forEach((employee) => {
     const appendEmployee = document.createElement('tr');
     appendEmployee.innerHTML = `
       <td>${employee.employee_id}</td>
+      <td>${employee.last_name}</td>
+      <td>${employee.first_name}</td>
+      <td>${employee.phone_number}</td>
+      <td>${employee.age}</td>
+      <td>${employee.employee_type}</td>
+      <td>${employee.shelter_id}</td>
       `;
     employeeTable.append(appendEmployee);
-    console.table(employeesData);
   });
 }
 

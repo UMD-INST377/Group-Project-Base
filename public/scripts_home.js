3862a854d5475edaff36bb2a0ea0213cc9ebcac9
gHenriquez99 <henriquez.gus99@gmail.com>
2021-04-28 10:26:43 -0400

    makes the image carousel 5 random images

--- public/scripts_home.js
+++ public/scripts_home.js
@@ -2,14 +2,27 @@ const myDisplay = document.getElementsByClassName('displayMe')[0];
 const left = document.getElementsByClassName('leftarrow')[0];
 const right = document.getElementsByClassName('rightarrow')[0];
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 async function loadMyData() {
-  myData = [];
   const products = await fetch('/api/products/');
   const jsonProducts = await products.json();
 
-  jsonProducts.data.forEach((product) => {
-    myData.push(product.image_link);
-  });
+  // gets an array of 5 random, unique index values that is within the range of jsonProducts
+  const indexArray = [];
+  while (indexArray.length < 5) {
+    const randomNumber = getRandomIntInclusive(0, jsonProducts.data.length - 1);
+    if (indexArray.indexOf(randomNumber) === -1) {
+      indexArray.push(randomNumber);
+    }
+  }
+
+  // gets the link associated to the 5 random index values above
+  const myData = indexArray.map((element) => jsonProducts.data[element].image_link);
 
   newMyData = await myData;
   return newMyData;

e5082ceeeaf49a9e9511d6fb8539b78dc12d56cf
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-04-29 12:02:20 +0800

    Update config.js

--- database/config.js
+++ database/config.js
@@ -2,21 +2,21 @@ export default {
   development: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'inst377_data',
+    database: 'inst377_imdb',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   test: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'inst377_data',
+    database: 'inst377_imdb',
     host: '3.236.243.212',
     dialect: 'mysql'
   },
   production: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'inst377_data',
+    database: 'inst377_imdb',
     host: '3.236.243.212',
     dialect: 'mysql'
   }

ed11b0689cfbcfc48b4325c7ccbc02606cfce989
xqiao30 <78039384+xqiao30@users.noreply.github.com>
2021-04-29 11:49:27 +0800

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ import actorHasMovies from './actor_has_movies.js';
 import earnings from './earnings.js';
 import genre from './genre.js';
 import movies from './movies.js';
-import movieHasGenre from './movie_has_genre';
+import movieHasGenre from './movie_has_genre.js';
 
 export default {
   actor,

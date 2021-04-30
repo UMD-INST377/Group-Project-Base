28adc1af141459f8c2b66733418826cd203dbfc4
Charles Pawling <cpawling@umd.edu>
2021-04-30 01:25:32 -0400

    Update SearchScript.js

--- public/SearchScript.js
+++ public/SearchScript.js
@@ -63,7 +63,8 @@ function displayMatches() {
 
 const searchInput = document.querySelector('.input');
 const teaminfo2 = document.querySelector('.TeamInfo');
-
+const formelement = document.querySelector('#formelement');
+formelement.addEventListener('submit', displayMatches)
 searchInput.addEventListener('change', displayMatches);
 searchInput.addEventListener('keyup', displayMatches);
 

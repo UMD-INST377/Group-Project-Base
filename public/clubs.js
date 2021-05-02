5d69eafd8b60d6d63ccd6050b899118cae5c730b
Yazeedz91 <yazeedz91@hotmail.com>
2021-05-02 19:41:56 -0400

    some fixes

--- public/clubs.js
+++ public/clubs.js
@@ -6,16 +6,22 @@ window.onload = async function windowActions() {
     const tableTarget = document.querySelector("#table-target")
     const form = document.querySelector("#search")
     const input = document.querySelector(".input")
-    const tableBox = document.querySelector("#table-box")
-    
+
+    tableTarget.innerHTML = `
+        <tr>
+            <th>Club's Name</th>
+            <th>Coach's Name</th>
+            <th>Number of Players</th>
+        </tr>
+    `
     clubs.forEach(element => {
         const clubName = element.club_name
-        const couchName = element.coach_first_name.concat(" ", element.coach_last_name)
+        const coachName = element.coach_first_name.concat(" ", element.coach_last_name)
         const numPlayers = element.num_of_players
         const table_row = document.createElement('tr')
         table_row.innerHTML = `
             <td>${clubName}</td>
-            <td>${couchName}</td>
+            <td>${coachName}</td>
             <td>${numPlayers}</td>
         `
         tableTarget.append(table_row)
@@ -30,19 +36,18 @@ window.onload = async function windowActions() {
                 console.log(element.club_name)
                 console.log(input.value)
                 const clubName = element.club_name
-                const couchName = element.coach_first_name.concat(" ", element.coach_last_name)
+                const coachName = element.coach_first_name.concat(" ", element.coach_last_name)
                 const numPlayers = element.num_of_players
 
-                const table_row = document.createElement('tr')
                 tableTarget.innerHTML = `
                     <tr>
                         <th>Club's Name</th>
-                        <th>Couch's Name</th>
+                        <th>Coach's Name</th>
                         <th>Number of Players</th>
                     </tr>
                     <tr>
                         <td>${clubName}</td>
-                        <td>${couchName}</td>
+                        <td>${coachName}</td>
                         <td>${numPlayers}</td>
                     </tr>
                 `

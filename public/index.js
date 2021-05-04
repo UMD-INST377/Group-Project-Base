0c5c3577b2177891178d9ab7d6d5f4f06c47596e
Andrea Tavakol <andreatavakol@gmail.com>
2021-05-04 19:12:00 -0400

    Gave the dropdown script its own file

--- public/index.js
+++
@@ -1,6 +0,0 @@
-let dropdown = document.querySelector('.dropdown');
-
-dropdown.addEventListener('click', function(event) {
-  event.stopPropagation();
-  dropdown.classList.toggle('is-active');
-});

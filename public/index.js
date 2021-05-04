b30acf0e00168d5b9376911e0ae1a6344ba426d4
Andrea Tavakol <andreatavakol@gmail.com>
2021-05-04 17:20:19 -0400

    Adjusted code indentation, added script so that dropdown menu isn't always open

---
+++ public/index.js
@@ -0,0 +1,6 @@
+let dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', function(event) {
+  event.stopPropagation();
+  dropdown.classList.toggle('is-active');
+});

b8e55f6edd77d6a38fd97f58f55dabd2c8a17191
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-05-06 00:04:03 +0800

    My part (review has bug)

---
+++ public/review.js
@@ -0,0 +1,51 @@
+const selectEle = document.getElementsByClassName('select')[0];
+const scoreText = document.getElementsByClassName('score-text')[0];
+const submitBtn = document.getElementsByClassName('submit-review')[0];
+
+axios.get(`/movie`,{
+  headers: {"Access-Control-Allow-Origin": "*"},
+  crossdomain: true,
+  crossorigin:true 
+}).then((res) => {
+  const data = res.data;
+  let html = ``
+  data.map((ele) => {
+    html+= `
+      <option value=${ele.movie_id}>${ele.movie_title}</option>
+    `
+  })
+  selectEle.innerHTML = `
+    <select>
+      ${html}
+    </select>
+  `
+}).catch((err) => {
+  console.log('err: ', err);
+})
+
+submitBtn.addEventListener('click', () => {
+  const selectSelEle = selectEle.getElementsByTagName('select')[0]
+  const index = selectSelEle.selectedIndex; // selectedIndex代表的是你所选中项的index
+  console.log('我输入了', changeText.value);
+  console.log('我选中了', selectSelEle.options[index].value, selectSelEle.options[index].text);
+  if (!scoreText.value || !selectSelEle.options[index].value) {
+    alert('请检查输入信息')
+    return;
+  }
+  axios({
+    method: "put",
+    url: `/movie_imdb_ratings`,
+    data: {
+      movie_id: selectSelEle.options[index].value,
+      imdb_score: scoreText.value,
+    },
+  }).then(function (res) { //回调函数  接受服务器参数
+    console.log(res);
+    const data = res.data;
+    data && alert('更新成功')
+  }).catch((error) => {
+    console.log('error: ', error);
+    alert('更新失败')
+  })
+
+})
---
+++ public/script.js
@@ -0,0 +1,29 @@
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
+axios.defaults.baseURL = "http://localhost:3000/api";
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});

55ed122e92132e6a7ffc14c34f23428076de0f5b
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-05-05 19:04:58 -0400

    use employee/shelter endpoint to populate employee page

--- public/Employee_async.js
+++ public/Employee_async.js
@@ -2,7 +2,7 @@
 async function populateEmployees() {
   console.log('data request');
 
-  const employeesRequest = await fetch('/api/Employees');
+  const employeesRequest = await fetch('/api/employeeMapCustom');
   const employeesData = await employeesRequest.json();
   const employeeTable = document.querySelector('.employeetable');
   console.log(employeesData);
@@ -10,13 +10,11 @@ async function populateEmployees() {
   employeesData.forEach((employee) => {
     const appendEmployee = document.createElement('tr');
     appendEmployee.innerHTML = `
-      <td>${employee.employee_id}</td>
-      <td>${employee.last_name}</td>
-      <td>${employee.first_name}</td>
+      <td>${employee.name}</td>
       <td>${employee.phone_number}</td>
       <td>${employee.age}</td>
       <td>${employee.employee_type}</td>
-      <td>${employee.Shelters_shelter_id}</td>
+      <td>${employee.shelter_name}</td>
       `;
     employeeTable.append(appendEmployee);
   });

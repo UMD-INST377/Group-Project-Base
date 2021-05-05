8c124be218a4432785dbddd6a814ad147d35a6c2
gHenriquez99 <henriquez.gus99@gmail.com>
2021-05-05 11:52:29 -0400

    selects display items and puts them in local storage

--- public/preferenceFormScript.js
+++ public/preferenceFormScript.js
@@ -8,6 +8,13 @@ const errorText = document.querySelector('.errorText');
 
 let data;
 
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  // eslint-disable-next-line max-len
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 function calculateResults(event) {
   event.preventDefault(); // stops the page from reloading
   console.log(data);
@@ -15,7 +22,7 @@ function calculateResults(event) {
   let max;
 
   errorText.innerHTML = ''; // clears the errorText if there is any there from before
-  if (budgetSelect.value == 1) { 
+  if (budgetSelect.value == 1) {
     min = 0;
     max = 100;
   } else if (budgetSelect.value == 2) {
@@ -34,7 +41,7 @@ function calculateResults(event) {
   console.log(min);
   console.log(max);
 
-  const preferenceSelections = []; // array that the furniture preferences will end up in 
+  const preferenceSelections = []; // array that the furniture preferences will end up in
   prefCheckboxes.forEach((checkBox) => {
     // eslint-disable-next-line no-param-reassign
     if (checkBox.checked === true) {
@@ -42,12 +49,12 @@ function calculateResults(event) {
     }
   });
   // eslint-disable-next-line max-len
-  if (preferenceSelections.length === 0) { // error handling for if no furniture preference is selected 
+  if (preferenceSelections.length === 0) { // error handling for if no furniture preference is selected
     errorText.innerHTML = 'You must select a furniture preference';
     return;
   }
 
-  const colorSelections = []; // array that the color preferences will end up in 
+  const colorSelections = []; // array that the color preferences will end up in
   colorCheckboxes.forEach((checkBox) => {
     // eslint-disable-next-line no-param-reassign
     if (checkBox.checked === true) {
@@ -68,6 +75,37 @@ function calculateResults(event) {
     };
   });
   console.log(selections);
+
+  // const randomValue = getRandomIntInclusive(0, value.length - 1);
+  // const displayProductId = value[randomValue][0].product_id;
+  // return {
+  //   displayProductId
+  // };
+  // ({
+  //   value.product_id
+  // }));
+
+  const displayItems = [];
+  for (let i = 0; i < selections.length; i += 1) {
+    const array = Object.values(selections[i]);
+    const randomValue = getRandomIntInclusive(0, array.length - 1);
+    if (array.length == 0) {
+      displayItems.push(-1);
+    } else {
+      displayItems.push(selections[i][randomValue]);
+    }
+  }
+  console.log('display Items', displayItems);
+
+  const jsonArr = JSON.stringify(displayItems);
+  localStorage.setItem('Array', jsonArr);
+
+  // localStorage.setItem('length', selections.length);
+  // let val = 1;
+  // for (let i = 0; i < displayIDs.length; i += 1) {
+  //   localStorage.setItem(val, displayIDs[i]);
+  //   val += 1;
+  // }
 }
 
 async function loadData() {

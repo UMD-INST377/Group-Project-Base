9c214fb5284f0a47119771b6d915ba69a4a07c99
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-05-05 23:29:48 +0800

    api fix put delete(Fail)

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -197,6 +197,7 @@ router.put('/movie_imdb_ratings', async (req, res) => {
         }
       }
     );
+    res.header('Access-Control-Allow-Origin','*');
     res.send('Successfully Updated');
   } catch (err) {
     console.error(err);
@@ -256,8 +257,11 @@ router.put('/movie_technical', async (req, res) => {
 /// ////////movie Endpoints/////////
 /// /////////////////////////////////
 router.get('/movie', async (req, res) => {
+  // console.log(111);
   try {
     const movies = await db.movie.findAll();
+    // console.log(movies);
+    res.header('Access-Control-Allow-Origin', '*');
     res.send(movies);
   } catch (err) {
     console.error(err);

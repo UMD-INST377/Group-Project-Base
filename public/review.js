9012d156981245a0f84729ae3a4a2087eb70bc2c
ZhengXiShen <78034421+ZhengXiShen@users.noreply.github.com>
2021-05-06 00:07:40 +0800

    review js fix 1

--- public/review.js
+++ public/review.js
@@ -2,11 +2,7 @@ const selectEle = document.getElementsByClassName('select')[0];
 const scoreText = document.getElementsByClassName('score-text')[0];
 const submitBtn = document.getElementsByClassName('submit-review')[0];
 
-axios.get(`/movie`,{
-  headers: {"Access-Control-Allow-Origin": "*"},
-  crossdomain: true,
-  crossorigin:true 
-}).then((res) => {
+axios.get(`/movie`).then((res) => {
   const data = res.data;
   let html = ``
   data.map((ele) => {
@@ -26,7 +22,7 @@ axios.get(`/movie`,{
 submitBtn.addEventListener('click', () => {
   const selectSelEle = selectEle.getElementsByTagName('select')[0]
   const index = selectSelEle.selectedIndex; // selectedIndex代表的是你所选中项的index
-  console.log('我输入了', changeText.value);
+  // console.log('我输入了', changeText.value);
   console.log('我选中了', selectSelEle.options[index].value, selectSelEle.options[index].text);
   if (!scoreText.value || !selectSelEle.options[index].value) {
     alert('请检查输入信息')
@@ -47,5 +43,4 @@ submitBtn.addEventListener('click', () => {
     console.log('error: ', error);
     alert('更新失败')
   })
-
 })

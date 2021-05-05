591f342a7539d1e54d2858451b87188e37d0a1b7
YaredDemissie <demissieyared2@gmail.com>
2021-05-05 08:47:13 -0400

    Added nav-bar responsiveness

--- public/scripts.js
+++ public/scripts.js
@@ -21,10 +21,11 @@
 // }
 // window.onload = getMuseumStaff;
 
-document.addEventListener('DOMContentLoaded', () => {
+/* This is for the nav-bar */
+document.addEventListener("DOMContentLoaded", () => {
   // Get all "navbar-burger" elements
   const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll('.navbar-burger'),
+    document.querySelectorAll(".navbar-burger"),
     0
   );
 
@@ -32,30 +33,30 @@ document.addEventListener('DOMContentLoaded', () => {
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
     $navbarBurgers.forEach(($el) => {
-      $el.addEventListener('click', () => {
+      $el.addEventListener("click", () => {
         // Get the target from the "data-target" attribute
         const { target } = $el.dataset;
         const $target = document.getElementById(target);
 
         // Toggle the class on both the "navbar-burger" and the "navbar-menu"
-        $el.classList.toggle('is-active');
-        $target.classList.toggle('is-active');
+        $el.classList.toggle("is-active");
+        $target.classList.toggle("is-active");
       });
     });
   }
 });
 
 async function getMuseumInfo() {
-  console.log('data request');
-  const result = document.querySelector('#resultInfo');
-  const request = await fetch('/api/museum_info');
+  console.log("data request");
+  const result = document.querySelector("#resultInfo");
+  const request = await fetch("/api/museum_info");
   const tableData = await request.json();
   console.table(tableData);
   // return tableData;
 
   tableData.data.forEach((museum) => {
     console.log(museum);
-    const appendItem = document.createElement('tr');
+    const appendItem = document.createElement("tr");
     // appendItem.classList.add('title', 'has-text-centered', 'is-parent', 'is-3');
     appendItem.innerHTML = `
       <td> ${museum.museum_id} </td>

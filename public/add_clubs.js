40e4528ca1d255462f568abf706e0e3daaa1de27
Yazeedz91 <yazeedz91@hotmail.com>
2021-05-06 12:12:29 -0400

    Added functionality for adding new clubs into database

--- public/add_clubs.js
+++ public/add_clubs.js
@@ -1,22 +1,44 @@
 window.onload = async function windowActions () {
     
-    const form = document.getElementById("#add-button")
+    const form = document.getElementById("add-form")
+    
 
     // ADDING CLUBS
-    function add_club() {
-        const name_input = document.getElementById("#club-name")
-        const coachfn_input = document.getElementById("#first-name")
-        const coachln_input = document.getElementById("#last-name")
-        const num_input = document.getElementById("#num-players")
+    async function add_club() {
+        const name_input = document.getElementById("club-name")
+        const coachfn_input = document.getElementById("first-name")
+        const coachln_input = document.getElementById("last-name")
+        const num_input = document.getElementById("num-players")
 
         const new_club = {
-            club_name: name_input,
-            coach_first_name: coachfn_input,
-            coach_last_name: coachln_input,
-            num_of_players: num_input
+            club_name: name_input.value,
+            coach_first_name: coachfn_input.value,
+            coach_last_name: coachln_input.value,
+            num_of_players: num_input.value
+        }
+        const req = await fetch('/api/clubs', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                club_name: new_club.club_name,
+                coach_first_name: new_club.coach_first_name,
+                coach_last_name: new_club.coach_last_name,
+                num_of_players: new_club.num_of_players
+            })
+        })
+
+        const msg = document.createElement('p')
+        if (req){
+            msg.innerHTML = "Club Successfully Added"
+            form.append(msg)
+        } else {
+            msg.innerHTML = `Failed to add club: ${req.status_code}`
+            form.append(msg)
         }
 
-        console.log(new_club)
+        return false
     }
 
     form.onsubmit = add_club
--- public/clubs.js
+++ public/clubs.js
@@ -59,20 +59,6 @@ window.onload = async function windowActions() {
     form.onsubmit = displaySearchResults
     form.onreset = setup
 
-    // ADDING CLUBS
-    const name_input = document.getElementById("#club-name")
-    const coachfn_input = document.getElementById("#first-name")
-    const coachln_input = document.getElementById("#last-name")
-    const num_input = document.getElementById("#num-players")
-
-    const new_club = {
-        club_name: name_input,
-        coach_first_name: coachfn_input,
-        coach_last_name: coachln_input,
-        num_of_players: num_input
-    }
-
-    console.log(new_club)
 }
 
 

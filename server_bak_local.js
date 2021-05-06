a783c8f63a77bc8eaa28f36f6ac55fa270dd8315
Rodolfo Castro <rodcasjr@terpmail.umd.edu>
2021-05-05 22:53:10 -0400

    to connect locally

---
+++ server_bak_local.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+import express from "express";
+import db from "./database/initializeDB.js";
+import apiRoutes from "./routes/apiRoutes.js";
+// import public from './public/scripts.js';
+
+const app = express();
+const staticFolder = "public";
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(staticFolder));
+app.use("/api", apiRoutes);
+
+// app.use('/public', public);
+
+async function bootServer() {
+  try {
+    const mysql = await db.sequelizeDB;
+    await mysql.sync({alter: true});
+    app.listen(PORT, () => {
+      console.log(`Listening on: http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bootServer();

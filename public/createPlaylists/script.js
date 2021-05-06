506dc5f3c40e6da67906ed9b1bd9c1c8e6d0918b
Takahiro Kawasaki <63230525+Hiro626@users.noreply.github.com>
2021-05-06 08:09:29 -0400

    js code for create playlist

---
+++ public/createPlaylists/script.js
@@ -0,0 +1,41 @@
+async function windowActions() {
+    const artistInfoEndpoint = '/api/artist_info'
+    const artistInfoRequest = await fetch(artistInfoEndpoint)
+    const artistInfoData = await request.json()
+
+    const playlistDetailsEndpoint = '/api/playlist_details'
+    const playlistDetailsRequest = await fetch(playlistDetailsEndpoint)
+    const playlistDetailsData = await request.json()
+
+    const songsEndpoint = '/api/songs'
+    const songsRequest = await fetch(songsEndpoint)
+    const songData = await request.json()
+
+    
+    const playlists = []
+    const createPlaylist = (ev) => {
+        ev.preventDefault(); 
+        let playlist = {
+            playlistName = document.getElementById('namePlaylist').value,
+            title = document.getElementById('songTitle').value,
+            artist = document.getElementById('artistBand').value,
+            duration = document.getElementById('songDuration').value,
+            explicity = document.
+        }
+        playlists.push(playlist)
+        document.querySelector('form').reset()
+
+        console.warn('added', {playlists})
+        let pre = document.querySelector('#msg pre')
+        pre.textContent = '\n' + JSON.stringify(playlists, '\t', 2)
+    }
+
+    document.addEventListener('DOMContentLoaded', ()=>{
+        document.getElementById('submit').addEventListener('click', createPlaylist)
+    })
+
+        
+    
+}
+
+// window.onload = windowActions();

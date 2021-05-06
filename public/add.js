84861d5aa17c4f3ba3a4eddbabbb0e8dc966b639
HaoLiangCQ <78054019+HaoLiangCQ@users.noreply.github.com>
2021-05-06 01:44:46 -0400

    Update add.js

--- public/add.js
+++ public/add.js
@@ -1,79 +1,42 @@
-import java.util.Arrays;
-public class Pakudex
-{
-    private int size, capacity;
-    Pakuri[] array; // to create an array to keep track of multiple elements
+const addMovieBtn = document.getElementsByClassName('add-movie')[0];
 
-    public Pakudex()
-    { array = new Pakuri[20]; }
-
-    public Pakudex(int capacity)
-    { array = new Pakuri[capacity]; }
-
-    public int getSize()
-    { return size; }
-
-    public int getCapacity()
-    { return capacity; }
-
-    public String[] getSpeciesArray()
-    {
-        String listOfPakuri[] = new String[size]; //initialize the array listOfPakuri[]
-        for (int i = 0; i < size; i++)
-            listOfPakuri[i] = array[i].getSpecies();
-        if (listOfPakuri == null)
-            return null;
-        return listOfPakuri;
-    }
-
-    public int[] getStats(String species)
-    {
-        int arr[] = new int[3];
-        for (int i = 0; i < arr.length; i++)
-        {
-            if (array[i].getSpecies().equals(species)) //checking the String value if matches
-            {
-                arr[0] = array[i].getAttack();
-                arr[1] = array[i].getDefense();
-                arr[2] = array[i].getSpeed();
-                return arr; //If one value is found, the entire array is returned.
-            }
-        }
-            return null;
-    }
-
-    public void sortPakuri()
-    {
-        Arrays.sort(array);
-    }
-
-    public boolean addPakuri(String species) //the new name of the species added
-    {
-        Pakuri newSpecies = new Pakuri(species); //new object
-        for (int i = 0; i < size; i++) //Size to keep track of the pakuri
-        {
-            if (array[i].getSpecies().equals(species))
-                return false;
-        }
-        if (size < capacity)
-        {
-            array[size] = newSpecies;
-            size++;
-            return true;
-        }
-        return false; //return false if it goes beyond the capacity
-    }
-
-    public boolean evolveSpecies(String species)
-    {
-        for (int i = 0; i < size; i++)
-        {
-            if (array[i].getSpecies().equals(species))
-            {
-                array[i].evolve();
-                return true;
-            }
-        }
-        return false;
-    }
+const getTextAreaText = (className) => {
+  return document.getElementsByClassName(`${className}-text`)[0].value
 }
+
+
+addMovieBtn.addEventListener('click', () => {
+  const idText = getTextAreaText('id')
+  const titleText = getTextAreaText('title')
+  if (!idText || !titleText) {
+    alert('请检查输入信息')
+    return;
+  }
+  const yearText = getTextAreaText('year') || undefined
+  const directorText = getTextAreaText('director') || undefined
+  const countryText = getTextAreaText('country') || undefined
+
+
+  axios({
+    method: "post",
+    url: `/movie`,
+    data: {
+      movie_id: idText,
+      movie_title: titleText,
+      title_year: yearText,
+      director_name: directorText,
+      country:countryText,
+    },
+  }).then(function (res) { //回调函数  接受服务器参数
+    console.log(res);
+    const data = res.data;
+    if (data) {
+      alert('保存成功')
+    }
+    // document.querySelector("h3").innerHTML = res.data;
+  }).catch((error) => {
+    console.log('error: ', error);
+    alert('保存失败')
+  })
+
+})

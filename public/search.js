810cc0ab1fe46db043c3dd5da40926673a6c1cef
Andrea Tavakol <andreatavakol@gmail.com>
2021-05-06 14:29:39 -0400

    ADD: Setting up search

--- public/search.js
+++ public/search.js
@@ -1,6 +1,27 @@
+function genreSearch() {
+    console.log("genre button clicked");
+}
+
+function artistSearch() {
+    console.log("artist button clicked");
+}
+
+function albumSearch() {
+    console.log("album button clicked");
+}
+
 async function windowActions(){
     console.log('Window loaded')
     const form = document.querySelector('.userform');
+
+    const genreButton = document.querySelector('#genrebutton');
+    genreButton.addEventListener("click", genreSearch);
+
+    const artistButton = document.querySelector('#artistbutton');
+    artistButton.addEventListener("click", artistSearch);
+
+    const albumButton = document.querySelector('#albumbutton');
+    albumButton.addEventListener("click", albumSearch);
 }
 
 window.onload = windowActions;

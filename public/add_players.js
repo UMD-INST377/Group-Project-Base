61444aa6612bf1e95a95f60dc110eacd04f0538c
JacobVanderVat <jacobvandervat@gmail.com>
2021-05-07 11:22:44 -0400

    Update add_players.js

--- public/add_players.js
+++ public/add_players.js
@@ -7,24 +7,24 @@ window.onload = async function windowActions() {
     const playerfn_input = document.getElementById("firstname");
     const playerln_input = document.getElementById("lastname");
     const shirtnum_input = document.getElementById("shirtnumber");
-    const position_input = document.getElementById("position")
+    const position_input = document.getElementById("position");
     const goals_input = document.getElementById("playergoals");
     const assists_input = document.getElementById("playerassists");
     const titles_input = document.getElementById("playertitles");
 
     // Fetching Clubs to get Club name associated with provided ID
-    const req_clubs = await fetch("/api/clubs")
-    const clubs_data = await req_clubs.json()
-    const clubs = clubs_data["data"]
-    let new_club_id = 0
+    const req_clubs = await fetch("/api/clubs");
+    const clubs_data = await req_clubs.json();
+    const clubs = clubs_data["data"];
+    let new_club_id = 0;
 
-    clubs.forEach(club => {
-        if(club.club_name === club_input.value){
-          new_club_id = club.club_id
-          console.log(new_club_id)
-        }      
+    clubs.forEach((club) => {
+      if (club.club_name === club_input.value) {
+        new_club_id = club.club_id;
+        console.log(new_club_id);
+      }
     });
-
+    
     const new_player = {
       club_name: club_input.value,
       club_id: new_club_id,
@@ -34,40 +34,40 @@ window.onload = async function windowActions() {
       player_titles_num: titles_input.value,
       player_position: position_input.value,
       player_goals: goals_input.value,
-      player_assists: assists_input.value
+      player_assists: assists_input.value,
     };
+    
+      // Adding new player to players table
+      const req_player = await fetch("/api/players", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: new_player.player_first_name,
+          last_name: new_player.player_last_name,
+          club_id: new_player.club_id,
+          club: new_player.club_name,
+          shirt_number: new_player.player_shirtnum,
+          best_player_counter: new_player.player_titles_num,
+          position: new_player.player_position,
+        }),
+      });
 
-    // Adding new player to players table
-    const req_player = await fetch('/api/players', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        first_name: new_player.player_first_name,
-        last_name: new_player.player_last_name,
-        club_id: new_player.club_id,
-        club: new_player.club_name,
-        shirt_number: new_player.player_shirtnum,
-        best_player_counter: new_player.player_titles_num,
-        position: new_player.player_position
-      })
-    })
-
-    // Adding new player to Player Goals table
-    const req_goals = await fetch('/api/player_goals', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        first_name: new_player.player_first_name,
-        last_name: new_player.player_last_name,
-        goals: new_player.player_goals,
-        assists: new_player.player_assists
-      })
-    })
-
+      // Adding new player to Player Goals table
+      const req_goals = await fetch("/api/player_goals", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: new_player.player_first_name,
+          last_name: new_player.player_last_name,
+          goals: new_player.player_goals,
+          assists: new_player.player_assists,
+        }),
+      });
+    
     const msg = document.createElement("p");
     if (req_player && req_goals) {
       msg.innerHTML = "Player successfully added";

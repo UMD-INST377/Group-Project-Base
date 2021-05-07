ec749cd2f77a54954266d99c894d7d16544bd307
femstussy <oluwafemiodekunle99@gmail.com>
2021-05-06 21:50:12 -0400

    add edit delete js for players

---
+++ public/delete_players.js
@@ -0,0 +1,27 @@
+window.onload = async function windowActions() {
+  const form = document.getElementById("remove-form");
+
+  // DELETE PLAYERS
+  async function delete_player() {
+    const id_input = document.getElementById("player-id");
+
+    const req = await fetch("/api/players/".concat(id_input.value), {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const msg = document.createElement("p");
+    if (req) {
+      msg.innerHTML = "Player successfully deleted";
+      form.append(msg);
+    } else {
+      msg.innerHTML = `Failed to delete player: ${req.status_code}`;
+      form.append(msg);
+    }
+
+    return false;
+  }
+
+  form.addEventListener("submit", delete_player);
+};

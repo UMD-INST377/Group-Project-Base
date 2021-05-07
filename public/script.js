6aa919ea87896c8c5d726a056c5945bd3112ab48
peytonmilstead <73447797+peytonmilstead@users.noreply.github.com>
2021-05-07 10:26:36 -0400

    create script to submit new record form

--- public/script.js
+++ public/script.js
@@ -1,1 +1,12 @@
-console.log('page loaded');
+function loadScript() {
+  console.log('script is loaded');
+}
+window.onload = loadScript;
+
+router.route('/api')
+  .post(async (req, res) => {
+    console.log('POST request detected');
+    console.log('Form data in res.body', req.body);
+    console.log('Now send something back to your client');
+    res.json({message: 'Form Submitted!'});
+  });

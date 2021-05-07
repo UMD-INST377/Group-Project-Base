d59a0b8fe5c3b836c01c333e5f96eddbce8c7891
JacobVanderVat <jacobvandervat@gmail.com>
2021-05-07 12:50:19 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,11 +1,10 @@
 import clubs from "./Clubs.js";
 import players from "./Players.js";
 import player_goals from "./Player_goals.js";
-import winners from "./Winners.js";
 
 export default {
   clubs,
   players,
   player_goals,
-  winners, // remember to seperate models with a comma
+// remember to seperate models with a comma
 };

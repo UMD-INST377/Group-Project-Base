714f65f83fc3b6d241d25e7d542b11df32496617
delmarrandolph410 <78332107+delmarrandolph410@users.noreply.github.com>
2021-05-07 11:07:28 -0400

    New Javascript for creating tables

--- public/index.js
+++ public/index.js
@@ -1,6 +1,8 @@
 import { classToInvokable } from 'sequelize/types/lib/utils';
+import Artists from '../models/Artists';
+import Songs from '../models/Songs';
 
-function createTable(json) {
+/*function createTable(json) {
   if (json == null || json.length === 0) return;
 
   function createTableHead(table) {
@@ -13,7 +15,7 @@ function createTable(json) {
       th.appendChild(label);
       row.appendChild(th);
     }
-    console.log('done making table head');
+    console.log('Done making table head');
   }
   function createTableBody(table) {
     for (key in json.data) {
@@ -43,3 +45,44 @@ function createTable(json) {
   createTableHead(table);
   table.setAttribute('class', 'table is-striped');
 }
+*/
+
+// Top 10 Songs, Artists, and Albums
+const copiedSongs = { ...Songs};
+const copiedArtists = { ...Artists};
+const copiedAlbums = { ...Albums};
+
+
+copiedArtists.sort((a, b) => parseFloat(a.ARTIST_POPULARITY)
+  - parseFloat(b.ARTIST_POPULARITY));
+
+copiedSongs.sort((a, b) => parseFloat(a.SONG_POPULARITY) - parseFloat(b.SONG_POPULARITY));
+
+copiedAlbums.sort((a, b) => parseFloat(a.ALBUM_POPULARITY)
+- parseFloat(b.ALBUM_POPULARITY));
+
+const topSongs = copiedSongs.slice(0, 10);
+const topArtists = copiedArtists.slice(0, 10);
+const topAlbums = copiedAlbums.slice(0, 10);
+
+// Creating table using Javascript
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTableHead(table) {
+  let thead = table.createTHead();
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(topSongs[0]);
+
+
+generateTableHead(table);

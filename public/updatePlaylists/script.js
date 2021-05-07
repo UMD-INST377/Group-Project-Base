802e3f0eb7ee98189465e8d2a4d8866ddf331a14
mlima <mlima@umd.edu>
2021-05-06 20:03:39 -0400

    Update script.js

--- public/updatePlaylists/script.js
+++ public/updatePlaylists/script.js
@@ -1,7 +1,15 @@
-function getName(){
+function windowActions(){function getName(){
     document.querySelector("#pname").innerHTML = localStorage.getItem("playlistName");
 }
 
+//Code pen https://codepen.io/Arckays/pen/eGjjPL
+// let addbutton = document.getElementById("add");
+// addbutton.addEventListener("click", function() {
+// let boxes = document.getElementById("boxes");
+// let clone = boxes.firstElementChild.cloneNode(true);
+// boxes.appendChild(clone);
+// });}
 
 
-window.onload = getName;
+
+window.onload = windowActions;

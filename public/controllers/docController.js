5794a218eb6dbad01cf21e25ff84346a32c1ef5d
Dominique Rafael <drafael@terpmail.umd.edu>
2021-05-10 16:00:19 -0400

    controllers

---
+++ public/controllers/docController.js
@@ -0,0 +1,6 @@
+export default 
+`SELECT media_title, media_release_year, media_description, CONCAT(creator_first_name, ' ', creator_last_name) as creator_name
+FROM all_media
+	JOIN all_media_creators_link USING(media_id)
+    JOIN creators USING(creator_id)
+WHERE media_type = 'Documentary';`

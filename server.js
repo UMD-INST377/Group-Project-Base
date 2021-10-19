08b771c28e78aba72e19a56bc101d6ba286dab28
Alex Leitch <aleitch1@umd.edu>
2021-10-19 11:28:35 -0400

    Fix db crash add static client folder

--- server.js
+++ server.js
@@ -6,16 +6,18 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'client';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 
 async function bootServer() {
   try {
-    const mysql = await db.sequelizeDB;
-    await mysql.sync();
+    // const mysql = await db.sequelizeDB;
+    // await mysql.sync();
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);
     });

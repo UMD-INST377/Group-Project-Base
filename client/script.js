190e9400a9e10ca3bf51ece184fd8c1fb031aa94
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-10-25 15:51:07 -0400

    Scrollable results
    showing case for "fake search" result behavior

--- client/script.js
+++ client/script.js
@@ -16,5 +16,13 @@ const navSlide = () => {
     });
   });
 };
-
 navSlide();
+
+const result = document.querySelector('.results')
+const submit = document.querySelector('.submit')
+submit.addEventListener('click', ()=> {
+    console.log('hi')
+    let currenthtml = result.innerHTML
+     currenthtml += '<div class="poster"><img src ="https://xl.movieposterdb.com/12_04/2012/948470/xl_948470_406a814a.jpg"></div>'
+    result.innerHTML = currenthtml
+  })

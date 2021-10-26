122897453d0c47772980b86d88a714f28dc2e7fb
marilynpothen <m3pothen@gmail.com>
2021-10-26 16:39:24 -0400

    server push1

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const staticFolder = 'client';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static(staticFolder)); 
 app.use('/api', apiRoutes);
+
 
 async function bootServer() {
   try {

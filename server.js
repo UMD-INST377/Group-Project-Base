9943a4083fbae189548ceb791e9ebce8cec5c7a7
enyu0510 <jonathanchen0510@gmail.com>
2021-10-27 17:49:44 -0400

    Setup

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 /* eslint-disable no-console */
-import express from 'express';
-import db from './database/initializeDB.js';
-import apiRoutes from './routes/apiRoutes.js';
+import express from "express";
+import db from "./database/initializeDB.js";
+import apiRoutes from "./routes/apiRoutes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const staticFolder = 'client';
+const staticFolder = "client";
+const staticFolder2 = "public";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(staticFolder));
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 async function bootServer() {
   try {

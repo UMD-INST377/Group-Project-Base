5ffddaeae2d7a44d51e76dcfa4b7c0d670002bb8
Jerry Crews <jerry.crews1@gmail.com>
2021-10-27 11:50:20 -0400

    Added public folder

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 /* eslint-disable no-console */
-import express from 'express';
-import db from './database/initializeDB.js';
-import apiRoutes from './routes/apiRoutes.js';
+import express from "express";
+import db from "./database/initializeDB.js";
+import apiRoutes from "./routes/apiRoutes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const staticFolder = 'client';
+const staticFolder = "public";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(staticFolder));
-app.use('/api', apiRoutes);
+app.use("/api", apiRoutes);
 
 async function bootServer() {
   try {

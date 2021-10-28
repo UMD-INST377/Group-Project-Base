23a73ebf9b05e6ccd04ad61e4009c6972a8de6c4
KamranDjourshari24 <kdjour00@umd.edu>
2021-10-28 18:04:32 -0400

    Created films endpoint with all neccessary http methods. Also added it to server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
+import kamranRoutes from './routes/kamranRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
+app.use('/api', kamranRoutes);
 
 async function bootServer() {
   try {

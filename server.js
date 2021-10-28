65e8db118a4a084af83badf26ac2d59e11fd80f3
jomerp12 <jpaulino@terpmail.umd.edu>
2021-10-28 10:35:10 -0400

    Initiated routes/updated static folder to public

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import apiRoutes from './routes/apiRoutes.js';
 const app = express();
 
 const PORT = process.env.PORT || 3000;
-const staticFolder = 'client';
+const staticFolder = 'public';
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,15 +15,15 @@ app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 
 async function bootServer() {
-  try {
-    // const mysql = await db.sequelizeDB;
-    // await mysql.sync();
-    app.listen(PORT, () => {
-      console.log(`Listening on: http//localhost:${PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        // const mysql = await db.sequelizeDB;
+        // await mysql.sync();
+        app.listen(PORT, () => {
+            console.log(`Listening on: http//localhost:${PORT}`);
+        });
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 bootServer();

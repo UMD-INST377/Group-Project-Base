535dc1caa2f301c7c7bfea4f1a8e7ebbb3280956
yiling0323 <duyiling0323@gmail.com>
2021-10-28 21:05:56 -0400

    Added animals' lifestyle endpoint

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -271,3 +271,53 @@ router.get('/custom', async (req, res) => {
 });
 
 export default router;
+
+
+// //////////////////////
+// Add to Yiling Branch
+// ////////////////////////
+router.route('/lifestyle')  //choose animals' lifestyle table
+  .get(async (req, res) => {
+    try { 
+      const url = //DATALINK;
+      const data = await fetch(url);
+      console.log('touched /lifestyle with GET');
+      res.json({message: 'get lifestyle endpoint'})
+    } catch (err) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })
+  .put(async (req, res) => {
+    try { 
+      const url = //DATALINK;
+      const data = await fetch(url);
+      console.log('touched /lifestyle with PUT');
+      res.json({message: 'put lifestyle endpoint'});
+    } catch (err) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })
+  .post(async (req, res) => {
+    try { 
+      const url = //DATALINK;
+      const data = await fetch(url);
+      console.log('touched /lifestyle with POST');
+      res.json({message: 'post lifestyle endpoint'});
+    } catch (err) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })
+  .delete(async (req, res) => {
+    try { 
+      const url = //DATALINK;
+      const data = await fetch(url);
+      console.log('touched /lifestyle with DELETE');
+      res.json({message: 'delete lifestyle endpoint'});
+    } catch (err) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })

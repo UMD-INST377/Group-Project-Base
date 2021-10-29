ca5c4ebeb2554342869602d95eb8ff42d11f751c
elvisaguilar704 <63757562+elvisaguilar704@users.noreply.github.com>
2021-10-29 13:36:19 -0400

    route-artist.3

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,43 @@ import { Router } from "express";
 
 router.route('/artists')
   .get((req,res) => {
-    console.log('you touched /artists with GET');
-    res.json({data: data});
+    try{
+      console.log('you touched /artists with GET');
+      res.json({data: data});
+    } catch (err) {
+      console.log(error);
+      res.json({error: 'something went wrong'});
+    }
   })
+
+  .put((req, res) => {
+    try{console.log('you touched /companies with PUT');
+    res.json({data: data});}
+    catch (err) {
+      console.log(error);
+      res.json({error: error});
+    }
+  })
+
+  .post((req, res) => {
+    try{
+      console.log('you touched /companies with POST');
+      res.json({data: data});}
+    catch (err) {
+      console.log(error);
+      res.json({error: error});
+    }
+  })
+
+  .delete((req, res) => {
+    try{
+      console.log('you touched /companies with DELETE');
+      res.json({data: data});
+    }
+    catch (err) {
+      console.log(error);
+      res.json({error: error});
+    }
+  })
+
+

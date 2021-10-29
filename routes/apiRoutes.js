a234d0113790855082d65806b8a2baf479796449
yiling0323 <duyiling0323@gmail.com>
2021-10-28 22:15:29 -0400

    Edited lifestyle endpoint

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,9 +4,9 @@ import sequelize from 'sequelize';
 
 import db from '../database/initializeDB.js';
 
-/*const router = express.Router();
+const router = express.Router();
 
-router.get('/', (req, res) => {
+/*router.get('/', (req, res) => {
   res.send('Welcome to the UMD Dining API!');
 }); */
 

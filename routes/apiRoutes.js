9eb264869671fbcfcfe35ec61aabc56f3562c319
noncomplex <71818162+noncomplex@users.noreply.github.com>
2021-10-30 10:13:52 -0400

    jim's branch + route and methods

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,46 @@ router.get('/', (req, res) => {
   res.send('Welcome to the UMD Dining API!');
 });
 
+/* Jim's lab 9 routes */
+router.route('/census')
+  .get((req, res) => {
+    try {
+      console.log('touched /census with GET');
+      res.json({data: []}); // get census data later
+    } catch(err) {
+      console.error(err);
+      res.json({error: 'Something went wrong on the server.'});
+    }
+  })
+  .put((req, res) => {
+    try {
+      console.log('touched /census with PUT');
+      res.json({message: 'put census endpoint'});
+    } catch(err) {
+      console.error(err);
+      res.json({error: 'Something went wrong on the server.'});
+    }
+  })
+  .post((req, res) => {
+    try {
+      console.log('touched /census with POST');
+      res.json({message: 'post census endpoint'});
+    } catch(err) {
+      console.error(err);
+      res.json({error: 'Something went wrong on the server.'});
+    }
+  })
+  .delete((req, res) => {
+    try {
+      console.log('touched /census with DELETE');
+      res.json({message: 'delete census endpoint'});
+    } catch(err) {
+      console.error(err);
+      res.json({error: 'Something went wrong on the server.'});
+    }
+  });
+
+	   
 /// /////////////////////////////////
 /// ////Dining Hall Endpoints////////
 /// /////////////////////////////////

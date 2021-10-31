6310cd8046c54c73ee690298916820f71d645417
TylerD01 <tdeaner@terpmail.umd.edu>
2021-10-31 19:32:32 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,7 @@ import db from '../database/initializeDB.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the Group 10s API!');
+  res.send('Welcome to the Group 16s API!');
 });
 
 /*Tyler D/animals table endpoint*/

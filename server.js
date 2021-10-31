1ad265f42f4fd2a6b33ccc91cc53042d1ef4c4e7
damores <Diego.amores14@gmail.com>
2021-10-31 18:33:50 -0400

    re-routed api to work with our own api

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import db from './database/initializeDB.js';
-import apiRoutes from './routes/apiRoutes.js';
+import apiRoutes from './routes/newRoutes.js';
 
 const app = express();
 

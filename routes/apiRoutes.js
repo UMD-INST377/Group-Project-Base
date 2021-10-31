75b57b9fc2b204b3b8afd3db35a34586bf0089ca
NKoyfish <43017705+NKoyfish@users.noreply.github.com>
2021-10-31 15:26:25 -0400

    test api

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,8 +10,9 @@ router.get('/', (req, res) => {
   res.send('You\'ve touched the default route!');
 });
 
-router.put('/', (req, res) => {
-
+router.put('/put', (req, res) => {
+  res.send('hello from put')
+  console.log('something cool happens here later')
 });
 
 router.post('/', (req, res) => {
@@ -21,5 +22,40 @@ router.post('/', (req, res) => {
 router.delete('/', (req, res) => {
   
 });
+
+router.route('/stuff')
+  .get((req, res) => {
+    try {
+      console.log('stuff default route reached')
+      res.json({id: 123})
+    }
+    catch (err) {
+      console.log(error)
+    }
+  })
+  .put((req,res) => {
+    try {
+      console.log('put from stuff')
+    }
+    catch (error) {
+      console.log(error)
+    }
+  })
+  .delete((req, res) => {
+    try {
+      console.log('delete from stuff')
+    }
+    catch (error) {
+      console.log(error)
+    }
+  })
+  .post((req, res) => {
+    try {
+      console.log('post from stuff')
+    }
+    catch (error) {
+      console.log(error)
+    }
+  })
 
 export default router;

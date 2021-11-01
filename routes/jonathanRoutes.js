f30dddf8eb563eb61abec805e3f5aab48d6080b6
enyu0510 <jonathanchen0510@gmail.com>
2021-10-31 22:34:35 -0400

    Added Jonathan routes

---
+++ routes/jonathanRoutes.js
@@ -0,0 +1,57 @@
+/* eslint-disable no-console */
+import express from "express";
+import sequelize from "sequelize";
+
+import db from "../database/initializeDB.js";
+
+const router = express.Router();
+
+/// /////////////////////////////////
+/// ////Animals Database -////////
+/// /////////////////////////////////
+router
+  .route("/biome")
+  .get("/biome", async (req, res) => {
+    try {
+      console.log("touched /biome with GET");
+      res.json({ data: data });
+      res.json({ reply: "closed GET request" });
+    } catch (err) {
+      console.error(err);
+      res.error("Server error");
+    }
+  })
+
+  .put("/biome", async (req, res) => {
+    try {
+      console.log("touched /biome with PUT");
+      res.json({ data: data });
+      res.json({ reply: "closed PUT request" });
+    } catch (err) {
+      console.error(err);
+      res.error("Server error");
+    }
+  })
+
+  .post("/biome", async (req, res) => {
+    try {
+      console.log("touched /biome with POST");
+      res.json({ data: data });
+      res.json({ reply: "closed POST request" });
+    } catch (err) {
+      console.error(err);
+      res.error("Server error");
+    }
+  })
+
+  .delete("/biome", async (req, res) => {
+    try {
+      console.log("touched /biome with DELETE");
+      res.json({ data: data });
+      res.json({ reply: "closed DELETE request" });
+    } catch (err) {
+      console.error(err);
+      res.error("Server error");
+    }
+  });
+export default router;

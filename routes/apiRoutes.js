0cfa17c89008f61f4017409418b75d50503d3e14
melloswideworld <89662058+mbroadna@users.noreply.github.com>
2021-10-31 21:51:16 -0400

    get

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -273,12 +273,13 @@ router.get('/custom', async (req, res) => {
 /// //////////////////////////////////
 /// ///////NBA Database////////
 /// /////////////////////////////////
-const nbaPlayerQuery = "SELECT * FROM AWARDS"
+const nbaPlayerQuery = 'SELECT * FROM AWARDS';
 router.get('/nba-players', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(nbaPlayerQuery, {
       type: sequelize.QueryTypes.SELECT
     });
+    console.log('touched /nba-players with GET');
     res.json(result);
   } catch (err) {
     console.error(err);

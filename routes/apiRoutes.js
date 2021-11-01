ef15557479d95f2f54209a370bc304fe18b70f7d
M-Schwartz99 <89875672+M-Schwartz99@users.noreply.github.com>
2021-11-01 11:53:46 -0400

    Database section uncommented. Added route for /review, fixed typo

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable camelcase */
 /* eslint-disable no-console */
 import express from 'express';
 import sequelize from 'sequelize';
@@ -14,56 +16,61 @@ router.get('/', (req, res) => {
 /// ////Hotel Finder Endpoints////////
 /// /////////////////////////////////
 router.get('/amenities', async (req, res) => {
-  //   try {
-  //     const const_name = await db.TableName.findAll();
-  //     const reply = const_name.length > 0 ? { data: const_name } : { message: 'no results found' };
-  //     res.json(reply);
-  //     console.log("you touched the amenities route")
-  //   } catch (err) {
-  //     console.error(err);
-  //   }
+  try {
+    const const_name = await db.TableName.findAll();
+    const reply = const_name.length > 0 ? { data: const_name } : { message: 'no results found' };
+    res.json(reply);
+    console.log('you touched the amenities route');
+  } catch (err) {
+    console.error(err);
+  }
   res.send('this is the amenities page');
-  console.log('you touched the search route')
+  console.log('you touched the search route');
 });
 
 router.get('/beds', async (req, res) => {
-  res.send('this is the amenities page');
-  console.log('you touched the search route')
+  res.send('this is the bed count page');
+  console.log('you touched the search route');
 });
 
 router.get('/hotel_amenities_join', async (req, res) => {
   res.send('this is the hotel_amenities_join page');
-  console.log('you touched the hotel_amenities_join route')
+  console.log('you touched the hotel_amenities_join route');
 });
 
 router.get('/hotel_overview', async (req, res) => {
   res.send('this is the hotel_overview page');
-  console.log('you touched the hotel_overview route')
+  console.log('you touched the hotel_overview route');
 });
 
 router.get('/hotel_type', async (req, res) => {
   res.send('this is the hotel_type page');
-  console.log('you touched the hotel_type route')
+  console.log('you touched the hotel_type route');
 });
 
 router.get('/locations', async (req, res) => {
   res.send('this is the locations page');
-  console.log('you touched the locations route')
+  console.log('you touched the locations route');
 });
 
 router.get('/restaurants', async (req, res) => {
   res.send('this is the restaurants page');
-  console.log('you touched the restaurants route')
+  console.log('you touched the restaurants route');
 });
 
 router.get('/room', async (req, res) => {
   res.send('this is the room page');
-  console.log('you touched the room route')
+  console.log('you touched the room route');
 });
 
 router.get('/room_type', async (req, res) => {
   res.send('this is the room_type page');
-  console.log('you touched the room_type route')
+  console.log('you touched the room_type route');
+});
+
+router.get('/review', async (req, res) => {
+  res.send('this is the reviews page');
+  console.log('you touched the room_type route');
 });
 
 export default router;

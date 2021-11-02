7ed7efde87ba093d6ef9f04b21f4205b5d1bcd98
Tracy <tracokwaisie@outlook.com>
2021-11-02 18:13:41 -0400

    Added other tables

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -47,6 +47,9 @@ router.route('/actor')
       res.json({message: 'Something went wrong'});
     }
   });
+
+
+
   router.route('/award')
   .get(async (req, res) => {
     try {
@@ -85,3 +88,82 @@ router.route('/actor')
     }
   });
   
+
+router.route('/director')
+  .get(async (req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /director with GET'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+  .post((req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /director with POST'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+  .delete((req, res) => {
+      try {
+        console.log();
+        res.json({message: 'Touched the /director with DELETE'});
+      } catch (error) {
+        console.log(error);
+        res.json({message: 'Something went wrong'});
+      }
+    });
+  .put((req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /director with PUT'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+
+
+  router.route('/film')
+  .get(async (req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /film with GET'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+  .post((req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /film with POST'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+  .delete((req, res) => {
+      try {
+        console.log();
+        res.json({message: 'Touched the /film with DELETE'});
+      } catch (error) {
+        console.log(error);
+        res.json({message: 'Something went wrong'});
+      }
+    });
+  .put((req, res) => {
+    try {
+      console.log();
+      res.json({message: 'Touched the /film with PUT'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+
+

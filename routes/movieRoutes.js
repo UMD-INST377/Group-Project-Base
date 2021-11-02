101a09c5f169056a305d73f4ea7f19467fec6a7c
jchamdi <70522271+jchamdi@users.noreply.github.com>
2021-11-02 17:27:33 -0400

    cleaned up code from demo

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -19,7 +19,7 @@ router.route('/actor')
       console.log(error);
       res.json({message: 'Something went wrong'});
     }
-  })
+  });
   .post((req, res) => {
     try {
       console.log();
@@ -29,78 +29,30 @@ router.route('/actor')
       res.json({message: 'Something went wrong'});
     }
   });
-
-
-/// /////////////////////////////////
-/// ////Dining Hall Endpoints////////
-/// /////////////////////////////////
-router.get('/actor', async (req, res) => {
-  console.log('Touched dining input with GET');
-});
-
-router.get('/dining/:hall_id', async (req, res) => {
-  try {
-    const hall = await db.DiningHall.findAll({
-      where: {
-        hall_id: req.params.hall_id
-      }
-    });
-
-    res.json(hall);
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
-
-router.post('/dining', async (req, res) => {
-  const halls = await db.DiningHall.findAll();
-  const currentId = (await halls.length) + 1;
-  try {
-    const newDining = await db.DiningHall.create({
-      hall_id: currentId,
-      hall_name: req.body.hall_name,
-      hall_address: req.body.hall_address,
-      hall_lat: req.body.hall_lat,
-      hall_long: req.body.hall_long
-    });
-    res.json(newDining);
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
-
-router.delete('/dining/:hall_id', async (req, res) => {
-  try {
-    await db.DiningHall.destroy({
-      where: {
-        hall_id: req.params.hall_id
-      }
-    });
-    res.send('Successfully Deleted');
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
-
-router.put('/dining', async (req, res) => {
-  try {
-    await db.DiningHall.update(
-      {
-        hall_name: req.body.hall_name,
-        hall_location: req.body.hall_location
-      },
-      {
+  router.delete('/dining/:hall_id', async (req, res) => {
+    try {
+      await db.DiningHall.destroy({
         where: {
-          hall_id: req.body.hall_id
+          hall_id: req.params.hall_id
         }
-      }
-    );
-    res.send('Successfully Updated');
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
+      });
+      res.send('Successfully Deleted');
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  });
+  
+  router.put('/dining', async (req, res) => {
+    try {
+      await db.DiningHall.update(
+        {
+          hall_name: req.body.hall_name,
+          hall_location: req.body.hall_location
+        },
+        {
+          where: {
+            hall_id: req.body.hall_id
+          }
+        }
+      );

5e516e69138950773a1a680c2c973b5a9c321e36
Alec M <alecmattu1234@gmail.com>
2021-11-02 19:26:16 -0400

    Fix 'No default engine was specified' error

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
 
 // Set default options
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

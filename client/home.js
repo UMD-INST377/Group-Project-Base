1e52c73bc2ca0eb3ee13d488ef80f0500b333bdf
jomerp12 <jpaulino@terpmail.umd.edu>
2021-11-02 16:51:32 -0400

    Add title

--- client/home.js
+++ client/home.js
@@ -1,7 +1,7 @@
 const config = {
-  type: 'carousel',
-  perView: 5,
-  focusAt: 'center'
+    type: 'carousel',
+    perView: 5,
+    focusAt: 'center'
 };
 new Glide('.glide', config).mount();
 
@@ -10,83 +10,84 @@ const container = document.querySelector('.container');
 const albums = document.querySelectorAll('img');
 
 function createDetail() {
-  const new_container = document.createElement('div');
-  new_container.className = 'detail';
-  new_container.style.cssText = `border: 5px solid grey;
+    const new_container = document.createElement('div');
+    new_container.className = 'detail';
+    new_container.style.cssText = `border: 5px solid grey;
                                    height: 400px;`;
 
-  // Created Tab and Content
-  const tab = document.createElement('div');
-  tab.className = 'tab';
-  const content = document.createElement('div');
-  content.className = 'contents';
+    // Created Tab and Content
+    const tab = document.createElement('div');
+    tab.className = 'tab';
+    const content = document.createElement('div');
+    content.className = 'contents';
 
-  // Created Buttons
-  const general_info = document.createElement('button');
-  general_info.className = 'link';
-  general_info.innerHTML = 'General Information';
-  general_info.addEventListener('click', (evt) => { openTab(0); });
+    // Created Buttons
+    const general_info = document.createElement('button');
+    general_info.className = 'link';
+    general_info.innerHTML = 'General Information';
+    general_info.addEventListener('click', (evt) => { openTab(0); });
 
-  const songs = document.createElement('button');
-  songs.className = 'link';
-  songs.innerHTML = 'Songs';
-  songs.addEventListener('click', (evt) => { openTab(1); });
+    const songs = document.createElement('button');
+    songs.className = 'link';
+    songs.innerHTML = 'Songs';
+    songs.addEventListener('click', (evt) => { openTab(1); });
 
-  const placements = document.createElement('button');
-  placements.className = 'link';
-  placements.innerHTML = 'Placement';
-  placements.addEventListener('click', (evt) => { openTab(2); });
+    const placements = document.createElement('button');
+    placements.className = 'link';
+    placements.innerHTML = 'Placement';
+    placements.addEventListener('click', (evt) => { openTab(2); });
 
-  const prices = document.createElement('button');
-  prices.className = 'link';
-  prices.innerHTML = 'Price';
-  prices.addEventListener('click', (evt) => { openTab(3); });
+    const prices = document.createElement('button');
+    prices.className = 'link';
+    prices.innerHTML = 'Price';
+    prices.addEventListener('click', (evt) => { openTab(3); });
 
-  // Created Content and Appended to Content Div
-  const general_info_content = document.createElement('h3');
-  general_info_content.className = 'heading';
-  general_info_content.innerHTML = 'Vinyl';
+    // Created Content and Appended to Content Div
+    const general_info_content = document.createElement('h3');
+    general_info_content.className = 'heading';
+    general_info_content.innerHTML = 'Vinyl';
 
-  const songs_content = document.createElement('h3');
-  songs_content.className = 'heading';
-  songs_content.innerHTML = 'Song';
+    const songs_content = document.createElement('h3');
+    songs_content.className = 'heading';
+    songs_content.innerHTML = 'Song';
 
-  const placements_content = document.createElement('h3');
-  placements_content.className = 'heading';
-  placements_content.innerHTML = 'Placement';
 
-  const prices_content = document.createElement('h3');
-  prices_content.className = 'heading';
-  prices_content.innerHTML = 'Price';
+    const placements_content = document.createElement('h3');
+    placements_content.className = 'heading';
+    placements_content.innerHTML = 'Placement';
 
-  content.append(general_info_content, songs_content, placements_content, prices_content);
+    const prices_content = document.createElement('h3');
+    prices_content.className = 'heading';
+    prices_content.innerHTML = 'Price';
 
-  // Appended to Container
-  tab.append(general_info, songs, placements, prices);
-  new_container.append(tab, content);
-  body.appendChild(new_container);
+    content.append(general_info_content, songs_content, placements_content, prices_content);
 
-  function openTab(tabIndex) {
-    const heading = document.querySelectorAll('.heading');
-    heading.forEach((item) => {
-      item.style.display = 'none';
-    });
+    // Appended to Container
+    tab.append(general_info, songs, placements, prices);
+    new_container.append(tab, content);
+    body.appendChild(new_container);
 
-    heading[tabIndex].style.display = 'block';
-  }
+    function openTab(tabIndex) {
+        const heading = document.querySelectorAll('.heading');
+        heading.forEach((item) => {
+            item.style.display = 'none';
+        });
+
+        heading[tabIndex].style.display = 'block';
+    }
 }
 
 console.log(albums);
 albums.forEach((item) => {
-  item.addEventListener('click', (evt) => {
-    console.log(evt.target.id);
-    // evt.target.style.hover = `transform: rotateY(-60deg);
-    //                             transition-duration: 1s;
-    //                            `
-    container.style.cssText = `height: 400px; 
+    item.addEventListener('click', (evt) => {
+        console.log(evt.target.id);
+        /* evt.target.style.hover = `transform: rotateY(-60deg);
+                                   transition-duration: 1s;
+                                  ` */
+        container.style.cssText = `height: 400px; 
                                    transition-duration: 1s`;
-    if (!body.contains(document.querySelector('.detail'))) {
-      createDetail();
-    }
-  });
+        if (!body.contains(document.querySelector('.detail'))) {
+            createDetail();
+        }
+    });
 });

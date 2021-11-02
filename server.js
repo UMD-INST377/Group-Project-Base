3f31a44bdca4fdab67e49b90fb7fefe1f9eb694e
Alec M <alecmattu1234@gmail.com>
2021-11-02 11:52:49 -0400

    Fix views folder reference

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
 
 // Set default options
-app.set('views', './views');
+app.set('views', 'views');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 

002e31f26b737833c66388e2884e073bae0e4f2d
Jayid Alam <jayid_07@yahoo.com>
2021-11-02 17:55:33 -0400

    Added JS code for dropdown button

---
+++ client/script.js
@@ -0,0 +1,17 @@
+//Toggle between showing and hiding dropdown
+function openDropdown() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }

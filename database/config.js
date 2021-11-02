d9e1a1d4c94c031c34ecd0d9a60e42a53cf48d87
Alex Leitch <aleitch1@umd.edu>
2021-11-02 10:39:35 -0400

    Fix DB connection

--- database/config.js
+++ database/config.js
@@ -1,23 +1,23 @@
 export default {
   development: {
-    username: 'student',
-    password: 'INST377@UMD',
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '34.207.233.215',
     dialect: 'mysql'
   },
   test: {
-    username: 'student',
-    password: 'INST377@UMD',
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '34.207.233.215',
     dialect: 'mysql'
   },
   production: {
-    username: 'student',
-    password: 'INST377@UMD',
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '34.207.233.215',
     dialect: 'mysql'
   }
 };

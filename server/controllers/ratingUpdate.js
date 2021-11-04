58d9722b2004c0909bf498c1a768ead581a0bebe
mirandavo1 <mvo18@terpmail.umd.edu>
2021-11-04 15:42:51 -0400

    added a query to update rating chart

---
+++ server/controllers/ratingUpdate.js
@@ -0,0 +1,6 @@
+/*Put means gets a record and updates the change*/
+
+/*Update a rating of a song*/
+export default `UPDATE rating
+SET ratings = '5', description = "This rating is 5 out of 5 stars"
+WHERE rating_id = 1`;

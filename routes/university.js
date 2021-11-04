5b5493fd4ba1a758933c8620e8ccd2207fcbf6c8
Alec M <alecmattu1234@gmail.com>
2021-11-04 10:20:58 -0400

    Add University Review page

--- routes/university.js
+++ routes/university.js
@@ -10,6 +10,32 @@ import express from 'express';
 // Instantiate router component
 const router = express.Router();
 
+/** TEMP CODE **/
+/**
+ * IMPORTANT NOTE
+ * (1) The code below is a temporary solution
+ * to the lack of database that we currently have
+ * this is just a emulation
+ */
+const universities = {
+  "university-of-maryland": {
+    name: "University of Maryland",
+    addr: "College Park, MD",
+    url: "https://umd.edu"
+  },
+  "pennsylvania-state-university": {
+    name: "Penn State University",
+    addr: "State College, PA",
+    url: "https://psu.edu"
+  },
+  "universiy-of-minnesota": {
+    name: "University of Minnesota",
+    addr: "Minneapolis, MN",
+    url: "https://twin-cities.umn.edu"
+  }
+}
+/** END OF TEMP CODE **/
+
 /*
  * Root directory
  *
@@ -17,33 +43,22 @@ const router = express.Router();
  * @date 2021-11-01 06:32PM
  */
 router.get('/:universityName', (request, response) => {
-  /**
-   * IMPORTANT NOTE
-   * (1) The code below is a temporary solution
-   * to the lack of database that we currently have
-   * this is just a emulation
-   */
-  const universities = {
-    "university-of-maryland": {
-      name: "University of Maryland",
-      addr: "College Park, MD",
-      url: "https://umd.edu"
-    },
-    "pennsylvania-state-university": {
-      name: "Penn State University",
-      addr: "State College, PA",
-      url: "https://psu.edu"
-    },
-    "universiy-of-minnesota": {
-      name: "University of Minnesota",
-      addr: "Minneapolis, MN",
-      url: "https://twin-cities.umn.edu"
-    }
-  }
+  /* Temp solution until DB is ready */
   const uni = universities[request.params.universityName];
 
   if (typeof(uni) === "object") {
     response.render('university', uni);
+  } else {
+    response.status(404).send('<h1>Requested university was not found</h1>');
+  }
+});
+
+router.get("/:universityName/review", (request, response) => {
+  /* Temp solution until DB is ready */
+  const uni = universities[request.params.universityName];
+
+  if (typeof(uni) === "object") {
+    response.render('newReview', uni);
   } else {
     response.status(404).send('<h1>Requested university was not found</h1>');
   }

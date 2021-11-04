3e7441604c5bc80b368b73ca3e4965138b782374
mirandavo1 <mvo18@terpmail.umd.edu>
2021-11-04 15:54:40 -0400

    Update ratingUpdate.js

--- server/controllers/ratingUpdate.js
+++ server/controllers/ratingUpdate.js
@@ -1,6 +1,6 @@
 /*Put means gets a record and updates the change*/
 
-/*Update a rating of a song*/
+/*Update a rating of a song, updated with rating_id of 1*/
 export default `UPDATE rating
 SET ratings = '5', description = "This rating is 5 out of 5 stars"
 WHERE rating_id = 1`;

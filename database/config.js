f4f16ddc9088fe62fb2c927f5193afc98589d560
KamranDjourshari24 <kdjour00@umd.edu>
2021-11-05 01:37:27 -0400

    Initialized our db server and controller files.

--- database/config.js
+++ database/config.js
@@ -2,22 +2,22 @@ export default {
   development: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    database: 'imdb_database',
+    host: '34.207.233.215',
     dialect: 'mysql'
   },
   test: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    database: 'imdb_database',
+    host: '34.207.233.215',
     dialect: 'mysql'
   },
   production: {
     username: 'student',
     password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    database: 'imdb_database',
+    host: '34.207.233.215',
     dialect: 'mysql'
   }
 };

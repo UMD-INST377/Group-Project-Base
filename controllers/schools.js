d26c9ff4dfb56d6a427bddc75141b0cd3aea5815
Alec M <alecmattu1234@gmail.com>
2021-11-05 07:42:59 -0400

    Add empty SQL controller (no SQL statements yet)

---
+++ controllers/schools.js
@@ -0,0 +1,19 @@
+/**
+ * Get all Big 10 schools from the database using SELECT
+ *
+ * @type {SQLStmt}
+ */
+const getAllSchools = ``;
+
+/**
+ * Get a specific Big 10 school by school ID
+ *
+ * @type {SQLStmt}
+ */
+const getSchool = ``;
+
+// Export variables
+export default {
+  getAllSchools,
+	getSchool
+};

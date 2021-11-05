9a1c9f522eeeba6c9906bf2484601467f10868f6
yiling0323 <duyiling0323@gmail.com>
2021-11-05 11:56:49 -0400

    for lab 10: write SQL, add a controller, edit GET-POST-PUT-DELETE

---
+++ server/controllers/yilingController.js
@@ -0,0 +1,9 @@
+export default 'SELECT Animal_ID,
+    lifestyle_id,
+    pack,
+    domestication,
+    diet
+Form
+    animals a
+JOIN lifestyle l
+    ON a.lifestyle_lifestyle_id = l.lifestyle_id;'

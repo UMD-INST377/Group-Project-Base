95c3b45645ff5366a18412c9e6ee9fc5db3cfea9
vihtle <vihtle2000@gmail.com>
2021-11-06 18:27:37 -0400

    added other CRUD functions for aoaController

--- server/controllers/aoaController.js
+++ server/controllers/aoaController.js
@@ -1,2 +1,15 @@
-export default `SELECT aoa_id, aoa
+const aoaGet = `SELECT aoa_id, aoa
 FROM eruption_aoa;`
+
+const aoaPut = `SELECT aoa_id, aoa
+FROM eruption_aoa;`
+
+const aoaPost = `SELECT aoa_id, aoa
+FROM eruption_aoa;`
+
+const aoaDelete = `SELECT aoa_id, aoa
+FROM eruption_aoa;`
+
+export default {
+  aoaGet, aoaPut, aoaPost, aoaDelete
+};

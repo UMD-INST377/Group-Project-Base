ad6a76b8f59373d82d945ba0a36773ba332e0b0e
Jia Li <liandjessica@gmail.com>
2021-11-07 17:26:22 -0500

    add info Controller

--- server/controllers/aoaController.js
+++ server/controllers/aoaController.js
@@ -14,6 +14,3 @@ export default {
   aoaGet, aoaPut, aoaPost, aoaDelete
 };
 
-
-/* Jia eruption_info endpoint*/
-export default 'SELECT eruption_id, eruption_number, year, month, day, volcano_id, aoa_id, vei_id, evidence_id, cagetory_id'

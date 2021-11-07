ce192a01bedc0b63482be87eb81bde69a3a323ab
jh-yng <joshyang@umd.edu>
2021-11-07 16:36:48 -0500

    git push main

--- models/index.js
+++ models/index.js
@@ -4,6 +4,7 @@ import Macros from './Macros.js';
 import Meals from './Meals.js';
 import animals from './animals.js';
 import fight_mode from './fight_mode.js';
+import biomes from './biomes.js';
 
 export default {
   DietaryRestrictions,
@@ -11,5 +12,6 @@ export default {
   Macros,
   Meals,
   animals,
-  fight_mode
+  fight_mode,
+  biomes
 };

0a0d962342c5935daf6b5789c36dca2e789f27c7
skanner2 <63942471+skanner2@users.noreply.github.com>
2021-11-07 18:55:32 -0500

    added post query

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -275,42 +275,53 @@ router.get('/custom', async (req, res) => {
 /// /////////////////////////////////
 const nbaPlayerQuery = 'SELECT * FROM AWARDS';
 router.route('/nba-players')
-    .get(async (req, res) => {
-        try {
-            const result = await db.sequelizeDB.query(nbaPlayerQuery, {
-                type: sequelize.QueryTypes.SELECT
-            });
-            console.log('touched /nba-players with GET');
-            res.json(result);
-        } catch (err) {
-            console.error(err);
-            res.error('Server error');
-        }
-    })
+  .get(async (req, res) => {
+    try {
+      const result = await db.NBA.query(nbaPlayerQuery, {
+        type: sequelize.QueryTypes.SELECT
+      });
+      console.log('touched /nba-players with GET');
+      res.json(result);
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  })
 
-    .post(async (req, res) => {
-        try {
-            res.json(newNBA);
-        } catch (err) {
-            console.error(err);
-            res.error('Server error');
-        }
-    })
-    .delete(async (req, res) => {
-        try {
-            res.json({ message: 'delete NBA Database endpoint' });
-        } catch (err) {
-            console.log(error);
-            res.json({ error: 'Something wend wrong on the server' });
-        }
-    })
-    .put(async (req, res) => {
-        try {
-            res.json({ message: 'delete NBA Database endpoint' });
-        } catch (err) {
-            console.log(error);
-            res.json({ error: 'Something wend wrong on the server' });
-        }
-    });
+  .post(async (req, res) => {
+    const players = await db.NBA.findAll();
+    const currentId = (await players.length) + 1;
+    try {
+      const newPlayer = await db.NBA.create({
+        player_id: currentId,
+        player_name: req.body.player_name,
+        position_id: req.body.position_id,
+        ppg: req.body.ppg,
+        assists: req.body.assists,
+        team_id: req.body.team_id,
+        player_number: req.body.player_number
+      });
+      res.json(newNBA);
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  })
+  .delete(async (req, res) => {
+    try {
+      res.json({ message: 'delete NBA Database endpoint' });
+    } catch (err) {
+      console.log(error);
+      res.json({ error: 'Something wend wrong on the server' });
+    }
+  })
+  .put(async (req, res) => {
+    try {
+      res.json({ message: 'delete NBA Database endpoint' });
+    } catch (err) {
+      console.log(error);
+      res.json({ error: 'Something wend wrong on the server' });
+    }
+  });
 
 export default router;

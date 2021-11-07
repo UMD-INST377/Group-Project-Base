8a4c7141ced9898b26775fdff79dfaa641aad3f8
Faith Wilkins El <44947772+lavendermistyco@users.noreply.github.com>
2021-11-07 17:49:16 -0500

    Added in the Actors controller using SQL

--- controllers/actorsController.js
+++ controllers/actorsController.js
@@ -1,1 +1,10 @@
-// controller for actors goes here 
+// controller for actors goes here using regular SQL statement
+// wanted to do a Left JOIN to get all the actors available in the 
+// database regardless if there is a film to match
+export default `SELECT actor_name
+FROM actors a 
+LEFT JOIN actors_linking al
+   ON a.actor_id = al.actor_id
+INNER JOIN films f 
+   ON al.film_id = f.film_id
+`;

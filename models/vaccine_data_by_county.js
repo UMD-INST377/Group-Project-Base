d82ab58d8eb3e21bc8e53ae4416e556df4d3653a
jayid00 <jayid_07@yahoo.com>
2021-11-07 15:47:16 -0500

    renamed file name

---
+++ models/vaccine_data_by_county.js
@@ -0,0 +1,24 @@
+export default (database, DataTypes) => {
+  const VaccineData = database.define("vaccine_data_by_county", {
+    county_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: false,
+    },
+    first_dose_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+    },
+    first_dose_prop: {
+        type: DataTypes.DECIMAL,
+    },
+    second_dose_count: {
+        type: DataTypes.INTEGER,
+        allowNull: false, 
+    },
+    first_dose_prop: {
+        type: DataTypes.DECIMAL,
+    },
+  }),
+  return VaccineData;
+}

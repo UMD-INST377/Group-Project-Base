6efa0605c84268bb1ec77bf9d297a7ba08f70c28
jayid00 <jayid_07@yahoo.com>
2021-11-07 12:55:06 -0500

    Defined the county_info table

---
+++ models/County_Info.js
@@ -0,0 +1,26 @@
+export default (sequelize, DataTypes) => {
+  const county = database.define("county", {
+    county_ID: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    county: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    population: {
+      type: DataTypes.INTEGER,
+    },
+    population_density: {
+      type: DataTypes.INTEGER,
+    },
+    uninsured: {
+      type: DataTypes.DECIMAL,
+    },
+    poverty_rate: {
+      type: DataTypes.DECIMAL,
+    },
+  });
+  return county;
+};

1ac46fe4f1451e9b8bb9a03eff65fe37f69b0f64
vihtle <vihtle2000@gmail.com>
2021-11-08 14:26:19 -0500

    fixed aoacontroller and eruptionCategoryController to use SELECT, INSERT, UPDATE, and DELETE statements properly

--- server/controllers/eruptionCategoryController.js
+++ server/controllers/eruptionCategoryController.js
@@ -1,14 +1,15 @@
 const categoryGet = `SELECT category_id, category
 FROM eruption_category;`
 
-const categoryPut = `SELECT category_id, category
-FROM eruption_category;`
+const categoryPut = `UPDATE eruption_category
+SET category = :category 
+WHERE category_id = :category_id;`
 
-const categoryPost = `SELECT category_id, category
-FROM eruption_category;`
+const categoryPost = `INSERT INTO eruption_category (category_id, category)
+VALUES(DEFAULT, :category);`
 
-const categoryDelete = `SELECT category_id, category
-FROM eruption_category;`
+const categoryDelete = `DELETE FROM eruption_category
+WHERE category_id = :category_id;`
 
 export default {
   categoryGet, categoryPut, categoryPost, categoryDelete

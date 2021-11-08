4f2165bc5de04061b6ecbf7ac722effaadb0eb6f
Chelsea McGovern <cgmcgovern1@gmail.com>
2021-11-07 21:13:23 -0500

    fixed name of database

--- database/config.js
+++ database/config.js
@@ -2,21 +2,21 @@ export default {
   development: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'team_4_data',
+    database: 'team4_data',
     host: '18.212.135.23',
     dialect: 'mysql'
   },
   test: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'team_4_data',
+    database: 'team4_data',
     host: '18.212.135.23',
     dialect: 'mysql'
   },
   production: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'team_4_data',
+    database: 'team4_data',
     host: '18.212.135.23',
     dialect: 'mysql'
   }

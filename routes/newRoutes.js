e76d36c5d8004167b24680543a429adfb2928f66
damores <Diego.amores14@gmail.com>
2021-11-08 00:39:19 -0500

    fixed minor naming convention

--- routes/newRoutes.js
+++ routes/newRoutes.js
@@ -62,7 +62,7 @@ router.route('/vinyl')
     }
   })
 
-  .put(async(req, res) => {
+  .post(async(req, res) => {
     try {
       const insertVinylSQL = `INSERT INTO vinyl (singer_id, album_name, genre, track_amount, producer_id, runtime, first_available, weight, is_explicit)
       VALUES (${req.body.singer_id}, "${req.body.album_name}", "${req.body.genre}", ${req.body.track_amount}, ${req.body.producer_id}, "${req.body.runtime}", "${req.body.first_available}", "${req.body.weight}", ${req.body.is_explicit});`;
@@ -75,7 +75,7 @@ router.route('/vinyl')
       res.json({ error: 'Oops Error' });
     }
   })
-  .post(async(req, res) => {
+  .put(async(req, res) => {
     try {
       const updateVinylSQL = `UPDATE vinyl SET album_name = "${req.body.ablum_name}" WHERE album_name = "${req.body.ablum_name}"`;
 

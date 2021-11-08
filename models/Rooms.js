ff6c1b6efa765629763a23d0765fba6ed814a83c
graceoneill <goneill1@terpmail.umd.edu>
2021-11-07 19:54:52 -0500

    Rooms.js created

---
+++ models/Rooms.js
@@ -0,0 +1,40 @@
+export default (sequelize, DataTypes) => {
+    const room = sequelize.define(
+      'room',
+      {
+        room_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          unique: true,
+          primaryKey: true
+        },
+        room_type: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        hotel_id: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        num_guest: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        room_view: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+        },
+        kitchenette: {
+          type: DataTypes.TINYINT,
+          allowNull: false
+        },
+        cost_per_night: {
+          type: DataTypes.DECIMAL,
+          allowNull: false
+        }
+      },
+      { freezeTableName: true, timestamps: false }
+    );
+    return room;
+  };
+  

4e13690f29c6d84b7bc08e479068c0fab59b82ea
Joshadjei1 <joshuaa18.adjei@gmail.com>
2021-11-07 23:29:45 -0500

    Edit to call our group table

--- contollers/food_inspectionController.js
+++ contollers/food_inspectionController.js
@@ -1,2 +1,2 @@
-const foodInspectionVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM Food_Inspection WHERE ';
+const foodInspectionVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE ';
 export default router;

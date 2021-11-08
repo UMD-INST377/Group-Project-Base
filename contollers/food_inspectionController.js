f0e1760d3863aedd678703e78375a79096f3e2f4
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-11-08 00:00:05 -0500

    Update food_inspectionController.js

--- contollers/food_inspectionController.js
+++ contollers/food_inspectionController.js
@@ -1,2 +1,4 @@
-const foodInspectionVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE ';
+
+const establishmentVar = 'SELECT name, category, inspection_date, inspection_results FROM ';
 export default router;
+

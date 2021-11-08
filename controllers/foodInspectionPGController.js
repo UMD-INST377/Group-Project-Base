2c6f8bd21fdad8c1a6b891a5a8030befc00db75c
menal-shams <mshams1@umd.edu>
2021-11-07 19:34:41 -0500

    added comments and post controller

--- controllers/foodInspectionPGController.js
+++ controllers/foodInspectionPGController.js
@@ -1,15 +1,42 @@
 export default controllers = {
-  getAllController: 'SELECT * FROM pg_county_food_inspections.food_inspections;',
 
-  seoncdController: `SELECT hall_name,
-    hall_address,
-    hall_lat,
-    hall_long,
-    meal_name
-    FROM
-    Meals m
-    INNER JOIN Meals_Locations ml 
-    ON m.meal_id = ml.meal_id
-    INNER JOIN Dining_Hall d
-    ON d.hall_id = ml.hall_id;`
+  // team:the table name is food_inspections in our pg_county_food_inspections database - ms
+  /* this statement simply retrieves the entire table, which our front-end will sort based on
+         what the user wants to see */
+  getController: 'SELECT * FROM pg_county_food_inspections.food_inspections;',
+
+  // this statement creates a new record in the database//
+  postController: `INSERT INTO food_inspections,
+  name,
+  category,
+  inspection_date,
+  inspection_results,
+  city,
+  state,
+  zip,
+  address_line_1,
+  address_line_2,
+  food_from_approved_source,
+  food_protected_from_contamination,
+  ill_workers_restricted,
+  proper_hand_washing,
+  cooling_time_and_temperature,
+  cold_holding_temperature,
+  hot_holding_temperature,
+  cooking_time_and_temperature,
+  reheating_time_and_temperature,
+  hot_and_cold_running_water_provided,
+  proper_sewage_disposal,
+  no_bare_hand_contact,
+  adequate_hand_washing_facilities,
+  rodent_and_insects,
+  food_contact_surfaces_and_equipment,
+  inspection_type,
+  owner,
+  type,
+  location`,
+
+  putController: '',
+
+  deleteController: ''
 };

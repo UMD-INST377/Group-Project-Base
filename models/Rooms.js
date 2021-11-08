f9aa9dccd08ba1e3f50bbdc7e4b68c348a408ce4
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-11-07 21:30:45 -0500

    Update Rooms.js

--- models/Rooms.js
+++ models/Rooms.js
@@ -5,26 +5,26 @@ export default (sequelize, DataTypes) => {
       room_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-          unique: true,
-          primaryKey: true
-        },
-        room_type: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        hotel_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        num_guest: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        room_view: {
-          type: DataTypes.INTEGER,
-          allowNull: false
-        },
-        cost_per_night: {
+        unique: true,
+        primaryKey: true
+      },
+      room_type: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      hotel_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      num_guest: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      room_view: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      cost_per_night: {
           type: DataTypes.DECIMAL,
           allowNull: false
         }

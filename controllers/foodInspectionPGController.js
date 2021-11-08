ee4e10e4500f5621339ca20488f85576fee2b983
menal-shams <mshams1@umd.edu>
2021-11-07 19:19:13 -0500

    created simple SQL select statement to retrieve entire table

--- controllers/foodInspectionPGController.js
+++ controllers/foodInspectionPGController.js
@@ -1,15 +1,5 @@
 export default controllers = {
-  getController: `SELECT hall_name,
-    hall_address,
-    hall_lat,
-    hall_long,
-    meal_name
-    FROM
-    Meals m
-    INNER JOIN Meals_Locations ml 
-    ON m.meal_id = ml.meal_id
-    INNER JOIN Dining_Hall d
-    ON d.hall_id = ml.hall_id;`,
+  getAllController: 'SELECT * FROM pg_county_food_inspections.food_inspections;',
 
   seoncdController: `SELECT hall_name,
     hall_address,

3ebcbdfd7ff946882bf36882965fb6da9dc36a74
damores <Diego.amores14@gmail.com>
2021-11-08 00:01:48 -0500

    fixed config

--- database/config.js
+++ database/config.js
@@ -16,11 +16,7 @@ export default {
   },
 
   production: {
-<<<<<<< HEAD
     username: 'ubuntu',
-=======
-    username: 'student',
->>>>>>> 7cd7b79a605db466d9f3fb935dfb5561ac5b696d
     password: 'Veracrypt@12!',
     database: 'vinyl',
     host: '18.212.135.23',

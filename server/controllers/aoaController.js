8e0900e7586dfeff4f47fbc1c9c2dfe7bdc2f97f
vihtle <vihtle2000@gmail.com>
2021-11-07 21:06:42 -0500

    added placeholder for aoaDelete

--- server/controllers/aoaController.js
+++ server/controllers/aoaController.js
@@ -7,8 +7,8 @@ FROM eruption_aoa;`
 const aoaPost = `SELECT aoa_id, aoa
 FROM eruption_aoa;`
 
-const aoaDelete = `SELECT aoa_id, aoa
-FROM eruption_aoa;`
+const aoaDelete = `DELETE FROM eruption_aoa
+WHERE aoa_id = ???;`
 
 export default {
   aoaGet, aoaPut, aoaPost, aoaDelete

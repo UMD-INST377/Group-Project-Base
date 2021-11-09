34e9e1f8242362bfad10b3625a44f56b0a652642
TylerD01 <tdeaner@terpmail.umd.edu>
2021-11-09 11:49:54 -0500

    start of animal controller (not yet working)

---
+++ controllers/animalsController.js
@@ -0,0 +1,15 @@
+const animalsGET = `SELECT Animal_ID, common_name, species, weight_lbs
+FROM animals;`;
+
+const animalPOST = ``;
+
+const animalPUT = ``;
+
+const animalDELETE = ``;
+
+export default {
+  animalsGET,
+  animalPOST,
+  animalPUT,
+  animalDELETE
+}

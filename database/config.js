540fa08079ca1e1ab2add96ceb4a6d4fd92eba98
jchamdi <70522271+jchamdi@users.noreply.github.com>
2021-11-09 16:45:14 -0500

    Added in new IP address for DB

--- database/config.js
+++ database/config.js
@@ -1,16 +1,16 @@
 export default {
   development: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    username: 'ubuntu', 
+    password: 'Veracrypt@12!', 
+    database: 'Group11Movies', 
+    host: '18.212.135.23', 
     dialect: 'mysql'
   },
   test: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    username: 'ubuntu', 
+    password: 'Veracrypt@12!', 
+    database: 'Group11Movies', 
+    host: '18.212.135.23', 
     dialect: 'mysql'
   },
   production: { 
@@ -18,6 +18,6 @@ export default {
     password: 'Veracrypt@12!', 
     database: 'Group11Movies', 
     host: '18.212.135.23', 
-    dialect: 'mysql',
+    dialect: 'mysql'
   }
 };

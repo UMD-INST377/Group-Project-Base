02eafcceb96c01383c31abcdba39b2d51f3ddbf8
VishnuRaja1 <vishnuraja@hotmail.com>
2021-11-09 13:23:35 -0500

    controller

--- server/controllers/volcanosController.js
+++ server/controllers/volcanosController.js
@@ -1,21 +1,18 @@
-const { DELETE } = require("sequelize/types/lib/query-types");
-
 const volcanosGet = `SELECT volcano_id, volcano_name, latitude, longitude, volcano_number
-                     FROM volcanos;`
+FROM volcanos`;
 
 const volcanosPut = `UPDATE volcanos
-                     SET volcano_name = :volcano_name,
-                     latitude = :latitude,
-                     longitude = :longitude,
-                     volcano_number = :volcano_number
-                     WHERE volcano_id = :volcano_id'
-
+SET volcano_name = :volcano_name,
+latitude = :latitude,
+longitude = :longitude,
+volcano_number = :volcano_number
+WHERE volcano_id = :volcano_id`;
 
 const volcanosPost = `INSERT INTO volcanos(volcano_id, volcano_name, latitude, longitude, volcano_number)
-                      VALUES(DEFAULT, :volcano_name, :latitude, :longitude, :volcano_number);`
+VALUES(DEFAULT, :volcano_name, :latitude, :longitude, :volcano_number)`;
 
 const volcanosDelete = `DELETE FROM volcanos
-                        WHERE volcano_id = :volcano_id`
+WHERE volcano_id = :volcano_id`;
 
 export default {
   volcanosGet, volcanosPut, volcanosPost, volcanosDelete

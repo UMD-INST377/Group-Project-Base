f73f6e063d8ab389409810a48eba48c1044eef81
jayid00 <jayid_07@yahoo.com>
2021-11-11 17:45:20 -0500

    Fixed the model to freeze table name and exclude timestamp

--- models/County_Info.js
+++ models/County_Info.js
@@ -1,26 +1,30 @@
 export default (database, DataTypes) => {
-  const County = database.define("county_information", {
-    county_ID: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
+  const County = database.define(
+    "county_information",
+    {
+      county_ID: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+      },
+      county: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      population: {
+        type: DataTypes.INTEGER,
+      },
+      population_density: {
+        type: DataTypes.INTEGER,
+      },
+      uninsured: {
+        type: DataTypes.DECIMAL,
+      },
+      poverty_rate: {
+        type: DataTypes.DECIMAL,
+      },
     },
-    county: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    population: {
-      type: DataTypes.INTEGER,
-    },
-    population_density: {
-      type: DataTypes.INTEGER,
-    },
-    uninsured: {
-      type: DataTypes.DECIMAL,
-    },
-    poverty_rate: {
-      type: DataTypes.DECIMAL,
-    },
-  });
+    { freezeTableName: true, timestamps: false }
+  );
   return County;
 };

e1bb1f6d716aec102d8de85780b7c0587cd6fe19
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2021-11-11 16:32:17 -0500

    lab10 controller

---
+++ controllers/test_scores.js
@@ -0,0 +1,14 @@
+/* eslint-disable indent */
+/**
+ * Get N reviews from database by University ID
+ *
+ * @type {SQLStmt}
+ */
+const getTestScores = 'SELECT * FROM test_scores';
+
+
+// Export variables
+export default {
+    getTestScores,
+    
+};

ee399e109055907aa9ba2e80023e2eed0cfdd067
Zhaojie Yin <yinzhaojie@gmail.com>
2021-11-11 22:25:30 -0500

    delete controller and route

--- server/controllers/price.js
+++
@@ -1,8 +0,0 @@
-const get = 'SELECT * FROM price';
-const put = 'INSERT INTO price VALUES (99,1,"https://www.callofduty.com/blackopscoldwar/buy",59.99)';
-const post = 'UPDATE price SET price_website="https://www.callofduty.com/blackopscoldwar/buy",listed_price=59.99 WHERE price_id=99';
-const remove = 'DELETE FROM price WHERE price_id=99';
-
-export default {
-  get, put, post, remove
-};

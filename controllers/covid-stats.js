206b8dcc31a4ece5cce6af2804dcbbd42620186e
LuisAlfaro2 <alfaro.luis123@gmail.com>
2021-11-13 09:14:33 -0500

    Update covid-stats.js

--- controllers/covid-stats.js
+++ controllers/covid-stats.js
@@ -1,2 +1,7 @@
-const covidStatsCustom = `SELECT confirmed_deaths, positive_cases, county_death_prop, county_ID FROM covid_statistics_by_county`;
+const covidStatsCustom = `SELECT confirmed_deaths, 
+    positive_cases, 
+    county_death_prop, 
+    county_ID 
+FROM 
+    covid_statistics_by_county`;
 export default covidStatsCustom

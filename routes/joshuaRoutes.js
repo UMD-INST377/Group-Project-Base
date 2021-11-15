aab686bca5a4ff0ebf3d25f0e7d9c75811df34f7
Joshua-Cyriac <90971637+Joshua-Cyriac@users.noreply.github.com>
2021-11-15 16:20:02 -0500

    fixed changes

--- routes/joshuaRoutes.js
+++ routes/joshuaRoutes.js
@@ -5,7 +5,7 @@ import chalk from 'chalk';
 import fetch from 'node-fetch';
 
 import db from '../database/initializeDB.js';
-import directorMapCustom from '../controllers/directorController.js';
+import directorMapCustom from '../controllers/directorsController.js';
 
 const router = express.Router();
 
@@ -34,37 +34,31 @@ router.route('/directors')
   })
 
   .put(async (req, res) => {
-    // Must pass in both director_id(used to find entry to change) and director_name(what the updates should be)
-
     try {
-      const updateStatement = `UPDATE directors 
-        SET director_name = '${req.body.director_name}'
-        WHERE director_id = '${req.body.director_id}' `;
-      await db.sequelizeDB.query(updateStatement, {
-        type: sequelize.QueryTypes.UPDATE
+      console.log(res.json(req.body));
+      const director = await db.sequelizeDB.query(directorMapCustom, {
+        type: sequelize.QueryTypes.SELECT
       });
-      res.send(`"${req.body.director_name}" Successfully Updated`);
+
+      const currentID = (await director.length) + 1;
+      const directorName = getDirectoridByValue(director_name, req.body.director_name);
+      const directorId = directorName.map((movDirector) => movDirector.director_id)[0];
+      const createStatement = `INSERT INTO directors (director_id, director_name) 
+        VALUES (${currentID}, ${req.body.director_name}`;
+      const result = await db.sequelizeDB.query(createStatement, {
+        type: sequelize.QueryTypes.INSERT
+      });
+      res.json(result);
     } catch (error) {
       console.log(error);
       res.json({error: 'Something went wrong on the server w/ /directors PUT'});
     }
   })
 
-
   .post(async (req, res) => {
     try {
-      const director = await db.sequelizeDB.query(directorMapCustom, {
-        type: sequelize.QueryTypes.SELECT
-      });
-      
-      const currentID = (await director.length) + 1;
-      const createStatement = `INSERT INTO directors (director_id, director_name) 
-        VALUES (${currentID}, '${req.body.director_name}')`;
-      const result = await db.sequelizeDB.query(createStatement, {
-        type: sequelize.QueryTypes.INSERT
-      });
-
-      res.send(`"${req.body.director_name}" Successfully Updated`);
+      console.log('touched /director with POST');
+      res.json({message: 'touched /director with POST'});
     } catch (error) {
       console.log(error);
       res.json({error: 'Something went wrong on the server w/ /directors POST'});
@@ -73,13 +67,13 @@ router.route('/directors')
 
   .delete(async (req, res) => {
     try {
-      const directorStatement = `SELECT * FROM imdb_database.directors WHERE director_name = "${req.body.director_name}"`;
+      const directorStatement = `SELECT * FROM directors WHERE director_name = "${req.body.director_name}"`;
       const selectedDirector = await db.sequelizeDB.query(directorStatement, {
         type: sequelize.QueryTypes.SELECT
       });
       const directorId = selectedDirector.map((movId) => movId.director_id)[0];
       const deleteStatement = `DELETE FROM directors 
-      WHERE director_name = "${req.body.director_name}"`;
+      WHERE film_id = "${directorId}"`;
       await db.sequelizeDB.query(deleteStatement, {
         type: sequelize.QueryTypes.DELETE
       });
@@ -90,9 +84,4 @@ router.route('/directors')
     }
   });
 
-
-
-
-
-export default router; 
-
+export default router;

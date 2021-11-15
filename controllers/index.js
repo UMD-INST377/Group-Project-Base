8c8ea9c7472475851069b5fe7cb7a395a5d75ca5
michaelknapp2 <mknapp@terpmail.umd.edu>
2021-11-14 20:56:54 -0500

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
+/* eslint-disable camelcase */
 // Import all controllers
 import university from './university.js';
+import reviews from './reviews.js';
+import test_scores from './test_scores.js';
+import Admission_rate from './Admission_rate.js'
 
 // Export controllers
 export default {
   university,
-}
+  reviews,
+  test_scores,
+  Admission_rate
+};

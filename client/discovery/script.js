ab6b01ff78e6841dd68c41f8ccb0442ae2b73c93
dannyely <55502725+dannyely@users.noreply.github.com>
2021-11-17 11:29:54 -0500

    added search and displays html

--- client/discovery/script.js
+++ client/discovery/script.js
@@ -1,20 +1,20 @@
 async function dataHandler() {
-  const searchInput = document.querySelector('.search')
-  const searchButton = document.querySelector('.searchButton')
+  const searchInput = document.querySelector('.search');
+  const searchButton = document.querySelector('.searchButton');
   const columns = document.querySelector('.columns');
 
-// get the films data from its endpoint up here
-  const apiRequestFilms = await fetch("/api/films");
+  // get the films data from its endpoint up here
+  const apiRequestFilms = await fetch('/api/films');
   const filmsJson = await apiRequestFilms.json();
+  console.log(filmsJson);
 
-  console.log(filmsJson)
-  //not sure what this part does
-  const coll = document.querySelectorAll(".collapsible");
+  // not sure what this part doesthat j
+  const coll = document.querySelectorAll('.collapsible');
   let i;
 
   for (i = 0; i < coll.length; i += 1) {
-    coll[i].addEventListener("click", function () {
-      this.classList.toggle("active");
+    coll[i].addEventListener('click', function () {
+      this.classList.toggle('active');
       const content = this.nextElementSibling;
       if (content.style.maxHeight) {
         content.style.maxHeight = null;
@@ -23,22 +23,36 @@ async function dataHandler() {
       }
     });
   }
-  function findMatches(phrase,film) {
+  function findMatches(phrase, film) {
     return filmsJson.filter((film) => {
-      //only look with matching category
+      // only look with matching category
       const regex = new RegExp(phrase, 'gi');
       return film.film_title.match(regex);
     });
   }
 
-  // function runs on submit button
+  // function runs on submit button; it clears out results, and then appends new ones
   function submitSearch() {
-    // console.log(searchInput.value);
-    // console.log(findMatches(searchInput.value))
-    const filmMatches = findMatches(searchInput.value))
-    
+    columns.innerHTML = '';
+    const filmMatches = findMatches(searchInput.value);
+    filmMatches.forEach((film) => appendMovieBox(film));
   }
-  searchButton.addEventListener("click", submitSearch);
+
+  // add html elements of movies
+  function appendMovieBox(film) {
+    const html = `<div class="box">
+                  <div class="movieBox">a</div>
+                  <div>${film.film_title} : Rating (${film.imdb_rating})<div>
+                </div>`;
+    columns.innerHTML += html;
+  }
+  searchButton.addEventListener('click', submitSearch);
+  searchInput.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+      submitSearch();
+    }
+  });
 }
 
 window.onload = dataHandler;

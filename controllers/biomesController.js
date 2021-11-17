1885e265a4f5875106a82d291009ef25de234703
jh-yng <joshyang@umd.edu>
2021-11-17 14:48:11 -0500

    commit on main

---
+++ controllers/biomesController.js
@@ -0,0 +1,9 @@
+//put sql queries
+const getBiomes = '';
+const putBiomes = '';
+const postBiomes = '';
+const deleteBiomes = '';
+
+export default {
+getBiomes, putBiomes, postBiomes, deleteBiomes
+};

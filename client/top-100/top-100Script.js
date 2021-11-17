6bf61fb9adabf9cb7f75314830dce10f70d8a4bb
Faith Wilkins El <44947772+lavendermistyco@users.noreply.github.com>
2021-11-17 18:04:39 -0500

    got the json response from the top100 endpoint

--- client/top-100/top-100Script.js
+++ client/top-100/top-100Script.js
@@ -1,13 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
   const top100List = document.querySelector('.top-100-movie-list');
 
+  const apiKey = 'fd78e1d1d80dd5c9a8cc46852b23aefc';
+  const apiImageLink = 'https://image.tmdb.org/t/p/w500';
+  
   // This will create the cards with the images attached
-  function displayImageCard() {
-  }
+  const makeMovieImageCard = (title, description, rating, num) => {
+    top100List.innerHTML += `
+    <div class="movie-container">
+      <div class="circle"> <div id='circleNum'>${num}</div></div>
+      <div class="movie-card"> 
+          <div class ="movie">
+              <img src='./images/avengersInfinity.jpg' alt='Avengers Infinity War Image'>
+              <h1 class="movie-name">${title}</h1>
+          </div>
+          <div class="movie-description">
+              <p class="description-text"> ${description} </p>
+          </div>
+          <div class="movie-rating">${rating}</div>
+      </div>
+    </div> 
+    `;
+  };
 
-  async function imageExtractor() {
+  async function imageExtractor(imageCardList) {
     // fetch the films table
     const response = await fetch('../api/top100');
     // extract the json data
+    const top100movies = await response.json();
+    console.log(top100movies);
   }
+
+  imageExtractor(top100List);
 });

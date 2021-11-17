cb8dc9be77243810a60efe537e041f20da15e37c
jh-yng <joshyang@umd.edu>
2021-11-17 14:58:04 -0500

    commit on JaeukY-Branch

--- controllers/biomesController.js
+++ controllers/biomesController.js
@@ -1,7 +1,13 @@
-//put sql queries
-const getBiomes = '';
-const putBiomes = '':
-const postBiomes = '':
-const deleteBiomes = '';
+const getBiomes = 'SELECT biome_id, animal_id, Tropical_Rainforest, Temperate_Forest, Desert, Tundra, Grassland, Savanna, Freshwater, Marine FROM biomes;';
+
+const putBiomes = `INSERT INTO biomes (biome_id, animal_id, Tropical_Rainforest, Temperate_Forest, Desert, Tundra, Grassland, Savanna, Freshwater, Marine)
+VALUES(DEFAULT, :animal_id, :species, :weight_lbs);`;
+
+const postBiomes = `UPDATE biomes
+SET animal_id = :animal_id, Tropical_Rainforest = :Tropical_Rainforest, Temperate_Forest = :Temperate_Forest, Desert = :Desert, Tundra = :Tundra, Grassland = :Grassland, Savanna = :Savanna, Freshwater = :Freshwater, Marine = :Marine
+WHERE biome_id = :biome_id;`;
+
+const deleteBiomes = `DELETE FROM biomes
+WHERE biome_id = :biome_id;`;
 
 export default {getBiomes, putBiomes, postBiomes, deleteBiomes};

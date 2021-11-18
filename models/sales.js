4188eefad1636ff164c6826041a9eb093077b514
caseyt711 <caseyt711@gmail.com>
2021-11-18 00:44:13 -0500

    Updated sales but endpoint not complete

--- models/sales.js
+++ models/sales.js
@@ -1,30 +1,28 @@
-export default (database, DataTypes) => {
-    const sales = database.define(
-      'sales',
-      {
-        sales_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          unique: true,
-          primaryKey: true
-        },
-        sales_game_id: {
-          type: DataTypes.INTEGER,
-          allowNull: true,
-          unique: true
-        },
-        annual_sales: {
-          type: DataTypes.TEXT
-        },
-        earnings: {
-          type: DataTypes.TEXT
-        },
-        budget: {
-          type: DataTypes.TEXT
-        }
+export default (sequelize, DataTypes) => {
+  const sales = sequelize.define(
+    'sales',
+    {
+      sales_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return sales;
-}
-  
+      sales_game_id: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        unique: true
+      },
+      annual_sales: {
+        type: DataTypes.TEXT
+      },
+      earnings: {
+        type: DataTypes.TEXT
+      },
+      budget: {
+        type: DataTypes.TEXT
+      }
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return sales;
+};
--- server/controllers/sales.js
+++ server/controllers/sales.js
@@ -1,2 +1,8 @@
-export default `SELECT sales_game_id, annual_sales, earnings, budget FROM sales`;
+const get = 'SELECT * FROM sales';
+const insert = 'INSERT INTO sales VALUES(99,1,NULL,NULL,NULL)';
+const update = 'UPDATE sales SET budget=NULL,earnings=NULL,annual_sales=NULL WHERE sales_id=99';
+const remove = 'DELETE FROM sales WHERE sales_id=99';
 
+export default {
+  get, insert, update, remove
+};

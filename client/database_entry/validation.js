dba187a4ab03582f531f9812b997fd3b5063e290
WilliamGiovanini <wgiovani@terpmail.umd.edu>
2021-11-18 09:24:20 -0500

    Update validation.js

--- client/database_entry/validation.js
+++ client/database_entry/validation.js
@@ -393,10 +393,11 @@ async function postVinyl() {
         // console.log(responseProducers);
 
         //Gets singer_id for vinyl being input
-        const singersRequest = await fetch('https://inst377-vinylweb.herokuapp.com/api/singers')
-        const singers = singersRequest.then()
-        console.log(singersRequest)
-        console.log(singers)
+        const singers = await fetch('https://inst377-vinylweb.herokuapp.com/api/singers')
+        .then(function(response) {
+            return response.json();
+        });
+        console.log(singers);
 
         /*
         let singer_id_vinyl;

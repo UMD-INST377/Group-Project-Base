11efd2c85855aea290588fd01f20a3e0ed0e54b6
jh-yng <joshyang@umd.edu>
2021-11-17 21:56:31 -0500

    commit on JaeukY-Branch

--- controllers/biomesController.js
+++ controllers/biomesController.js
@@ -1,4 +1,4 @@
-const getBiomes = 'SELECT biome_id, Biome, Continent FROM biomes';
+const getBiomes = 'SELECT biome_id, Biome, Continent FROM biome';
 
 const putBiomes = `INSERT INTO biome (biome_id, Biome, Continent)
 VALUES(DEFAULT, :biome_id, :Biome, :Continent);`;

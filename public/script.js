35f1062f0e28cbc687f3cd538e0ff26d3afc7098
jakew173 <63312771+jakew173@users.noreply.github.com>
2021-11-19 12:45:52 -0500

    names/id added

---
+++ public/script.js
@@ -0,0 +1,30 @@
+const updatePresident = document.querySelector('#update_button');
+updatePresident.addEventListener('click', function_name_here);
+
+<form action="signup.html" method="post" id="signup">
+	<h1>Sign Up</h1>
+	<div class="field">
+		<label for="name">Name:</label>
+		<input type="text" id="name" name="name" placeholder="Enter your fullname" />
+		<small></small>
+	</div>
+	<div class="field">
+		<label for="email">Email:</label>
+		<input type="text" id="email" name="email" placeholder="Enter your email address" />
+		<small></small>
+	</div>
+	<button type="submit">Subscribe</button>
+</form>
+
+let data = {"president_id": ,
+"first_name": createForm.elements['first_name'],
+"last_name":  createForm.elements['last_name'],
+"date_inaurg": createForm.elements['date_inaurg'],
+"age_inaurg": createForm.elements['age_inaurg'],
+"terms_served": createForm.elements['terms_served'],
+"birth_date": createForm.elements['birth_date'],
+"death_date": createForm.elements['death_date'],
+"home_state": createForm.elements['home_state'],
+"president_image": createForm.elements['president_image'],
+"party": createForm.elements['party']
+}

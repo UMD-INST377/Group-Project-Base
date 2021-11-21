ae2247c1a993dac384bb1ae063418c31778ef7d9
skanner2 <63942471+skanner2@users.noreply.github.com>
2021-11-21 18:32:22 -0500

    added data table to homepage

---
+++ public/script.js
@@ -0,0 +1,24 @@
+async function windowActions() {
+  const endpoint = '../api/nba-players';
+  const request = await fetch(endpoint);
+  const players = await request.json();
+  console.log(players);
+
+  function displayTable() {
+    const html = players.map((player) => `<tr>
+                        <th>Player</th>
+                        <th>PPG</th> 
+                        <th>Assists</th>
+                    </tr>
+                    <tr>
+                        <td>${player.player_name}</td>
+                        <td>${player.ppg}</td> 
+                        <td>${player.assists}</td>
+                    </tr>`);
+    player_table.innerHTML = html;
+  }
+
+  const player_table = document.querySelector('.table');
+  displayTable();
+}
+window.onload = windowActions;

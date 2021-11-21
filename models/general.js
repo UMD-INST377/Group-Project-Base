c0ac2743dc6d897475854dbbb56c1d433bdf1a37
Zhaojie Yin <yinzhaojie@gmail.com>
2021-11-21 13:00:33 -0500

    Update general.js

--- models/general.js
+++ models/general.js
@@ -1,26 +1,27 @@
-export default (database, DataTypes) => {
-  const general_information = database.define(
-    'general_information',
-    {
-      game_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
+export default (database, DataTypes) =>{
+  const general_information=database.define(
+      'general_information',
+      {
+         game_id:{
+             type:DataTypes.INTEGER,
+             allowNull: false,
+             primaryKey: true
+         },
+         game_name:{
+              type:DataTypes.TEXT,
+          },
+          release_date:{
+              type:DataTypes.TEXT
+          },
+          free_to_play:{
+              type:DataTypes.TEXT
+          },
+          player_populaion:{
+              type:DataTypes.TEXT
+          }
+
       },
-      game_name: {
-        type: DataTypes.TEXT,
-      },
-      release_date: {
-        type: DataTypes.TEXT,
-      },
-      free_to_play: {
-        type: DataTypes.TEXT,
-      },
-      player_populaion: {
-        type: DataTypes.TEXT,
-      },
-    },
-    { freezerTableName: true, timestamps: false }
+      {freezerTableName:true, timestamps:false}
   );
   return general_information;
-};
+}

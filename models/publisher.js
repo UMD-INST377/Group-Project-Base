af557da28cfa6c93e4902198b0dcf074e130f6a6
Zhaojie Yin <yinzhaojie@gmail.com>
2021-11-21 13:03:07 -0500

    1

--- models/publisher.js
+++ models/publisher.js
@@ -1,23 +1,24 @@
-export default (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) =>{
   const publisher = sequelize.define(
-    'publisher',
-    {
-      publisher_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-      },
-      publisher_game_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-      },
+      'publisher',
+      {
+          publisher_id:{
+             type:DataTypes.INTEGER,
+             allowNull: false,
+             primaryKey: true
+          },
+          publisher_game_id: {
+              type:DataTypes.INTEGER,
+             allowNull: false,
+             primaryKey: true
+          },
 
-      publisher_name: {
-        type: DataTypes.TEXT,
+          publisher_name: {
+              type:DataTypes.TEXT
+             
+          }
       },
-    },
-    { freezerTableName: true, timestamps: false }
+      {freezerTableName:true, timestamps:false}
   );
   return publisher;
-};
+}
--- models/sales.js
+++ models/sales.js
@@ -1,27 +1,26 @@
-export default (database, DataTypes) => {
-  const sales = database.define(
+export default (sequelize, DataTypes) => {
+  const sales = sequelize.define(
     'sales',
     {
       sales_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
-        unique: true,
-        primaryKey: true,
+        primaryKey: true
       },
       sales_game_id: {
         type: DataTypes.INTEGER,
         allowNull: true,
-        unique: true,
+        unique: true
       },
       annual_sales: {
-        type: DataTypes.TEXT,
+        type: DataTypes.TEXT
       },
       earnings: {
-        type: DataTypes.TEXT,
+        type: DataTypes.TEXT
       },
       budget: {
-        type: DataTypes.TEXT,
-      },
+        type: DataTypes.TEXT
+      }
     },
     { freezeTableName: true, timestamps: false }
   );

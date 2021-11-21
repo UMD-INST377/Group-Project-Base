2c98770c8866c87aeec1951bc0ab7b6703218829
Oluwatobiloba Odebo <62259460+OluwatobilobaOdebo@users.noreply.github.com>
2021-11-20 21:07:34 -0500

    Update teamscontroller.js

--- public/controllers/teamscontroller.js
+++ public/controllers/teamscontroller.js
@@ -22,42 +22,42 @@ const router = express.Router();
 const teamsController = require('../public/controllers/teamsController.js');
 
 exports.get = router.get('/teams', async (req, res) => {
-    try {
-      const result = await db.teams.query(teamsCustom, {
-        type: sequelize.QueryTypes.SELECT,
-      });
-      res.json(result);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-
-exports.get = router.get('/teams', async (req, res) => {
-    try {
-      const team = await db.teams.findAll();
-      res.json(teams);
-    } catch (err) {
-      console.error(err);
-      res.error('Server error');
-    }
-  });
-
-exports.get = router.get("/teams/:team_id", async (req, res) => {
-    try {
-        const team = await db.teams.findAll({
-          where: {
-            team_id: req.params.team_id,
-          },
-        });
-    res.json(team);
+  try {
+    const result = await db.teams.query(teamsCustom, {
+      type: sequelize.QueryTypes.SELECT,
+    });
+    res.json(result);
   } catch (err) {
     console.error(err);
-    res.error("Server error");
+    res.error('Server error');
   }
 });
 
-exports.post = router.post("/teams", async (req, res) => {
+exports.get = router.get('/teams', async (req, res) => {
+  try {
+    const team = await db.teams.findAll();
+    res.json(teams);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+exports.get = router.get('/teams/:team_id', async (req, res) => {
+  try {
+    const team = await db.teams.findAll({
+      where: {
+        team_id: req.params.team_id,
+      },
+    });
+    res.json(team);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+exports.post = router.post('/teams', async (req, res) => {
   const currentId = (await teams.length) + 1;
   try {
     const newTeam = await db.teams.create({
@@ -72,41 +72,41 @@ exports.post = router.post("/teams", async (req, res) => {
     res.json(newTeam);
   } catch (err) {
     console.error(err);
-    res.error("Server error");
+    res.error('Server error');
   }
 });
 
-exports.delete = router.delete("/teams/:team_id", async (req, res) => {
+exports.delete = router.delete('/teams/:team_id', async (req, res) => {
   try {
     await db.teams.destroy({
       where: {
         team_id: req.params.team_id,
       },
     });
-    res.send("Successfully Deleted");
+    res.send('Successfully Deleted');
   } catch (err) {
     console.error(err);
-    res.error("Server error");
+    res.error('Server error');
   }
 });
 
-exports.put = router.put("/teams", async (req, res) => {
-    const currentId = (await teams.length) + 1;
-    try {
-      const newTeam = await db.teams.create({
-        team_id: currentId,
-        team_name: req.body.team_name,
-        conference: req.body.conference,
-        division: req.body.division,
-        general_manager: req.body.general_manager,
-        coach: req.body.coach,
-        arena_id: req.body.arena_id
-      });
-      res.json(newTeam);
-    } catch (err) {
-      console.error(err);
-      res.error("Server error");
-    }
-  });
+exports.put = router.put('/teams', async (req, res) => {
+  const currentId = (await teams.length) + 1;
+  try {
+    const newTeam = await db.teams.create({
+      team_id: currentId,
+      team_name: req.body.team_name,
+      conference: req.body.conference,
+      division: req.body.division,
+      general_manager: req.body.general_manager,
+      coach: req.body.coach,
+      arena_id: req.body.arena_id
+    });
+    res.json(newTeam);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
 
 export default router;

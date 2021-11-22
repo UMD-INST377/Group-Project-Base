deb02d789db91fde697023d6617cba4c8486c908
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-11-22 00:14:51 -0500

    More changes to try and fix search script but not complete :(

--- client/searchscript.js
+++ client/searchscript.js
@@ -1,23 +1,43 @@
-function searchHotels() {
-    // Get data
-    const request = await fetch('/api/HotelOverview');
-    const d = await request.json();
-    const dat = d.data;
-        // Declare variables
-        var input, filter, ul, li, a, i, txtValue;
-        input = document.getElementById('myInput');
-        filter = input.value.toUpperCase();
-        ul = document.getElementById("myUL");
-        li = ul.getElementsByTagName('li');
-      
-        // Loop through all list items, and hide those who don't match the search query
-        for (i = 0; i < li.length; i++) {
-          a = li[i].getElementsByTagName("a")[0];
-          txtValue = a.textContent || a.innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            li[i].style.display = "";
-          } else {
-            li[i].style.display = "none";
-          }
-        }
-      }
+async function windowActions() {
+  // Get data
+  const request = await fetch("/api/HotelOverview");
+  const arrayName = await request.json();
+
+  function findMatches(wordToMatch, arrayName) {
+    return arrayName.filter((place) => {
+      const regex = RegExp(wordToMatch, "gi");
+      return (
+        place.hotel_name.match(regex)
+      );
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, arrayName);
+    const html = matchArray
+      .map((place) => {
+        const regex = new RegExp(event.target.value, "gi");
+        place.hotel_name.replace(
+          regex,
+          `<span class="h1">${event.target.value.toUpperCase()}</span>`
+        );
+        return `
+              <ul>
+              <li><div>${place.hotel_name}</div></li>
+              </ul>
+                  `;
+      })
+      .join("");
+
+    suggestions.innerHTML = html;
+  }
+
+  const searchInput = document.querySelector(".search");
+  const suggestions = document.querySelector(".suggestions");
+
+  searchInput.addEventListener("change", displayMatches);
+  searchInput.addEventListener("keyup", (evt) => {
+    displayMatches(evt);
+  });
+}
+window.onload = windowActions;

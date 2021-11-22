7f93bd9121a9e0d9c7be9644678eebd0c09fb85c
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-11-22 13:50:36 -0500

    changes

---
+++ routes/generalRoutes.js
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+import express from 'express';
+
+import db from '../database/initializeDB.js';
+
+import general from '../server/controllers/general.js';
+
+const router = express.Router();
+
+/// ////Genral Information Endpoints////////
+router.route('/general')
+  .get(async(req, res) => {
+    try {
+      const retrieveGames = await db.sequelizeDB.query(
+        general.get
+      );
+      res.send(retrieveGames);
+    } catch (err) {
+      console.log(err);
+      res.json({error: 'Server error'});
+    }
+  })
+  .put(async(req, res) => {
+    try {
+      const newGames = await db.sequelizeDB.query(
+        general.put
+      );
+      res.send(newGames);
+    } catch (err) {
+      console.log(err);
+      res.json({error: 'Server error'});
+    }
+  })
+  .post(async(req, res) => {
+    try {
+      const updateGames = await db.sequelizeDB.query(
+        general.post
+      );
+      res.send(updateGames);
+    } catch (err) {
+      console.log(err);
+      res.json({error: 'Server error'});
+    }
+  })
+  .delete(async(req, res) => {
+    try {
+      const removeGames = await db.sequelizeDB.query(
+        general.remove
+      );
+      res.send(removeGames);
+    } catch (err) {
+      console.log(err);
+      res.json({error: 'Server error'});
+    }
+  });
+export default router;

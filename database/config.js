004e02dd844cdcefe9dae5c14486cedd86139239
enyu0510 <jonathanchen0510@gmail.com>
2021-11-21 22:24:00 -0500

    Connect to our database

--- database/config.js
+++ database/config.js
@@ -1,23 +1,23 @@
 export default {
   development: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
+    username: "student",
+    password: "INST377@UMD",
+    database: "extinct_animals_db",
+    host: "3.236.243.212",
+    dialect: "mysql",
   },
   test: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
+    username: "student",
+    password: "INST377@UMD",
+    database: "extinct_animals_db",
+    host: "3.236.243.212",
+    dialect: "mysql",
   },
   production: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
-    dialect: 'mysql'
-  }
+    username: "student",
+    password: "INST377@UMD",
+    database: "extinct_animals_db",
+    host: "3.236.243.212",
+    dialect: "mysql",
+  },
 };

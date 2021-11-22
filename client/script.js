b0f8de9d63fdea4f13ce9220f9ff41a58765600c
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-11-21 20:58:09 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -2,11 +2,11 @@ let slidePosition = 0;
 const slides = document.querySelectorAll('.carousel_item');
 const totalSlides = slides.length;
 
-document.
-    querySelector('#carousel_button--next')
-    .addEventListener("click", function() {
+document
+  .querySelector('#carousel_button--next')
+  .addEventListener('click', function() {
         moveToNextSlide();
-    })
+  })
 
 document.
     querySelector('#carousel_button--prev')

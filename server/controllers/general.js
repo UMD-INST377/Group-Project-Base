0ad58b6592e92a947deb9a7e72c52fd198b6eec7
Zhaojie Yin <yinzhaojie@gmail.com>
2021-11-22 17:52:44 -0500

    update general controller

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,6 +1,6 @@
 const get = 'SELECT * FROM general_information';
 const put = 'INSERT INTO general_information VALUES (99,"fifa","jan","idk","12222")';
-const post = 'UPDATE platforms SET game_name="fifa",release_date="jun",free_to_play="idk",player_population="322" WHERE game_id = 1';
+const post = 'UPDATE general_information SET game_name="fifa",release_date="jun",free_to_play="idk",player_population="322" WHERE game_id = 1';
 const remove = 'DELETE FROM general_information WHERE game_id=99';
 
 export default {

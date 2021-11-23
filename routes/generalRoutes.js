992228ac0e399aba23fe29f9d4f399c1c3953e91
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-11-22 23:28:51 -0500

    fixing api

--- routes/generalRoutes.js
+++ routes/generalRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 
 import db from '../database/initializeDB.js';
 
-import general from '../server/controllers/platforms.js';
+import general from '../server/controllers/general.js';
 
 const router = express.Router();
 

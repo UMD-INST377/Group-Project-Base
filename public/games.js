6ce768f81c3037a8706e0b28ffd8091072f17c03
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-11-22 23:40:31 -0500

    table

--- public/games.js
+++ public/games.js
@@ -1,18 +1,19 @@
+/* eslint-disable no-const-assign */
 fetch('http://localhost:3000/api/general').then(
   res => {
     res.json().then(
       data => {
         console.log(data.data);
         if (data.data.length > 0) {
-
-          var temp = "";
+          const temp = '';
           data.data.forEach((itemData) => {
+            // eslint-disable-next-line no-const-assign
             temp += '<tr>';
-            temp += '<td>' + itemData.game_id + "</td>";
-            temp += "<td>" + itemData.game_name + "</td>";
-            temp += "<td>" + itemData.release_date + "</td>";
-            temp += "<td>" + itemData.free_to_play + "</td>";
-            temp += "<td>" + itemData.player_population + "</td></tr>";
+            temp += '<td>' + itemData.game_id + '</td>';
+            temp += '<td>' + itemData.game_name + '</td>';
+            temp += '<td>' + itemData.release_date + '</td>';
+            temp += '<td>' + itemData.free_to_play + '</td>';
+            temp += '<td>' + itemData.player_population + '</td></tr>';
           });
           document.getElementById('data').innerHTML = temp;
         }

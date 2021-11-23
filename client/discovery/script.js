8dd6178af854130ad7ccb99d8de36ca7fa40db00
KamranDjourshari24 <kdjour00@umd.edu>
2021-11-23 18:21:41 -0500

    Nav bar changes

--- client/discovery/script.js
+++ client/discovery/script.js
@@ -1,3 +1,15 @@
+const burgerIcon = document.getElementById('burger');
+
+function toggleBurgerMenu(burger) {
+  const dropMenu = document.getElementById('navbarBasicExample');
+  burger.classList.toggle('is-active');
+  dropMenu.classList.toggle('is-active');
+}
+
+burgerIcon.addEventListener('click', () => {
+  toggleBurgerMenu(burgerIcon);
+});
+
 async function dataHandler() {
   async function buttonHandler() {
     const genreBox = document.getElementById('genre-input').value;

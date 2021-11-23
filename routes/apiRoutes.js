18a8c51ab0ffb118797badf3212f09cef4729ed2
jasmineokebugwu <jokebugw@umd.edu>
2021-11-22 19:47:53 -0500

    continued to fix endpoints

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,9 @@
 /* eslint-disable no-console */
 import express from 'express';
-import sequelize from 'sequelize';
+import sequelize, { QueryTypes } from 'sequelize';
 
 import db from '../database/initializeDB.js';
 import foodInspectionVar from '../contollers/food_inspectionController.js';
-
 
 const router = express.Router();
 
@@ -17,7 +16,8 @@ router.get('/', (req, res) => {
 /// /////////////////////////////////
 router.route('/foodServicePG').get(async (req, res) => {
   try {
-    const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const url =
+      'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const data = await fetch(url);
     const json = await data.json();
     console.log(json);
@@ -42,7 +42,11 @@ router.get('establishments/establishment_id', async (req, res) => {
 
 router.put('establishments/establishment_id', async (req, res) => {
   try {
-    const businesses = await db.sequelizeDB.query();
+    const businesses = await db.sequelizeDB.query(
+      {
+        type: QueryTypes.SELECT
+      }
+    );
 
     res.json(businesses);
   } catch (err) {
@@ -76,7 +80,7 @@ router.delete('establishments/establishment_id', async (req, res) => {
   }
 });
 
-/*router.put('/FOOD_INSPECTION_GROUP8.Food_Inspection/:Establishment_id', async (req, res) => {
+/* router.put('/FOOD_INSPECTION_GROUP8.Food_Inspection/:Establishment_id', async (req, res) => {
   try {
     const businesses = await db.FoodInspection.findAll({
       where: {
@@ -110,8 +114,8 @@ router.get('/meals/:meal_id', async (req, res) => {
   try {
     const meals = await db.Meals.findAll({
       where: {
-        meal_id: req.params.meal_id
-      }
+        meal_id: req.params.meal_id,
+      },
     });
     res.json(meals);
   } catch (err) {
@@ -125,12 +129,12 @@ router.put('/meals', async (req, res) => {
     await db.Meals.update(
       {
         meal_name: req.body.meal_name,
-        meal_category: req.body.meal_category
+        meal_category: req.body.meal_category,
       },
       {
         where: {
-          meal_id: req.body.meal_id
-        }
+          meal_id: req.body.meal_id,
+        },
       }
     );
     res.send('Meal Successfully Updated');
@@ -157,8 +161,8 @@ router.get('/macros/:meal_id', async (req, res) => {
   try {
     const meals = await db.Macros.findAll({
       where: {
-        meal_id: req.params.meal_id
-      }
+        meal_id: req.params.meal_id,
+      },
     });
     res.json(meals);
   } catch (err) {
@@ -180,12 +184,12 @@ router.put('/macros', async (req, res) => {
         sodium: req.body.sodium,
         carbs: req.body.carbs,
         protein: req.body.protein,
-        fat: req.body.fat
+        fat: req.body.fat,
       },
       {
         where: {
-          meal_id: req.body.meal_id
-        }
+          meal_id: req.body.meal_id,
+        },
       }
     );
     res.send('Successfully Updated');
@@ -212,8 +216,8 @@ router.get('/restrictions/:restriction_id', async (req, res) => {
   try {
     const restrictions = await db.DietaryRestrictions.findAll({
       where: {
-        restriction_id: req.params.restriction_id
-      }
+        restriction_id: req.params.restriction_id,
+      },
     });
     res.json(restrictions);
   } catch (err) {
@@ -225,11 +229,12 @@ router.get('/restrictions/:restriction_id', async (req, res) => {
 /// //////////////////////////////////
 /// ///////Custom SQL Endpoint////////
 /// /////////////////////////////////
-const macrosCustom = 'SELECT `Dining_Hall_Tracker`.`Meals`.`meal_id` AS `meal_id`,`Dining_Hall_Tracker`.`Meals`.`meal_name` AS `meal_name`,`Dining_Hall_Tracker`.`Macros`.`calories` AS `calories`,`Dining_Hall_Tracker`.`Macros`.`carbs` AS `carbs`,`Dining_Hall_Tracker`.`Macros`.`sodium` AS `sodium`,`Dining_Hall_Tracker`.`Macros`.`protein` AS `protein`,`Dining_Hall_Tracker`.`Macros`.`fat` AS `fat`,`Dining_Hall_Tracker`.`Macros`.`cholesterol` AS `cholesterol`FROM(`Dining_Hall_Tracker`.`Meals`JOIN `Dining_Hall_Tracker`.`Macros`)WHERE(`Dining_Hall_Tracker`.`Meals`.`meal_id` = `Dining_Hall_Tracker`.`Macros`.`meal_id`)';
+const macrosCustom =
+  'SELECT `Dining_Hall_Tracker`.`Meals`.`meal_id` AS `meal_id`,`Dining_Hall_Tracker`.`Meals`.`meal_name` AS `meal_name`,`Dining_Hall_Tracker`.`Macros`.`calories` AS `calories`,`Dining_Hall_Tracker`.`Macros`.`carbs` AS `carbs`,`Dining_Hall_Tracker`.`Macros`.`sodium` AS `sodium`,`Dining_Hall_Tracker`.`Macros`.`protein` AS `protein`,`Dining_Hall_Tracker`.`Macros`.`fat` AS `fat`,`Dining_Hall_Tracker`.`Macros`.`cholesterol` AS `cholesterol`FROM(`Dining_Hall_Tracker`.`Meals`JOIN `Dining_Hall_Tracker`.`Macros`)WHERE(`Dining_Hall_Tracker`.`Meals`.`meal_id` = `Dining_Hall_Tracker`.`Macros`.`meal_id`)';
 router.get('/table/data', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(macrosCustom, {
-      type: sequelize.QueryTypes.SELECT
+      type: sequelize.QueryTypes.SELECT,
     });
     res.json(result);
   } catch (err) {
@@ -252,7 +257,7 @@ ON d.hall_id = ml.hall_id;`;
 router.get('/map/data', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(mealMapCustom, {
-      type: sequelize.QueryTypes.SELECT
+      type: sequelize.QueryTypes.SELECT,
     });
     res.json(result);
   } catch (err) {
@@ -263,7 +268,7 @@ router.get('/map/data', async (req, res) => {
 router.get('/custom', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(req.body.query, {
-      type: sequelize.QueryTypes.SELECT
+      type: sequelize.QueryTypes.SELECT,
     });
     res.json(result);
   } catch (err) {

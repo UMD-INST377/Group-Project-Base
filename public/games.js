03055dd5e7c86c5127faf2190b3d2381ada9ae2f
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-11-23 00:36:38 -0500

    table

--- public/games.js
+++ public/games.js
@@ -1,23 +1,42 @@
-/* eslint-disable no-const-assign */
-fetch('http://localhost:3000/api/general').then(
-  res => {
-    res.json().then(
-      data => {
-        console.log(data.data);
-        if (data.data.length > 0) {
-          const temp = '';
-          data.data.forEach((itemData) => {
-            // eslint-disable-next-line no-const-assign
-            temp += '<tr>';
-            temp += '<td>' + itemData.game_id + '</td>';
-            temp += '<td>' + itemData.game_name + '</td>';
-            temp += '<td>' + itemData.release_date + '</td>';
-            temp += '<td>' + itemData.free_to_play + '</td>';
-            temp += '<td>' + itemData.player_population + '</td></tr>';
-          });
-          document.getElementById('data').innerHTML = temp;
-        }
+function fetchData() {
+  fetch('http://localhost:3000/api/general')
+    .then((response) => {
+      if (!response.ok) {
+        throw Error('ERROR');
       }
-    )
-  }
-)
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data.data);
+      const html = data.data.map(user => {
+          return `
+          <table class = "table is-fullwidth is-striped is-bordered">
+            <thead>
+                <tr>
+                    <th>Game ID</th>
+                    <th>Game Name</th>
+                    <th>Release Date</th>
+                    <th>Free to Play</th>
+                    <th>Player Population</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th>${user.game_id}</th>
+                    <td>${user.game_name}</td>
+                    <td>${user.release_date}</td>
+                    <td>${user.free_to_play}</td>
+                    <td>${user.player_population}</td>
+                </tr>
+            </tbody>
+        </table>
+            `}).join('');
+        console.log(html);
+        document.querySelector('#app').insertAdjacentHTML('afterbegin', html);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+fetchData();

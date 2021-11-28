48ee1cf18d2daa4689708966c615bb84a9f91163
Matt Gendreau <g3ndreau@umd.edu>
2021-11-28 21:38:27 +0900

    Update date.js

--- controllers/date.js
+++ controllers/date.js
@@ -1,7 +1,7 @@
 const date = {
     getDate: 'SELECT * FROM date',
     getDateByID: 'SELECT * FROM date WHERE earthquake_id = :id',
-    putDate: 'UPDATE date SET date = :date WHERE earthquake_id = :id'
+    putDate: 'UPDATE date SET day_of = :date WHERE earthquake_id = :id'
 }
 
 export default date;

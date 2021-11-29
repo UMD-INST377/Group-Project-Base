03374bdfb311dca1b34ca3990fa226daba2bcb5a
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-11-28 23:11:01 -0500

    Changed around search page and database config

--- client/searchscript.js
+++ client/searchscript.js
@@ -1,9 +1,13 @@
-async function windowActions() {
-  // Get data
-  const url = "/api/HotelOverview";
-  data = [];
-  const request = await fetch(url);
-  if (request.ok) {
-    data = await request.json();
+const displayData = async () => {
+  let data = await fetch('https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview')
+  let res = await data.json();
+
+  let text = "";
+  res.data.forEach(element => {
+      text += `${element.hotel_name} ${element.name} \n`;
+  })
+
+  document.getElementById('data').innerText = text;
 }
-window.onload = windowActions;
+
+displayData();
--- database/config.js
+++ database/config.js
@@ -1,23 +1,26 @@
 export default {
   development: {
-    username: "ubuntu",
-    password: "Veracrypt@12!",
-    database: "maui_hotels",
-    host: "18.212.135.23",
-    dialect: "mysql",
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'maui_hotels',
+    host: '18.212.135.23',
+    port: '3306',
+    dialect: 'mysql'
   },
   test: {
-    username: "ubuntu",
-    password: "Veracrypt@12!",
-    database: "maui_hotels",
-    host: "18.212.135.23",
-    dialect: "mysql",
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'maui_hotels',
+    host: '18.212.135.23',
+    port: '3306',
+    dialect: 'mysql'
   },
   production: {
-    username: "ubuntu",
-    password: "Veracrypt@12!",
-    database: "maui_hotels",
-    host: "18.212.135.23",
-    dialect: "mysql",
-  },
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'maui_hotels',
+    host: '18.212.135.23',
+    port: '3306',
+    dialect: 'mysql'
+  }
 };

4a06bef02d8c1be1f4bbed3b74241ab9c0953e92
Zhaojie Yin <yinzhaojie@gmail.com>
2021-11-29 11:43:22 -0500

    match price id and game name in the bar chart

--- public/price_scripts.js
+++ public/price_scripts.js
@@ -1,66 +1,46 @@
-function BuildChart(labels, values, chartTitle) {
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: chartTitle,
-      data: values,
-      backgroundColor: ['rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)',
-        'rgb(54, 162, 235)'
-      ]
-    }]
-  };
+let gameLabel = [];
+let gamePrice = [];
+
+async function getDummyData() {
+  // price table
+  const data= await fetch('http://localhost:3000/api/price');
+  const newData = await data.json();
+  const barChatData =newData[0];
+  // General table
+  const secdata= await fetch('http://localhost:3000/api/general');
+  const secnewData = await secdata.json();
+  const secbarChatData =secnewData[0];
+  // Get Data from table
+  const price = barChatData.map((x) => x.listed_price)
+  console.log(price)
+  const name = secbarChatData.map((x) => x.game_name)
+  console.log(name)
+
+  gamePrice = price
+  gameLabel = name
+}
+async function dummyChart() {
+  await getDummyData()
 
   const ctx = document.getElementById('myChart').getContext('2d');
-  const myChart = new Chart(ctx, {
-    type: 'horizontalBar',
-    data: data,
+
+  const chart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: gameLabel,
+      datasets: [{
+        label: 'Game Price',
+        backgroundColor: 'rgb(54, 162, 235)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: gamePrice
+      }
+      ]
+    },
     options: {
-      responsive: true, 
-      maintainAspectRatio: false,
-      scales: {
-        xAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'Listed Price'
-          }
-        }],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: 'price Id'
-          }
-        }]
+      tooltips: {
+        mode: 'index'
       }
     }
-  });
+})}
 
-  return myChart;
-}
-const xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-  if (this.readyState === 4 && this.status === 200) {
-    const json = JSON.parse(this.response)[0];
-    console.log(json)
-
-    const labels = json.map((e) => e.price_id);
-    console.log(labels)
-
-    const values = json.map((e) => (e.listed_price) 
-    );
-
-    BuildChart(labels, values, 'Real Time Price');
-  }
-};
-
-// const array1 = ['http://localhost:3000/api/price', 'http://localhost:3000/api/general'];
-xhttp.open('GET', 'http://localhost:3000/api/price', false);
-// array1.forEach(element => xhttp.open('GET', element, false));
-xhttp.send();
+dummyChart()

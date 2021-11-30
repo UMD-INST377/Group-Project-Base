2d8ae351b7d8ca1d4e78a0d5e359069d60832eb7
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-11-30 18:27:23 -0500

    Fixed Form

--- server/controllers/general.js
+++ server/controllers/general.js
@@ -1,6 +1,6 @@
 const get = 'SELECT * FROM general_information';
 const put = 'INSERT INTO general_information VALUES (99,"fifa","jan","idk","12222")';
-const post = 'UPDATE general_information SET game_name="fifa",release_date="jun",free_to_play="idk",player_population="322" WHERE game_id = 1';
+const post = 'UPDATE general_information SET game_name= :name, free_to_play= :free_to_play,player_population= :population WHERE game_id = :game_id';
 const remove = 'DELETE FROM general_information WHERE game_id=99';
 
 export default {

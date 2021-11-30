de74bcd03ba579eaf3362d20d15fc8ba6b2e8b15
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-11-30 01:43:11 -0500

    Hotel search page completed and functional.

--- client/search/script.js
+++ client/search/script.js
@@ -1,15 +1,43 @@
-const displayData = async () => {
-  let data = await fetch(
-    "https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview"
-  );
-  let res = await data.json();
+const hotelsList = document.getElementById("hotelsList");
+const searchBar = document.getElementById("searchBar");
+let hotelsOverview = [];
 
-  var mainContainer = document.getElementById("myData");
-  for (var i = 0; i < res.length; i++) {
-    var div = document.createElement("div");
-    div.innerHTML = res[i].hotel_name;
-    mainContainer.appendChild(div);
+searchBar.addEventListener("keyup", (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredHotels = hotelsOverview.filter((hotel) => {
+    return (
+      hotel.hotel_name.toLowerCase().includes(searchString) ||
+      hotel.street_address.toLowerCase().includes(searchString)
+    );
+  });
+  displayHotels(filteredHotels);
+});
+
+const loadHotels = async () => {
+  try {
+    const res = await fetch(
+      "https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview"
+    );
+    hotelsOverview = await res.json();
+    displayHotels(hotelsOverview);
+  } catch (err) {
+    console.error(err);
   }
 };
 
-displayData();
+const displayHotels = (hotels) => {
+  const htmlString = hotels
+    .map((hotel) => {
+      return `
+            <li class="results">
+                <ul>${hotel.hotel_name}</ul>
+                <ul>${hotel.street_address}</ul>
+            </li>
+        `;
+    })
+    .join("");
+  hotelsList.innerHTML = htmlString;
+};
+
+loadHotels();

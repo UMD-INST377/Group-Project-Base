d5998c85888ccc3aece5cf4a531b9147f71069d3
n-a-iliev <55454912+n-a-iliev@users.noreply.github.com>
2021-11-30 11:29:47 -0500

    added post & delete, updated get

--- controllers/apiRoutesController.js
+++ controllers/apiRoutesController.js
@@ -1,15 +1,21 @@
 const player = {
 
-  getPlayer: `SELECT player_name, position_name, ppg, assists, name
+  getPlayer: `SELECT player_id, player_name, position_name, ppg, assists, name
                         FROM Positions JOIN players
                           USING(position_id) JOIN Team
                           USING(team_id);`,
 
-  postPlayer: '',
+  postPlayer: `UPDATE Positions JOIN players USING(poistion_id) JOIN Team USING(team_id)
+                SET player_id = $GET['player_id], player_name = $GET['player_name'], 
+                poistion_name=$GET['poisiton_name'], ppg=$GET['ppg'],
+                assists=$GET['assists'],name=$GET['name']`,
 
   putPlayer: '',
 
-  deletePlayer: ''
+  deletePlayer: `DELETE player FROM Positions JOIN players 
+                  USING(poistion_id) JOIN Team
+                  USING(team_id)
+                  WHERE player_id = $GET['player_id'];`
 
 };
 

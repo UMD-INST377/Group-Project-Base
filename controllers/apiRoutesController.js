21b250290c0a2ef812807869ef143c2094f457b9
skanner2 <63942471+skanner2@users.noreply.github.com>
2021-12-01 16:50:57 -0500

    fixed controllers

--- controllers/apiRoutesController.js
+++ controllers/apiRoutesController.js
@@ -5,13 +5,13 @@ const player = {
                           USING(position_id) JOIN Team
                           USING(team_id);`,
 
-  postPlayer: `UPDATE Positions JOIN players USING(poistion_id) JOIN Team USING(team_id)
+  postPlayer: `INSERT INTO players (player_name, ppg, assists, team)
+                VALUES ($GET['player_name'], $GET['ppg'], '$GET['assists']', $GET['team']);`,
+
+  putPlayer: `UPDATE Positions JOIN players USING(poistion_id) JOIN Team USING(team_id)
                 SET player_id = $GET['player_id], player_name = $GET['player_name'], 
                 poistion_name=$GET['poisiton_name'], ppg=$GET['ppg'],
                 assists=$GET['assists'],name=$GET['name']`,
-
-  putPlayer: `INSERT INTO players (player_name, ppg, assists, team)
-                VALUES ($GET['player_name'], $GET['ppg'], '$GET['assists']', $GET['team']);`,
 
   deletePlayer: `DELETE player FROM Positions JOIN players 
                   USING(poistion_id) JOIN Team

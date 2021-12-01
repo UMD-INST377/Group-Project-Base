9f831153cdf02d7725e6f076372b7b1c7084189d
M-Schwartz99 <89875672+M-Schwartz99@users.noreply.github.com>
2021-12-01 10:40:25 -0500

    added filters for search bar

--- client/search/script.js
+++ client/search/script.js
@@ -1,23 +1,34 @@
-const hotelsList = document.getElementById("hotelsList");
-const searchBar = document.getElementById("searchBar");
+/* eslint-disable no-unused-expressions */
+/* eslint-disable prefer-const */
+/* eslint-disable no-plusplus */
+/* eslint-disable eqeqeq */
+/* eslint-disable no-param-reassign */
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
+const hotelsList = document.getElementById('hotelsList');
+const searchBar = document.getElementById('searchBar');
 let hotelsOverview = [];
 
-searchBar.addEventListener("keyup", (e) => {
-  const searchString = e.target.value.toLowerCase();
-
-  const filteredHotels = hotelsOverview.filter((hotel) => {
-    return (
-      hotel.hotel_name.toLowerCase().includes(searchString) ||
-      hotel.street_address.toLowerCase().includes(searchString)
-    );
-  });
-  displayHotels(filteredHotels);
-});
-
+const displayHotels = (hotels) => {
+  const htmlString = hotels
+    .map((hotel) => `
+            <li class="results${hotel.beachside === 1 ? ' beachside' : ''}${
+  hotel.family_friendly === 1 ? ' family_friendly' : ''
+}${hotel.pet_friendly === 1 ? ' pet_friendly' : ''}"><a href=#>
+                <ul>${hotel.hotel_name}</ul>
+                <ul>${hotel.street_address}</ul>
+            </a></li>
+        `)
+    .join('');
+  hotelsList.innerHTML = htmlString;
+};
 const loadHotels = async () => {
   try {
-    const res = await fetch("https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview");
+    const res = await fetch(
+      'https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview'
+    );
     hotelsOverview = await res.json();
+
     displayHotels(hotelsOverview);
   } catch (err) {
     console.error(err);
@@ -43,5 +54,83 @@ const displayHotels = (hotels) => {
     .join("");
   hotelsList.innerHTML = htmlString;
 };
+function searchHotels() {
+  const btnContainer1 = document.getElementById('myBtnContainer');
+  const btns1 = btnContainer1.getElementsByClassName('btn1');
+  for (let i = 0; i < btns1.length; i += 1) {
+    const [current] = document.getElementsByClassName('active');
+    current
+      ? (current.className = current.className.replace(' active', ''))
+      : '';
+  }
+  const showAllbtn1 = document.getElementById('showAllbtn');
+  showAllbtn1.classList.add('active');
+
+  // Declare variables
+  const input = document.getElementById('searchBar');
+  const filter = input.value.toUpperCase();
+  const ul = document.getElementById('hotelsList');
+  const li = ul.getElementsByTagName('li');
+  let a; let
+    txtValue;
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (let i = 0; i < li.length; i += 1) {
+    [a] = li[i].getElementsByTagName('a');
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      li[i].style.display = '';
+    } else {
+      li[i].style.display = 'none';
+    }
+  }
+}
+
+const btnContainer = document.getElementById('myBtnContainer');
+const btns = btnContainer.getElementsByClassName('btn1');
+for (let i = 0; i < btns.length; i += 1) {
+  btns[i].addEventListener('click', function () {
+    const [current] = document.getElementsByClassName('active');
+    current.className = current.className.replace(' active', '');
+    this.className += ' active';
+  });
+}
+
+function filterSelection(c) {
+  document.getElementById('searchBar').value = '';
+  let x; let i; let
+    ul;
+  ul = document.getElementById('hotelsList');
+  x = ul.getElementsByTagName('li');
+  // revert all to display all again
+  for (i = 0; i < x.length; i += 1) {
+    x[i].style.display = '';
+  }
+  if (c == 'all') c = '';
+  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
+  for (i = 0; i < x.length; i += 1) {
+    x[i].style.display = '';
+    if (c) {
+      if (x[i].className.indexOf(c) > -1) {
+        x[i].style.display = '';
+      } else {
+        x[i].style.display = 'none';
+      }
+    }
+  }
+}
+
+filterSelection('all');
+
+// Show filtered elements
+function w3AddClass(element, name) {
+  const arr1 = element.className.split(' ');
+  const arr2 = name.split(' ');
+  for (let i = 0; i < arr2.length; i++) {
+    if (arr1.indexOf(arr2[i]) == -1) {
+      element.className += ` ${arr2[i]}`;
+    }
+  }
+}
 
 loadHotels();

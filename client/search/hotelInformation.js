0eb626d90b5272d7aa182ca93e3964da02162a37
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-12-01 03:50:47 -0500

    Click to see more info fully functional.

--- client/search/hotelInformation.js
+++ client/search/hotelInformation.js
@@ -1,3 +1,23 @@
-const {spec_hotel_id} = require('script.js')
-document.getElementById("info").innerHTML = spec_hotel_id;
+const queryString = window.location.search;
+const idNum = queryString.replace(/^\D+/g, "");
 
+const loadHotels = async () => {
+  try {
+    const res = await fetch("https://group4-final-inst377fa2021.herokuapp.com/api/hotel_overview");
+    hotelsOverview = await res.json();
+    console.log(hotelsOverview);
+  } catch (err) {
+    console.error(err);
+  }
+  const overviewHTML = `
+                <li class="content" style="list-style-type: none">
+                <ul>${hotelsOverview[idNum - 1].hotel_name}</ul>
+                <ul>${hotelsOverview[idNum - 1].street_address}</ul>
+                <ul>${hotelsOverview[idNum - 1].hotel_phone_number}</ul>
+                <ul>${hotelsOverview[idNum - 1].hotel_rating}</ul>
+                </li>
+                    `;
+  document.getElementById("info").innerHTML = overviewHTML;
+};
+
+loadHotels();

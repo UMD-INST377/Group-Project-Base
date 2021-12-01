deaaf15ab53f48d943868d6ddb5e48b747330f8b
Alec M <alecmattu1234@gmail.com>
2021-12-01 11:27:26 -0500

    Add university requirements

--- controllers/university.js
+++ controllers/university.js
@@ -10,10 +10,12 @@ const getAllUniversities = `SELECT university_id, university_name FROM universit
  *
  * @type {SQLStmt}
  */
-const getUniversity = `SELECT a.university_id, a.university_name, b.univ_location, b.univ_region, b.univ_zip
+const getUniversity = `SELECT a.university_id, a.university_name, b.univ_location, b.univ_region, b.univ_zip, c.coalition, c.common_app, c.required_test_score
   FROM university a
   INNER JOIN location b
     ON a.university_id = b.university_id
+  INNER JOIN requirements c
+    ON a.university_id = c.university_id
   WHERE a.university_id = :rank_id
   LIMIT 0,1`;
 

5243a5f3551a593b6a5eb98718b259f112dedcb5
damores <Diego.amores14@gmail.com>
2021-12-01 04:40:45 -0500

    added modal for sign up

---
+++ public/javascript/modals.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-plusplus */
+// sign-up modal
+
+const signupButton = document.querySelector('#signup');
+const signupClose = document.querySelector('#signup-close');
+const signupModal = document.querySelector('#signupModal');
+
+signupButton.addEventListener('click', () => {
+  signupModal.classList.add('is-active');
+});
+
+signupClose.addEventListener('click', () => {
+  signupModal.classList.remove('is-active');
+});
+
+// cancel modal
+
+const cancelButton = document.querySelector('#cancel');
+
+cancelButton.addEventListener('click', () => {
+  signupModal.classList.remove('is-active');
+});

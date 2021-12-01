d570ca45d24718a0fa79b7193fc20763bb83a412
Jia Li <liandjessica@gmail.com>
2021-11-30 21:37:39 -0500

    search volcano name

--- client/script.js
+++ client/script.js
@@ -1,43 +1,54 @@
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
 
+async function windowActions() {
+  /* const volcanoInfo = document.querySelector('.volcanoName = arryName'); */
+  const data = await fetch('/api/volcanos');
 
-async function getVolcano() {
-  const volcanoInfo = document.querySelector('.volcanoName');
+  /* const arrayName = await request.json();
+  console.log(volcanoName); */
 
-  const request = await fetch('/api/volcanos'); 
-}
+  function findMatches(wordToMatch, volcanoName) {
+    return volcanoName.filter((result) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      console.log(wordToMatch);
+      console.log(string(result.volcano_id));
+      if (wordToMatch === '') {
+        return null;
+      }
 
-function findMatches(wordToMatch, arrayName) {
-    if (wordToMatch.length != 0) {
-      return arrayName.filter((place) => {
-        const regex = new RegExp(wordToMatch, 'gi');
-        return place.volcano_name.match(regex);
-      });
-    }
-    suggestions.innerHTML = '';
+      return String(result.volcano_id).match(regex);
+    });
   }
+  /* return place=result.volcano_name.match(regex); */
 
   function displayMatches(event) {
-    const matchArray = findMatches(event.target.value, arrayName);
+    const matchArray = findMatches(event.target.value, volcanoName);
     console.log(matchArray);
     if (matchArray) {
-      const html = matchArray.map((place) => {
+      const html = matchArray.map((result) => {
         const regex = new RegExp(event.target.value, 'gi');
         const volcanoName = place.volcano_name;
         return `
         
+          <ul>
             <li>
                 <p class='name'>${volcanoName}<br/>
             </li>
+          </ul>
             `;
       }).join('');
       suggestions.innerHTML = html;
     }
   }
 
+  const newData = await data.json();
+  console.log(newData[0]);
+  const volcanoNam = newData[0];
   searchInput.addEventListener('change', displayMatches);
-  searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
+  searchInput.addEventListener('keyup', (evt) => {
+    displayMatches(evt);
+  });
 }
 
 window.onload = windowActions;

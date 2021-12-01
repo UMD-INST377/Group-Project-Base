7ae327dde58e2d6ae10efa384517b712d0b01690
Jia Li <liandjessica@gmail.com>
2021-12-01 15:12:41 -0500

    11

--- client/script.js
+++ client/script.js
@@ -1,12 +1,28 @@
+
+/* const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+/*
+function structureVolcano(volcanoInfo){
+    const getVolcano = {
+        title:{
+            media:{
+                url:
+            }
+        }
+    }
+}
+*/
+
 async function windowActions() {
   const endpoint = '/api/eruption_info';
   const request = await fetch(endpoint);
   const names = await request.json();
-
+  
   const searchInput = document.querySelector('.nameSearch');
   const suggestions = document.querySelector('.suggestions');
 
-  console.log(names);
+console.log(names)
   function findMatches(wordToMatch, names) {
     return names.filter((info) => {
       const regex = new RegExp(wordToMatch, 'gi');
@@ -18,11 +34,19 @@ async function windowActions() {
     const html = matchArray.map((info) => {
       const regex = new RegExp(event.target.value, 'gi');
       return `
-  
-        <li>
-          <p class='name'>${volcanoName}<br/>
-        </li>
-          
+      <div class='photo-grid-container'>
+                <div class='photo-grid'>
+                <div class="box">
+                <div class="name">Name: ${info.volcano_name}</div>
+                <div class="num">Eruption Number: ${info.eruption_number} </div>
+                <div class="date"> Date of Eruption: ${info.year}-${info.month}-${info.day} </div>
+                <div class="aoa">Eruption AOA: ${info.aoa}</div>
+                <div class="vei">Eruption VEI: ${info.vei}</div>
+                <div class="method">Method: ${info.method}</div>
+                <div class="category">Eruption Category: ${info.category}</div>
+              </div>
+             </div>
+            </div>
     `;
     }).join('');
     suggestions.innerHTML = html;
@@ -32,6 +56,7 @@ async function windowActions() {
   searchInput.addEventListener('keyup', (evt) => { displayMatches(evt); });
 }
 window.onload = windowActions;
+
 
 /* const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');

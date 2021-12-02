3d9569222988dcd02ca2cbdd9a3222d8c25e9b54
jchamdi <70522271+jchamdi@users.noreply.github.com>
2021-12-02 14:30:33 -0500

    Added js to consume data from endpoint

--- client/script.js
+++ client/script.js
@@ -1,12 +1,26 @@
-async function test(name, id) {
-  const response = await fetch('/api/actor', {
-    method: 'PUT',
+
+function formToObject(htmlFormElement) {
+  const formItem = new FormData(htmlFormElement).entries();
+  const formArray = Array.from(formItem);
+  const formObject = formArray.reduce((collection, item, index) => {
+    if (!collection[item[0]]) {
+      collection[item[0]] = item[1];
+    }
+    return collection;
+  }, {});
+  return formObject
+}
+async function fetchRequest(name, id) {
+  const data= formToObject(document.querySelector('#form-id'));
+
+  const response = await fetch('/api/film', {
+    method: data.answer,
     headers: {
       'Content-Type': 'application/json'
     },
-    body: {
-      name: name,
-      id: id
-    }
+    body: JSON.stringify(data),
   });
+  console.log(data); 
 }
+const button = document.querySelector('#submit-button');
+button.onclick = fetchRequest;

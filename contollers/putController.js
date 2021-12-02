f1242779c14eea016958065eb38788c8fb7b5ed8
jasmineokebugwu <jokebugw@umd.edu>
2021-12-02 16:13:20 -0500

    renamed controller

--- contollers/putController.js
+++ contollers/putController.js
@@ -1,6 +1,6 @@
 const establishmentVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE ';
 const updateVar = '';
 export default {
-    establishmentVar, 
-    updateVar
-}
+  establishmentVar,
+  updateVar
+};

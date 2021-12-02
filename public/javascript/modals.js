9eaae25bcfa1697aee7d5a81da6ccdac95ca7e07
damores <Diego.amores14@gmail.com>
2021-12-02 03:50:25 -0500

    moved and added new vinyl modals to separate js file for organization

--- public/javascript/modals.js
+++ public/javascript/modals.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-plusplus */
-// sign-up modal
 
+// sign-up modal
 const signupButton = document.querySelector('#signup');
 const signupClose = document.querySelector('#signup-close');
 const signupModal = document.querySelector('#signupModal');
@@ -14,9 +14,24 @@ signupClose.addEventListener('click', () => {
 });
 
 // cancel modal
-
 const cancelButton = document.querySelector('#cancel');
 
 cancelButton.addEventListener('click', () => {
   signupModal.classList.remove('is-active');
+});
+
+// vinyl modal
+const triggers = document.querySelectorAll('.trigger');
+const triggerArray = Array.from(triggers);
+const vinylModals = document.querySelectorAll('#vinylmodal');
+const cancelVinyl = document.querySelectorAll('#cancel-vinyl');
+const closeVinyl = document.querySelectorAll('#close-vinyl');
+
+triggerArray.forEach((trigger, index) => {
+  const toggleModal = () => {
+    vinylModals[index].classList.toggle('is-active');
+  };
+
+  trigger.addEventListener('click', toggleModal);
+  cancelVinyl[index].addEventListener('click', toggleModal);
 });

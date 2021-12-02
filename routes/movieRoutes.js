e9e8307f8e7811da005de12be20f2162ce56ab15
Tracy <tracokwaisie@outlook.com>
2021-12-01 20:06:48 -0500

    l

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -9,7 +9,10 @@ import film from '../controllers/filmController.js';
 
 const router = express.Router();
 
-/* Films */
+
+/**
+ * Films
+ */
 router.route('/film')
   .get(async (req, res) => {
     try {
@@ -66,6 +69,10 @@ router.route('/film')
     }
   });
 
+/**
+ * Genres
+ */
+
 router.route('/genre')
   .get(async (req, res) => {
     try {
@@ -121,6 +128,10 @@ router.route('/genre')
       res.json({error: 'Can\'t be deleted'});
     }
   });
+
+/**
+ * Actor
+ */
 
 router.route('/actor')
   .get(async (req, res) => {

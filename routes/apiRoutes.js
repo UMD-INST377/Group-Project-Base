6a21a46a8a1aced48f82a411925df7de875865f9
Daniel Flanagan <danielflanagan777@gmail.com>
2021-12-01 16:55:15 -0800

    fixing apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -52,3 +52,83 @@ router.route('/phone').delete(async(req, res) => {
     res.json({error: 'Error Occured'})
   }
 })
+
+/// /////////////////////////////////
+/// ////Congress Endpoints////////
+/// /////////////////////////////////
+router.route('/contact')
+  .get(async(req, res) => {
+    try {
+      console.log('You touched /contact with GET');
+      res.json({message: 'GET Congress endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  })
+  .put(async(req, res) => {
+    try {
+      console.log('You touched /contact with PUT');
+      res.json({message: 'GET Congress endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  })
+  .post(async(req, res) => {
+    try {
+      console.log('You touched /contact with POST');
+      res.json({message: 'GET Congress endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  })
+  .delete(async(req, res) => {
+    try {
+      console.log('You touched /contact with DELETE');
+      res.json({message: 'GET Congress endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({message: 'Something went wrong'});
+    }
+  });
+
+  //Jake Stark General Congress Members Table Endpoint
+router.route('/members')
+.get(async(req, res) => {
+  try {
+    console.log('touched /members endpoint GET')
+    res.send(' GET members endpoint');
+  } catch (error) {
+    console.log(error)
+    res.json({message: 'Error'})
+  }
+})
+.put(async(req, res) => {
+  try {
+    console.log('touched /members endpoint PUT')
+    res.send('PUT members endpoint');
+  } catch (error) {
+    console.log(error)
+    res.json({message: 'Error'})
+  }
+})
+.post(async(req, res) => {
+  try {
+    console.log('touched /members endpoint POST')
+    res.send('POST members endpoint');
+  } catch (error) {
+    console.log(error)
+    res.json({message: 'Error'})
+  }
+})
+.delete(async(req, res) => {
+  try {
+    console.log('touched /members endpoint DELETE')
+    res.send('DELETE members endpoint');
+  } catch (error) {
+    console.log(error)
+    res.json({message: 'Error'})
+  }
+})

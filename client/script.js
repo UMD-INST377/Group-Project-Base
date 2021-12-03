d93d0804cc86c6b79caea950958d52b76c27c870
Seungjoon Lee <sd529576@gmail.com>
2021-12-02 19:09:24 -0500

    Popup functions & html

--- client/script.js
+++ client/script.js
@@ -1,10 +1,11 @@
+
 async function windowActions() {
   const endpoint = '/api/eruption_info';
   const request = await fetch(endpoint);
   const names = await request.json();
   
   const searchInput = document.querySelector('.nameSearch');
-  const suggestions = document.querySelector('.suggestions');
+  const suggestions = document.querySelector('.suggestions .photo-grid-container .photo-grid');
 
 console.log(names)
   function findMatches(wordToMatch, names) {
@@ -18,9 +19,9 @@ console.log(names)
     const html = matchArray.map((info) => {
       const regex = new RegExp(event.target.value, 'gi');
       return `
-      <div class='photo-grid-container'>
-                <div class='photo-grid'>
-                <div class="box", id = 'info_box'>
+      <div class='photo-grid-container' id='info_box'>
+                <div class='photo-grid-item'>
+                <div class="box">
                 <div class="name">Name: ${info.volcano_name}</div>
                 <div class="num">Eruption Number: ${info.eruption_number} </div>
                 <div class="date"> Date of Eruption: ${info.year}-${info.month}-${info.day} </div>
@@ -31,14 +32,35 @@ console.log(names)
               </div>
              </div>
             </div>
+            <div class="modal">
+                        <div class="modal-background"></div>
+                        <div class="modal-card">
+                          <header class="modal-card-head">
+                            <p class="modal-card-title">Modal title</p>
+                            <button class="delete" aria-label="close"></button>
+                          </header>
+                          <section class="modal-card-body">
+                            Content is NOT THERE YET UNFORTUNATELY
+                          </section>
+                          <footer class="modal-card-foot">
+                          </footer>
+                        </div>
+                      </div>
     `;
+    
     }).join('');
     suggestions.innerHTML = html;
-    document.getElementById('info_box').addEventListener('click',function(){
-      document.querySelector('.modal').style.display = 'flex';
-    });
-    document.querySelector('info_box').addEventListener('click',function(){
-      document.querySelector('.modal').style.display = 'none';
+    const info_box1 = document.querySelector('#info_box');//everytime when info box clicks, it runs document.querySelector('.modal').style.display = 'flex';
+    const elements = document.querySelectorAll('#info_box');
+    const delete_box = document.querySelector('.delete');
+    elements.forEach(info_box1 => {
+      info_box1.addEventListener('click', function(){
+        document.querySelector('.modal').style.display = 'flex';
+        delete_box.addEventListener('click',
+                           function () {
+      document.querySelector('.modal').style.display = 'none'
+    })
+      });
     });
   }
 
@@ -51,4 +73,3 @@ function loadFile(event) {
   const image = document.getElementById('output');
   image.src = URL.createObjectURL(event.target.files[0]);
 }
-

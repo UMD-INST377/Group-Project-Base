fdce6dd4af56dec7919c87f069a7b6900b0be65d
Shane Lett <shanealexlett@gmail.com>
2021-12-03 16:02:20 -0500

    Update POSTController.js

--- controllers/POSTController.js
+++ controllers/POSTController.js
@@ -2,3 +2,11 @@ export default postController();
 
 constant addMember = "INSERT INTO CongressMembers(Name, Info_ID, 'Contact Info ID')
 VALUES('${req.body.name}','${req.body.info_id}','${req.body.contact_info}'";
+
+const memberProfiles = `SELECT Internet Profile ID,
+  Twitter,
+  Instagram,
+  Facebook,
+  Website
+FROM
+  Internet Profiles`;

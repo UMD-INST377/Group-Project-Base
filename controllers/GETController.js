6dfc43a5e9500127c95caf62f630f95270af4ad5
Shane Lett <shanealexlett@gmail.com>
2021-12-03 16:02:18 -0500

    Update GETController.js

--- controllers/GETController.js
+++ controllers/GETController.js
@@ -1,3 +1,11 @@
 export default getController();
 
 constant members ="""SELECT 'Member Full Name' AS 'Name' , 'Personal Info ID' AS 'Info_ID', 'Contact Info ID' FROM  CongressMembers""";
+
+const memberProfiles = `SELECT Internet Profile ID,
+  Twitter,
+  Instagram,
+  Facebook,
+  Website
+FROM
+  Internet Profiles`;

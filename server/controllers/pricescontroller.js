41bb04808a036879c0f56e9ba42a255188f27528
suhailxyz <suhailj17@gmail.com>
2021-12-03 11:26:28 -0500

    fixed indent error prices

--- server/controllers/pricescontroller.js
+++ server/controllers/pricescontroller.js
@@ -6,6 +6,6 @@ const pricesPost = 'SELECT vinyl_id, highest_discog, lowerst_discog, highest_ama
 
 const pricesDelete = 'SELECT vinyl_id, highest_discog, lowerst_discog, highest_amazon FROM prices;'
 
-export default{
-    pricesGet, pricesPut, pricesPost, priceDelete
+export default {
+  pricesGet, pricesPut, pricesPost, priceDelete
 };

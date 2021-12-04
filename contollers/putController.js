29771b094458c848370809ef17e1f56f3c608ab2
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-12-03 20:33:52 -0500

    Updated controller to properly update column when zip code condition is met.

--- contollers/putController.js
+++ contollers/putController.js
@@ -1,5 +1,5 @@
-const establishmentVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE ';
-const updateVar = '';
+const establishmentVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE';
+const updateVar = 'UPDATE establishment_id SET category = Seafood WHERE zip= 20744 ';
 export default {
   establishmentVar,
   updateVar

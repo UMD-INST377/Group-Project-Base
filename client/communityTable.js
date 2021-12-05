f2fc2040a07c4ceb14b408fba08aa35422a4c9cf
MasterKaveman <50510650+Kaveh-Vakili@users.noreply.github.com>
2021-12-04 22:04:22 -0500

    added community.js file

---
+++ client/communityTable.js
@@ -0,0 +1,11 @@
+/* eslint-disable import/no-unresolved */
+
+import census from './census.js';
+import community from './community.js';
+import metro from './metro.js';
+import population from './population.js';
+import companies from './companies.js';
+
+export default {
+  census, community, metro, population, companies
+};

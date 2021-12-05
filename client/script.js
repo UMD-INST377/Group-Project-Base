58a49fbcdf27365a4c3044ab5f73f6f4daedb7f1
Matt Gendreau <g3ndreau@umd.edu>
2021-12-05 18:34:07 +0900

    testing with just ./api/

--- client/script.js
+++ client/script.js
@@ -4,7 +4,7 @@ async function windowAction() {
     const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const endpoint2 = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
     
-    const earthquakes = fetch('https://stark-beach-65958.herokuapp.com/api/').then(response => response.json())
+    const earthquakes = fetch('./api/').then(response => response.json())
     console.log(earthquakes)
     
     const searchInput = document.querySelector('.search')

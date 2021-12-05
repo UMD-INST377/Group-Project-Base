5e10576f3aefbeefff8872ff77b22b9b3f28bd2e
Matt Gendreau <g3ndreau@umd.edu>
2021-12-05 18:57:19 +0900

    6:57pm

--- client/script.js
+++ client/script.js
@@ -4,9 +4,7 @@ async function windowAction() {
     const endpoint = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const endpoint2 = 'https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json'
     
-    const request = await fetch('./api/')
-    const earthquakes = await request.json()
-    console.log(earthquakes)
+    const earthquakes = fetch('./api/').then(response => response.json())
     
     function findMatches(wordToMatch, earthquakes) {
         return earthquakes.filter(earthquake => {

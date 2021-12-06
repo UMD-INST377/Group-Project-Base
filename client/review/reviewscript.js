68896956c17228fc6c34f92d3c86642fe613c809
Matviy Volkov <39872839+Myathu@users.noreply.github.com>
2021-12-05 21:57:05 -0500

    Update reviewscript.js
    Format

--- client/review/reviewscript.js
+++ client/review/reviewscript.js
@@ -1,12 +1,10 @@
 function windowActions() {
   const dropdown = document.querySelector('#dropdown');
 
-dropdown.onclick = function(e) { 
- console.log(e)
- e.path[3].classList.toggle('is-active');
-}
-
-
+  dropdown.onclick = function(e) { 
+    console.log(e)
+    e.path[3].classList.toggle('is-active');
+  }
 }
 
 

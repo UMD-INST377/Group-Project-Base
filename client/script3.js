c46055be65657bef599332d53da135704c743372
Matt Gendreau <g3ndreau@umd.edu>
2021-12-07 15:03:50 +0900

    3:03pm

--- client/script3.js
+++ client/script3.js
@@ -1,10 +1,11 @@
 async function getByID() {
-    const address = './api/:' + document.querySelector('#earthquakeid').value
+    let address = './api/' + document.querySelector('#earthquakeid').value
     const message = document.querySelector('#results')
+    console.log(address)
     const earthquake = await fetch(address).then(response => response.json())
     console.log(earthquake)
     message.innerHTML = `<p>${(earthquake.City)}</p>`
 }
 
 const send = document.querySelector('#submitID');
-send.onclick = getByID();
+send.onclick = getByID;

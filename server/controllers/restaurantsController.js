4a2c8bbc78a9ae27b021bea8224d27d6ac9d36b2
Christina Ananth <cjothia@gmail.com>
2021-12-07 11:17:58 -0500

    adding address and type to controllers

--- server/controllers/restaurantsController.js
+++ server/controllers/restaurantsController.js
@@ -1,5 +1,5 @@
 const restaurantMapCustom = {
-  getRestaurant: 'SELECT Name, Inspection_results, City, Zip FROM Food_Inspection;',
+  getRestaurant: 'SELECT Name, Inspection_results, City, Zip, Address_line_1, Type FROM Food_Inspection;',
 
   putRestaurant: 'UPDATE Food_Inspection SET Establishment_id = $GET["Establishment_id"], Name = $GET["Name"], City = $GET["City"], Zip = $GET["Zip"]',
 

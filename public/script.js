cdaaf835d7511cd2bd27b548d1cbaabc99bd0aba
wyatthickman <89927529+wyatthickman@users.noreply.github.com>
2021-12-08 11:29:46 -0500

    Update script.js

--- public/script.js
+++ public/script.js
@@ -177,7 +177,7 @@ async function getartists() {
 getartists();
 
 async function getAriana() {
-  const endPoint = await fetch('./api/Artists/Arianagrande');
+  const endPoint = await fetch('./api/Artists/Ariana_Grande');
   const songs = await endPoint.json();
   const arianaContainer = document.querySelector('#arianaContainer');
 

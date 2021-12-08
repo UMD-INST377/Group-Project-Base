4e26b74940af93aea99918abd8e4c541ac408a8e
Benjamin Lam <blam12@terpmail.umd.edu>
2021-12-08 18:01:20 -0500

    test

--- public/script.js
+++ public/script.js
@@ -1,30 +1,31 @@
 const displayTableData = async () => {
-    let data = await fetch('https://polar-mesa-33091.herokuapp.com/api/foodInspectionPG', { mode: 'no-cors' })
     let res = await data.json();
+  const data = await fetch('https://polar-mesa-33091.herokuapp.com/api/foodInspectionPG', { mode: 'no-cors' });
+  const res = await data.json();
 
-    let text = "";
-    let table = document.getElementById('table');
+  let text = '';
+  const table = document.getElementById('table');
 
-    res.data.forEach(element => {
-        text += `${element.establishment_id} ${element.name} \n`;
-        let row = document.createElement('tr');
+  res.data.forEach((element) => {
+    text += `${element.establishment_id} ${element.name} \n`;
+    const row = document.createElement('tr');
 
-        // let td = document.createElement('td');
-        // td.innerText = `${element.establishment_id}`;
-        // row.appendChild(td);
+    // let td = document.createElement('td');
+    // td.innerText = `${element.establishment_id}`;
+    // row.appendChild(td);
 
-        // td = document.createElement('td');
-        // td.innerText = `${element.name}`;
-        // row.appendChild(td);
+    // td = document.createElement('td');
+    // td.innerText = `${element.name}`;
+    // row.appendChild(td);
 
-        Object.keys(element).forEach(column => {
-            let td = document.createElement('td');
-            td.innerText = `${element[column]}`;
-            row.appendChild(td);
-        });
-
-        table.appendChild(row);
+    Object.keys(element).forEach((column) => {
+      const td = document.createElement('td');
+      td.innerText = `${element[column]}`;
+      row.appendChild(td);
     });
-}
+
+    table.appendChild(row);
+  });
+};
 
 displayTableData();

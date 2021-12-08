41b0f0a331dcecc91859ed9ad80ea23c2eaa9271
mabail6 <mabail6@terpmail.umd.edu>
2021-12-08 15:50:51 -0500

    Added D3 functionality. Have basic bar chart working

--- client/script.js
+++ client/script.js
@@ -8,7 +8,7 @@ async function fetchRequest(url) {
     console.error(err);
     return err;
   }
-}
+};
 
 // Function to parse through the JSONs and sum based on the 'id' num
 function sumData(id, data) {
@@ -19,20 +19,33 @@ function sumData(id, data) {
     }
   });
   return tot;
-}
+};
 
 // Main thread function
 async function mainThread() {
   // fetch request to get the data from the api's
   const collision_type = await fetchRequest('./api/collisionType');
   const crash_information = await fetchRequest('./api/crashInformation');
-  const driver_demographics = await fetchRequest('./api/driverDemographics')
+  const driver_demographics = await fetchRequest('./api/driverDemographics');
 
   // summing up the number of collision types from the crashInformation
   const numCollType = [];
   for (let i = 1; i < collision_type.length; i++) {
     numCollType[i - 1] = sumData(i, crash_information);
   }
-}
+
+    // creating a chart but not loading data
+document.addEventListener('DOMContentLoaded', function(e) {
+  var data = [4, 8, 15, 16, 23, 42];
+
+  d3.select(".chart")
+      .selectAll("div")
+      .data(data)
+      .enter().append("div")
+      .style("width", function(d) { return d + "px"; })
+      .text(function(d) { return d; });
+});
+
+};
 
 mainThread();

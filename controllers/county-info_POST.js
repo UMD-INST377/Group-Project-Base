9cd98a639be1b2912c6afef13a1caa6ee8422bf8
jayid00 <jayid_07@yahoo.com>
2021-12-07 20:20:26 -0500

    Fixing yet another merge conflict

--- controllers/county-info_POST.js
+++
@@ -1,4 +0,0 @@
-const addNewCounty = `INSERT INTO county_information
-                      (county_ID, county, population, population_density)
-                      VALUES (16, "Kent", 55666, 99)`;
-export default addNewCounty

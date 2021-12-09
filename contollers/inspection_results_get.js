687a9957e805f0f2e23e8987771718ccd737de0b
Joshadjei1 <joshuaa18.adjei@gmail.com>
2021-12-09 12:04:04 -0500

    Wrote controller for inspection_results endpoint

--- contollers/inspection_results_get.js
+++ contollers/inspection_results_get.js
@@ -1,2 +1,2 @@
-const inspectionResultsVar = ''
+const inspectionResultsVar = 'SELECT Establishment_id, Name, Inspection_results FROM FOOD_INSPECTION_GROUP8.Food_Inspection';
 export default inspectionResultsVar;

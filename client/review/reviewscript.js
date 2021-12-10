96f45cdfd94bb280f6fe68083bc93f60a3e4e45d
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-12-09 20:53:26 -0500

    Merged arko's changes and fixed api route to work on heroku.

--- client/review/reviewscript.js
+++ client/review/reviewscript.js
@@ -1,16 +1,48 @@
+// import chalk from 'chalk';
+// import fetch from 'node-fetch';
+
+// import db from '../database/initializeDB.js';
+
+// const router = express.Router();
+
+// router.get('/', (req, res) => {
+//   console.log('You touched the default route');
+// });
+
 function windowActions() {
-  const dropdown = document.querySelector('#dropdown');
+  const inputForm = document.querySelector('#form');
 
-  dropdown.onclick = function(e) { 
-    console.log(e)
-    e.path[3].classList.toggle('is-active');
+
+  function formToObject(htmlFormElement) {
+    const formItem = new FormData(htmlFormElement).entries();
+    const formArray = Array.from(formItem);
+    const formObject = formArray.reduce((collection, item, index) => {
+      if (!collection[item[0]]) {
+        collection[item[0]] = item[1];
+      }
+      return collection;
+    }, {});
+    return formObject;
   }
-}
 
-
-function test(e) {
-  console.log(e);
+  
+  async function test(e) {
+    e.preventDefault();
+    console.log(e);
+    const formObject = formToObject(inputForm);
+    console.log(formObject);
   // e.target.parentnode.classList.toggle('is-active');
+  await fetch('https://group4-final-inst377fa2021.herokuapp.com/api/review', {
+    method:'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formObject)
+  }
+  )
+  }
+
+  inputForm.addEventListener('submit', test);
 }
 
 window.onload = windowActions;

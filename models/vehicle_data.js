281247f91b8b055a0276299c869d9ccf76278380
mchen606 <89974778+mchen606@users.noreply.github.com>
2021-12-10 14:22:22 -0500

    .

---
+++ models/vehicle_data.js
@@ -0,0 +1,37 @@
+export default (sequalize, DataTypes) => {
+    const vehicle_data = sequelize.define(
+        'vehicle_data',
+        {
+          report_id: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            unique: true,
+            primaryKey: true
+          },
+          vin_num: {
+            type: DataTypes.STRING,
+            allowNull: true
+          },
+          harm_code_event: {
+            type: DataTypes.INTERGER
+          },
+          harm_event_desc: {
+            type: DataTypes.STRING
+          },
+          towed_away: {
+            type: DataTypes.STRING
+          },
+          airbag_deployed: {
+            type: DataTypes.STRING
+          },
+          hit_and_run: {
+             type: DataType.STRING
+          },
+          speed_limit: {
+              type: DataTypes.INTERGER
+          }
+        },
+        { freezeTableName: true, timestamps: false }
+      );
+      return vehicle_data;
+}

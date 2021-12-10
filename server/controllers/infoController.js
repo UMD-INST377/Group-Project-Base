6198f8cf6689a987b865264deb0298276ee95aef
vihtle <vihtle2000@gmail.com>
2021-12-09 20:24:04 -0500

    added missing fields to eruption info controller

--- server/controllers/infoController.js
+++ server/controllers/infoController.js
@@ -19,8 +19,8 @@ evidence_id = :evidence_id,
 category_id = :category_id
 WHERE eruption_id = :eruption_id;`
 
-const infoPost = `INSERT INTO eruption_info(eruption_id, eruption_number, year, month, day, volcano_id, aoa_id, vei_id, evidence_id, cagetory_id)
-VALUES(DEFAULT, :eruption_number, :year, :month, :day);`
+const infoPost = `INSERT INTO eruption_info(eruption_id, eruption_number, year, month, day, volcano_id, aoa_id, vei_id, evidence_id, category_id)
+VALUES(DEFAULT, :eruption_number, :year, :month, :day, :volcano_id, :aoa_id, :vei_id, :evidence_id, :category_id);`
 
 const infoDelete = `DELETE FROM eruption_info
 WHERE eruption_id = :eruption_id;`

2df94e31c7f1a83fb2ec55846cf115077872bbf9
Shane Lett <shanealexlett@gmail.com>
2021-12-09 20:36:51 -0500

    Update POSTController.js

--- controllers/POSTController.js
+++ controllers/POSTController.js
@@ -2,6 +2,8 @@ const addMember = "INSERT INTO CongressMembers(Name, Info_ID, 'Contact Info ID')
 
 const addContact = "INSERT INTO ContactInformation('Phone_Number', 'Email_Address', 'Office_Address') VALUES('{$req.body.Phone_Number}','{$req.body.Email_Address}','{$req.body.Office_Address}'";
 
+const addProfiles = "INSERT INTO InternetProfiles('profile_ID', 'twitter', 'instagram', 'facebook', 'website') VALUES('{$req.body.profile_ID}',n'{$req.body.twitter}','{$req.body.instagram}', '{$req.body.facebook}', '{$req.body.website}')";
+
 export default {
     addMember, addContact,
 }

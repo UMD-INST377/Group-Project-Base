43f31f8c093549da0e4987ad5748cb86770eeacf
reinesmaliksi <rmaliksi@umd.edu>
2021-12-09 20:09:30 -0500

    create vaccine-site-info.js

---
+++ controllers/vaccine-site-info.js
@@ -0,0 +1,14 @@
+const vaccSitesInfo = `SELECT site_name,
+    street_address,
+    street_address2,
+    city,
+    zip_code,
+    active,
+    site_type,
+    operating_hours,
+    contact_phone,
+    website,
+    site_ID
+FROM
+    vaccine_site_info`;
+export default vaccSitesInfo

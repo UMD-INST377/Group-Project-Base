5852ea018c0617d66ab90940beeaad0d94cb1f58
Shane Lett <shanealexlett@gmail.com>
2021-12-09 20:36:53 -0500

    Update GETController.js

--- controllers/GETController.js
+++ controllers/GETController.js
@@ -2,6 +2,10 @@ const members ="SELECT 'Member Full Name' AS 'Name' , 'Personal Info ID' AS 'Inf
 
 const contact ="SELECT 'Phone Number' AS 'Phone_Number', 'Email Address' AS 'Email_Address', 'Office Address' AS 'Office_Address' FROM ContactInformation";
 
+const contact ="SELECT 'Phone Number' AS 'Phone_Number', 'Email Address' AS 'Email_Address', 'Office Address' AS 'Office_Address' FROM ContactInformation";
+
+const profiles ="SELECT 'Internet Profile ID' AS 'profile_ID', 'Twitter' AS 'twitter', 'Instagram' AS 'instagram', 'Facebook' AS 'facebook', 'Website' AS 'website' FROM InternetProfiles";
+
 export default {
     members,contact,
 }

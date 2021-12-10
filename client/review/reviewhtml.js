0f69369c242849adf5362a325652b4cfaddeb967
mclay11 <49288194+mclay11@users.noreply.github.com>
2021-12-10 16:46:54 -0500

    Reviews from database now display.

--- client/review/reviewhtml.js
+++ client/review/reviewhtml.js
@@ -1,18 +1,28 @@
-const queryString = window.location.search;
-const idNum = queryString.replace(/^\D+/g, "");
+hotelReviews = [];
 
-const loadHotels = async () => {
+const loadReviews = async () => {
   try {
-    const res = await fetch("https://group4-final-inst377fa2021.herokuapp.com/");
-    hotelsOverview = await res.json();
-    console.log(hotelsOverview);
+    const res = await fetch("https://group4-final-inst377fa2021.herokuapp.com/api/reviews");
+    hotelReviews = await res.json();
+    console.log(hotelReviews);
+    displayReviews(hotelReviews);
   } catch (err) {
     console.error(err);
-  }
-  const overviewHTML = `
-              
-                    `;
-  document.getElementById("info").innerHTML = overviewHTML;
+  }}
+
+  const displayReviews = (reviews) => {
+    const htmlString = reviews
+    .map((review) =>`
+                <div class = "box has-background-light">
+                <li class="review_container" style="list-style-type: none">
+                <ul style = "font-size:25px"><strong>${review.hotel_name}</strong></ul>
+                <ul>${review.review_text}</ul>
+                </li>
+                </div>
+                `)
+                .join('');
+  reviewList.innerHTML = htmlString;
 };
 
-loadHotels();
+loadReviews();
+

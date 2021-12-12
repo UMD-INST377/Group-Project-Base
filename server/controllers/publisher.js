711f43c5302e3689777d66e3f9b8eb32f9a4c0ea
maxwell-morris28 <mmorri2000@gmail.com>
2021-12-12 14:48:53 -0500

    Redid publisher endpoints- GET error

--- server/controllers/publisher.js
+++ server/controllers/publisher.js
@@ -1,2 +1,11 @@
-export default `SELECT publisher_id, publisher_game_id, publisher_name
-                FROM publisher`
+const get = 'SELECT * FROM publisher';
+const post = 'INSERT INTO publisher (publisher_id, publisher_game_id ,publisher_name) VALUES (:publisher_id,:publisher_game_id,:publisher_name)';
+const put = 'UPDATE publisher SET publisher_name= :publisher_name, publisher_game_id= :publisher_game_id WHERE publisher_id= :publisher_id';
+const remove = 'DELETE FROM publisher WHERE publisher_id= :publisher_id';
+
+export default {
+  get,
+  put,
+  post,
+  remove,
+};

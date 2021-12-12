55442e34b9f9f72fd9b8c8d26e6d11fdbffdf661
damores <Diego.amores14@gmail.com>
2021-12-12 02:40:53 -0500

    added bar chart certification back-end

--- public/javascript/graphs.js
+++ public/javascript/graphs.js
@@ -1,9 +1,8 @@
 /* eslint-disable no-sequences */
-// Graphs
-// eslint-disable-next-line func-names
-
+/* eslint-disable no-return-assign */
+/* eslint-disable no-param-reassign */
 async function pieChartGenrePerc() {
-  const response = await fetch('./api/vinyl');
+  const response = await fetch('./api/vinyls');
   const vinyls = await response.json();
   const genres = [];
 
@@ -13,7 +12,7 @@ async function pieChartGenrePerc() {
   }
 
   // eslint-disable-next-line no-return-assign
-  const result = genres.reduce((acc, o) => (acc[o] = (acc[o] || 0) + 1, acc), {});
+  const result = genres.reduce((arr, val) => (arr[val] = (arr[val] || 0) + 1, arr), {});
 
   const dict = [];
   // eslint-disable-next-line no-restricted-syntax
@@ -43,37 +42,75 @@ async function pieChartGenrePerc() {
     }]
   });
 
-  const dataPoints = [];
+  chart.render();
+}
+async function barChartCert() {
+  const response = await fetch('./api/certifications');
+  const jsonCert = await response.json();
+  const gold = [];
+  const platinum = [];
+  const multiPlat = [];
+  const diamond = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < jsonCert.length; i++) {
+    gold.push(jsonCert[i].Gold);
+    platinum.push(jsonCert[i].Platinum);
+    multiPlat.push(jsonCert[i]['Multi-Platinum']);
+    diamond.push(jsonCert[i].Diamond);
+  }
+
+  const goldResult = gold.reduce((arr, val) => (arr[val] = (arr[val] || 0) + 1, arr), {});
+  const platResult = platinum.reduce((arr, val) => (arr[val] = (arr[val] || 0) + 1, arr), {});
+  const multiResult = multiPlat.reduce((arr, val) => (arr[val] = (arr[val] || 0) + 1, arr), {});
+  const diamondResult = diamond.reduce((arr, val) => (arr[val] = (arr[val] || 0) + 1, arr), {});
+
+  const dict = [];
+
+  Object.keys(goldResult);
+  Object.keys(platResult);
+  Object.keys(multiResult);
+  Object.keys(diamondResult);
+
+  dict.push({
+    y: goldResult[Object.keys(goldResult)[Object.keys(goldResult).length - 1]],
+    label: 'Gold'
+  });
+
+  dict.push({
+    y: platResult[Object.keys(platResult)[Object.keys(platResult).length - 1]],
+    label: 'Platinum'
+  });
+
+  dict.push({
+    y: multiResult[Object.keys(multiResult)[Object.keys(multiResult).length - 1]],
+    label: 'Multi-Platinum'
+  });
+
+  dict.push({
+    y: diamondResult[Object.keys(diamondResult)[Object.keys(diamondResult).length - 1]],
+    label: 'Diamond'
+  });
 
   const chart2 = new CanvasJS.Chart('bar-graph', {
     animationEnabled: true,
-    theme: 'light2',
+    theme: 'light1',
     title: {
-      text: 'Daily Sales Data'
+      text: 'Vinyls Based on Certifications'
     },
     axisY: {
-      title: 'Units',
+      title: 'Count',
       titleFontSize: 24,
       includeZero: true
     },
     data: [{
       type: 'column',
-      yValueFormatString: '#,### Units',
-      dataPoints: dataPoints
+      dataPoints: dict
     }]
   });
 
-  function addData(data) {
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < data.length; i++) {
-      dataPoints.push({
-        x: new Date(data[i].date),
-        y: data[i].units
-      });
-    }
-    chart2.render();
-  }
-  chart.render();
-  $.getJSON('https://canvasjs.com/data/gallery/javascript/daily-sales-data.json', addData);
+  chart2.render();
 }
+
 pieChartGenrePerc();
+barChartCert();

a7c2765075ea7d15b6d338e57002ccc0f81c903b
skim225 <70543515+skim225@users.noreply.github.com>
2021-12-12 18:39:05 -0500

    Updated data that is being taken from SQL

--- server/controller/mdCensusData.js
+++ server/controller/mdCensusData.js
@@ -5,8 +5,15 @@ export default `SELECT census_zcta,
     total_population,
     homeowner_rate,
     percent_homeowner_without_mortgage,
-    percent_rent
+    percent_rent,
+    community_identifier,
+    pct_foreign_born,
+    pct_poverty,
+    pct_unemployed,
+    pct_bachelors,
+    median_household_income,
+    pct_little_english
 FROM
     md_census_data
-INNER JOIN tax_credit_companies 
-    ON md_census_data.census_zcta = tax_credit_companies.company_zcta;`;
+INNER JOIN community_survey 
+    ON md_census_data.census_identifier = community_survey.community_identifier;`;

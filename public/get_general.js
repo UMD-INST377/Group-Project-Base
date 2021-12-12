e7caf95ac9e2eb113f9f84ed91c863202079db6f
Lesly-Zouantcha <leslyaudrey@yahoo.com>
2021-12-12 16:22:20 -0500

    get endpoint

---
+++ public/get_general.js
@@ -0,0 +1,60 @@
+const searchInput = document.querySelector('.search');
+const suggestions = document.querySelector('.suggestions');
+
+async function windowActions() {
+  const data= await fetch('/api/general');
+
+  
+  function findMatches(wordToMatch, gameGeneral) {
+    return gameGeneral.filter((result) => {
+      const regex = new RegExp(wordToMatch, 'gi');
+      console.log(wordToMatch);
+      console.log(String(result.range_game_id));
+      if (wordToMatch === '')
+      {
+        return null; //Ensures no search result display if input field is empty
+      }
+      else {
+        return String(result.range_game_id).match(regex); 
+      }
+      
+    });
+  }
+
+  function displayMatches(event) {
+    const matchArray = findMatches(event.target.value, gameGeneral);
+    console.log(matchArray);
+    if (matchArray) {
+      const html = matchArray
+        .map((result) => {
+          const regex = new RegExp(event.target.value, 'gi');
+
+          return `
+
+          <ul>
+            <li class="game-id">ID: ${result.game_id}</li>
+            <li>Game Name: ${result.game_name}</l>
+            <li>Free to Play: ${result.free_to_play}</l>
+            <li>Player Population: ${result.player_population}</l>
+            
+          </ul>
+          <br></br>
+        
+            
+            `;
+        })
+        .join('');
+      suggestions.innerHTML = html;
+    }
+  }
+  const newData = await data.json();
+  console.log(newData[0])
+  const gamePrice = newData[0];
+  searchInput.addEventListener('change', displayMatches);
+  searchInput.addEventListener('keyup', (evt) => {
+    displayMatches(evt);
+  });
+  
+}
+
+window.onload = windowActions;

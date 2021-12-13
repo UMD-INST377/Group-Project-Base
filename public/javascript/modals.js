1798942f8456dec7832efa724211f71ae462cdf6
damores <Diego.amores14@gmail.com>
2021-12-13 02:55:16 -0500

    added back-end for modals

--- public/javascript/modals.js
+++ public/javascript/modals.js
@@ -1,5 +1,4 @@
 /* eslint-disable no-plusplus */
-
 // sign-up modal
 const signupButton = document.querySelector('#signup');
 const signupClose = document.querySelector('#signup-close');
@@ -20,17 +19,53 @@ cancelButton.addEventListener('click', () => {
   signupModal.classList.remove('is-active');
 });
 
-// vinyl modal
-const triggers = document.querySelectorAll('.trigger');
-const triggerArray = Array.from(triggers);
-const vinylModals = document.querySelectorAll('#vinylmodal');
-const cancelVinyl = document.querySelectorAll('#cancel-vinyl');
+document.addEventListener('DOMContentLoaded', () => {
+  const test = document.querySelector('.test');
 
-triggerArray.forEach((trigger, index) => {
-  const toggleModal = () => {
-    vinylModals[index].classList.toggle('is-active');
-  };
+  function vinylInfo(row, index) {
+    return `
+    <div class="modal" id="vinylmodal">
+            <div class="modal-background"></div>
+            <div class="modal-card">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">Modal title</p>
+                </header>
+                <section class="modal-card-body">
+                    <!-- Content ...-->
+                    <p>Album Name: ${row[index]['Vinyl Name']}</p>
+                    <p>Genre: ${row[index].Genre}</p>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button is-danger">Add Record</button>
+                    <button class="button" id="cancel-vinyl">Cancel</button>
+                </footer>
+            </div>
+            </div>
+          `;
+  }
 
-  trigger.addEventListener('click', toggleModal);
-  cancelVinyl[index].addEventListener('click', toggleModal);
+  async function displayVinylModalTrending() {
+    const response = await fetch('./api/vinyls');
+    const vinyls = await response.json();
+
+    const triggers = document.querySelectorAll('.trigger');
+
+    for (let i = 0; i < triggers.length; i++) {
+      test.innerHTML += (vinylInfo(vinyls, i));
+    }
+
+    const triggerArray = Array.from(triggers);
+    const vinylModal = document.querySelectorAll('#vinylmodal');
+    const cancelVinyl = document.querySelectorAll('#cancel-vinyl');
+
+    triggerArray.forEach((trigger, index) => {
+      const toggleModal = () => {
+        vinylModal[index].classList.toggle('is-active');
+      };
+
+      trigger.addEventListener('click', toggleModal);
+      cancelVinyl[index].addEventListener('click', toggleModal);
+    });
+  }
+  displayVinylModalTrending();
 });

b9ba801445c565cae16f77a394010a7e152c7f0d
caseyt711 <caseyt711@gmail.com>
2021-12-12 22:24:45 -0500

    Completed sales table

---
+++ public/insert_sales.js
@@ -0,0 +1,32 @@
+async function windowActions () {
+  function formToObject(htmlFormElement) {
+    const formItem = new FormData(htmlFormElement).entries();
+    const formArray = Array.from(formItem);
+    const formObject = formArray.reduce((collection, item, index) => {
+      if (!collection[item[0]]) {
+        collection[item[0]] = item[1];
+      }
+      return collection;
+    }, {});
+    return formObject;
+  }
+  
+  async function formSubmit() {
+    const form = document.querySelector('#sales');
+    const data = formToObject(form);
+    console.log(data);
+    const response = await fetch('/api/sales', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  
+    });
+  }
+  
+  const button = document.querySelector('#insert_button');
+  button.onclick = formSubmit;
+}
+  
+window.onload = windowActions;

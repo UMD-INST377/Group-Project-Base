7d2391c68eada88198290255b31c54e47864ae22
Elijah Falope <efalope@umd.edu>
2021-12-13 17:45:28 -0500

    Fixed edit fight mode ID

--- client/public/editfightrecord.js
+++ client/public/editfightrecord.js
@@ -1,6 +1,6 @@
 async function updatefightRecord() {
   const recordUpdate = document.querySelector('#id_update');
-  const nameUpdate = document.querySelector('#name_update');
+  // const nameUpdate = document.querySelector('#name_update'); //
   const specskillUpdate = document.querySelector('#specskill_update');
   const wocUpdate = document.querySelector('#woc_update');
   const request = await fetch('/api/fight_mode', {
@@ -8,10 +8,10 @@ async function updatefightRecord() {
     headers: {
       'Content-Type': 'application/json'
     },
+    // changing put to match fight mode table //
     body: JSON.stringify({
-      Animal_ID: recordUpdate.value,
-      common_name: nameUpdate.value,
-      species: specskillUpdate.value,
+      fight_mode_id: recordUpdate.value,
+      special_skill: specskillUpdate.value,
       weapon_of_choice: wocUpdate.value
     })
 

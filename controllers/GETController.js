3f3bf4c0f1d94d07188e717b9dc10bfd4a65c6af
Shane Lett <shanealexlett@gmail.com>
2021-12-13 19:13:03 -0500

    Update GETController.js

--- controllers/GETController.js
+++ controllers/GETController.js
@@ -2,7 +2,7 @@ const members ="SELECT 'Member Full Name' AS 'Name' , 'Personal Info ID' AS 'Inf
 
 const contact ="SELECT 'Phone Number' AS 'Phone_Number', 'Email Address' AS 'Email_Address', 'Office Address' AS 'Office_Address' FROM ContactInformation";
 
-const profiles ="SELECT 'Internet Profile ID' AS 'profile_ID', 'Twitter' AS 'twitter', 'Instagram' AS 'instagram', 'Facebook' AS 'facebook', 'Website' AS 'website' FROM InternetProfiles";
+const profiles = "SELECT * FROM Group26_congress_db.`Internet Profiles`"
 
 const membersTest = "SELECT `First Name`, `Last Name`, `Age`, `Gender` FROM Group26_congress_db.`Personal Information`;"
 export default {

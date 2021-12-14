642f9180c1dff0844f23cd92a0b1f9ef2f6c30fb
reinesmaliksi <rmaliksi@umd.edu>
2021-12-13 23:05:13 -0500

    remove categories from vacc site controller

--- controllers/vaccine-site-info.js
+++ controllers/vaccine-site-info.js
@@ -1,14 +1,11 @@
 const vaccSitesInfo = `SELECT site_name,
     street_address,
-    street_address2,
     city,
     zip_code,
-    active,
     site_type,
     operating_hours,
     contact_phone,
     website,
-    site_ID
 FROM
     vaccine_site_info`;
 export default vaccSitesInfo

6129d8c8b5fe02d075085d18dff8b8970ff4b3e9
vihtle <vihtle2000@gmail.com>
2021-12-13 22:57:45 -0500

    Changed some fields to be selects, valued pulled from database. Also changed some formatting and added an instructional box

--- client/scripts/erupform.js
+++ client/scripts/erupform.js
@@ -1,3 +1,92 @@
+let volcanoValue=1;
+let aoaValue=1;
+let veiValue=1;
+let evidenceValue=1;
+let categoryValue=1;
+
+async function windowActions() {
+    //getting data from volcanos endpoint and putting it in the select menu
+    const volcanoendpoint = '/api/volcanos';
+    const volcanorequest = await fetch(volcanoendpoint);
+    const volcano = await volcanorequest.json();
+    const volcanoSelect = document.querySelector('#volcano-select');
+    for (let i = 0; i < volcano.length; i++) {
+      option = document.createElement('option');
+      option.setAttribute('value', volcano[i].volcano_id);
+      option.innerHTML = volcano[i].volcano_name;
+      volcanoSelect.appendChild(option);
+    }
+  
+  //getting data from aoa endpoint and putting it in the select menu
+  const aoaendpoint = '/api/eruption_aoa';
+  const aoarequest = await fetch(aoaendpoint);
+  const aoa = await aoarequest.json();
+  const aoaSelect = document.querySelector('#aoa-select');
+  for (let i = 0; i < aoa.length; i++) {
+    option = document.createElement('option');
+    option.setAttribute('value', aoa[i].aoa_id);
+    option.innerHTML = aoa[i].aoa;
+    aoaSelect.appendChild(option);
+  }
+
+  //getting data from vei endpoint and putting it in the select menu
+  const veiendpoint = '/api/vei';
+  const veirequest = await fetch(veiendpoint);
+  const vei = await veirequest.json();
+  const veiSelect = document.querySelector('#vei-select');
+  for (let i = 0; i < vei.length; i++) {
+    option = document.createElement('option');
+    option.setAttribute('value', vei[i].vei_id);
+    option.innerHTML = vei[i].vei;
+    veiSelect.appendChild(option);
+  }
+
+  //getting data from evidence endpoint and putting it in the select menu
+  const evidenceendpoint = '/api/evidence';
+  const evidencerequest = await fetch(evidenceendpoint);
+  const evidence = await evidencerequest.json();
+  const evidenceSelect = document.querySelector('#evidence-select');
+  for (let i = 0; i < evidence.length; i++) {
+    option = document.createElement('option');
+    option.setAttribute('value', evidence[i].evidence_id);
+    option.innerHTML = evidence[i].method;
+    evidenceSelect.appendChild(option);
+  }
+
+//getting data from category endpoint and putting it in the select menu
+  const categoryendpoint = '/api/eruption_category';
+  const categoryrequest = await fetch(categoryendpoint);
+  const category = await categoryrequest.json();
+  const categorySelect = document.querySelector('#category-select');
+  for (let i = 0; i < category.length; i++) {
+    option = document.createElement('option');
+    option.setAttribute('value', category[i].category_id);
+    option.innerHTML = category[i].category;
+    categorySelect.appendChild(option);
+  }
+
+  //updates global values to be used in form
+  function updateValue() {
+    const volcanoOption = volcanoSelect.options[volcanoSelect.selectedIndex];
+    const aoaOption = aoaSelect.options[aoaSelect.selectedIndex];
+    const veiOption = veiSelect.options[veiSelect.selectedIndex];
+    const evidenceOption = evidenceSelect.options[evidenceSelect.selectedIndex];
+    const categoryOption = categorySelect.options[categorySelect.selectedIndex];
+    volcanoValue=(volcanoOption.value);
+    aoaValue=(aoaOption.value);
+    veiValue=(veiOption.value);
+    evidenceValue=(evidenceOption.value);
+    categoryValue=(categoryOption.value);
+  }
+  volcanoSelect.addEventListener('change', updateValue);
+  aoaSelect.addEventListener('change', updateValue);
+  veiSelect.addEventListener('change', updateValue);
+  evidenceSelect.addEventListener('change', updateValue);
+  categorySelect.addEventListener('change', updateValue);
+}
+window.onload = windowActions;
+
+
 async function updateEruption() {
   const eruptionUpdateInfo = {
     eruption_id: document.getElementById('eruption-id-input').value,
@@ -5,11 +94,11 @@ async function updateEruption() {
     year: document.getElementById('year-input').value,
     month: document.getElementById('month-input').value,
     day: document.getElementById('day-input').value,
-    volcano_id: document.getElementById('volcano-id-input').value,
-    aoa_id: document.getElementById('aoa-id-input').value,
-    vei_id: document.getElementById('vei-id-input').value,
-    evidence_id: document.getElementById('evidence-id-input').value,
-    category_id: document.getElementById('category-id-input').value
+    volcano_id: volcanoValue,
+    aoa_id: aoaValue,
+    vei_id: veiValue,
+    evidence_id: evidenceValue,
+    category_id: categoryValue
   };
   await fetch('/api/eruption_info', {
     method: 'PUT',
@@ -26,11 +115,11 @@ async function insertEruption() {
     year: document.getElementById('year-input').value,
     month: document.getElementById('month-input').value,
     day: document.getElementById('day-input').value,
-    volcano_id: document.getElementById('volcano-id-input').value,
-    aoa_id: document.getElementById('aoa-id-input').value,
-    vei_id: document.getElementById('vei-id-input').value,
-    evidence_id: document.getElementById('evidence-id-input').value,
-    category_id: document.getElementById('category-id-input').value
+    volcano_id: volcanoValue,
+    aoa_id: aoaValue,
+    vei_id: veiValue,
+    evidence_id: evidenceValue,
+    category_id: categoryValue
   };
   await fetch('api/eruption_info', {
     method: 'POST',

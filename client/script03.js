a0e19fcaecd21bc86cafd84c9fda2966a8261960
marilynpothen <m3pothen@gmail.com>
2021-12-13 21:07:44 -0500

    script03 & displaying vacDataAndCounty

---
+++ client/script03.js
@@ -0,0 +1,37 @@
+// Toggle between showing and hiding dropdown
+function openDropdown() {
+    document.getElementById('myDropdown').classList.toggle('show');
+  }
+  
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      const dropdowns = document.getElementsByClassName('dropdown-content');
+      let i;
+      for (i = 0; i < dropdowns.length; i++) {
+        const openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  };
+
+endpoint = '/api/vacDataAndCounty'; 
+async function VCInfo(endpoint, table) {
+  const response = await fetch(endpoint);
+  const data = await response.json();
+  console.log(data[1]);
+
+  for (var key in data){
+    var row = `<tr>
+                  <td>${data[key].population}</td>
+                  <td>${data[key].confirmed_deaths}</td>
+                  <td>${data[key].first_dose_count}</td>
+                  <td>${data[key].second_dose_count}</td>
+              </tr>`
+
+    table.innerHTML += row
+  }
+}
+VCInfo(endpoint, document.querySelector('VCtable'));
+

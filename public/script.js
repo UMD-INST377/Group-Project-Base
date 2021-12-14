73d38d83b72f021cb566b0a8a2894692a3b89536
Daniel Flanagan <danielflanagan777@gmail.com>
2021-12-13 20:40:43 -0800

    my own table

--- public/script.js
+++ public/script.js
@@ -76,6 +76,45 @@ const displayTableData = async () => {
     
   };
 
+  const displayTableDataThree = async () => {
+    const data = await fetch('https://vast-lake-04060.herokuapp.com/api/contact', { mode: 'no-cors' });
+    const res = await data.json();
+  
+    const table = document.getElementById('table3');
+    table.innerHTML = '';
+
+    const header = document.createElement('tr');
+    header.classList.add("head");
+    const head = ["Phone Number", "Email Address", "Office Address"];
+  for (var i = 0; i < head.length; i++) {
+    const td = document.createElement('td');
+    td.innerText = head[i];
+    header.appendChild(td)
+    }
+    table.appendChild(header);
+
+  
+    res.data.forEach((element) => {
+      const row = document.createElement('tr');
+      let phonenumber;
+  
+      Object.keys(element).forEach((column) => {
+        const td = document.createElement('td');
+        td.innerText = `${element[column]}`;
+  
+        if (column === 'Phone Number') {
+          phonenumber = element[column];
+        }
+        row.appendChild(td);
+      });
+  
+      table.appendChild(row);
+    }
+    
+    );
+    
+  };
+
  function show(nr) {
     document.getElementById("table1").style.display="none";
     document.getElementById("table2").style.display="none";

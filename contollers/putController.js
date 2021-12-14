8ee114ea84ff45adf47ffabcb55a1cae44e28723
Monsterlaser <73480524+Monsterlaser@users.noreply.github.com>
2021-12-13 22:49:08 -0500

    Update putController.js

--- contollers/putController.js
+++ contollers/putController.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
 const establishmentVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE';
 const updateVar = 'UPDATE establishment_id SET category = Seafood WHERE zip= 20744 ';
 export default {
   establishmentVar,
   updateVar
 };
+=======
+const updateVar = 'UPDATE Food_Inspection SET Location= Montgomery County WHERE ';
+export default updateVar;
+>>>>>>> parent of f1114c1 (fixed every endpoint except for post)

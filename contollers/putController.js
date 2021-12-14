be7888fa2f077143ed6b98cb98662f587fd8f2de
jasmineokebugwu <jokebugw@umd.edu>
2021-12-13 23:00:31 -0500

    fixing everything that i broke :(

--- contollers/putController.js
+++ contollers/putController.js
@@ -1,6 +1,2 @@
-const establishmentVar = 'SELECT establishment_id, name, category, inspection_date, inspection_results, city, state, zip FROM FOOD_INSPECTION_GROUP8.Food_Inspection WHERE';
-const updateVar = 'UPDATE establishment_id SET category = Seafood WHERE zip= 20744 ';
-export default {
-  establishmentVar,
-  updateVar
-};
+const updateVar = 'UPDATE Food_Inspection SET Inspection_results = "Fully Compliant" WHERE City = "College Park"';
+export default updateVar; 

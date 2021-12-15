d399c3ede73b5895d6612cc4ebc0d33452efb176
Theophile Sadio Nanzo <tsadio@terpmail.umd.edu>
2021-12-15 17:26:41 -0500

    adding a script.js for unemployment

--- client/script01.js
+++ client/script01.js
@@ -1,29 +1,29 @@
 // Toggle between showing and hiding dropdown
 function openDropdown() {
     document.getElementById('myDropdown').classList.toggle('show');
-  }
-  
-  window.onclick = function(event) {
+}
+
+window.onclick = function(event) {
     if (!event.target.matches('.dropbtn')) {
-      const dropdowns = document.getElementsByClassName('dropdown-content');
-      let i;
-      for (i = 0; i < dropdowns.length; i++) {
-        const openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
+        const dropdowns = document.getElementsByClassName('dropdown-content');
+        let i;
+        for (i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
         }
-      }
     }
-  };
+};
 
-  urlVS = '/api/vaccine-site-info';
+urlVS = '/api/vaccine-site-info';
 async function vsStats(urlVS, tableVS) {
-  const responseVS = await fetch(urlVS);
-  const dataVS = await responseVS.json();
-  console.log(dataVS[1]);
+    const responseVS = await fetch(urlVS);
+    const dataVS = await responseVS.json();
+    console.log(dataVS[1]);
 
-  for (var key in dataVS){
-    var rowVS = `<tr>
+    for (var key in dataVS) {
+        var rowVS = `<tr>
                   <td>${dataVS[key].site_name}</td>
                   <td>${dataVS[key].street_address}</td>
                   <td>${dataVS[key].city}</td>
@@ -33,8 +33,8 @@ async function vsStats(urlVS, tableVS) {
                   <td>${dataVS[key].contact_phone}</td>
                   <td>${dataVS[key].website}</td>
               </tr>`
-    //table.append(row);
-    tableVS.innerHTML += rowVS
-  }
+            //table.append(row);
+        tableVS.innerHTML += rowVS
+    }
 }
 vsStats(urlVS, document.querySelector('#tableVS'));

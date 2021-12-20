aa4634b4efece8ff9d3ca3d35c40ea256c74252a
iglesias-johnluke <iglesiasjohnluke@gmail.com>
2021-12-20 17:24:40 -0500

    acceptance rate filter

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,8 @@
+/* eslint-disable prefer-const */
+/* eslint-disable max-len */
+/* eslint-disable keyword-spacing */
+/* eslint-disable no-multiple-empty-lines */
+/* eslint-disable spaced-comment */
 /* eslint-disable padded-blocks */
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
@@ -6,100 +11,81 @@
 /* eslint-disable space-before-blocks */
 /* eslint-disable no-console */
 
-/* displays school sugestions in suggestions bar according to selected
-SAT radio button */
-function displaySuggestions(event){
-  // eslint-disable-next-line no-multiple-empty-lines
+/*returns array of dictionaries of universities according to user preferences  */
+function getFilteredData(){
   let filterData;
-  if (event.target.id === 'upper-scores'){ /* filter school data according to radio selected */
-    filterData = data.data.filter((item) => item.SAT_average > 1355);
-  } else if (event.target.id === 'middle-scores'){
-    console.log('middle scores');
-    filterData = data.data.filter((item) => item.SAT_average >= 1256 && item.SAT_average < 1356);
-  } else { // filter data for lower sat scores
-    filterData = data.data.filter((item) => item.SAT_average < 1256);
-  }
-  const html = filterData.map((item) => // return filtered school data as list items
-     `
-    <li>
-      <div class="card">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-content">
-              <p class="title is-4">${item.university_name}</p>
-              <p class="subtitle is-6">${item.univ_location}</p>
-            </div>
-          </div>
-        </div>
-        <footer class="card-footer">
-          <a href="/university/${item.testscore_id}/" class="card-footer-item">Read More</a>
-        </footer>
-      </div>
-    </li>
-    `
-  ).join('');
+  const satSelection = document.querySelector('input[name="sat-scores"]:checked');
+  const acceptanceRateSelection = document.querySelector('input[name="acceptance-rate"]:checked');
 
-  const suggestions = document.querySelector('.results');
-  suggestions.innerHTML = html;
+  if(satSelection != null){
+    const satValue = satSelection.value;
+    if (satValue === 'upper-scores'){ /* filter school data according to radio selected */
+      filterData = data.data.filter((item) => item.SAT_average > 1355);
+    } else if (satValue === 'middle-scores'){
+      filterData = data.data.filter((item) => item.SAT_average >= 1256 && item.SAT_average < 1356);
+    } else { // filter data for lower sat scores
+      filterData = data.data.filter((item) => item.SAT_average < 1256);
+    }
+  }
+  if(acceptanceRateSelection != null){
+    const acceptanceRateValue = acceptanceRateSelection.value;
+    if (acceptanceRateValue === 'upper'){ /* filter school data according to radio selected */
+      filterData = filterData.filter((item) => item.admission_rate > 71);
+    } else if (acceptanceRateValue === 'middle'){
+      filterData = filterData.filter((item) => item.admission_rate >= 55 && item.admission_rate <= 71);
+    } else { // filter data for lower acceptance rate
+      filterData = filterData.filter((item) => item.admission_rate < 55);
+    }
+  }
+  return filterData;
 }
 
-/* initializes sat radiobuttons so suggestions are displayed are displayed
-according to what button is selected */
-async function initSATRadioButtons() {
+/* displays school sugestions in suggestions bar according to selected
+SAT radio button */
+function displaySuggestions(){
+  // eslint-disable-next-line no-multiple-empty-lines
+  let filterData = getFilteredData();
+
+  if(filterData != null){
+    const html = filterData.map((item) => // return filtered school data as list items
+    `
+   <li>
+     <div class="card">
+       <div class="card-content">
+         <div class="media">
+           <div class="media-content">
+             <p class="title is-4">${item.university_name}</p>
+             <p class="subtitle is-6">${item.univ_location}</p>
+           </div>
+         </div>
+       </div>
+       <footer class="card-footer">
+         <a href="/university/${item.testscore_id}/" class="card-footer-item">Read More</a>
+       </footer>
+     </div>
+   </li>
+   `
+    ).join('');
+    const suggestions = document.querySelector('.results');
+    suggestions.innerHTML = html;
+  }
+
+}
+
+/* retrieves college data from database relevant to user preferences, initializes as data array*/
+async function getCollegeData() {
   const url = '/api/test_scores';
   data = [];
 
   const request = await fetch(url);
   if (request.ok) {
     data = await request.json();
-
-    const upperScoreButton = document.querySelector('#upper-scores');
-    const middleScoreButton = document.querySelector('#middle-scores');
-    const lowerScoreButton = document.querySelector('#lower-scores');
-
-    upperScoreButton.addEventListener('change', displaySuggestions);
-    middleScoreButton.addEventListener('change', displaySuggestions);
-    lowerScoreButton.addEventListener('change', displaySuggestions);
+    console.log('fetched test_scores (data))', data);
   } else {
     console.log('messed up');
   }
 }
 
-initSATRadioButtons();
+getCollegeData();
 
-function displayMatches(event){
-  let matchData;
-  if (event.target.id === 'upper-acceptance-rate'){
-    matchData = data.data.filter((item) => item.acceptence_rate > 72);
-  } else if (event.target.id === 'middle-acceptance-rate'){
-    console.log('middle rate');
-    matchData = data.data.filter((item) => item.acceptence_rate >= 55 && item.acceptence_rate < 71);
-  } else {
-    matchData = data.data.filter((item) => item.acceptence_rate < 55);
-  }
-  const html = matchData.map(item);
-  const matches = document.querySelector('.results');
-  matches.innerHTML = html;
-}
 
-async function initAcceptanceRadioButtons() {
-  const url = '/api/Admissions_rate';
-  data = [];
-
-  const request = await fetch(url);
-  if (request.ok) {
-    data = await request.json();
-
-    const lowerRateButton = document.querySelector('#lower-acceptance-rate');
-    const middleRateButton = document.querySelector('#middle-acceptance-rate');
-    const upperRateButton = document.querySelector('#upper-acceptance-rate');
-
-    upperRateButton.addEventListener('change', displayMatches);
-    middleRateButton.addEventListener('change', displayMatches);
-    lowerRateButton.addEventListener('change', displayMatches);
-  } else {
-    console.log('messed up');
-  }
-}
-
-initAcceptanceRadioButtons();

f3bc8d88d38b9d14a7cbc45d011c6557ff25c09f
Alex Leitch <54922132+aleitch1@users.noreply.github.com>
2022-01-23 13:08:22 -0500

    Update config.js
    add new ip address for SP2022, add note to not mess w file at risk of "weird problems" later in term

--- database/config.js
+++ database/config.js
@@ -1,23 +1,30 @@
+/* 
+   don't mess with this file or remove environment entries in the object
+   even for your group project, because it will stop your DB from connecting
+   due to the many loops in the initializeDB file
+   ~ Prof A.
+*/
+
 export default {
   development: {
     username: 'student',
     password: 'INST377@UMD',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '174.129.198.86',
     dialect: 'mysql'
   },
   test: {
     username: 'student',
     password: 'INST377@UMD',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '174.129.198.86',
     dialect: 'mysql'
   },
   production: {
     username: 'student',
     password: 'INST377@UMD',
     database: 'Dining_Hall_Tracker',
-    host: '3.236.243.212',
+    host: '174.129.198.86',
     dialect: 'mysql'
   }
 };

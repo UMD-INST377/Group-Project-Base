c715583c66bfb9cd2c4416035788fd83c16cc6c3
vezfly <vez@terpmail.umd.edu>
2022-03-20 00:53:51 -0400

    add h1 tag

--- server.js
+++ server.js
@@ -3,10 +3,31 @@ import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
 
-const app = express();
+//const app = express();
 
+//const PORT = process.env.PORT || 3000;
+//const staticFolder = 'client';
+
+dotenv.config();
+const __dirname = path.resolve();
+// import db from './server/database/initializeDB.js';
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
+let liveReloadServer;
+
+// Add some auto-reloading to our server
+if (process.env.CONTEXT === 'development') {
+  liveReloadServer = reload.createServer();
+  liveReloadServer.watch(path.join(__dirname, staticFolder));
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

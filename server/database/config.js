6771d3c5d2c8c111681b2e2712f32c032e3333b4
bryan-pham-124 <72675857+bryan-pham-124@users.noreply.github.com>
2022-04-07 19:32:23 -0400

    BP - updated config.js
    Trying to fix error where endpoint does not work on heroku.

--- server/database/config.js
+++ server/database/config.js
@@ -3,14 +3,20 @@
    even for your group project, because it will stop your DB from connecting
    due to the many loops in the initializeDB file
    ~ Prof A.
+
+       username: 'root',
+    password: 'Veracrypt@12',
+    database: 'Dining_Hall_Tracker',
+    host: 'localhost',
+    dialect: 'mysql'
 */
 
 export default {
   development: {
-    username: 'root',
-    password: 'Veracrypt@12',
+    username: 'student',
+    password: 'INST377@UMD',
     database: 'Dining_Hall_Tracker',
-    host: 'localhost',
+    host: '174.129.198.86',
     dialect: 'mysql'
   },
   test: {

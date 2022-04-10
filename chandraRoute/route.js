a1f541a8238f58997d30f2b5697a496d78baee45
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-10 15:59:48 -0400

    new route

--- chandraRoute/route.js
+++ chandraRoute/route.js
@@ -1,8 +1,19 @@
 import express from 'express';
 import Sequelize from 'sequelize';
+import db from '../database/initializeDB.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Welcome to the group 22 database');
 });
+
+router.route('/Group22_Dining_Hall_Tracker')
+  .get(async (req, res) => {
+    try {
+      const data = await db.Group22_Dining_Hall_Tracker.findAll();
+      res.json(data);
+    } catch (err) {
+      console.error(err);
+    }
+  });

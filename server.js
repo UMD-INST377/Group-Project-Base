d7bfc204ea1bc1f45896718358d0f862b6937ae7
Chonathon <79057519+Chonathon@users.noreply.github.com>
2022-04-10 19:05:46 -0400

    Revert "Revert "group 11 db added""
    This reverts commit 86b0b06f8ec148af49f0ea99ae787a96ec5b931f.

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 /* eslint-disable no-console */
 import express from 'express';
+import reload from 'livereload';
+import connectReload from 'connect-livereload';
+import dotenv from 'dotenv';
+import path from 'path';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
+
+dotenv.config();
+const __dirname = path.resolve();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
+let liveReloadServer;
+
+// auto reloading
+if (process.env.CONTEXT === 'development') {
+  liveReloadServer = reload.createServer();
+  liveReloadServer.watch(path.join(__dirname, staticFolder));
+}
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -20,6 +40,7 @@ async function bootServer() {
     // await mysql.sync();
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);
+      console.log('environment', process.env.CONTEXT);
     });
   } catch (err) {
     console.error(err);
@@ -27,3 +48,10 @@ async function bootServer() {
 }
 
 bootServer();
+if (process.env.CONTEXT === 'development') {
+  liveReloadServer.server.once('connection', () => {
+    setTimeout(() => {
+      liveReloadServer.refresh('/');
+    }, 100);
+  });
+}

0701740a3d1588eecb4f6607bb50d9f935021ad0
ElaineT23 <elainetse94@gmail.com>
2022-04-10 18:15:37 -0400

    Elaine Tse TPC: Get Controllers
    http://localhost:3000/api/dining

--- server/routes/etRoutes.js
+++ server/routes/etRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 import sequelize from 'sequelize';
 import db from '../database/initializeDB.js';
 
-const diningHallQuery = 'SELECT SELECT `hall_id`, `hall_name`, `hall_address`, `hall_lat`, `hall_long` FROM `Dining_Hall` AS `Dining_Hall;';
+const diningHallQuery = 'SELECT `hall_id`, `hall_name`, `hall_address`, `hall_lat`, `hall_long` FROM `dining_hall` AS `Dining_Hall;';
 
 const router = express.Router();
 
@@ -23,8 +23,8 @@ router.route('/dining').get(async (req, res) => {
 });
 
 // Get Dining Hall with specific id
-router.get('/:hall_id', async (req, res) => {
-  const diningHallIDQuery = `SELECT * FROM Dining_Hall; WHERE hall_id =${req.params.hall_id}`;
+router.get('dining/:hall_id', async (req, res) => {
+  const diningHallIDQuery = `SELECT * FROM dining_Hall; WHERE hall_id =${req.params.hall_id}`;
   try {
     const halls = await db.sequelize.query(diningHallIDQuery, {
       type: sequelize.QueryTypes.SELECT

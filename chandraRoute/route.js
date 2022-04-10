24a122c4f2ac445fa859e1f98440fe8d63ed51da
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-09 23:34:11 -0400

    route dinning hall

--- chandraRoute/route.js
+++ chandraRoute/route.js
@@ -3,6 +3,20 @@ import Sequelize from 'sequelize';
 
 const router = express.Router();
 
-router.get('/Group22_Dining_Hall_Tracker', (req, res) => {
+router.get('/', (req, res) => {
   res.send('Welcome to the UMD Dining API!');
+});
+
+/// /////////////////////////////////
+/// ////Dining Hall Endpoints////////
+/// /////////////////////////////////
+router.get('/Group22_Dining_Hall_Tracker', async (req, res) => {
+  try {
+    const halls = await db.DiningHall.findAll();
+    const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
 });

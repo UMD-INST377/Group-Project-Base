aba38f322e9f971e7330fea1354471ea0d06664e
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-10 17:04:05 -0400

    fixed folder path

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
-app.use('./chandra', route);
+app.use('/chandra', route);
 
 async function bootServer() {
   try {

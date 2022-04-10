09e40e2420c126fd9205e9b2f639751af83d806f
sravvel <98434706+sravvel@users.noreply.github.com>
2022-04-10 18:27:32 -0400

    original models index.js

--- models/index.js
+++ models/index.js
@@ -1,25 +1,11 @@
-// import DietaryRestrictions from './DietaryRestrictions.js';
-// import DiningHall from './DiningHall.js';
-// import Macros from './Macros.js';
-// import Meals from './Meals.js';
-import address from './address.js';
-import cusine from './cusine.js';
-import customers from './customers.js';
-import descriptions from './descriptions.js';
-import hours from './hours.js';
-import linking from './linking.js';
-import rating from './rating.js';
+import DietaryRestrictions from './DietaryRestrictions.js';
+import DiningHall from './DiningHall.js';
+import Macros from './Macros.js';
+import Meals from './Meals.js';
 
 export default {
-  // DietaryRestrictions,
-  // DiningHall,
-  // Macros,
-  // Meals
-  address,
-  cusine,
-  customers,
-  descriptions,
-  hours,
-  linking,
-  rating
+  DietaryRestrictions,
+  DiningHall,
+  Macros,
+  Meals
 };

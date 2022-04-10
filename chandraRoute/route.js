bf18e27aad17714935061d355a974998afaa55d9
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-10 15:22:04 -0400

    added message

--- chandraRoute/route.js
+++ chandraRoute/route.js
@@ -4,19 +4,5 @@ import Sequelize from 'sequelize';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the UMD Dining API!');
-});
-
-/// /////////////////////////////////
-/// ////Dining Hall Endpoints////////
-/// /////////////////////////////////
-router.get('/Group22_Dining_Hall_Tracker', async (req, res) => {
-  try {
-    const halls = await db.DiningHall.findAll();
-    const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
-    res.json(reply);
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
+  res.send('Welcome to the group 22 database');
 });

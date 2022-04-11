7e586a8214e45880f58098ad86871ae9efa72ec0
isaac20021 <isaac1santamaria@gmail.com>
2022-04-11 17:38:09 -0400

    Get request random
    Finished get random genre get request

--- routes/Isaacsroutes.js
+++ routes/Isaacsroutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 import sequelize from 'sequelize';
 
 import db from '../database/initializeDB.js';
+import genres from '../models/genres.js';
 
 const router = express.Router();
 
@@ -11,10 +12,16 @@ router.get('/', (req, res) => {
 });
 router.get('/randomgenres', async(req, res) => {
     try {
-        const randomgenres = await db.genres.findAll();
+        const genres = await db.genres.findAll();
         //CODE TO PICK X RANDOM GENRES FROM 'genres' array
-        
-        res.json({data:randomgenres});
+        const num = 2;
+        const range = [...Array(num).keys()];
+        const randomgenres = range.map((item) =>{
+          const choice = Math.floor(Math.random()* genres.length);
+          return genres[choice];
+        })
+
+        res.json({data: randomgenres});
     } catch (error){
         console.error(error);
         res.send('Something went wrong.');

7bb6cf1afb6e10176f22825243cfa30ac0a2f3e9
yang <ylk01413@gmail.com>
2022-04-10 22:14:02 -0400

    changes

--- models/Genres.js
+++ models/Genres.js
@@ -1,18 +1,19 @@
 export default (sequelize, DataTypes) => {
-  const Genres = sequelize.define(
+  const genres = sequelize.define(
     'genres',
     {
       genre_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        autoIncrement: true,
         primaryKey: true
       },
       genre_name: {
-        type: DataTypes.STRING,
+        type: DataTypes.CHAR(45),
         allowNull: false
       }
     },
     { freezeTableName: true, timestamps: false }
   );
-  return Genres;
+  return genres;
 };

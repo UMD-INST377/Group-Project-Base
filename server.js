af7bc4cf7afbbdfa1d76a8d3b183b5bec5289e44
aguayo16 <98487148+aguayo16@users.noreply.github.com>
2022-04-10 20:55:34 -0400

    luis work

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+/* eslint-disable import/no-unresolved */
 /* eslint-disable no-console */
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
+import luisRoutes from './routes/luisRoutes.js';
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use(express.json());
 
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
+app.use('/api', luisRoutes);
 
 async function bootServer() {
   try {

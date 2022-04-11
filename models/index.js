3b3c7d79fc44afa1a5ab65b3c54451334d44094b
ryan-lashuk <rlashuk5@terpmail.umd.edu>
2022-04-11 00:02:48 -0400

    Ryan GET

--- models/index.js
+++ models/index.js
@@ -2,12 +2,17 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
-import students from './students.js'
+
+
+import students from './students.js';
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
   Meals,
+
+
   students
 };
+// test

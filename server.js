3e1d85fc784f925c4ca08422e70dd02bc41b4ee8
sbakare03 <98419213+sbakare03@users.noreply.github.com>
2022-04-10 20:59:40 -0400

    pull #4 (weather)

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
+import sherifatRoutes from './routes/sherifatRoutes.js';
 
 const app = express();
 
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
+app.use('/api', sherifatRoutes)
 
 async function bootServer() {
   try {

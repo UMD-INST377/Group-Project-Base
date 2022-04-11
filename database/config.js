cdc64de08b731d0144a756c1bc96d280209542fb
aguayo16 <98487148+aguayo16@users.noreply.github.com>
2022-04-10 20:30:40 -0400

    fixed config.js

--- database/config.js
+++ database/config.js
@@ -14,16 +14,16 @@ export default {
     dialect: 'mysql'
   },
   test: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'earthquake',
     host: '174.129.198.86',
     dialect: 'mysql'
   },
   production: {
-    username: 'student',
-    password: 'INST377@UMD',
-    database: 'Dining_Hall_Tracker',
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'earthquake',
     host: '174.129.198.86',
     dialect: 'mysql'
   }

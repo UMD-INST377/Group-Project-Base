ce2d5fc872c783948a047825198b970d7872c4e0
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-04-12 13:08:41 -0400

    Forked + Clone Class Repository

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
+app.use(express.static(staticFolder));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());

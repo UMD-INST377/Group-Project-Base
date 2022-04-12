64076e6dcd55e9043c0a08e74e381bdda8a4919d
svarz <svarz@terpmail.umd.edu>
2022-04-12 11:58:03 -0400

    Initial Commit spencerRoute route.js

--- spencerRoute/route.js
+++ spencerRoute/route.js
@@ -1,1 +1,11 @@
-//
+import express from 'express';
+import Sequelize from 'sequelize';
+import db from '../database/initializeDB.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+	res.send('Welcome to the group 22 database');
+  });
+
+  

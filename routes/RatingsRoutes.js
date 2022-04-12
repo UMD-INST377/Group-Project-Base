4ad79493fdaabdcc276a711df84a81f513e05cb5
Vara <varanikasharma1@gmail.com>
2022-04-12 10:13:08 -0400

    commit

--- routes/RatingsRoutes.js
+++ routes/RatingsRoutes.js
@@ -19,15 +19,41 @@ router.route('/ratings')
   });
 router.route('/ratings/:rating_id').get(async(req, res) => {
   try {
-    const rating = await db.ratings.findAll({
+    const {ratingId} = req.params;
+    const rating = await db.ratings.findOne({
       where: {
-        rating_id: req.params.rating_id
+        rating_id: `${ratingId}`
       }
     });
-    res.json(rating);
+    res.json({data: rating});
   } catch (err) {
     console.error(err);
     res.send('Error in server');
   }
 });
+router.post('/ratings', async (req, res) => {
+  console.info(chalk.bgRedBright.bold('Post request to /ratings'), req.body);
+
+  const existingRatings = await db.ratings.findAll({
+    where: {
+      rating_id: req.body.rating_id
+    }
+  });
+  const ratings = await db.ratings.findAll();
+  console.log(chalk.bgBlueBright.bold('existingRating'), existingRatings);
+  const currentRatingId = (await ratings.length) + 1;
+  try {
+    const newRating = await db.ratings.create({
+      rating_id: currentRatingId,
+      rating: req.body.rating,
+      description: req.body.description
+    });
+    // res.json(newDining);
+    res.json({message: 'not yet'});
+  } catch (err) {
+    console.error(err);
+    res.json('Server error');
+  }
+});
+
 export default router;

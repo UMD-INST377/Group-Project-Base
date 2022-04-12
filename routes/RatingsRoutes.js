63dbe01f84056cd52fedfa02e60cd15536f24b71
Vara <varanikasharma1@gmail.com>
2022-04-12 10:29:47 -0400

    commit

--- routes/RatingsRoutes.js
+++ routes/RatingsRoutes.js
@@ -17,43 +17,43 @@ router.route('/ratings')
       res.send('Error in Server');
     }
   });
-router.route('/ratings/:rating_id').get(async(req, res) => {
+router.route('/ratings/:ratingId').get(async(req, res) => {
   try {
     const {ratingId} = req.params;
-    const rating = await db.ratings.findOne({
+    const ratingList = await db.ratings.findOne({
       where: {
         rating_id: `${ratingId}`
       }
     });
-    res.json({data: rating});
+    res.json({data: ratingList});
   } catch (err) {
     console.error(err);
     res.send('Error in server');
   }
 });
-router.post('/ratings', async (req, res) => {
-  console.info(chalk.bgRedBright.bold('Post request to /ratings'), req.body);
+// router.post('/ratings', async (req, res) => {
+//   console.info(chalk.bgRedBright.bold('Post request to /ratings'), req.body);
 
-  const existingRatings = await db.ratings.findAll({
-    where: {
-      rating_id: req.body.rating_id
-    }
-  });
-  const ratings = await db.ratings.findAll();
-  console.log(chalk.bgBlueBright.bold('existingRating'), existingRatings);
-  const currentRatingId = (await ratings.length) + 1;
-  try {
-    const newRating = await db.ratings.create({
-      rating_id: currentRatingId,
-      rating: req.body.rating,
-      description: req.body.description
-    });
-    // res.json(newDining);
-    res.json({message: 'not yet'});
-  } catch (err) {
-    console.error(err);
-    res.json('Server error');
-  }
-});
+//   const existingRatings = await db.ratings.findAll({
+//     where: {
+//       rating_id: req.body.rating_id
+//     }
+//   });
+//   const ratings = await db.ratings.findAll();
+//   console.log(chalk.bgBlueBright.bold('existingRating'), existingRatings);
+//   const currentRatingId = (await ratings.length) + 1;
+//   try {
+//     const newRating = await db.ratings.create({
+//       rating_id: currentRatingId,
+//       rating: req.body.rating,
+//       description: req.body.description
+//     });
+//     // res.json(newDining);
+//     res.json({message: 'not yet'});
+//   } catch (err) {
+//     console.error(err);
+//     res.json('Server error');
+//   }
+// });
 
 export default router;

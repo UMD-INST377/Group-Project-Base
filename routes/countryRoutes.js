3adc68b5720bbc2943205787e8505e782b37e58b
loranikasharma <sharma_loranika@yahoo.com>
2022-04-12 08:57:38 -0400

    commit

--- routes/countryRoutes.js
+++ routes/countryRoutes.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-unresolved */
 import express from 'express';
 import sequelize from 'sequelize';
 import chalk from 'chalk';
@@ -5,10 +6,11 @@ import fetch from 'node-fetch';
 import db from '../database/initializeDB.js';
 
 const router = express.Router();
+// eslint-disable-next-line no-shadow
 
 router.route('/country').get(async (req, res) => {
   try {
-    const countryId = await db.country.findOne();
+    const countryId = await db.country_id.findAll();
     const reply = countryId.length > 0 ? {data: countryId} : {message: 'No Result'};
     res.json(reply);
   } catch (err) {
@@ -18,7 +20,7 @@ router.route('/country').get(async (req, res) => {
 });
 router.route('/country/:country_id').get(async(req, res) => {
   try {
-    const countryId = await db.country.findOne({
+    const countryId = await db.country_id.findAll({
       where: {
         country_id: req.params.country_id
       }

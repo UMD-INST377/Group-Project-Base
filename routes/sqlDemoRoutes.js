5e6ca59002ff887641c8cba3a34e1b3bf0ec164f
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-04-12 19:59:10 -0400

    Basic sql query works for spotify database

--- routes/sqlDemoRoutes.js
+++ routes/sqlDemoRoutes.js
@@ -5,18 +5,23 @@ import fetch from 'node-fetch';
 
 const router = express.Router();
 
+import db from '../database/initializeDB.js';
+import songDisplayQuery from '../controllers/songDisplay_query.js'
 
 router.route('/')
-.get(async (req, res) => {
+  .get(async (req, res) => {
     try {
-        console.log('Touched sqlDemo get');
-        res.json({data: []})
-        
+      console.log('Touched sqlDemo get');
+      const result = await db.sequelizeDB.query(songDisplayQuery, {
+        type: sequelize.QueryTypes.SELECT
+      })
+      res.json({data: result})
+
     } catch (error) {
-        console.log('Touched sqlDemo get error', error)
-        res.json({message: "error in sqlDemo"})
-        
+      console.log('Touched sqlDemo get error', error)
+      res.json({message: "error in sqlDemo"})
+
     }
-})
+  })
 
 export default router;

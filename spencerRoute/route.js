7255aa90a4759af1c7f0cc4d485b6bb03d446be0
svarz <svarz@terpmail.umd.edu>
2022-04-12 12:17:10 -0400

    C1 spencerRoute route.js

--- spencerRoute/route.js
+++ spencerRoute/route.js
@@ -8,4 +8,27 @@ router.get('/', (req, res) => {
 	res.send('Welcome to the group 22 database');
   });
 
+router.get('/Group22_Dining_Hall_Tracker', async (req, res) => {
+	try {
+
+		// GET request body
+
+	} catch (err) {
+		console.error(err);
+		res.error('Server error');
+	}
+});
   
+router.get('/Group22_Dining_Hall_Tracker/:schedule_id', async (req, res) => {
+	try {
+
+	  	// GET request body;
+		  
+	res.json(hour);
+	} catch (err) {
+		console.error(err);
+	  	res.error('Server error');
+	}
+});
+  
+export default router;

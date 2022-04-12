ddbd8df557c058ea391f3cdd6b60234a3d718cf9
ryessem7 <ryanessem7@gmail.com>
2022-04-12 16:29:33 -0400

    GET Contribution ryan

--- database/config.js
+++ database/config.js
@@ -2,21 +2,21 @@ export default {
   development: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'hispanic_restaurants',
+    database: 'hispanic_eats',
     host: '174.129.198.86',
     dialect: 'mysql'
   },
   test: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'hispanic_restaurants',
+    database: 'hispanic_eats',
     host: '174.129.198.86',
     dialect: 'mysql'
   },
   production: {
     username: 'ubuntu',
     password: 'Veracrypt@12!',
-    database: 'hispanic_restaurants',
+    database: 'hispanic_eats',
     host: '174.129.198.86',
     dialect: 'mysql'
   }

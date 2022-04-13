97d4cddebda1224d555d8fc5144857a56a97387c
jscoll <98493265+jscoll@users.noreply.github.com>
2022-04-13 16:21:30 -0400

    Add /genres API route with ID parameter

--- routes/judesRoutes.js
+++ routes/judesRoutes.js
@@ -7,16 +7,30 @@ import db from '../database/initializeDB.js';
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('Welcome to the UMD Dining API!');
+    res.send("Welcome to Jude's portion of Group 20's project API!");
 });
 
 router.get('/genres', async (req, res) => {
     try {
         const genres = await db.genres.findAll();
         res.json({ data: genres });
-    } catch (error) {
-        console.error(error);
-        res.send('Something went wrong.');
+    } catch (err) {
+        console.error(err);
+        res.send("Error in '/genres'!");
+    }
+});
+
+router.get('/genres/:genre_id', async (req, res) => {
+    try {
+        const genres = await db.genres.findAll({
+            where: {
+                genre_id: req.params.genre_id
+            }
+        });
+        res.json({ data: genres });
+    } catch (err) {
+        console.error(err);
+        res.error("Error in '/genres' or 'genre_id' is invalid!");
     }
 });
 

dafa2fdeb7784b425122f4c1d68bfb9d8f1d8da2
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2022-04-13 10:54:04 -0400

    added post method

--- routes/imagesRoutes.js
+++ routes/imagesRoutes.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import validator from 'validator';
 import db from '../database/initializeDB.js';
 
 const router = express.Router();
@@ -11,6 +12,25 @@ router.route('/')
       res.json(reply);
     } catch (err) {
       res.json('Server error');
+    }
+  })
+  .post(async (req, res) => {
+    const images = await db.Images.findAll();
+    const currentId = (await images.length) + 1;
+    const imageURL = req.body.image_url;
+    if (validator.isURL(imageURL)
+      && ['jpg', 'png', 'jpeg', 'img', 'flixster'].some((str) => imageURL.includes(str))) {
+      try {
+        await db.Images.create({
+          image_id: currentId,
+          image_url: req.body.image_url
+        });
+        res.send('Successfully added');
+      } catch (err) {
+        res.send('Server error');
+      }
+    } else {
+      res.send('Please enter a valid image URL ending in .jpg, .png, or .jpeg');
     }
   });
 

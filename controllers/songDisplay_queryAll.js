a904be7fdec75263bf833486b234c4c003056910
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-04-12 20:37:10 -0400

    Added GET requests with and without ID

---
+++ controllers/songDisplay_queryAll.js
@@ -0,0 +1,1 @@
+export default `SELECT tr.track_name, pll.name, ou.company_name FROM tracks AS tr JOIN music AS mu ON tr.track_id = mu.track_id JOIN playlist AS pl ON mu.music_id = pl.music_id JOIN playlist_ledger AS pll ON pl.p_list_id = pll.playlist_track_id JOIN station AS st ON pll.playlist_track_id = st.playlist_ledg_id JOIN outlet AS ou ON st.outlet_id = ou.outlet_id`;
--- routes/songDisplayRoute.js
+++ routes/songDisplayRoute.js
@@ -5,23 +5,54 @@ import fetch from 'node-fetch';
 
 const router = express.Router();
 
+import db from '../database/initializeDB.js';
+import songDisplayQuery from '../controllers/songDisplay_query.js';
+import songDisplayQueryAll from '../controllers/songDisplay_queryAll.js';
+
 //router.get('/', (req, res) => {
 //    console.log('You touched the Song Display Route.');
 //    res.json({message: 'Welcome to the Group 4 Spotify Database.'});
 //});
 
 router.route('/')
-.get(async (req, res) => {
+  .get(async (req, res) => {
+    try {
+      console.log('Touched songDisplayRoute get');
+      const result = await db.sequelizeDB.query(songDisplayQueryAll, {
+        type: sequelize.QueryTypes.SELECT
+      });
+      res.json({data: result});
+    } catch (error) {
+      console.log('Touched songDisplayRoute get error', error);
+      res.json({message: 'error in songDisplayRoute'});
+    }
+  });
 
-})
-.post((req, res) => {
+router.route('/Implant/')
+  .get(async (req, res) => {
+    try {
+      console.log('Touched songDisplayRoute get');
+      const result = await db.sequelizeDB.query(songDisplayQuery, {
+        type: sequelize.QueryTypes.SELECT
+      });
+      res.json({data: result});
+    } catch (error) {
+      console.log('Touched songDisplayRoute get error', error);
+      res.json({message: 'error in songDisplayRoute'});
+    }
+  })
 
-})
-.put((req, res) => {
 
-})
-.delete((req, res) => {
 
-});
+
+  .post((req, res) => {
+
+  })
+  .put((req, res) => {
+
+  })
+  .delete((req, res) => {
+
+  });
 
 export default router;

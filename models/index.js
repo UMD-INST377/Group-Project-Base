2ea3c3a0b011829ba55d8dfafbfbd2c07e2c71cb
racesaunders <race.saunders@gmail.com>
2022-04-13 19:12:58 -0400

    add parks model update database config

--- models/index.js
+++ models/index.js
@@ -2,10 +2,12 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
+import Parks from './Parks.js';
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
-  Meals
+  Meals, 
+  Parks,
 };

89a3a016f5116d8ea7ce1bbd60415755e498c824
ryan-lashuk <rlashuk5@terpmail.umd.edu>
2022-04-12 20:04:14 -0400

    ryan

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ import DietaryRestrictions from './DietaryRestrictions.js';
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
-import advisors from './advisors.js';
+import Advisors from './advisors.js';
 import careerServices from './careerServices.js';
 
 export default {
@@ -10,6 +10,6 @@ export default {
   DiningHall,
   Macros,
   Meals,
-  advisors,
+  Advisors,
   careerServices
 };

151250dd3b0be6a3347be68268947d75427599a1
prozario1 <44505881+prozario1@users.noreply.github.com>
2022-04-13 14:10:36 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,12 +1,22 @@
 import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
-import Artist from './artists.js';
 
+import artists from './artists.js';
+import artwork from './artwork.js';
+import country from './country.js';
+import customer from './customer.js';
+import galleries from './galleries.js';
+import genres from './genres.js';
+import reservation from './reservation.js';
 
 export default {
-  Artist,
-  DiningHall,
-  Macros,
-  Meals
+  artists,
+  artwork,
+  country,
+  customer,
+  galleries,
+  genres,
+  reservation
+
 };

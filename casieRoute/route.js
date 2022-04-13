90acc90ef90bc0eab6ccea71a974f4e8737bafe9
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-04-13 19:03:58 -0400

    Update route.js

--- casieRoute/route.js
+++ casieRoute/route.js
@@ -1,1 +1,22 @@
-//
+import express from 'express';
+import Sequelize from 'sequelize';
+import db from '../database/initializeDB.js';
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Welcome to the group 22 database');
+});
+
+router.get('/Group22_Dining_Hall_Tracker', async (req, res) => {
+  try {
+    const halls = await db.DiningHall.findAll();
+    const reply = halls.length > 0 ? { data: halls } : { message: 'no dining hall found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+export default router;

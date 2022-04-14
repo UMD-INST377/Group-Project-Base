3e38bff76c4c090dad53a01e6425530254ed00be
vezfly <vez@terpmail.umd.edu>
2022-04-14 16:52:05 -0400

    fix vez error

--- controller/dietaryrestrictions_query.js
+++ controller/dietaryrestrictions_query.js
@@ -1,2 +1,2 @@
 /*added link to comment*/
-export default 'SELECT * FROM Dietary_Restrictions';
+export default 'SELECT * FROM dietary_restrictions';
--- routes/vezRoutes.js
+++ routes/vezRoutes.js
@@ -2,19 +2,21 @@ import express from 'express';
 import sequelize from 'sequelize';
 
 import db from '../database/initializeDB.js';
-import restrictionsQuery from '../controller/dietaryrestrictions_query.js';
+import dietary_restrictionsQuery from '../controller/dietaryrestrictions_query.js';
 
 const router = express.Router();
 
 router.route('/')
   .get(async (req, res) => {
     try {
-      const result = await db.sequelizeDB.query(restrictionsQuery, {
+      console.log('Touched sqlDemo get');
+      const result = await db.sequelizeDB.query(dietary_restrictionsQuery, {
         type: sequelize.QueryTypes.SELECT
       });
       res.json({ data: result});
     } catch (error) {
-      console.log('error', error);
+      console.log('Touched sqlDemo get', error);
+      res.json({message: 'error in sqlDemo'});
     }
   });
 

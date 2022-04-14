25c1efe2f626af8ed408a935070da5e662a10f5a
Kevin Monsefu <46975253+KevMonsefu@users.noreply.github.com>
2022-04-14 04:52:43 -0400

    .

---
+++ models/Crew.js
@@ -0,0 +1,30 @@
+let newDate = new Date (2019, 3, 51)
+export default (database, DataTypes) => {
+  const crew = database.define('crew', {
+    crew_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    fname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lname: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    birthyear: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: newDate
+    },
+    deathyear: {
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: newDate
+    }
+  });
+  return crew
+};

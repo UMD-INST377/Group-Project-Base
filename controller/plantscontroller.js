d7fb685828267d1cd0412fb6725b970efaafc2c2
ZehraPatwary1 <98408063+ZehraPatwary1@users.noreply.github.com>
2022-04-14 11:58:21 -0400

    fixed id

--- controller/plantscontroller.js
+++ controller/plantscontroller.js
@@ -3,13 +3,13 @@ FROM plants`;
 
 const plantsPut = `UPDATE plants
 SET plants = :plants
-WHERE fruit_id = :fruit_id;`;
+WHERE plant_id = :plant_id;`;
 
 const plantsPost = `INSERT INTO plants (plant_id, botanical_name, common_name, origin, growth_habit, max_height, max_width, duration, family_id)
 VALUES(DEFAULT, :plants);`
 
 const plantsDelete = `DELETE FROM plants
-WHERE fruit_id = :fruit_id;`
+WHERE plant_id = :plant_id;`
 
 export default {
     plantsGet, plantsPut, plantsPost, plantsDelete

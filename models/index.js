d56a7766e25399f23f47a78c6e8c5f3092067aec
Gerson <80487990+groldan22@users.noreply.github.com>
2022-04-13 23:42:47 -0400

    my contribution
    contribution

--- models/index.js
+++ models/index.js
@@ -3,11 +3,13 @@
 // import Macros from './Macros.js';
 // import Meals from './Meals.js';
 import Actor from './Actors.js';
+import Titles from './Titles.js';
 
 export default {
   // DietaryRestrictions,
   // DiningHall,
   // Macros,
   // Meals,
-  Actor
+  Actor,
+  Titles,
 };

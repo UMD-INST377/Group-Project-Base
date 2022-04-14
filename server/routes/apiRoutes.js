0bd58b2d145b122b14ad89817e87388b1cb6d365
racesaunders <race.saunders@gmail.com>
2022-04-13 23:31:58 -0400

    working again

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import sequelize from 'sequelize';
 import chalk from 'chalk';
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 import db from '../../database/initializeDB.js';
 

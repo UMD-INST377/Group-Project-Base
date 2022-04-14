ef9c8377890bdd8fba9d0e0bda89df8a6874452f
vezfly <vez@terpmail.umd.edu>
2022-04-14 15:28:52 -0400

    "lockfileVersion": 2,   "lockfileVersion": 1,

--- routes/vezRoutes.js
+++ routes/vezRoutes.js
@@ -2,14 +2,14 @@ import express from 'express';
 import sequelize from 'sequelize';
 
 import db from '../database/initializeDB.js';
-import dietaryRestrictionsQuery from '../controller/dietaryrestrictions_query.js';
+import dietaryrestrictionsQuery from '../controller/dietaryrestrictions_query.js';
 
 const router = express.Router();
 
 router.route('/')
   .get(async(req, res) => {
     try {
-      const result = await db.sequelizeDB.query(dietaryRestrictionsQuery, {
+      const result = await db.sequelizeDB.query(dietaryrestrictionsQuery, {
         type: sequelize.QueryTypes.SELECT
       });
       res.json({data : result});

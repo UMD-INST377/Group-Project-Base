f9b240c81146575410a1b1278510ae936ce355b9
grodriUMD <87157811+grodriUMD@users.noreply.github.com>
2022-04-13 23:57:01 -0400

    uhhhh is this it? (remove dotenv param)

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 const staticFolder = 'client';
-const envConfig = dotenv.config().parsed?.NODE_ENV;
+const envConfig = 'production';
 let liveReloadServer;
 
 // console.log(a);

559b97e6c0280bf150f9c48681606d2ec4606c32
racesaunders <race.saunders@gmail.com>
2022-04-13 23:34:34 -0400

    removed unnecessary models

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use('/api', anneRoutes);
 
 async function bootServer() {
   try {
-    // const mysql = await db.sequelizeDB;
+    const mysql = await db.sequelizeDB;
     // await mysql.sync();
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);

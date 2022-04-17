d172583d8952e9a95370e6e00c01ac06b9cda3a0
DevonMilley <dmilley@terpmail.umd.edu>
2022-04-17 15:55:01 -0400

    dm routes

--- server/routes/dmRoutes.js
+++ server/routes/dmRoutes.js
@@ -4,4 +4,35 @@ import express from 'express';
 import sequelize from 'sequelize';
 import db from '../database/initializeDB.js';
 
+const foodInfoQuery = 'SELECT * FROM macros';
+
 const router = express.Router();
+
+// Get food information
+router.route('/').get(async (req, res) => {
+    try {
+      const foodInfo = await db.sequelizeDB.query(foodInfoQuery, {
+        type: sequelize.QueryTypes.SELECT
+      });
+      res.json(foodInfo);
+    } catch (err) {
+      console.error(err);
+      res.json({message: 'Error'});
+    }
+  });
+
+// Get food information with meal_id
+router.get('/:meal_id', async (req, res) => {
+    const diningHallIDQuery = `SELECT * FROM macros WHERE meal_id =${req.params.meal_id}`;
+    try {
+      const foodInfo = await db.sequelizeDB.query(foodInfoQuery, {
+        type: sequelize.QueryTypes.SELECT
+      });
+      res.json(foodInfo);
+    } catch (err) {
+      console.error(err);
+      res.json({message: 'Error'});
+    }
+  });
+
+export default router;

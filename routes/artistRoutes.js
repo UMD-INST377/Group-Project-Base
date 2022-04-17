78bce9def8287c6f00d446253c23f988a1e47216
tokyodrift7 <mkwakye99@gmail.com>
2022-04-17 18:45:58 -0500

    Update artistRoutes.js

--- routes/artistRoutes.js
+++ routes/artistRoutes.js
@@ -1,4 +1,7 @@
+<<<<<<< Updated upstream
 /* eslint-disable no-console */
+=======
+>>>>>>> Stashed changes
 import express from 'express';
 import sequelize from 'sequelize';
 
@@ -9,6 +12,7 @@ const router = express.Router();
 // const songsQuery = `SELECT * FROM songs.artist;`;
 
 router.route('/artist').get(async (req, res) => {
+<<<<<<< Updated upstream
     try {
       const art = await db.Artist.findAll();
       res.json(art);
@@ -34,3 +38,80 @@ router.route('/artist').get(async (req, res) => {
   });
 
 export default router;
+=======
+  try {
+    const art = await db.Artist.findAll();
+    res.json({ data: art });
+  } catch (error) {
+    console.error(error);
+    res.send('Server Error');
+  }
+});
+
+router.get('/artist/:artist_id', async (req, res) => {
+  try {
+    const art = await db.Artist.findAll({
+      where: {
+        artist_id: req.params.artist_id,
+      },
+    });
+    res.json({ data: art });
+  } catch (error) {
+    console.error(error);
+    res.send('Server Error');
+  }
+});
+
+router.post('/artist', async (req, res) => {
+  try {
+    const newArtist = await db.Artist.create({
+      artist_id: 19,
+      label_id: 4,
+      stage_name: 'Kehlani',
+      first_name: 'Kehlani',
+      last_name: 'Parrish',
+      gender: 'Female',
+      age: '26'
+    });
+    res.json(newArtist)
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error');
+  }
+});
+
+router.put('/artist/', async (req, res) => {
+  try {
+    const artistUpdate = await db.Artist.update(
+      {
+        artist_id: req.body.artist_id,
+        stage_name: req.stage_name
+      },
+      {
+        where: {
+          artist_id: req.body.artist_id
+        }
+      });
+    res.json('Success. Record Updated')
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error');
+  }
+});
+
+router.delete('/artist/:artist_id', async (req, res) => {
+  try {
+    const artistDelete = await db.Artist.destroy({
+      where: {
+        artist_id: req.params.artist_id
+      }
+    });
+    res.json('Success. Record Deleted')
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error');
+  }
+});
+
+export default router;
+>>>>>>> Stashed changes

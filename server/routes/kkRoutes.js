28bc24df8b0a694bfaf2023e1ff6bfa794044cb8
KrehlK <krehlkasayan@gmail.com>
2022-04-18 10:34:46 -0400

    added put method

--- server/routes/kkRoutes.js
+++ server/routes/kkRoutes.js
@@ -4,7 +4,7 @@ import express from 'express';
 import sequelize from 'sequelize';
 import db from '../database/initializeDB.js';
 
-const foodRestrictions = 'select * from meal_restrictions';
+const foodRestrictions = `select * from meal_restrictions`;
 const router = express.Router();
 
 //get restrictions
@@ -22,7 +22,7 @@ router.route('/').get(async (req, res) => {
 
 // search for specific allergen
 router.route('/:restriction_id').get(async (req, res) => {
-  const restrictionQuery = 'SELECT * FROM meal_restrictions WHERE restriction_id =${req.params.restriction)_id}';
+  const restrictionQuery = `SELECT * FROM meal_restrictions WHERE restriction_id =${req.params.restriction)_id}`;
   try {
     const restrictions = await db.sequelizeDB.query(foodRestrictions, {
       type: sequelize.QueryTypes.SELECT
@@ -36,6 +36,27 @@ router.route('/:restriction_id').get(async (req, res) => {
 
 
 //update
+router.put('/update', async (req, res) => {
 
+  const updateQuery =`
+    UPDATE dietary_restrictions 
+    SET restriction_id =${req.query['new_restriction_id']}'
+    where restriction_ = ${req.query['']}
+
+  `;
+
+  try {
+
+    const result = await db.sequelizeDB.query(updateQuery, {
+
+      type: sequelize.QueryTypes.UPDATE
+    });
+
+    res.json(`Updated the row entry with the new value: ${req.query['new_entry']}`);
+  } catch(err;){
+    console.log(' the following has occured' + e);
+    res.send('result could not be fulfilled because of' + e);
+  }
+})
 
 export default router;

d08c497104b4da77115125f7428ad0f43b140090
agyarai <agyarai3109@gmail.com>
2022-04-18 18:12:24 -0400

    post and put created

--- routes/agyaRoutes.js
+++ routes/agyaRoutes.js
@@ -30,8 +30,49 @@ router.get('/roles/:role_id', async (req, res) => {
         res.json({ data: roles });
     } catch (err) {
         console.error(err);
-        res.error("Error in '/roles' or 'roles_id' is invalid!");
+        res.error("Error in '/roles' or 'role_id' is invalid!");
     }
 });
 
 export default router;
+
+/*  Write Interactive Controllers For Your Route: ```  */
+/*  role_id, actor_id, movie_id, role    */
+router.post("/roles", async (req, res) => {
+    try {
+        const roles = await db.sequelizeDB.query(
+            `SELECT * FROM roles WHERE role LIKE '%${req.body.role}%'`
+        );
+        res.send({ data: roles[0] });
+    } catch (error) {
+        console.error(error);
+        res.send("Server error");
+    }
+});
+
+router.put("/roles", async (req, res) => {
+    try {
+        const roles = await db.sequelizeDB.query(
+            `SELECT * FROM roles WHERE role_id = ${req.body.role_id}`
+        );
+        res.send({ data: roles[0] });
+    } catch (error) {
+        console.error(error);
+        res.send("Server error");
+    }
+});
+
+// Using GET for required DELETE request so it can be used from HTML 5 forms
+router.get('/delete_roles/:role_id', async (req, res) => {
+    try {
+        const roles = await db.roles.destroy({
+            where: {
+                role_id: req.params.role_id
+            }
+        });
+        res.send('Successfully deleted!');
+    } catch (err) {
+        console.error(err);
+        res.error("Error in GET '/roles' or 'roles_id' is invalid!");
+    }
+});

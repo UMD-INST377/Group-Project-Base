c1e42cc815bf81880b1bc0c7347e70c05d55b620
Ian Winslow <41134386+EeyanW@users.noreply.github.com>
2022-04-17 21:33:30 -0400

    Team Project IC 2
    single id value (string) for the 'address' and all other corresponding variables. The 'address' object is a 7-variable object as follows:
    address_id: Integer
    address_1: String
    address_2: String
    city: String
    state: String
    zip_code: String
    restaurant_id: String

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -69,6 +69,63 @@ router.get('/address/:city', async (req, res) => {
   }
 });
 
+// POST 
+router.post('/address', async (req, res) => {
+  const addresses = await db.address.findAll();
+  const currentId = (await addresses.length) + 1;
+  try {
+    const newAddress = await db.address.create({
+      address_id: currentId,
+      address_1: req.body.address_1,
+      address_2: req.body.address_2,
+      city: req.body.city,
+      state: req.body.state,
+      zipe_code_id: req.body.zipe_code_id, 
+      restaurant_id: req.body.restaurant_id
+    });
+    res.json(newAddress);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+// PUT
+router.put('/address', async (req, res) => {
+  try {
+    await db.address.update(
+      {
+        address_1: req.body.address_1,
+        city: req.body.city
+      },
+      {
+        where: {
+          address_id: req.body.address_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+// DELETE
+router.delete('/address/:address_id', async (req, res) => {
+  try {
+    await db.address.destroy({
+      where: {
+        address_id: req.params.address_id
+      }
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
 // Trieuduong GET requests 
 // Endpoint 1
 router.get('/meals', async (req, res) => {

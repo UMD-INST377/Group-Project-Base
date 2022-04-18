a4480c887592e04c697359c09df81ea7b2ad1077
ryan-lashuk <rlashuk5@terpmail.umd.edu>
2022-04-17 22:08:18 -0400

    students

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -117,6 +117,58 @@ router.get('/students/:id', async (req, res) => {
   }
 });
 
+router.post('/students', async (req, res) => {
+  const halls = await db.ischool.findAll();
+  const currentId = (await halls.length) + 1;
+  try {
+    const newDining = await db.ischool.create({
+      hall_id: currentId,
+      hall_name: req.body.hall_name,
+      hall_address: req.body.hall_address,
+      hall_lat: req.body.hall_lat,
+      hall_long: req.body.hall_long,
+    });
+    res.json(newDining);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+/*router.delete('/dining/:hall_id', async (req, res) => {
+  try {
+    await db.DiningHall.destroy({
+      where: {
+        hall_id: req.params.hall_id,
+      },
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/dining', async (req, res) => {
+  try {
+    await db.DiningHall.update(
+      {
+        hall_name: req.body.hall_name,
+        hall_location: req.body.hall_location,
+      },
+      {
+        where: {
+          hall_id: req.body.hall_id,
+        },
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});*/
+
 /// //////////////////////////////////////////
 /// //////////Job Title Info Endpoints////////
 /// //////////////////////////////////////////

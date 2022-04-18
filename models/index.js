32e33e81e6d456fd7b9586698ebf49c6ca873bda
ryan-lashuk <rlashuk5@terpmail.umd.edu>
2022-04-17 21:31:31 -0400

    ;

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@ import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
 import careerServices from './careerServices.js';
-import jobTitleCompany from './jobTitleCompany.js'
+import jobTitleCompany from './jobTitleCompany.js';
 import company from './company.js';
 import job from './job_title_info.js';
 

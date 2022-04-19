71f9651086bea98f6d481060cc244c98d06fb320
lkillea <64662431+lkillea@users.noreply.github.com>
2022-04-19 18:13:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static(staticFolder));
-app.use('/api', apiRoutes);
+//app.use('/api', apiRoutes);
 app.use('/api', AlbumsRoutes);
 
 async function bootServer() {

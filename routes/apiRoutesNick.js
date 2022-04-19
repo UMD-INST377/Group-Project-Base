388a387fb9927df2a4bbb9802899d47510abe3f3
TempestIndra <43696183+TempestIndra@users.noreply.github.com>
2022-04-19 16:16:43 -0400

    second part of API

--- routes/apiRoutesNick.js
+++ routes/apiRoutesNick.js
@@ -17,6 +17,14 @@ router.route('/').get(async (req, res) => {
   }
 });
 
-
+router.get('/:cuisine_id', async (req, res) => {
+  const Query = `SELECT * FROM cuisine WHERE cuisine_id = ${req.cuisine_id}`;
+  try {
+    const cuisine = await db.sequelizeDB.query(Query);
+    res.json(cuisine);
+  } catch (err) {
+    res.json({message: err});
+  }
+});
 
 export default router;

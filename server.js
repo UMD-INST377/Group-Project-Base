eab81d6b1071032af69c68537332688b6e153743
lkillea <64662431+lkillea@users.noreply.github.com>
2022-04-19 14:39:06 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-console */
 import express from 'express';
 import db from './database/initializeDB.js';
+import Album from './models/Album.js';
 import apiRoutes from './routes/apiRoutes.js';
 
 const app = express();
@@ -13,6 +14,7 @@ app.use(express.json());
 
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
+app.use('/api', AlbumsRoutes);
 
 async function bootServer() {
   try {

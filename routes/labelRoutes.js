4e4bade95fdfc6d431f63f6db5362583a0fa618f
patrick-jun9 <98505923+patrick-jun9@users.noreply.github.com>
2022-04-19 13:53:09 -0400

    change of routes to have interactive controllers

--- routes/labelRoutes.js
+++ routes/labelRoutes.js
@@ -29,3 +29,51 @@ router.get('/label/:label_id', async (req, res) => {
   }
 });
 export default router;
+
+router.post('/label', async (req, res) => {
+  const labels = await db.label.findAll();
+  const currentId = (await labels.length) + 1;
+  try {
+    const newLabels = await db.label.create({
+      label_id: currentId,
+      label_name: req.body.label_name,
+    });
+    res.json(newLabels);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.put('/label', async (req, res) => {
+  try {
+    await db.label.update(
+      {
+        label_name: req.body.label_name,
+      },
+      {
+        where: {
+          label_id: req.body.label_id
+        }
+      }
+    );
+    res.send('Successfully Updated');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.delete('/label/:label_id', async (req, res) => {
+  try {
+    await db.label.destroy({
+      where: {
+        label_id: req.params.label_id
+      }
+    });
+    res.send('Successfully Deleted');
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});

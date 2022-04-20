d49531df4c02574e57ec96d492c5247f7f771482
Vara <varanikasharma1@gmail.com>
2022-04-20 11:25:34 -0400

    commit

--- client/public/script.js
+++ client/public/script.js
@@ -11,10 +11,24 @@ async function populateGenres() {
     }
   });
 }
+async function populateRatings() {
+  const dropdwn = document.querySelector('#ratings-input');
+  const RatingResult = await fetch('/api/ratings');
+  const arrayFromJson = await RatingResult.json();
+  arrayFromJson.data.forEach((item) => {
+    if (item.rating !== 'Other') {
+      const RatingOption = document.createElement('option');
+      option.text = item.rating;
+      option.value = item.rating;
+      dropdwn.appendChild(option);
+    }
+  })
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
   // populate genres dropdown
   await populateGenres();
+  await populateRatings();
 
   // select dropdowns
   M.Dropdown.init(document.querySelectorAll('.dropdown-trigger'));

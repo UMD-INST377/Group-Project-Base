c040020d05bd09751e664520d4e0f184bd485cc5
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2022-04-21 17:47:38 -0400

    Update apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-// import sequelize from 'sequelize';
-
+import sequelize from 'sequelize';
+import db from '../database/initializeDB.js';
 import availability from './availabilityRoutes.js';
 import genres from './genreRoutes.js';
 import images from './imagesRoutes.js';
@@ -21,4 +21,50 @@ router.use('/languages', languages);
 router.use('/movies', movies);
 router.use('/', ratings);
 
+<<<<<<< Updated upstream
 export default router;
+=======
+const tableQuery = `select movie_id, title, year, genre, language as 'lang', 
+rating, image_url, is_on_netflix, is_on_hulu, is_on_prime, is_on_disney from movies
+  left join images using(image_id)
+  left join availability using(availability_id)
+  left join genres using(genre_id)
+  left join ratings using(rating_id)
+  left join languages using(language_id)
+order by movie_id`;
+
+router.route('/table/data').get(async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(`${tableQuery};`, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    res.send('Error');
+  }
+});
+
+router.route('/display').get(async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(`${tableQuery} limit 8;`, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    res.send('Error');
+  }
+});
+
+router.route('/custom').get(async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(req.body.query, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json(result);
+  } catch (err) {
+    res.send('Error');
+  }
+});
+
+export default router;
+>>>>>>> Stashed changes

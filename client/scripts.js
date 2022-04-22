4a6aa26fa575586032cd27bbe58e5dba4886ac4b
loranikasharma <sharma_loranika@yahoo.com>
2022-04-22 14:24:33 -0400

    commit

--- client/scripts.js
+++ client/scripts.js
@@ -1,17 +1,14 @@
-/* eslint-disable no-plusplus */
-/* eslint-disable camelcase */
-/* eslint-disable no-unused-vars */
-async function mainEvent() { // the async keyword means we can make API requests
-  // const form = document.querySelector('.');
-  // const submit = document.querySelector('.form-rows');
-  const results = await fetch('/api/artists'); // This accesses some data from our API
-  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-  console.table(arrayFromJson);
-  if (arrayFromJson.data.length > 0) {
-    submit.style.display = 'block';
-    form.addEventListener('submit', async (submitEvent) => {
-      submitEvent.preventDefault();
-      console.log('form submission');
-    });
-  }
+async function loadIntoTable(url) {
+  const reponse = await fetch(url);
+  const arrayFromJson = await reponse.json(); // This changes it into data we can use - an object
+  const tableFinder = document.querySelector('.table');
+  // creating the rows
+  const row = document.createElement('tr');
+  arrayFromJson.forEach((element) => {
+    row.innerHTML = `<td>`
+
+   
+  });
+  tableFinder.appendChild(row);
 }
+loadIntoTable('/api/artists');

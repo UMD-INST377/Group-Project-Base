a518f28ad161294061910e97fa33e1d8c3514bca
Stefan <sclarke2@terpmail.umd.edu>
2022-04-22 14:15:32 -0400

    ic2n

--- routes/stefRoutes.js
+++ routes/stefRoutes.js
@@ -38,7 +38,7 @@ router.get('/movies/:movie_id', async (req, res) => {
 router.post('/movies', async (req, res) => {
   try {
     const movies = await db.sequelizeDB.query(
-      `SELECT * FROM genres WHERE genre LIKE '%${req.body.movie_name}%'`
+      `SELECT * FROM movies WHERE movie LIKE '%${req.body.movie_name}%'`
     );
     res.send({ data: movies[0] });
   } catch (error) {

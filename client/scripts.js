fec2557e3fd3bb792ddc2ab68aa98e0f647b2fc7
loranikasharma <sharma_loranika@yahoo.com>
2022-04-22 16:07:27 -0400

    commit

--- client/scripts.js
+++ client/scripts.js
@@ -1,14 +1,28 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
 async function loadIntoTable(url) {
   const reponse = await fetch(url);
   const arrayFromJson = await reponse.json(); // This changes it into data we can use - an object
+  data = await arrayFromJson.data;
+  console.log(arrayFromJson);
   const tableFinder = document.querySelector('.table');
   // creating the rows
-  const row = document.createElement('tr');
-  arrayFromJson.forEach((element) => {
-    row.innerHTML = `<td>`
 
-   
+  data.forEach((element) => {
+    const row = document.createElement('tr');
+    const row_artist_id = document.createElement('td');
+    const row_first_name = document.createElement('td');
+    const row_last_name = document.createElement('td');
+    const row_country = document.createElement('td');
+    row_artist_id.innerHTML = element.artist_id;
+    row_first_name.innerHTML = element.first_name;
+    row_last_name.innerHTML = element.last_name;
+    row_country.innerHTML = element.country_id;
+    row.appendChild(row_artist_id);
+    row.appendChild(row_first_name);
+    row.appendChild(row_last_name);
+    row.appendChild(row_country);
+    tableFinder.appendChild(row);
   });
-  tableFinder.appendChild(row);
 }
 loadIntoTable('/api/artists');

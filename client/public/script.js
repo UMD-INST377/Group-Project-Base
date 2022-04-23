65bf4ac6f564ba0176588b70d53335bfdbb73bf3
Vara <varanikasharma1@gmail.com>
2022-04-22 22:39:38 -0400

    commit

--- client/public/script.js
+++ client/public/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 async function populateGenres() {
   const dropdown = document.querySelector('#genres-input');
   const results = await fetch('/api/genres');
@@ -22,9 +23,41 @@ async function populateRatings() {
       option.value = item.rating;
       dropdwn.appendChild(option);
     }
-  })
+  });
 }
-
+function createHtmlList(collection) {
+  // console.log('created HTML creator');
+  // console.log(collection);
+  const targetList = document.querySelector('#movies-list');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {name} = item;
+    const displayName = name.toLowerCase();
+    const injectThis = `<li>${item.name}</li>`;
+    targetList.innerHTML += injectThis;
+  });
+}
+async function loadTable(url) {
+  const res = await fetch(url);
+  const arrayFromJson = await res.json();
+  data = await arrayFromJson.data;
+  console.log(arrayFromJson);
+  const tableFinder = document.querySelector('table');
+  data.forEach((element) => {
+    const row = document.createElement('tr');
+    const row_rating_id = document.createElement('td');
+    const row_rating = document.createElement('td');
+    const row_description = document.createElement('td');
+    row_rating_id.innerHTML = element.rating_id;
+    row_rating.innerHTML = element.rating;
+    row_description.innerHTML = element.description;
+    row.appendChild(row_rating_id);
+    row.appendChild(row_rating);
+    row.appendChild(row_description);
+    tableFinder.appendChild(row);
+  });
+}
+loadTable('/api/ratings');
 document.addEventListener('DOMContentLoaded', async () => {
   // populate genres dropdown
   await populateGenres();

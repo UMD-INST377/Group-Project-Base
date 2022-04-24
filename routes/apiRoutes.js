ee0536d96ea9fdcd96f424b6a26b2b916d2b3e62
sravvel <98434706+sravvel@users.noreply.github.com>
2022-04-24 14:29:02 -0400

    sravya's PUT, POST, DELETE and TA changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -52,7 +52,65 @@ router.get('/restaurants/:rest_id', async (req, res) => {
   }
 });
 
+///
+/// Sravya IC2 REQUESTS
+///
+// POST request RESTAURANTS
+router.post('/restaurants', async (req, res) => {
+  try {
+    const newRest = await db.restaurants.create({
+      restaurant_id: currentId,
+      restaurant_name: req.body.restaurant_name,
+      phone_number: req.body.phone_number,
+      price: req.body.price,
+      description: req.body.description,
+      website: req.body.website,
+      cuisine_id: req.body.cuisine_id,
+      rating_id: req.body.rating_id,
+      description_id: req.body.description_id
+    });
+    res.json(newRest);
+  } catch (err) {
+    console.error(err);
+    res.send('Server error');
+  }
+});
 
+// PUT request
+router.put('/restaurants', async (req, res) => {
+  try {
+    await db.restaurants.update(
+      {
+        restaurant_name: req.body.restaurant_name,
+        description: req.body.description
+      },
+      {
+        where: {
+          restaurant_id: req.body.restaurant_id
+        }
+      }
+    );
+    res.send('Successfully Updated Restaurant');
+  } catch (err) {
+    console.error(err);
+    res.send('Server error');
+  }
+});
+
+// DELETE request
+router.delete('/restaurant/:rest_id', async (req, res) => {
+  try {
+    await db.restaurants.destroy({
+      where: {
+        restaurant_id: req.params.restaurant_id
+      }
+    });
+    res.send('Successfully Deleted Restaurant id');
+  } catch (err) {
+    console.error(err);
+    res.send('Server error');
+  }
+});
 
 // Sana Hassan GET requests
 // endpoint 1

3b491e23e4c0802b3873a772882d70d6a7bfc0ef
yang <ylk01413@gmail.com>
2022-04-24 18:36:35 -0400

    results show but ugly

--- client/js/script.js
+++ client/js/script.js
@@ -1,3 +1,9 @@
+function getRandomIntInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
+
 function restArrayMake(dataArray) {
   const range = [...Array(15).keys()];
   const listItems = range.map((item, index) => {
@@ -8,73 +14,57 @@ function restArrayMake(dataArray) {
 }
 
 function createHtmlList(collection) {
-  const targetList = document.querySelector('.restList');
-  targetList.innerHTML = '';
+  const targetList = document.querySelector(".result_list");
+  targetList.innerHTML = "";
   collection.forEach((item) => {
-    const { name } = item;
-    const displayName = name.toLowerCase();
-    const injectThis = `<li>${displayName}</li>`;
+    const { album_name } = item;
+    const { artist_id } = item;
+    const displayName = album_name.toLowerCase();
+    const displayCity = artist_id;
+    const injectThis = `<li>Album Name: ${displayName}</li>`;
+    const injectThisCity = `<li>Album Artist: ${displayCity}</li>`;
     targetList.innerHTML += injectThis;
+    targetList.innerHTML += injectThisCity;
   });
 }
 async function mainEvent() {
   // the async keyword means we can make API requests
-  const form = document.querySelector('#results');
-  const submitButton = document.querySelector('#submit_button');
-  const searchbar = document.querySelector('#searchbar');
-  const retrevialVar = 'restaurants';
+  const form = document.querySelector("#results");
+  const submitButton = document.querySelector("#submit_button");
+  const restName = document.querySelector("#init_search");
 
-  refreshList(refresh, retrevialVar);
+  const results = await fetch("/api/albums"); // This accesses some data from our API
+  const arrayFromJson = await results.json(); // This changes it into data we can use - an object
 
-  if (localStorage.getItem(retrevialVar) === undefined) {
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    localStorage.setItem(retrevialVar, JSON.stringify(arrayFromJson.data));
-  }
-  const storedData = localStorage.getItem(retrevialVar);
-  const storedDataArray = JSON.parse(storedData);
-  if (storedDataArray.length > 0) {
-    submitButton.style.display = 'block';
-    console.log('start');
+  if (arrayFromJson.data.length > 0) {
+    submitButton.style.display = "block";
+    console.log("start");
 
     let currentArray = [];
-    searchbar.addEventListener('input', async (event) => {
+    restName.addEventListener("input", async (event) => {
       console.log(event.target.value);
       if (event.length < 1) {
-        console.log('caught');
+        console.log("caught");
         return;
       }
-
-      const selectRest = storedDataArray.filter((item) => {
-        const lowerName = item.name.toLowerCase();
+      // change arrayFromJson.data to currentArray if needed
+      const selectRest = arrayFromJson.data.filter((item) => {
+        const lowerName = item.album_name.toLowerCase();
         const lowerValue = event.target.value.toLowerCase();
         return lowerName.includes(lowerValue);
       });
       console.log(selectRest);
-      createHtmlList(selectRest);
-      addMapMarker(map, selectRest);
+      createHtmlList(restArrayMake(selectRest));
     });
 
-    city.addEventListener('input', async (event) => {
-      console.log(event.target.value);
-      const cityRest = storedDataArray.filter((item) => {
-        const lowerName = item.city.toLowerCase();
-        const lowerCity = event.target.value.toLowerCase();
-        return lowerName.includes(lowerCity);
-      });
-      console.log(cityRest);
-      createHtmlList(cityRest);
-      addMapMarker(map, cityRest);
-    });
-    form.addEventListener('submit', async (submitEvent) => {
+    form.addEventListener("submit", async (submitEvent) => {
       // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
-      console.log('form submission'); // this is substituting for a 'breakpoint'
-      currentArray = restArrayMake(storedDataArray);
+      console.log("form submission"); // this is substituting for a 'breakpoint'
+      currentArray = restArrayMake(arrayFromJson.data);
       createHtmlList(currentArray);
-      addMapMarker(map, currentArray);
     });
   }
 }
 // this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests

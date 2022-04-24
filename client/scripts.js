f0a7123ed8c21b04bc3ad5f9b3d1d4c5e4d93499
GeraldReyes00 <98285451+GeraldReyes00@users.noreply.github.com>
2022-04-24 02:46:24 -0400

    Attempt at adding event listener

--- client/scripts.js
+++ client/scripts.js
@@ -1,27 +1,30 @@
 function formToObject(htmlFormElement) {
-    const formItem = new FormData(htmlFormElement).entries();
-    const formArray = Array.from(formItem);
-    const formObject = formArray.reduce((collection, item, index) => {
+  const formItem = new FormData(htmlFormElement).entries();
+  const formArray = Array.from(formItem);
+  const formObject = formArray.reduce((collection, item, index) => {
     if (!collection[item[0]]) {
-        collection[item[0]] = item[1];
+      collection[item[0]] = item[1];
     }
     return collection;
-}, {});
-return formObject;
+  }, {});
+  return formObject;
 }
 
-form.addEventListner('submit_song', async (submitEvent) => {
-    submitEvent.preventDefault();
-    const formObj = formToObject(form);
-    console.log('check the form for filters', formObj);
-    const postResult = await fetch('/api/sqlDemo', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formObj)
-    });
-    const postResultJSON = await postResult.json()
-    console.log('return from Post', postResult)
-    console.log('return from Post JSON', postResultJSON)
-})
+const form = document.querySelector('submit_song');
+const submit = document.querySelector('submit_button');
+
+form.addEventListner('submit', async (submitEvent) => {
+  submitEvent.preventDefault();
+  const formObj = formToObject(form);
+  console.log('check the form for filters', formObj);
+  const postResult = await fetch('/api/sqlDemo', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(formObj)
+  });
+  const postResultJSON = await postResult.json();
+  console.log('return from Post', postResult);
+  console.log('return from Post JSON', postResultJSON);
+});

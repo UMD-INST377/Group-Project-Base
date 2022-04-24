a44db3f52698b8b3e63fc4d0128281a08884e9ed
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-04-24 18:50:18 -0400

    show data on webpage

--- client/scripts.js
+++ client/scripts.js
@@ -1,36 +1,3 @@
-/*
-function getRandomIntInclusive(min, max) {
-  const newMin = Math.ceil(min);
-  const newMax = Math.floor(max);
-  return Math.floor(
-    Math.random() * (newMax - newMin + 1) + newMin
-  );
-}
-
-function dataHandler(dataArray) {
-  console.log('fired dataHandler');
-  const range = [...Array(15).keys()];
-  const listItems = range.map((item, index) => {
-    const restNum = getRandomIntInclusive(0, dataArray.length - 1);
-    return dataArray[restNum];
-  });
-  return listItems;
-}
-*/
-
-function createHtmlList(collection) {
-  console.log('fired HTML creator');
-  console.log(collection);
-  const targetList = document.querySelector('.monday');
-  // console.log(targetList)
-  targetList.innerHTML = '';
-  collection.forEach((item) => {
-    const {name} = item;
-    const displayName = name;
-    targetList.innerHTML += displayName;
-  });
-}
-
 // Main function
 async function mainEvent() { // the async keyword means we can make API requests
   console.log('script loaded');
@@ -39,16 +6,9 @@ async function mainEvent() { // the async keyword means we can make API requests
   const results = await fetch('chandra/meals');
   const breakfastItems = await results.json();
   console.log(breakfastItems);
-  
-  breakfastItems.forEach((item) => {
-    // const {name} = item;
-    // const displayName = name;
-    targetList.innerHTML += item;
-  });
-  
-  if (breakfastItems.length > 0) {
-    createHtmlList(breakfastItems);
-  }
+
+  const dataJson = JSON.stringify(breakfastItems.data);
+  targetList.innerHTML = dataJson;
 }
 
 // this actually runs first! It's calling the function above

d5e15f9dcdb6f48fef338bf216a1bb34c22feba0
kevipun1234 <47250967+kevipun1234@users.noreply.github.com>
2022-04-24 18:51:51 -0400

    123

--- routes/languagesRoutes.js
+++ routes/languagesRoutes.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import db from '../database/initializeDB.js';
+import Languages from '../models/Languages.js';
+import sequelize from 'sequelize';
 
 const router = express.Router();
 
@@ -12,7 +14,37 @@ router.route('/')
     } catch (err) {
       res.json('Server error');
     }
-  });
+  })
+
+  .post(async (req, res) => {
+    // try {
+    //   const languageId = req.body.Languages || 0;
+    //   const result = await db.sequelizeDB.query(Languages, {
+    //     replacements: { language_id: languageId},
+    //     type: sequelize.QueryTypes.SELECT
+    //   });
+    //   res.json({data: result});
+    // } catch (err) {
+    //   console.log(err);
+    //   res.send({message: err});
+    // }
+    const languageId = await db.Languages.findAll();
+    const current = (await languageId.length) + 1;
+    try {
+    const newLanguage = await db.Languages.create({
+      language_id: current,
+      language: req.body.language,
+      description: req.body.description
+
+    });
+    res.send('languages added');
+    } catch (err) {
+    console.log(err);
+    console.log(current);
+    res.send(err);
+  }
+});
+  
 
 router.route('/:language_id')
   .get(async (req, res) => {

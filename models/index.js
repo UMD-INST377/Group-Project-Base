a6158233d3c67c36a1e7319da0c5ebdfd921aa12
lkillea <64662431+lkillea@users.noreply.github.com>
2022-04-24 17:40:20 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -3,11 +3,13 @@ import DiningHall from './DiningHall.js';
 import Macros from './Macros.js';
 import Meals from './Meals.js';
 import Artist from './Artist.js';
+import album from './Album.js';
 
 export default {
   DietaryRestrictions,
   DiningHall,
   Macros,
   Meals,
-  Artist
+  Artist,
+  album
 };

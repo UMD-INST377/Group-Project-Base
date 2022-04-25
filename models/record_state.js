98593787f347e493e335c94787895330b0dbbb57
rybo1217 <brownryan1217@yahoo.com>
2022-04-25 18:56:45 -0400

    ic2

--- models/record_state.js
+++ models/record_state.js
@@ -3,7 +3,7 @@ export default (sequelize, DataTypes) => {
     'record_state',
     {
       record_id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         allowNull: false,
         primaryKey: true
       },

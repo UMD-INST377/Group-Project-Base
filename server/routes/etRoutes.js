38e3c9dfb94e161d7e701d96b6c50e0711f4d200
ElaineT23 <elainetse94@gmail.com>
2022-04-25 00:05:53 -0400

    IC2 Controllers Start and adjustment to webpage htmls
    - Created post and update for dining
    - Need to create delete method
    - Issues running post and delete
    
    -Removed add restaurant page and changed it to restaurant info
    
    *Need to remove favorites html

--- server/routes/etRoutes.js
+++ server/routes/etRoutes.js
@@ -8,7 +8,43 @@ const diningHallQuery = 'SELECT * FROM dining_hall';
 
 const router = express.Router();
 
-// Get Dining Hall
+// update dining hall
+router.put('/update', async (req, res) => {
+  const updateQuery = `UPDATE dining_hall 
+    SET hall_name ='${req.query.new_dining_hall}
+    WHERE hall_id ='${req.query.hall_id};
+    `;
+
+  try {
+    const result = await db.sequelizeDB.query(updateQuery, {
+      type: sequelize.QueryTypes.UPDATE
+    });
+
+    res.json(`Updated row entry with new value: ${req.query.new_dining_hall}`);
+  } catch (err) {
+    console.error(err);
+    res.json({message: 'Error has occured'});
+  }
+});
+
+
+// post new dining hall entry in dining
+router.post('/diningpost', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(`INSERT INTO dining_hall (hall_id, hall_name, hall_address, hall_lat, hall_long)
+    VALUES (${req.body.hall_id}, '${req.body.hall_name}', '${req.body.hall_address}', ${req.body.hall_lat}, ${req.body.hall_long})`);
+  } catch (err) {
+    console.error(err);
+    res.json({message: 'Error has occured'});
+  }
+});
+
+// delete dining hall entry in dining
+// router.delete()
+
+
+
+// get dining halls
 router.route('/').get(async (req, res) => {
   try {
     const halls = await db.sequelizeDB.query(diningHallQuery, {
@@ -21,9 +57,9 @@ router.route('/').get(async (req, res) => {
   }
 });
 
-// Get Dining Hall with specific id
+// get dining hall with specific id
 router.get('/:hall_id', async (req, res) => {
-  const diningHallIDQuery = `SELECT * FROM dining_hall WHERE hall_id =${req.params.hall_id}`;
+  const diningHallIDQuery = `SELECT * FROM dining_hall WHERE hall_id = ${req.params.hall_id}`;
   try {
     const halls = await db.sequelizeDB.query(diningHallIDQuery, {
       type: sequelize.QueryTypes.SELECT

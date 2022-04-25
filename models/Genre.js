a43b1b0ded95bfd30271f1d37bf1679b74279114
Brandon Leer <bleer1281@gmail.com>
2022-04-24 21:48:57 -0400

    main

--- models/Genre.js
+++ models/Genre.js
@@ -1,16 +1,18 @@
 export default (database, DataTypes) => {
-    const genre = database.define(
-      'genre',
-      {
-        genre_id:
-            type: DataTypes.INTEGER,
-            allowNull: false
+  const genre = database.define(
+    'genre',
+    {
+      genre_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
       },
-        genre_name:
-            type: DataTypes.STRING,
-            allowNull: false
-      },
-      { freezeTableName: true, timestamps: false }
-    );
-    return genre;
-  }; 
+      genre_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
+    }, {
+      freezeTableName: true, timestamps: false
+    }
+  );
+  return genre;
+};

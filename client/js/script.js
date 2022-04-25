f1399b9065c403845c3e1f7aae15ec609d1fb8f7
Tayo Ojo <54911634+tayoojo@users.noreply.github.com>
2022-04-24 20:46:28 -0400

    Contribute Functionality
    Completed Functionality for Contribute

--- client/js/script.js
+++ client/js/script.js
@@ -1,80 +1,40 @@
-document.addEventListener('DOMContentLoaded', async () => {
-
-async function postFormDataAsJson({url, formData}) {
-    const plainFormData = Object.fromEntries(formData.entries());
-    const formDataJsonString = JSON.stringify(plainFormData);
-
-    const fetchOptions = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Accept': 'application/json',
-        },
-        body: formDataJsonString,
-    };
-
-    const response = await fetch(url, fetchOptions);
-
-    if (!response.ok) {
-        const errorMsg = await response.text();
-        throw new Error(errorMsg);
-    }
-    return response.json();
-}
-
-async function formSubmission(event) {
-    event.preventDefault();
-    const form = event.currentTarget;
-    const url = 'api/artist';
-
-    try {
-        const formData = new FormData(form);
-
-        const responseData = await postFormDataAsJson({url, formData});
-
-        console.log({responseData});
-    }
-    catch (error) {
-        console.error(error);
-    }
-}
-
-
-const contributeForm = document.querySelector('.form_block');
-contributeForm.addEventListener('submit', formSubmission);
-})
-
-
-/*
-const submitButton = document.querySelector('button[type="submit"]');
+const contributeForm = document.querySelector('#contribute_form');
 
 function formToObject(htmlFormElement) {
     const formItem = new FormData(htmlFormElement).entries();
     const formArray = Array.from(formItem);
     const formObject = formArray.reduce((collection, item, index) => {
-      if (!collection[item[0]]) {
+    if (!collection[item[0]]) {
         collection[item[0]] = item[1];
-      }
-      return collection;
+    }
+    return collection;
     }, {});
     return formObject;
-  }
+};
 
-  const formObj = formToObject(form);
+console.log(contributeForm)
 
-  let artist = {
-      first_name: formObj[0],
-      last_name: formObj[1]
-  };
+contributeForm.addEventListener('submit', async (submitEvent) => {
+    submitEvent.preventDefault();
+    console.log(submitEvent)
+    const formObj = formToObject(contributeForm);
 
-  const postResult = await fetch('/api/artist', {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(artist)
-  });
-  const postResultJSON = await postResult.json();
+    
+    const postResult = await fetch('api/artist', {
+        
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formObj)
 
-*/
+    
+    });
+    
+    const postResultJSON = await postResult.json();
+    console.log('return from POST', postResult);
+    console.log('return from POST JSON', postResultJSON)
+    
+
+});
 

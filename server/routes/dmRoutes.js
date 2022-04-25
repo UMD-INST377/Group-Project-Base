de26220d5a4d86fdf43de37121400352f31ca4ea
DevonMilley <dmilley@terpmail.umd.edu>
2022-04-25 14:50:26 -0400

    devon ic 2

--- server/routes/dmRoutes.js
+++ server/routes/dmRoutes.js
@@ -8,6 +8,53 @@ const foodInfoQuery = 'SELECT * FROM macros';
 
 const router = express.Router();
 
+// delete meal information record in macros
+router.delete('/delete', async (req, res) => {
+  const deleteQuery = 
+       `DELETE FROM macros WHERE meal_id = ${req.query['meal_id']};`;
+  try {
+      const result = await db.sequelizeDB.query(deleteQuery, {
+          type: sequelize.QueryTypes.DELETE
+      });
+
+      res.json(`Deleted row entry with meal_id:  ${req.query['meal_id']};`);
+    } catch (err) {
+      console.error(err);
+      res.json({message: 'Error has occured'});
+    }
+  });
+
+// update macros
+router.put('/update', async (req, res) => {
+  const updateQuery = `UPDATE macros 
+    SET calories ='${req.query.new_calories}
+    WHERE meal_id ='${req.query.meal_id};
+    `;
+
+  try {
+    const result = await db.sequelizeDB.query(updateQuery, {
+      type: sequelize.QueryTypes.UPDATE
+    });
+
+    res.json(`Updated row entry with new value: ${req.query.new_calories}`);
+  } catch (err) {
+    console.error(err);
+    res.json({message: 'Error has occured'});
+  }
+});
+
+
+// post new meal information entry in macros
+router.post('/post', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(`INSERT INTO macros (macro_id, calories, serving_size, cholesterol, sodium, carbs, protein, meal_id, fat)
+    VALUES (${req.body.macro_id}, '${req.body.calories}', '${req.body.serving_size}', ${req.body.cholesterol}, ${req.body.sodium}, '${req.body.carbs}', '${req.body.protein}', '${req.body.meal_id}', '${req.body.fat}')`);
+  } catch (err) {
+    console.error(err);
+    res.json({message: 'Error has occured'});
+  }
+});
+
 // Get food information
 router.route('/').get(async (req, res) => {
     try {

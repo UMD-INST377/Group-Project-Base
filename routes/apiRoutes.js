052e8cfada242d15a8019c267a5c734c51e9f4d0
yang <ylk01413@gmail.com>
2022-04-26 18:39:23 -0400

    top 10 genre graph

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,8 +5,8 @@ import express from 'express';
 import sequelize from 'sequelize';
 
 import db from '../database/initializeDB.js';
+import graphquery from './graphquery.js';
 import rawqueries from './rawqueries.js';
-
 
 const router = express.Router();
 
@@ -49,7 +49,7 @@ router.post('/artists', async (req, res) => {
   try {
     const newArtist = await db.artist.create({
       artist_id: currentId,
-      name: req.body.name,
+      name: req.body.name
     });
     res.json(newArtist);
   } catch (err) {
@@ -76,7 +76,7 @@ router.put('/artists', async (req, res) => {
   try {
     await db.artists.update(
       {
-        name: req.body.name,
+        name: req.body.name
       },
       {
         where: {
@@ -191,7 +191,7 @@ router.delete('/genres/:genre_name', async (req, res) => {
 
 router.get('/playlists', async (req, res) => {
   try {
-    const playlistItems = await db.playlists.findAll(); 
+    const playlistItems = await db.playlists.findAll();
     const reply = playlistItems.length > 0 ? { data: playlistItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -204,7 +204,7 @@ router.get('/playlists/:playlist_id', async (req, res) => {
   try {
     const hall = await db.playlists.findAll({
       where: {
-        playlist_id : req.params.playlist_id
+        playlist_id: req.params.playlist_id
       }
     });
 
@@ -292,7 +292,7 @@ router.get('/artist_albums/:artist_id', async (req, res) => {
   try {
     const artist_albums_items = await db.artistAlbums.findAll({
       where: {
-        artist_id : req.params.artist_id
+        artist_id: req.params.artist_id
       }
     });
 
@@ -358,7 +358,7 @@ router.delete('/artist_albums/:artist_id', async (req, res) => {
 
 router.get('/albums', async (req, res) => {
   try {
-    const albumItems = await db.albums.findAll(); 
+    const albumItems = await db.albums.findAll();
     const reply = albumItems.length > 0 ? { data: albumItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -371,7 +371,7 @@ router.get('/albums/:album_id', async (req, res) => {
   try {
     const albumItems = await db.albums.findAll({
       where: {
-        album_id : req.params.album_id
+        album_id: req.params.album_id
       }
     });
 
@@ -442,7 +442,7 @@ router.delete('/albums/:album_id', async (req, res) => {
 
 router.get('/album_songs', async (req, res) => {
   try {
-    const genreItems = await db.albumSongs.findAll(); 
+    const genreItems = await db.albumSongs.findAll();
     const reply = genreItems.length > 0 ? { data: genreItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -455,7 +455,7 @@ router.get('/album_songs/:album_id', async (req, res) => {
   try {
     const genreItems = await db.albumSongs.findAll({
       where: {
-        album_id : req.params.album_id
+        album_id: req.params.album_id
       }
     });
 
@@ -520,7 +520,7 @@ router.delete('/album_songs/:album_id', async (req, res) => {
 
 router.get('/artist_songs', async (req, res) => {
   try {
-    const styleItems = await db.artistSongs.findAll(); 
+    const styleItems = await db.artistSongs.findAll();
     const reply = styleItems.length > 0 ? { data: styleItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -533,7 +533,7 @@ router.get('/artist_songs/:artist_id', async (req, res) => {
   try {
     const styleItems = await db.artistSongs.findAll({
       where: {
-        artist_id : req.params.artist_id
+        artist_id: req.params.artist_id
       }
     });
 
@@ -596,7 +596,7 @@ router.delete('/artist_songs/:artist_id', async (req, res) => {
 /// /////////////////////////////////
 router.get('/song_genres', async (req, res) => {
   try {
-    const halls = await db.songGenres.findAll(); 
+    const halls = await db.songGenres.findAll();
     const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -609,7 +609,7 @@ router.get('/song_genres/:song_id', async (req, res) => {
   try {
     const hall = await db.songGenres.findAll({
       where: {
-        song_id : req.params.song_id
+        song_id: req.params.song_id
       }
     });
 
@@ -626,7 +626,7 @@ router.post('/song_genres', async (req, res) => {
   try {
     const newDining = await db.playlists.create({
       song_id: currentId,
-      genre_id: req.body.genre_id,
+      genre_id: req.body.genre_id
     });
     res.json(newDining);
   } catch (err) {
@@ -639,7 +639,7 @@ router.put('/song_genres', async (req, res) => {
   try {
     await db.songGenres.update(
       {
-        genre_id: req.body.genre_id,
+        genre_id: req.body.genre_id
       },
       {
         where: {
@@ -674,7 +674,7 @@ router.delete('/song_genres/:song_id', async (req, res) => {
 
 router.get('/playlist_songs', async (req, res) => {
   try {
-    const styleItems = await db.playlistSongs.findAll(); 
+    const styleItems = await db.playlistSongs.findAll();
     const reply = styleItems.length > 0 ? { data: styleItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -687,7 +687,7 @@ router.get('/playlist_songs/:playlist_id', async (req, res) => {
   try {
     const styleItems = await db.playlistSongs.findAll({
       where: {
-        playlist_id : req.params.playlist_id
+        playlist_id: req.params.playlist_id
       }
     });
 
@@ -756,7 +756,7 @@ router.delete('/playlist_songs/:playlist_id', async (req, res) => {
 
 router.get('/songs', async (req, res) => {
   try {
-    const styleItems = await db.songs.findAll(); 
+    const styleItems = await db.songs.findAll();
     const reply = styleItems.length > 0 ? { data: styleItems } : { message: 'no results found' };
     res.json(reply);
   } catch (err) {
@@ -769,7 +769,7 @@ router.get('/songs/:song_id', async (req, res) => {
   try {
     const styleItems = await db.songs.findAll({
       where: {
-        song_id : req.params.song_id
+        song_id: req.params.song_id
       }
     });
 
@@ -851,18 +851,35 @@ router.delete('/songs/:song_id', async (req, res) => {
 });
 
 /// /////////////////////////////////
-/// ////Maintable Endpoints////////
+/// ////Supportint Endpoints////////
 /// /////////////////////////////////
-const results = rawqueries
+
+/// Results of Search///
+const results = rawqueries;
 router.get('/main', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(results, {
       type: sequelize.QueryTypes.SELECT
     });
-    res.json({data:result});
+    res.json({data: result});
   } catch (err) {
     console.error(err);
     res.error('Server error');
   }
 });
+
+/// Num of Genres Graph///
+const graphgen = graphquery;
+router.get('/graphgenres', async (req, res) => {
+  try {
+    const result = await db.sequelizeDB.query(graphgen, {
+      type: sequelize.QueryTypes.SELECT
+    });
+    res.json({data: result});
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
 export default router;

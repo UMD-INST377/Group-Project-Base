7108e12dad5812ac26f0aae68f72606b501a7c2d
racesaunders <race.saunders@gmail.com>
2022-04-26 18:16:40 -0400

    removed engines key from package.json

--- client/js/trailsScript.js
+++ client/js/trailsScript.js
@@ -19,6 +19,18 @@ function restoArrayMake(dataArray) {
   }); */
 }
 
+function formToObject(htmlFormElement) {
+  const formItem = new FormData(htmlFormElement).entries();
+  const formArray = Array.from(formItem);
+  const formObject = formArray.reduce((collection, item, index) => {
+    if (!collection[item[0]]) {
+      collection[item[0]] = item[1];
+    }
+    return collection;
+  }, {});
+  return formObject;
+}
+
 function createHtmlList(collection) {
   // console.table(collection);
   const targetList = document.querySelector('.parks-list');

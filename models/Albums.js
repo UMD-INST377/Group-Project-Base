7be86431b6abe8a7c6f384ffe69b6858abccbf0a
diegolemus301 <dlemus11@terpmail.umd.edu>
2022-04-26 11:29:07 -0400

    new merge

--- models/Albums.js
+++ models/Albums.js
@@ -3,38 +3,27 @@ export default (sequelize, DataTypes) => {
     'albums',
     {
       album_id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.CHAR(22),
         allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
+        primaryKey: true,
+        unique: true
+      },
+      release_date: {
+        type: DataTypes.DATETIME,
+        allowNull: false
+      },
+      label: {
+        type: DataTypes.CHAR(400),
+        allowNull: false
+      },
+      cover_url: {
+        type: DataTypes.CHAR(100)
       },
       album_name: {
         type: DataTypes.CHAR(80),
         allowNull: false
-      },
-      number_of_songs: {
-        type: DataTypes.DECIMAL(3, 0),
-        allowNull: false
-      },
-      average_song_length: {
-        type: DataTypes.DECIMAL(5, 3)
-      },
-      album_link: {
-        type: DataTypes.BLOB,
-        allowNull: false
-      },
-      album_versions: {
-        type: DataTypes.DECIMAL(4, 0)
-      },
-      release_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        foreignKey: true
-      },
-      artist_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-      },
+      }
+      
     },
     { freezeTableName: true, timestamps: false }
   );

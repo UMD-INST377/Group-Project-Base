f6b4459d33215a11290bd221986ac8419e6e9f4f
yang <ylk01413@gmail.com>
2022-04-27 14:17:33 -0400

    reorganize

--- client/js/script.js
+++ client/js/script.js
@@ -1,8 +1,3 @@
-/* eslint-disable no-unused-expressions */
-/* eslint-disable indent */
-/* eslint-disable no-console */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-alert */
 function restArrayMake(dataArray) {
   const listItems = dataArray.slice(0, 50);
   return listItems;
@@ -15,42 +10,24 @@ function createHtmlList(collection) {
     const { album_name } = item;
     const displayArtist = name.toLowerCase();
     const displayAlbum = album_name.toLowerCase();
-    const injectThis = `<th>${displayArtist}</th>`;
-    const injectThisAlbum = ` <td>${displayAlbum}</td>`;
-    targetList.innerHTML += injectThisAlbum + injectThis;
+    const injectThisArtist = `<td>${displayArtist}</td>`;
+    const injectThisAlbum = ` <th>${displayAlbum}</th>`;
+    targetList.innerHTML += injectThisArtist + injectThisAlbum;
   });
 }
-async function albumDelete() {
-  console.log('hello from delete');
-  const request = `api/albums/${formbox.value}`;
-  const resp = await fetch(request, {method: 'DELETE'});
-  console.log(resp);
-  if (resp.status === 200) { alert(`${formbox.value}.deleted`); } else {
-    alert('Not_Found');
-  }
-}
-async function albumAdd() {
-  console.log('hello from add');
-  const request = `api/albums/${formbox.value}`;
-  const resp = await fetch(request, {method: 'ADD'});
-  console.log(resp);
-  if (resp.status === 200) { alert(`${formbox.value}.added`); } else {
-    alert('Not_Found');
-  }
-}
+
 async function mainEvent() {
   // the async keyword means we can make API requests
   const form = document.querySelector('#results');
-  const submitButton = document.querySelector('#submit_button');
+  // not needed since result is limited to 50
+  // const submitButton = document.querySelector('#submit_button');
   const restName = document.querySelector('#init_search');
-  const add = document.querySelector('#add');
-  const del = document.querySelector('#delete');
 
   const results = await fetch('/api/main'); // This accesses some data from our API
   const arrayFromJson = await results.json(); // This changes it into data we can use - an object
 
   if (arrayFromJson.data.length > 0) {
-    submitButton.style.display = 'block';
+    // submitButton.style.display = 'block';
     console.log('start');
 
     let currentArray = [];
@@ -74,7 +51,8 @@ async function mainEvent() {
       } else {
         document.querySelector('.result_list').innerHTML = '';
       }
-      });
+    });
+
     form.addEventListener('submit', async (submitEvent) => {
       // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
@@ -82,8 +60,6 @@ async function mainEvent() {
       currentArray = arrayFromJson.data;
       createHtmlList(restArrayMake(currentArray));
     });
-    // add.addEventListener('input', albumAdd);
-    // del.addEventListener('input', albumDelete);
   }
 }
 // this actually runs first! It's calling the function above

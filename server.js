4cbc866d05a4773b1657ec0126cd8a29be080d8b
NnamdiEde <72045679+NnamdiEde@users.noreply.github.com>
2022-04-27 11:31:18 -0400

    Updated routes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import db from './database/initializeDB.js';
 import apiRoutes from './server/routes/apiRoutes.js';
 import ryanRoutes from './server/routes/ryanRoutes.js';
-import  nnamdiRoutes from './server/routes/nnamdiRoutes.js';
+import nnamdiRoutes from './server/routes/nnamdiRoutes.js';
 
 const app = express();
 

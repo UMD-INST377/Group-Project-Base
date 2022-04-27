b771803761436c78700472dfdce2847ed6197e3d
wwwilll <98497952+wyaiam@users.noreply.github.com>
2022-04-27 16:49:24 -0400

    both working get controllers

--- routes/will.js
+++ routes/will.js
@@ -7,7 +7,7 @@ import mealsQuery from '../controller/meals_query.js';
 
 const router = express.Router();
 
-// /will
+// api/will/meals
 router.get('/meals', async (req, res) => {
   try {
     const result = await db.sequelizeDB.query(mealsQuery, {
@@ -20,19 +20,18 @@ router.get('/meals', async (req, res) => {
   }
 });
 
+// api/will/meals/:meal_id
 router.get('/meals/:meal_id', async (req, res) => {
   try {
-    const meal = await db.Meals.findAll({
-      where: {
-        meal_id: req.params.meal_id
-      }
-    });
-    res.json(meal);
+    const result = await db.sequelizeDB.query('SELECT * FROM meals WHERE meal_id = (:meal_id)', {
+      replacements: {meal_id: req.params.meal_id},
+      type: sequelize.QueryTypes.SELECT
+    }); 
+    res.json({ data: result});
   } catch (error) {
     console.log(error);
     res.json({ message: 'Server error' });
   }
 });
-
 
 export default router;

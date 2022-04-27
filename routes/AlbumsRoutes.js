688856f372a1cb52d0864ac649f3324ebe1158ae
lkillea <64662431+lkillea@users.noreply.github.com>
2022-04-27 13:20:20 -0400

    Update AlbumsRoutes.js

--- routes/AlbumsRoutes.js
+++ routes/AlbumsRoutes.js
@@ -2,7 +2,7 @@
 import express from 'express';
 import sequelize from 'sequelize';
 import chalk from 'chalk';
-
+import util from 'util';
 import db from '../database/initializeDB.js';
 
 const router = express.Router();
@@ -30,4 +30,58 @@ router.get('/album/:album_id', async (req, res) => {
   }
 });
 
+router.post('/album', async (req, res) => {
+  const art = db.album.findAll();
+  const currentId = (await art.length) + 1;
+  try {
+    console.log(util.inspect(req.body, {showHidden: false, depth: null, colors: true}));
+    const newAlbum = await db.album.create({
+      album_id: currentId,
+      release_id: req.body.release_id,
+      genre_id: req.body.genre_id,
+      album_name: req.body.album_name
+    });
+    res.json(newAlbum);
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error')
+  }
+});
+
+router.put('/album/', async (req, res) => {
+  try {
+    console.log(util.inspect(req.body, {showHidden: false, depth: null, colors: true}))
+    const albumUpdate = await db.album.upsert(
+      {
+        album_id: req.body.album_id,
+        release_id: req.body.release_id,
+        genre_id: req.body.genre_id,
+        album_name: req.body.album_name
+      },
+      {
+        where: {
+          album_id: req.body.album_id,
+        }
+      }
+    );
+    res.json('Success. Record Updated')
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error')
+  }
+});
+
+router.delete('/album/:album_id', async (req, res) => {
+  try {
+    const albumDelete = await db.album.destroy({
+      where: {
+        album_id: req.params.album_id
+      }
+    });
+    res.json('Success. Record Deleted')
+  } catch (error) {
+    console.log(error);
+    res.send('Server Error');
+  }
+});
 export default router;

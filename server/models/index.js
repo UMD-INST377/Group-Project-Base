1e320327ec45432a200c3eca0e811907bf9c1533
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2022-04-27 14:11:03 -0400

    Create index.js

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+import Availability from './Availability.js';
+import Genres from './Genres.js';
+import Images from './Images.js';
+import Languages from './Languages.js';
+import Movies from './Movies.js';
+import ratings from './Ratings.js';
+
+export default {
+  Availability,
+  Genres,
+  Images,
+  Languages,
+  Movies,
+  ratings
+};

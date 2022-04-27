fa4bd9c368a801e762e7c796a28c79d3c8e20f33
racesaunders <race.saunders@gmail.com>
2022-04-27 19:31:23 -0400

    separate submit reviews page

--- client/js/reviewScript.js
+++ client/js/reviewScript.js
@@ -25,38 +25,7 @@ function updateParks(collection) {
   });
 }
 
-function initMap(targetID) {
-  const latLong = [37.901984, -75.3526373]; // assateague island start
-
-  const map = L.map(targetID).setView(latLong, 13); // lat long zoom
-  L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
-  }).addTo(map);
-  return map;
-}
-
-function addMapMarkers(map, collection) {
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-  collection.forEach((item) => {
-    console.log(item.park_lat);
-    console.log(item.park_long);
-
-    const point = [item.park_lat, -item.park_long];
-    // console.log(item.geocoded_column_1?.coordinates);
-    L.marker([item.park_lat, -item.park_long]).addTo(map);
-  });
-}
-
-function findPark(park, parkArray){
+function findPark(park, parkArray) {
 
 }
 // function refreshList (target, storage) {
@@ -83,14 +52,12 @@ function findPark(park, parkArray){
 //   });
 // }
 async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // change this selector to match the id or classname of your actual form
+  const reviewForm = document.querySelector('.review_form');
 
   const submit = document.querySelector('.submit_button');
 
   const parks = document.querySelector('#park');
-  const refresh = document.querySelector('.refresh_list');
 
-  const map = initMap('map');
   const retrievalVar = 'parks';
   submit.style.display = 'none';
 
@@ -110,26 +77,38 @@ async function mainEvent() { // the async keyword means we can make API requests
 
     let currentArray = parksArray;
     // inputListener(parks);
-    addMapMarkers(map, currentArray);
     parks.addEventListener('change', async (event) => {
       console.log(event.target.value);
       console.log(parksArray);
-      matchingPark = findPark(event.target.value,parksArray)
+      matchingPark = findPark(event.target.value, parksArray);
     });
 
-    form.addEventListener('submit', async (submitEvent) => {
+    reviewForm.addEventListener('submit', async (submitEvent) => {
       // async has to be declared all the way to get an await
       submitEvent.preventDefault(); // This prevents your page from refreshing!
       // console.log('form submission'); // this is substituting for a "breakpoint"
-    });
-    refresh.addEventListener('click', async (submitEvent) => {
-      // async has to be declared all the way to get an await
-      submitEvent.preventDefault(); // This prevents your page from refreshing!
-      // console.log('form submission'); // this is substituting for a "breakpoint"
-      currentArray = parksArray;
-      console.log(currentArray);
-      updateParks(currentArray);
-      addMapMarkers(map, currentArray);
+      const rev = formToObject(reviewForm);
+      // console.log(rev);
+      const reviewJson = {
+        park_id: 1,
+        title: rev.review_title,
+        author: rev.review_author,
+        description: rev.review
+      };
+
+      fetch('/api/race/reviews', {
+        method: 'post',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json'
+        },
+
+        // make sure to serialize your JSON body
+        body: JSON.stringify(reviewJson)
+      })
+        .then((response) => {
+          console.log(response);
+        });
     });
   }
 }

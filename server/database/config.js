020a7ad695e8804865c2b0000e8adbcd17156560
nurulhaya <8146896+nurulhaya@users.noreply.github.com>
2022-04-27 14:11:10 -0400

    Create config.js

---
+++ server/database/config.js
@@ -0,0 +1,23 @@
+export default {
+  development: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'group13_movies',
+    host: '174.129.198.86',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'group13_movies',
+    host: '174.129.198.86',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'ubuntu',
+    password: 'Veracrypt@12!',
+    database: 'group13_movies',
+    host: '174.129.198.86',
+    dialect: 'mysql'
+  }
+};

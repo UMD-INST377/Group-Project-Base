43617bff647b211d534579315d17820f245c970f
svarz <svarz@terpmail.umd.edu>
2022-04-27 14:39:25 -0400

    Fixed Home, About, & Data buttons. Formatted Data

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const staticFolder = 'client';
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//TODO: first should be /API
+// TODO: first should be /API
 app.use(express.static(staticFolder));
 app.use('/api', apiRoutes);
 app.use('/chandra', route);

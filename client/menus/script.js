beb96ca7f62c3da77b3fb73f62817cf9a3f16402
Nicholas-aurq <nicholas.aurq@gmail.com>
2022-04-27 22:23:01 -0400

    Updated menu script to add meal name

--- client/menus/script.js
+++ client/menus/script.js
@@ -1,8 +1,12 @@
 function createHTMLtable(currentArray) {
+  //takes in array, prints html table
+  //create table
   let result = '<table border=1>';
+  //create table headers
   result += `
   <tr>
-    <th>macro_id</th>
+    <th>meal_name</th>
+    <th>meal_id</th>
     <th>calories</th>
     <th>serving_size</th>
     <th>cholesterol</th>
@@ -12,10 +16,12 @@ function createHTMLtable(currentArray) {
     <th>meal_id</th>
     <th>fat</th>
   </tr>`;
+  //loop through each object in array, print cell values
   for (let i = 0; i < currentArray.length; i++) {
     result += '<tr>';
     result += `
-      <td>${currentArray[i].macro_id}</td>
+      <td>${currentArray[i].meal_name}</td>
+      <td>${currentArray[i].meal_id}</td>
       <td>${currentArray[i].calories}</td>
       <td>${currentArray[i].serving_size}</td>
       <td>${currentArray[i].cholesterol}</td>
@@ -27,26 +33,32 @@ function createHTMLtable(currentArray) {
     result += '</tr>';
   }
   result += '</table>';
+  //close table
 
   const targetList = document.querySelector('.macro_table');
+  //get div class macro table
+
+  //inject the table
   targetList.innerHTML = '';
   targetList.innerHTML += result;
 
 }
 
 function dataHandler(arr) {
-  return arr.slice(0, 10);
+  return arr.slice(0, 10); //get first 10 rows from array
 }
 
-async function mainEvent() {
-  const macroTable = document.querySelector('.macro_table');
-  const results = await fetch('/api/macros');
-  const macroArrayFromJson = await results.json();
-  let currentArray = [];
+async function mainEvent() { //mainEvent refers to page loading
+  const macroTable = document.querySelector('.macro_table'); //get macro_table div class
+  const results = await fetch('/api/allmeals'); //call get function from api routes for macros
+  console.log(results)
+  const macroArrayFromJson = await results.json(); //convert to array
+  //get first 10 head of macros table
+  let currentArray = []; 
   if (macroArrayFromJson.data.length > 0) {
     currentArray = dataHandler(macroArrayFromJson.data);
   }
-  console.log(currentArray)
+  //create the table
   createHTMLtable(currentArray);
   
 }

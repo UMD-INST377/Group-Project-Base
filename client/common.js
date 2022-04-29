e3e69c454e242dbfe2ad3424a9c6cabea53ef41c
jscoll <98493265+jscoll@users.noreply.github.com>
2022-04-29 17:10:30 -0400

    Split shared functions into common.js

---
+++ client/common.js
@@ -0,0 +1,7 @@
+// Fetch data from database
+async function getData(endpoint, options = {}) {
+    console.log('getData()');
+    const raw = await fetch(endpoint, options);
+    const json = await raw.json();
+    return json.data;
+}

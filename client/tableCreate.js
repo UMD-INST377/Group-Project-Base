1b6a9d333cf4b78d8f88e90565e54f07ef25f81b
vez0star@gmail.com <vez@terpmail.umd.edu>
2022-04-28 20:04:16 -0400

    filter table

--- client/tableCreate.js
+++ client/tableCreate.js
@@ -1,13 +1,8 @@
-//const arrayFromJson = await fetch('/api/meals');
-//const data = await arrayFromJson.json();
-//console.log(data);
-async function createTable() {
+async function loadTable() {
+
     const arrayFromJson = await fetch('/api/meals');
     let data = await arrayFromJson.json();
-
     data = data.data;
-
-    
 
     const form = document.querySelector('.form');
     const table = document.querySelector('.table');
@@ -35,6 +30,76 @@ async function createTable() {
     });
 }
 
+async function createFilteredTable() {
+    let currentData = [];
+    let data = [];
+
+    const form = document.querySelector(".form");
+    const table = document.querySelector(".table");
+
+    const mealidSelector = document.querySelector(".meal_id");
+    const mealnameSelector = document.querySelector(".meal_name");
+    const mealcategorySelector = document.querySelector(".meal_category");
+
+    function filterCheck (array) {
+        let filterArray = array;
+        if (mealidSelector.value !== "") {
+            const mealId = filterArray.filter(
+                (item) => item.meal_id === parseInt(mealidSelector.value)
+            );
+            filterArray = mealId;
+        }
+        if (mealnameSelector.value !== "") {
+            const nameFilter = filterArray.filter((item) =>
+            item.name.toLowerCase().includes(mealnameSelector.value.toLowerCase())
+            );
+            filterArray = nameFilter;
+        }
+        if (mealcategorySelector.value !== "") {
+            const nameFilter = filterArray.filter((item) =>
+            item.name.toLowerCase().includes(mealcategorySelector.value.toLowerCase())
+            );
+            filterArray = nameFilter;
+        }
+        return filterArray;
+}
+
+    document.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        if (currentData.length === 0) {
+            const arrayFromJson = await fetch("/api/movies");
+            data = await arrayFromJson.json();
+
+            data = await data.data;
+
+            currentData = filterCheck(data);
+            loadTable(currentData);
+            else {
+
+            currentData = filterCheck(data);
+            await loadTable(currentData);
+            filmnameSelector.addEventListener("change", async () => {
+                currentData = filterCheck(data);
+                await loadTable(currentData);
+            });
+            currentData = filterCheck(data);
+            await loadTable(currentData);
+            filmidSelector.addEventListener("change", async () => {
+                currentData = filterCheck(data);
+                await loadTable(currentData);
+            });
+            currentData = filterCheck(data);
+            await loadTable(currentData);
+            filmcategorySelector.addEventListener("change", async () => {
+                currentData = filterCheck(data);
+                await loadTable(currentData);
+            });
+            }
+        }
+        createFilteredTable();
+    });
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
-    createTable();
+    await createFilteredTable();
 });

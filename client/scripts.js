58aca1336584704bcc0a46cc95678a27ab1897d0
isaac20021 <isaac1santamaria@gmail.com>
2022-04-29 17:06:56 -0400

    Bar chart framework
    Structure for actors barchart

--- client/scripts.js
+++ client/scripts.js
@@ -1,5 +1,27 @@
+import Chart from 'chart.js/auto';
+async function actorchart(){
+    const Chart = require('chart.js');
+    const myChart = new Chart(ctx, {...});
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {
+          responsive: true,
+          plugins: {
+            legend: {
+              position: 'top',
+            },
+            title: {
+              display: true,
+              text: 'Chart.js Bar Chart'
+            }
+          }
+        },
+      };
+}
 async function mainEvent() {
-    console.log("mainEvent()");
+  console.log('mainEvent()');
+  actorchart
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

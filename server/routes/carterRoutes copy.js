37588c48f2b2d31bc3970ba2740fb0de01a676e1
cartercavalier <59623915+cartercavalier@users.noreply.github.com>
2022-04-30 01:40:01 -0400

    Update carterRoutes copy.js
    post request commit

--- server/routes/carterRoutes copy.js
+++ server/routes/carterRoutes copy.js
@@ -2,16 +2,45 @@
 import express from 'express';
 import sequelize from 'sequelize';
 import db from '../database/initializeDB.js';
+import path from 'path';
 
 const router = express.Router();
 
+/// /////////////////////////////////
+/// ////// Review Endpoints ///////// by Carter C
+/// /////////////////////////////////
+
 router.get('/review', async(req, res) => {
   try{
-    const review = await db.sequelizeDB.query('select * from Reviews')
+    const review = await db.sequelizeDB.query(
+      'SELECT * FROM Reviews');
     res.json({data:review})
   }catch{res.send('error')}
+})
+
+router.get('/review/:id', async (req, res) => {
+  try{
+    const { id } = req.params;
+    console.log(req.params);
+    const result = await db.sequelizeDB.query(
+      'SELECT * FROM Reviews WHERE review_id = ${id}'
+    );
+    res.json({data: result });
+  }catch(error){
+    console.error(error);
+    res.send('Server error')
+  }
+})
+
+router.post('/review', async (req, res) => {
+  try{
+    const query = 'INSERT INTO Review (review_id, review_desc, avg_star_rating, resturant_id) VALUES ('${parseInt(req.body.review_id)}','${parseInt(req.body.review_desc)}','${parseInt(req.body.avg_star_rating)}','${parseInt(req.body.resturant_id)}')';
+    console.log(query);
+    const newReview = await db.sequelizeDB.query(query);
+    res.send('UPDATED');
+  } catch (error) {
+    console.error(error);
+  }
 });
 
-/// /////////////////////////////////
-/// ////////               ////////// by Carter C
-/// /////////////////////////////////
+export default router;

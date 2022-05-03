f8abcd5a992a5ed07b5b1635383107674e90b899
fcfisk27 <74329174+fcfisk27@users.noreply.github.com>
2022-05-03 15:22:15 -0400

    Update HallHours.js

--- models/HallHours.js
+++ models/HallHours.js
@@ -1,24 +1,23 @@
 export default (sequelize, DataTypes) => {
-    const HallHours = sequelize.define(
-      "hall_hours",
-      {
-        hall_hours_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          primaryKey: true,
-        },
-        day: {
-          type: DataTypes.STRING,
-        },
-        schedule_id: {
-            type: DataTypes.STRING,
-        },
-        hall_id: {
-            type: DataTypes.INTEGER,
-          },
+  const HallHours = sequelize.define(
+    'hall_hours',
+    {
+      hall_hours_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true
       },
-      { freezeTableName: true, timestamps: false }
-    );
-    return HallHours;
-  };
-  
+      day: {
+        type: DataTypes.STRING
+      },
+      schedule_id: {
+        type: DataTypes.STRING
+      },
+      hall_id: {
+        type: DataTypes.INTEGER
+      }
+    },
+    { freezeTableName: true, timestamps: false }
+  );
+  return HallHours;
+};

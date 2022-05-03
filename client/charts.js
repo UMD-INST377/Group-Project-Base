d12077b8744e1714bae8d6589da6fcfd91d8c762
jscoll <98493265+jscoll@users.noreply.github.com>
2022-05-02 20:43:40 -0400

    Fix weird issue where Git doesn't see changes

--- client/charts.js
+++ client/charts.js
@@ -153,7 +153,7 @@ function loadAgyaChart(roleData, actorData) {
             }
         }
     };
-    const myChart = new Chart(chartElement, chartConfig);
+    const agyaChart = new Chart(chartElement, chartConfig);
 }
 
 async function loadCharts() {

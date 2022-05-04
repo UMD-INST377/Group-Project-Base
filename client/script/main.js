5e742f35e9ace14adec6aba3e3aa22159cef21f2
TempestIndra <43696183+TempestIndra@users.noreply.github.com>
2022-05-03 20:16:21 -0400

    fix swiper

--- client/script/main.js
+++ client/script/main.js
@@ -1,6 +1,9 @@
-var swiper = new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+var swiper = new Swiper('.swiper-container', {
+  pagination: '.swiper-pagination',
+  slidesPerView: 4,
+  centeredSlides: true,
+  paginationClickable: true,
+  loop: true,
+  spaceBetween: 30,
+  slideToClickedSlide: true
 });

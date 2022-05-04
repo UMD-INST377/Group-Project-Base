0c82d6c12f7962fb563249ae235510dc965fc54b
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-05-04 18:49:35 -0400

    api for form

--- client/subpage/data/scripts.js
+++ client/subpage/data/scripts.js
@@ -1,23 +1,39 @@
+let oldMeal;
+let newMeal;
+
 async function mainEvent() {
   const form = document.querySelector('.main_form');
   const originalName = document.querySelector('#original_name');
   const newName = document.querySelector('#new_name');
   const submit = document.querySelector('.button');
 
-  const database = await fetch('/chandra/allmeals');
-  const allItems = await database.json();
-  console.log(allItems);
-
   originalName.addEventListener('input', async (event) => {
-    console.log(event.target.value);
+    oldMeal = event.target.value;
+    console.log(oldMeal);
   });
 
   newName.addEventListener('input', async (event) => {
+    newMeal = event.target.value;
     console.log(event.target.value);
   });
 
+  const database = await fetch('/chandra/allmeals');
+  const allItems = await database.json();
+  
+  const arr = [];
+  allItems.data.forEach((item) => {
+    const {meal_name} = item;
+    arr.push(meal_name);
+  });
+  console.log(arr);
+
   form.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
+    if (arr.includes(oldMeal)) {
+      console.log('it is true');
+    } else {
+      console.log('it is false');
+    }
     console.log('Form Submitted');
   });
 }

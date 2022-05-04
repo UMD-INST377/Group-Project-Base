7100c446ac05a7b3df114cdea9c5b9208ad1f0f7
Stefan <sclarke2@terpmail.umd.edu>
2022-05-03 23:40:07 -0400

    sc ic

--- models/movies.js
+++ models/movies.js
@@ -1,31 +1,31 @@
 export default (sequelize, DataTypes) => {
-    const movies = sequelize.define(
-      'movies',
-      {
-        movie_id: {
-          type: DataTypes.INTEGER,
-          allowNull: false,
-          unique: true,
-          primaryKey: true
-        },
-        director_id: {
-          type: DataTypes.INTEGER
-        },
-        movie_name: {
-          type: DataTypes.STRING
-        },
-        movie_year: {
-            type: DataTypes.INTEGER
-        },
-        movie_description: {
-          type: DataTypes.STRING
-        },
-        rating: {
-            type: DataTypes.INTEGER
-        },
-        duration_of_movie: {
-            type: DataTypes.INTEGER
-        },
+  const movies = sequelize.define(
+    'movies',
+    {
+      movie_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        unique: true,
+        primaryKey: true,
+      },
+      director_id: {
+        type: DataTypes.INTEGER,
+      },
+      movie_name: {
+        type: DataTypes.STRING,
+      },
+      movie_year: {
+        type: DataTypes.INTEGER,
+      },
+      movie_description: {
+        type: DataTypes.STRING,
+      },
+      rating: {
+        type: DataTypes.INTEGER,
+      },
+      duration_of_movie: {
+        type: DataTypes.INTEGER,
+      },
     },
     { freezeTableName: true, timestamps: false }
   );

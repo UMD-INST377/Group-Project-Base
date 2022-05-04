d8239db36a47eff032a0303d89d58439579464eb
Chandra Tamang <73456516+C-tamang@users.noreply.github.com>
2022-05-04 15:57:11 -0400

    wrote event listener for form

--- client/subpage/data/scripts.js
+++ client/subpage/data/scripts.js
@@ -4,6 +4,10 @@ async function mainEvent() {
   const newName = document.querySelector('#new_name');
   const submit = document.querySelector('.button');
 
+  const database = await fetch('/chandra/allmeals');
+  const allItems = await database.json();
+  console.log(allItems);
+
   originalName.addEventListener('input', async (event) => {
     console.log(event.target.value);
   });
@@ -14,7 +18,7 @@ async function mainEvent() {
 
   form.addEventListener('submit', async (submitEvent) => {
     submitEvent.preventDefault();
-    console.log("Form Submitted");
+    console.log('Form Submitted');
   });
 }
 

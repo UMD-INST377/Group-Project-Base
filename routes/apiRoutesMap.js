0612f18a971a6154ef3bdc344c7b45247dafff28
TempestIndra <43696183+TempestIndra@users.noreply.github.com>
2022-05-05 19:22:57 -0400

    fix map intergratin

---
+++ routes/apiRoutesMap.js
@@ -0,0 +1,41 @@
+import express from "express";
+import sequelize from "sequelize";
+
+import db from "../database/initializeDB.js";
+
+const router = express.Router();
+
+router.route("/testing").get(async (req, res) => {
+  try {
+    const testQuery = "SELECT * FROM location";
+    const test = await db.sequelizeDB.query(testQuery);
+
+    res.json(test);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.route("/address").get(async (req, res) => {
+  try {
+    const testQuery = "SELECT * FROM location";
+    const test = await db.sequelizeDB.query(testQuery);
+
+    res.json(test);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+router.route('/restaurant').get(async (req, res) => {
+  try {
+    const testQuery = 'SELECT * FROM restaurant';
+    const test = await db.sequelizeDB.query(testQuery);
+
+    res.json(test);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+export default router;

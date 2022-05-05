faf321e9e850789c661441881314814d852d78d0
fcfisk27 <74329174+fcfisk27@users.noreply.github.com>
2022-05-05 14:43:08 -0400

    adding markers to the map

--- client/mapScript.js
+++ client/mapScript.js
@@ -7,3 +7,23 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
   zoomOffset: -1,
   accessToken: 'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw'
 }).addTo(map);
+const scdcircle = L.circle([38.983, -76.9438], {
+  color: 'red',
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(map);
+
+const ncdcircle = L.circle([38.99235, -76.9465], {
+  color: 'blue',
+  fillColor: '#0000FF',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(map);
+
+const circle251 = L.circle([38.9925, -76.95], {
+  color: 'green',
+  fillColor: '#00FF00',
+  fillOpacity: 0.5,
+  radius: 50
+}).addTo(map);

0ca04750f8df94c3a05361143a5e67413fb029ba
sosier7007 <sosier@terpmail.umd.edu>
2022-05-05 15:35:06 -0400

    ic 1

--- controller/mapcontroller.js
+++ controller/mapcontroller.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 const mapGet = `SELECT location_code, plant_id, botanical_name, common_name, origin, growth_habit, max_height, max_width, duration
 FROM plants.plant_locations JOIN plants.plants USING(plant_id);`;
 

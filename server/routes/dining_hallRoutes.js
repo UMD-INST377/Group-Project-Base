b049e0a4371d7751f1babc6fcf221d7d6e7f4d1f
Peter G <peterbennnettgerard@gmail.com>
2022-05-04 22:12:00 -0400

    making the test version the real version

--- server/routes/dining_hallRoutes.js
+++ server/routes/dining_hallRoutes.js
@@ -31,3 +31,31 @@ router.get('/meals', async (req, res) => {
       res.error('Server error');
     }
   });
+
+
+
+router.get('/dining', async (req, res) => {
+  try {
+    const halls = await db.DiningHall.findAll();
+    const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
+    res.json(reply);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});
+
+router.get('/dining/:hall_id', async (req, res) => {
+  try {
+    const hall = await db.DiningHall.findAll({
+      where: {
+        hall_id: req.params.hall_id
+      }
+    });
+
+    res.json(hall);
+  } catch (err) {
+    console.error(err);
+    res.error('Server error');
+  }
+});

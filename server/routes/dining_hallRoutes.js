6d2822d9ed2a78c1db2386cdeb0aece6780cfebc
Peter G <peterbennnettgerard@gmail.com>
2022-05-05 01:15:20 -0400

    Peter Gerard IC1

--- server/routes/dining_hallRoutes.js
+++ server/routes/dining_hallRoutes.js
@@ -34,29 +34,30 @@ router.get('/meals', async (req, res) => {
 
 
 
+
 router.get('/dining', async (req, res) => {
-  try {
-    const halls = await db.DiningHall.findAll();
-    const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
-    res.json(reply);
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
-
-router.get('/dining/:hall_id', async (req, res) => {
-  try {
-    const hall = await db.DiningHall.findAll({
-      where: {
-        hall_id: req.params.hall_id
-      }
-    });
-
-    res.json(hall);
-  } catch (err) {
-    console.error(err);
-    res.error('Server error');
-  }
-});
+    try {
+      const halls = await db.DiningHall.findAll();
+      const reply = halls.length > 0 ? { data: halls } : { message: 'no results found' };
+      res.json(reply);
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  });
+  
+  router.get('/dining/:hall_id', async (req, res) => {
+    try {
+      const hall = await db.DiningHall.findAll({
+        where: {
+          hall_id: req.params.hall_id
+        }
+      });
+  
+      res.json(hall);
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  });
 

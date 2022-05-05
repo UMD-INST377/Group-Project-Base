c04cae6baccc32f30bf2561668a93690d19d3e17
Peter G <peterbennnettgerard@gmail.com>
2022-05-05 01:40:05 -0400

    Peter Gerard IC2 stuff

--- server/routes/dining_hallRoutes.js
+++ server/routes/dining_hallRoutes.js
@@ -61,3 +61,20 @@ router.get('/dining', async (req, res) => {
     }
   });
 
+  router.post('/dining', async (req, res) => {
+    const halls = await db.DiningHall.findAll();
+    const currentId = (await halls.length) + 1;
+    try {
+      const newDining = await db.DiningHall.create({
+        hall_id: currentId,
+        hall_name: req.body.hall_name,
+        hall_address: req.body.hall_address,
+        hall_lat: req.body.hall_lat,
+        hall_long: req.body.hall_long
+      });
+      res.json(newDining);
+    } catch (err) {
+      console.error(err);
+      res.error('Server error');
+    }
+  });

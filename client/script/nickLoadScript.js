5e1269d5769d77bfeaa7b4936b5287e356d9b8fc
TempestIndra <43696183+TempestIndra@users.noreply.github.com>
2022-05-05 21:38:11 -0400

    filter

--- client/script/nickLoadScript.js
+++ client/script/nickLoadScript.js
@@ -1,3 +1,5 @@
+let collection;
+
 function docReady(fn) {
   // see if DOM is already available
   if (document.readyState === "complete" || document.readyState === "interactive") {
@@ -18,27 +20,23 @@ docReady(function() {
 
 function createHtmlList(collection) {
 
-  // console.log('fired HTML creator');
-  // const results = ; 
-  // const arrayFromJson = await results.json();
   output = document.getElementById('cuisineTable');
-  console.log(collection[0]);
+  collection = collection[0];
   var tableContent = "";
 
   for (i = 0; i < collection[0].length; i++) {
       tableContent += "<tr><td>" + collection[0][i].type_id + "</td>"
+      + "<td>" + collection[0][i].type + "</td>"
+      + `<td><input type='checkbox' id=${collection[0][i].type_id}></td></tr>`;
 
-      + "<td>" + collection[0][i].type + "</td></tr>";
-
-  }
+}
   output.innerHTML = tableContent;
 
-  // const targetList = document.querySelector(".resto-list");
-  // targetList.innerHTML = "";
-  // collection.forEach((item) => {
-  //   const { restaurant_name } = item;
-  //   const displayName = restaurant_name.toLowerCase();
-  //   const injectThisItem = `<li>${displayName}</li>`;
-  //   targetList.innerHTML += injectThisItem;
-  // });
+ });
+
+function cuisineSubmit(){
+    for (i = 0; i < collection.length;i++){
+      let check = document.querySelector('#accept');
+      console.log(check.checked); // false
+    }
 }

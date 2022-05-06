e0eaf05d8da3b66842a65c3d674f33f3f84b969c
Nicholas-aurq <nicholas.aurq@gmail.com>
2022-05-06 00:04:40 -0400

    Created accordion style list for each meal option

--- client/menus/script.js
+++ client/menus/script.js
@@ -1,117 +1,67 @@
-function createHTMLtable(currentArray) {
-  //takes in array, prints html table
-  //create table
+function createHTMLtable(entry) {
+  // takes in array, prints html table
+  // create table
   let result = '<table border=1>';
-  //create table headers
+  // create table headers
   result += `
   <tr>
-    <th>meal_name</th>
-    <th>meal_id</th>
     <th>calories</th>
     <th>serving_size</th>
     <th>cholesterol</th>
     <th>sodium</th>
     <th>carbs</th>
     <th>protein</th>
-    <th>meal_id</th>
     <th>fat</th>
-    <th>location</th>
   </tr>`;
-  //loop through each object in array, print cell values
-  for (let i = 0; i < currentArray.length; i++) {
-    result += '<tr>';
-    result += `
-      <td>${currentArray[i].meal_name}</td>
-      <td>${currentArray[i].meal_id}</td>
-      <td>${currentArray[i].calories}</td>
-      <td>${currentArray[i].serving_size}</td>
-      <td>${currentArray[i].cholesterol}</td>
-      <td>${currentArray[i].sodium}</td>
-      <td>${currentArray[i].carbs}</td>
-      <td>${currentArray[i].protein}</td>
-      <td>${currentArray[i].meal_id}</td>
-      <td>${currentArray[i].fat}</td>
-      <td>${currentArray[i].location}</td>`;
-    result += '</tr>';
-  }
+  // loop through each object in array, print cell values
+  result += '<tr>';
+  result += `
+      <td>${entry.calories}</td>
+      <td>${entry.serving_size}</td>
+      <td>${entry.cholesterol}</td>
+      <td>${entry.sodium}</td>
+      <td>${entry.carbs}</td>
+      <td>${entry.protein}</td>
+      <td>${entry.fat}</td>`;
+  result += '</tr>';
   result += '</table>';
-  //close table
-
-  const targetList = document.querySelector('.meal_table');
-  //get div class meal table
-
-  //inject the table
-  targetList.innerHTML = '';
-  targetList.innerHTML += result;
-
+  return result;
 }
-
-function dataHandler(arr) {
-  return arr.slice(0, 10); //get first 10 rows from array
-}
-
-
-
-
-
-
-
-
-async function mainEvent() { //mainEvent refers to page loading
-  const mealTable = document.querySelector('.meal_table'); //get meal_table div class
-  const results = await fetch('/api/allmeals'); //call get function from api routes for meals
-  const mealArrayFromJson = await results.json(); //convert to array
-  //get first 10 head of meals table
-  let currentArray = []; 
-  if (mealArrayFromJson.data.length > 0) {
-    currentArray = dataHandler(mealArrayFromJson.data);
-  }
-
-
-
-  //This chunk of code is for adding the names to the buttons on the accordion
-  const api_results = await fetch('/api/dining'); //call get function from api routes for dining
-  const hallArray = await api_results.json(); //convert to array
-
-  //The Diner
-  const hall0Button = document.querySelector('.the_diner'); //Selects the element to insert the name
+function addNames(hallArray) {
+  // The Diner
+  const hall0Button = document.querySelector('.the_diner'); // Selects the element to insert the name
   hall0Button.innerHTML = '';
-  hall0Button.innerHTML+=`${hallArray.data[0].hall_name}`; //inserts the hall name into the html
+  hall0Button.innerHTML += `${hallArray.data[0].hall_name}`; // inserts the hall name into the html
 
-  //South Campus Dinning Hall
+  // South Campus Dinning Hall
   const hall1Button = document.querySelector('.sc_diner');
   hall1Button.innerHTML = '';
-  hall1Button.innerHTML+=`${hallArray.data[1].hall_name}`;
+  hall1Button.innerHTML += `${hallArray.data[1].hall_name}`;
 
-  //North Campus Dinning Hall
+  // North Campus Dinning Hall
   const hall2Button = document.querySelector('.nc_diner');
   hall2Button.innerHTML = '';
-  hall2Button.innerHTML+=`${hallArray.data[2].hall_name}`;
+  hall2Button.innerHTML += `${hallArray.data[2].hall_name}`;
+}
 
-
-
-  //This chunk is for adding each dining hall's list of meals to their dropdown
-  const loc = await fetch('/api/mealsByHall');
-  const locArray = await loc.json();
-  locData = locArray.data
-
-  //Each hall has their own seperate list for the dropdowns
-  let hall0Meals = '<ul>' //The Diner
-  let hall1Meals = '<ul>' //South Campus
-  let hall2Meals = '<ul>' // North Campus
-  for(i=0; i<locData.length; i++){ //This creates a list of each hall's menu items
-    if(locData[i]['hall_name'] == 'The Diner'){
-      hall0Meals += `<li> ${locData[i].meal_name} </li>`
-    }else if(locData[i]['hall_name'] == 'South Campus Dining Hall'){
-      hall1Meals += `<li> ${locData[i].meal_name} </li>`
-    }else if(locData[i]['hall_name'] == '251 North Dining Hall'){
-      hall2Meals += `<li> ${locData[i].meal_name} </li>`
+function addMeals(locData) {
+  // Each hall has their own seperate list for the dropdowns
+  let hall0Meals = '<ul>'; // The Diner
+  let hall1Meals = '<ul>'; // South Campus
+  let hall2Meals = '<ul>'; // North Campus
+  for (i = 0; i < locData.length; i++) { // This creates a list of each hall's menu items
+    if (locData[i].location == 'The Diner') {
+      panelInfo = createHTMLtable(locData[i]);
+      hall0Meals += `<li><button class = "accordion">${locData[i].meal_name}</button><div class = "panel">${panelInfo}</div></li>`;
+    } else if (locData[i].location == 'South Campus Dining Hall') {
+      panelInfo = createHTMLtable(locData[i]);
+      hall1Meals += `<li><button class = "accordion">${locData[i].meal_name}</button><div class = "panel">${panelInfo}</div></li>`;
+    } else if (locData[i].location == '251 North Dining Hall') {
+      panelInfo = createHTMLtable(locData[i]);
+      hall2Meals += `<li><button class = "accordion">${locData[i].meal_name}</button><div class = "panel">${panelInfo}</div></li>`;
     }
   }
-  hall0Meals += '<ul>'
-  hall0Meals += '<ul>'
-  hall0Meals += '<ul>'
-  //The lists are then inserted into the dropdowns
+  // The lists are then inserted into the dropdowns
   const target0 = document.querySelector('.the_diner_menu');
   target0.innerHTML = '';
   target0.innerHTML += hall0Meals;
@@ -123,28 +73,49 @@ async function mainEvent() { //mainEvent refers to page loading
   const target2 = document.querySelector('.nc_diner_menu');
   target2.innerHTML = '';
   target2.innerHTML += hall2Meals;
+}
 
+async function mainEvent() { // mainEvent refers to page loading
+  const results = await fetch('/api/allmeals'); // call get function from api routes for meals
+  const mealArrayFromJson = await results.json(); // convert to array
+  // get first 10 head of meals table
 
+  // inject the names of the dining halls into the html buttons
+  const api_results = await fetch('/api/dining'); // call get function from api routes for dining
+  const hallArray = await api_results.json(); // convert to array
+  addNames(hallArray);
 
-  //This block of code turns the buttons into working accordions
-  const accordion = document.getElementsByClassName("accordion"); //Grabs the button's class
-  for (i = 0; i < accordion.length; i++) {
-    accordion[i].addEventListener("click", function() { //For changing which button is active
-      this.classList.toggle("active");
+  // inject the list of meals into the dropdowns
+  const loc = await fetch('/api/allmeals');
+  const locArray = await loc.json();
+  addMeals(locArray.data);
+
+  // This block of code turns the buttons into working button_block s
+  const button_block = document.getElementsByClassName('button_block '); // Grabs the button's class
+  for (i = 0; i < button_block.length; i++) {
+    button_block[i].addEventListener('click', function() { // For changing which button is active
+      this.classList.toggle('active');
 
       const drop = this.nextElementSibling;
-      if (drop.style.display === "flex") { //Toggles the dropdown open and close
-        drop.style.display = "none";
+      if (drop.style.display === 'flex') { // Toggles the dropdown open and close
+        drop.style.display = 'none';
       } else {
-        drop.style.display = "flex";
+        drop.style.display = 'flex';
+        const acc = drop.getElementsByClassName('accordion'); // Grabs the button's class
+        for (j = 0; j < acc.length; j++) {
+          acc[j].addEventListener('click', function() { // For changing which button is active
+            this.classList.toggle('active');
+            const panel = this.nextElementSibling;
+            if (panel.style.display === 'flex') { // Toggles the dropdown open and close
+              panel.style.display = 'none';
+            } else {
+              panel.style.display = 'flex';
+            }
+          });
+        }
       }
     });
-    
   }
-
-
-  //create the table
-  
 }
 
 document.addEventListener('DOMContentLoaded', async() => mainEvent());

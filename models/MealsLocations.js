1a3541fe165c482e8a3f5547d545e29776df9158
JKMensah <jmensah2@terpmail.umd.edu>
2022-05-09 14:10:55 -0400

    fixed MealsLocations page

--- models/MealsLocations.js
+++ models/MealsLocations.js
@@ -5,13 +5,22 @@ export default (sequelize, DataTypes) => {
       hall_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        references: {
+          model: 'dining_hall',
+          key: 'hall_id'
+        }
       },
       meal_id: {
         type: DataTypes.INTEGER,
         allowNull: false,
+        references: {
+          model: 'meals',
+          key: 'meal_id'
+        }
       },
     },
     { freezeTableName: true, timestamps: false }
   );
+  MealsLocations.removeAttribute('id');
   return MealsLocations;
 };

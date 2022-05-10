acbd19907644e24a98b20ebd0243725d8ae3a06f
lkillea <64662431+lkillea@users.noreply.github.com>
2022-05-10 14:11:18 -0400

    Delete attempt
    Still having issues getting HTML dropdown to work

--- client/delrec.js
+++ client/delrec.js
@@ -7,6 +7,18 @@ artistDelForm.getElementsByClassName.display = 'none';
 genreDelForm.getElementsByClassName.display = 'none';
 labelDelForm.getElementsByClassName.display = 'none';
 
+function formToObject(htmlFormElement) {
+  const formItem = new FormData(htmlFormElement).entries();
+  const formArray = Array.from(formItem);
+  const formObject = formArray.reduce((collection, item, index) => {
+    if (!collection[item[0]]) {
+      collection[item[0]] = item[1];
+    }
+    return collection;
+  }, {});
+  return formObject;
+};
+
 albumDelForm.addEventListener('submit', async (SubmitEvent) => {
   SubmitEvent.preventDefault();
   console.log(SubmitEvent);
@@ -85,3 +97,109 @@ labelDelForm.addEventListener('submit', async (submitEvent) => {
   console.log('return from POST', postResult);
   console.log('return from POST JSON', postResultJSON);
 });
+
+// CREATES TABLE FOR LABEL USING JSON DATA FROM DATABASE
+tableMakeLabel = fetch('/api/label').then((data) => {
+  console.log(data);
+  return data.json();
+}).then((objectData) => {
+  console.log(objectData);
+  let tableData = '';
+  objectData.map((values) => {
+    tableData += `<tr>
+      <td>${values.label_id}</td>
+      <td>${values.label_name}</td>
+    </tr>`;
+  });
+  document.getElementById('table_body_label').innerHTML = tableData;
+}).catch((err) => {
+  console.log(err);
+});
+  
+// Selectors for different tables within our application/database
+const x = document.getElementById('artist');
+const y = document.getElementById('album');
+const z = document.getElementById('genre');
+const a = document.getElementById('label');
+x.style.display = 'none';
+y.style.display = 'none';
+z.style.display = 'none';
+a.style.display = 'none';
+  
+// Function to display full details for records
+function changeTable() {
+  let dropdown = document.getElementById('selectBox')
+  let index = dropdown.selectedIndex;
+  if (index === 1) {
+    x.style.display = 'block';
+    y.style.display = 'none';
+    z.style.display = 'none';
+    a.style.display = 'none';
+  }
+  
+  else if (index === 2) {
+    y.style.display = 'block';
+    x.style.display = 'none';
+    z.style.display = 'none';
+    a.style.display = 'none';
+  }
+  
+  else if (index === 3) {
+    z.style.display = 'block';
+    x.style.display = 'none';
+    y.style.display = 'none';
+    a.style.display = 'none';
+  }
+  
+  else if (index === 4) {
+    a.style.display = 'block';
+    x.style.display = 'none';
+    y.style.display = 'none';
+    z.style.display = 'none'
+  }
+  else if (index !== 1 || index !== 2 || index !== 3 || index !== 4) {
+    x.style.display = 'none';
+    y.style.display = 'none';
+    z.style.display = 'none';
+    a.style.display = 'none';
+  }
+}
+
+// Functon to Show Form to Delete Table
+function deleteTable() {
+  let dropdown = document.getElementById('delBox')
+  let index = dropdown.selectedIndex;
+  if (index === 1) {
+    albumForm.style.display = 'none';
+    artistForm.style.display = 'block';
+    genreForm.style.display = 'none';
+    labelForm.style.display = 'none';
+  }
+  
+  else if (index === 2) {
+    albumForm.style.display = 'block';
+    artistForm.style.display = 'none';
+    genreForm.style.display = 'none';
+    labelForm.style.display = 'none';
+  }
+  
+  else if (index === 3) {
+    albumForm.style.display = 'none';
+    artistForm.style.display = 'none';
+    genreForm.style.display = 'block';
+    labelForm.style.display = 'none';
+  }
+  
+  else if (index === 4) {
+    albumForm.style.display = 'none';
+    artistForm.style.display = 'none';
+    genreForm.style.display = 'none';
+    labelForm.style.display = 'block';
+  }
+  else if (index !== 1 || index !== 2 || index !== 3 || index !== 4) {
+    albumForm.style.display = 'none';
+    artistForm.style.display = 'none';
+    genreForm.style.display = 'none';
+    labelForm.style.display = 'none';
+  }
+}

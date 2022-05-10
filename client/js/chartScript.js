f615a7b2bf78fcbfda14913237aec173984deedc
Danny <ddeneau@terpmail.umd.edu>
2022-05-10 01:32:59 -0400

    Added the basic steps for getting data from our API into our front page.
    See charts.html and chartScript for details.
    Main issues:
    Data coming causes the usually horizontal rows of album covers to flow vertically down the page in a column (why?)

--- client/js/chartScript.js
+++ client/js/chartScript.js
@@ -1,9 +1,32 @@
+function createHTMLList(listIn) { // Populates the list elements in the HTML model.
+    // console.log('Fired HTML creator')
+    // console.log(listIn);
+    const targetlist = document.querySelector('.songs');
+    targetlist.innerHTML = '';
+    listIn.forEach((item) => {
+      const {name} = item;
+      const displayName = "placeholder_name"
+      targetlist.innerHTML += injectThisItem;
+      const injectThis =  `<li><img src="placeholder.png" alt="Placeholder Image"> ${displayName} </li>`
+      // console.log(injectThisItem);
+      targetlist.innerHTML += injectThis;
+    });
+  
+    return targetlist;
+  }
+
 async function mainEvent() {
     const billboard = document.querySelector('#billboard');
     const spotify = document.querySelector('#spotitfy');
     const apple = document.querySelector('#apple');
     const pandora = document.querySelector('#pandora');
     console.log('Query selectors selected successfully');
+
+    billboard.addEventListener('click', async () => {
+        const resp = await fetch('/api/songs')
+        const songsJson = await resp.json()
+        createHTMLList(songsJson)
+    });
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); 

d972da629fb4b1ecd0164f9291e94a35cd9af29c
dcathcart22 <dylancathcart@gmail.com>
2022-05-09 20:42:34 -0400

    fixes
    cleaning up

--- server/routes/dylanRoutes.js
+++ server/routes/dylanRoutes.js
@@ -17,18 +17,7 @@ router.get('/reviews', async (req, res) => {
     res.send("Server error");
   }
 });
-console.log('hello');
 
-/// /////////////////////////////////
-/// ////Dining Hall Endpoints////////
-/// /////////////////////////////////
-router.get("/area", async (req, res) => {
-  try {
-    const result = await db.sequelizeDB.query("SELECT * FROM Area");
-    res.json({ data: result[0] });
-  } catch (err) {
-    console.error(err);
-    res.send("Server error");
-  }
-});
-export default router;
+
+
+

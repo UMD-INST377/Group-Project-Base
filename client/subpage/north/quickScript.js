bfb51455aa6d0e52ede1c3bc08866d4056d4c45c
SquishyWizard <cthoma25@terpmail.umd.edu>
2022-05-10 15:26:14 -0400

    Update quickScript.js

--- client/subpage/north/quickScript.js
+++ client/subpage/north/quickScript.js
@@ -1,39 +1,50 @@
 // Data Handler
 function dataHandler(dataArray) {
-    console.log('dataHandler');
-    const range = [...Array(10).keys()];
-    const listItems = range.map((item, index) => dataArray[index]);
-    return listItems;
-  }
-  
-  async function createMealTable(){
-    const results = await fetch('/api/meals');
-    const mealsJ = await results.json();
-    const mealJson = mealsJ.data
-  
-    let mealCat = '<ul>';
-    let northMeals = '<ul>';
-    for (i = 0; i < mealJson.length; i++){
-       northMeals += `<li> ${mealJson[i].meal_name} </li>`;
-       mealCat += `<li> ${mealJson[i].meal_category} </li>`;
-    } 
-  
-    northMeals += '</ul>';
-    mealCat += '</ul>';
-  
-  
-    const nameList = document.querySelector(".meal_names");
-    nameList.innerHTML = "";
-    nameList.innerHTML += northMeals;
-  
-    const catList = document.querySelector(".category");
-    catList.innerHTML = "";
-    catList.innerHTML += mealCat;
-  }
+  console.log('dataHandler');
+  const range = [...Array(dataArray.length).keys()];
+  const listItems = range.map((item, index) => dataArray[index]);
+  return listItems;
+}
 
-  async function mainEvent() {
-    createMealTable();
-  }
+// create category list
+function createCategory(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector('.category');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {meal_category} = item;
+    const injectThisItem = `<li>${meal_category}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+// create category list
+function createMeals(collection) {
+  console.log('fired HTML creator');
+  console.log(collection);
+  const targetList = document.querySelector('.meal_names');
+  targetList.innerHTML = '';
+  collection.forEach((item) => {
+    const {meal_name} = item;
+    const injectThisItem = `<li>${meal_name}</li>`;
+    targetList.innerHTML += injectThisItem;
+  });
+}
+
+// Main function
+async function mainEvent() {
+  console.log('script loaded');
+
+  // data
+  const ans = await fetch('/chandra/allmeals');
+  const entireMeal = await ans.json();
+  console.log(entireMeal);
   
-  
-  document.addEventListener('DOMContentLoaded', async () => mainEvent());
+  const inputMeal = dataHandler(entireMeal.data);
+  createCategory(inputMeal);
+  createMeals(inputMeal);
+}
+
+// this actually runs first! It's calling the function above
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

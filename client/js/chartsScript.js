fb00e8a5eb470affc0d8d9931a1ed821c269a59f
Danny <ddeneau@terpmail.umd.edu>
2022-05-10 23:32:27 -0400

    Final touches

--- client/js/chartsScript.js
+++ client/js/chartsScript.js
@@ -3,7 +3,10 @@ const coverArt =
 {1 : 'https://upload.wikimedia.org/wikipedia/en/e/e7/Moneybagg_Yo_-_A_Gangsta%27s_Pain.png',
  2 : './covers/holy_2.png',
  3: 'https://i.scdn.co/image/ab67616d0000b273ef017e899c0547766997d874',
- 4: 'https://i1.sndcdn.com/artworks-aMU4GrPiwKtpGnu1-vdrqhQ-t500x500.jpg'}
+ 4: 'https://i1.sndcdn.com/artworks-aMU4GrPiwKtpGnu1-vdrqhQ-t500x500.jpg',
+ 5: '/covers/placeholder.png'}
+
+const pageLimit = 4;
 
 //Charts Maxim
 console.log('hi');
@@ -24,11 +27,15 @@ function createGenreList(collection) {
   // collection = Object.values(collection);
   // const entries = Object.entries(collection);
   // console.log(entries);
-
+  let i = 0;
   collection.data.forEach((item) => {
+    if (i > pageLimit) {
+      return;
+    }
     const injectThisItem = `<li><img class="placeholder" src="${getImage(item.song_id)}" alt="Placeholder Image">${item.song_name}</li>`;
     console.log(item.song_name);
     targetList.innerHTML += injectThisItem;
+    i++;
   });
   console.log(Object.values(collection));
 }

372a8f34e7d6710673d7c8d26744043064b94e3a
Davidmccoy4 <73482046+Davidmccoy4@users.noreply.github.com>
2022-05-10 20:38:43 -0400

    added html elements to map page and maps to nav bar

--- client/home_script.js
+++ client/home_script.js
@@ -1,25 +1,26 @@
+const { captureRejections } = require("mysql2/typings/mysql/lib/Connection");
 
   
 
 async function mainEvent() { //mainEvent refers to page loading
-    //const hallTable = document.querySelector('.hall_list'); //get hall_list div class
-    //const results = await fetch('/api/dining'); //call get function from api routes for dining
-    //const hallArrayFromJson = await results.json(); //convert to array 
+  //const hallTable = document.querySelector('.hall_list'); //get hall_list div class
+  //const results = await fetch('/api/dining'); //call get function from api routes for dining
+  //const hallArrayFromJson = await results.json(); //convert to array 
     
-    //create the table
-    //createHTMLtable(hallArrayFromJson.data);
+  //create the table
+  //createHTMLtable(hallArrayFromJson.data);
   
-const table = document.querySelector('.table')
-const mealData = await fetch('/api/josh')
-const data = await mealData.json()
-let currentData = data
-console.log(data);
-const searchBar = document.querySelector('#search')
+  const table = document.querySelector('.table')
+  const mealData = await fetch('/api/josh')
+  const data = await mealData.json()
+  let currentData = data
+  console.log(data);
+  const searchBar = document.querySelector('#search')
 
-function createHTMLtable(currentArray) {
-  currentData = currentArray.filter((item) => item.meal_name.toLowerCase().includes(searchBar.value.toLowerCase()))
-  console.log(currentData);
-  table.innerHTML = `<tr>
+  function createHTMLtable(currentArray) {
+    currentData = currentArray.filter((item) => item.meal_name.toLowerCase().includes(searchBar.value.toLowerCase()))
+    console.log(currentData);
+    table.innerHTML = `<tr>
   <th>
   Meal Name
   </th>
@@ -30,13 +31,13 @@ function createHTMLtable(currentArray) {
  Dining Hall 
   </th>
 </tr>`
-currentData.forEach(element => {
-  table.innerHTML += `<tr> <th>${element.meal_name}</th> <th>${element.meal_category}</th> <th>${element.hall_name}</th> </tr>`
-  
-});
-}
+    currentData.forEach(element => {
+      table.innerHTML += `<tr> <th>${element.meal_name}</th> <th>${element.meal_category}</th> <th>${element.hall_name}</th> </tr>`
 
-searchBar.addEventListener('change', () => {createHTMLtable(data)})
+    });
   }
+
+  searchBar.addEventListener('change', () => {createHTMLtable(data)})
+}
   
-  document.addEventListener('DOMContentLoaded', async() => mainEvent());
+document.addEventListener('DOMContentLoaded', async() => mainEvent());

318ae9872c93f7284f2d623156fd5a879f377ae5
RandallMentzos <theguy77@umd.edu>
2022-05-10 22:40:36 -0400

    add Randall's template

---
+++ client/diets/scriptR.js
@@ -0,0 +1,10 @@
+async function mainEvent() {
+  const form = document.querySelector('.form-r')
+  const button = document.querySelector('.submit_button')
+
+  const vegan = document.querySelectorAll('')
+
+}
+
+
+document.addEventListener('DOMContentLoaded', async() => mainEvent());

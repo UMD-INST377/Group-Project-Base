bf14654a92bfe866e065312b7e3e83b2d2f6ceb6
Alex Leitch <aleitch1@umd.edu>
2022-06-20 16:20:08 -0400

    simplify final project server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-console */
 import express from 'express';
-import db from './database/initializeDB.js';
 import apiRoutes from './routes/apiRoutes.js';
 
 const app = express();
@@ -16,8 +15,6 @@ app.use('/api', apiRoutes);
 
 async function bootServer() {
   try {
-    const mysql = await db.sequelizeDB;
-    await mysql.sync();
     app.listen(PORT, () => {
       console.log(`Listening on: http//localhost:${PORT}`);
     });

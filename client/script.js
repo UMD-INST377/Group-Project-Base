5ea54b43ef6e39d3ae666022329f90cd0c40d0b1
gdemattia <gdematti@terpmail.umd.edu>
2022-11-15 16:15:16 -0500

    slight changes to html

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 // Code for year drop down list
-const checkList = document.getElementById('list1');
+const checkList = document.getElementById('yearsList');
 checkList.getElementsByClassName('anchor')[0].onclick = function(evt) {
   if (checkList.classList.contains('visible')) { checkList.classList.remove('visible'); } 
   else { checkList.classList.add('visible'); }

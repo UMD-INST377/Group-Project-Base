3077f0ef4b4014b5eae021292742bd8a65579fd4
Michael Campbell <macbestcam@gmail.com>
2022-11-16 14:42:05 -0500

    added genre counter

---
+++ requests/getGenresfromArtist.js
@@ -0,0 +1,16 @@
+// Sends the authentication token and author ids
+// Returns an object with genre counts given the authors ids
+const getTracklist = async (id,req_token) => {
+  const response = await fetch('https://umd-spotify-backend.herokuapp.com/genreslist?', {
+    method: 'GET',
+    body: JSON.stringify({
+      ids: id,
+      token: req_token
+    }), 
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  return response.json(); 
+  
+}

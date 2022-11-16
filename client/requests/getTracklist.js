1dc0ca21b00bef8382b41959548ebf87caf6857e
Michael Campbell <macbestcam@gmail.com>
2022-11-15 20:26:37 -0500

    getTracklist added

---
+++ client/requests/getTracklist.js
@@ -0,0 +1,16 @@
+// Sends the authentication token and time_frame - (short_term,medium_term,long_term)
+// Returns an array of songs
+const getTracklist = async (req_term,req_token) => {
+    const response = await fetch('https://umd-spotify-backend.herokuapp.com/tracklist?', {
+      method: 'GET',
+      body: JSON.stringify({
+        term: req_term,
+        token: req_token
+      }), 
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    return response.json(); 
+    
+  }

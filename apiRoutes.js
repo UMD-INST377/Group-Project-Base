f5be688d65bcf5429aa1161657e5e648cca81471
Biniam Zemedkun <ccobra230@gmail.com>
2022-11-18 15:27:46 -0500

    added new files

--- apiRoutes.js
+++ apiRoutes.js
@@ -2,17 +2,17 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
-import foodService from './foodService.js';
+import speedCameras from './speedCameras.js';
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the UMD Dining API!');
+  res.send('Welcome to the PG County API!');
 });
 
 // Generic API inclusion demonstration
 // Replace this with the group member's actual route
 // This leads to /api/member1
-router.use('/foodService', foodService);
+router.use('/speedCameras', speedCameras);
 
 export default router;

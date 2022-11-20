7347bd82b37df5d19d379ef704155c07a1455998
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2022-11-20 12:50:21 -0500

    makes getData async and with error catch

--- Middleware/getLitterAPI.js
+++ Middleware/getLitterAPI.js
@@ -1,10 +1,8 @@
-
-
-function getData() {
+async function getData() {
   try {
     const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json'
-    const response = await fetch('https://ghibliapi.herokuapp.com/films')
-    const data = await response.json()
+    const response = await fetch(url);
+    const data = await response.json();
   } catch (err) {
     console.log('Data request failed', err);
     res.json({ message: 'Data request failed', error: err });

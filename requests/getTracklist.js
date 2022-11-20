0924b0478d7161b60140ab71218f5ef70092722a
Michael Campbell <macbestcam@gmail.com>
2022-11-20 15:06:58 -0500

    fixed/tested request and added docs

--- requests/getTracklist.js
+++ requests/getTracklist.js
@@ -1,16 +1,24 @@
-// Sends the authentication token and time_frame - (short_term,medium_term,long_term)
-// Returns an array of songs
-const getTracklist = async (req_term,req_token) => {
-    const response = await fetch('https://umd-spotify-backend.herokuapp.com/tracklist?', {
-      method: 'GET',
-      body: JSON.stringify({
-        term: req_term,
-        acesss_token: req_token
-      }), 
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    return response.json(); 
-    
+/*
+  Fetch request to get top 50 songs - Name, popularity, 
+  Receives: Access token(obtained through login),term(short_term,medium_term,long_term)
+  Note: Short term may not return anything if you haven't listened to spotify recently.
+  Response Example: 
+  {
+  "name": "Rich Flex",
+  "popularity": 94,
+  "artists": ["Drake","21 Savage"]
   }
+*/
+
+const getTracklist = async (req_term, req_token) => {
+  url = "https://umd-spotify-backend.herokuapp.com/tracklist?";
+  const response = await fetch(
+    url +
+      new URLSearchParams({
+        access_token: req_token,
+        term: req_term,
+      })
+  );
+  const data = await response.json();
+  return data;
+};

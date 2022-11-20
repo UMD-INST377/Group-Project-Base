af2355a81457763f4e99fa7e15a927ed6dc72b39
Michael Campbell <macbestcam@gmail.com>
2022-11-20 14:14:08 -0500

    initial requst example

--- pages/graph_folder/script.js
+++ pages/graph_folder/script.js
@@ -1,24 +1,27 @@
 let params = new URL(document.location).searchParams;
 let token = params.get("token");
 localStorage.setItem("acceess_token", token);
-
+url = "https://umd-spotify-backend.herokuapp.com/tracklist?";
 // Sends the authentication token and time_frame - (short_term,medium_term,long_term)
 // Returns an array of songs
-const getTracklist = async (req_term,req_token) => {
-  const response = await fetch('https://umd-spotify-backend.herokuapp.com/tracklist?', {
-    method: 'GET',
-    body: JSON.stringify({
-      term: req_term,
-      accesss_token: req_token
-    }), 
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return response.json(); 
-  
-}
-const data = getTracklist(token,term='short_term')
-const page = document.getElementById("data");
-page.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`
+const getTracklist = async (req_term, req_token) => {
+  const response = await fetch(
+    url +
+      new URLSearchParams({
+        access_token: req_token,
+        term: req_term,
+      })
+  );
+  const data = await response.json();
+  return data;
+};
 
+const page = document.getElementById("data");
+getTracklist("short_term", token).then((data) => {
+  console.log("page");
+  console.log(data);
+  page.innerHTML = `<pre>${JSON.stringify(data, null, 2)}</pre>`;
+});
+
+console.log("token");
+console.log(token);

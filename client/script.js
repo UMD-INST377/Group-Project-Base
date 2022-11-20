b0ff18036f9b3acaff8daef00d7fcd9a5fdd9f20
emilyklomparens <emilyklomparens@gmail.com>
2022-11-20 15:55:01 -0500

    fixed sizing of chart

--- client/script.js
+++ client/script.js
@@ -1,17 +1,3 @@
-function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector('#restaurant_list');
-  target.innerHTML = '';
-
-  const listEl = document.createElement('ol');
-  target.appendChild(listEl);
-  list.forEach((item) => {
-    const el = document.createElement('li');
-    el.innerText = item.name;
-    listEl.appendChild(el);
-  });
-}
-
 function processCrimes(list, sector) {
   return list.filter((item) => item.pgpd_sector === (sector));
 }
@@ -47,7 +33,7 @@ function changeChart(chart, dataObject) {
   chart.update();
 }
 
-function shapeDataForLineChart(array) {
+function shapeDataForBarChart(array) {
   return array.reduce((collection, item) => {
     if (!collection[item.clearance_code_inc_type]) {
       collection[item.clearance_code_inc_type] = [item];
@@ -67,31 +53,17 @@ async function getData() {
 }
 async function mainEvent() {
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  // const submit = document.querySelector('#get-resto'); // get a reference to your submit button
-  //  const loadAnimation = document.querySelector('.lds-ellipsis'); // get a reference to our loading animation
   const chartTarget = document.querySelector('#myChart');
-  // submit.style.display = 'none'; // let your submit button disappear
 
   const chartData = await getData();
 
-  const shapedData = shapeDataForLineChart(chartData);
+  const shapedData = shapeDataForBarChart(chartData);
   const myChart = initChart(chartTarget, shapedData);
 
-  // This IF statement ensures we can't do anything if we don't have information yet
-  // if (chartData?.length > 0) { // the question mark in this means "if this is set at all"
-  // submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
-
-  // Lets hide the load button now that we have some data to manipulate
-  // loadAnimation.classList.remove('lds-ellipsis');
-  // loadAnimation.classList.add('lds-ellipsis_hidden'); // Lets turn the submit button back on by setting its display as a block when we have data
-
   form.addEventListener('input', (event) => {
-    console.log(event.target.value);
     sector = event.target.value;
     const filteredList = processCrimes(chartData, event.target.value);
-    console.log(filteredList);
-    //injectHTML(filteredList);
-    const localData = shapeDataForLineChart(filteredList);
+    const localData = shapeDataForBarChart(filteredList);
     changeChart(myChart, localData);
   });
 }

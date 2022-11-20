ec3154fd1b9c062a59f755f4f9d85de513b1435e
Parilane <112584129+Parilane@users.noreply.github.com>
2022-11-20 12:46:21 -0500

    populating

--- Middleware/getLitterAPI.js
+++ Middleware/getLitterAPI.js
@@ -1,4 +1,16 @@
+
+
 function getData() {
   const response = await fetch('https://ghibliapi.herokuapp.com/films')
   const data = await response.json()
+}
+
+var data = JSON.parse(this.response)
+
+if (request.status >= 200 && request.status < 400) {
+  data.forEach(movie => {
+    console.log(movie.title)
+  })
+} else {
+  console.log('error')
 }

07840fe17682656c87dfb1a952b2c7e687b0e8d3
Yabing Yang <98364345+yyang7171@users.noreply.github.com>
2022-11-20 12:46:47 -0500

    edits to getData

--- Middleware/getLitterAPI.js
+++ Middleware/getLitterAPI.js
@@ -1,4 +1,10 @@
 function getData() {
-  const response = await fetch('https://ghibliapi.herokuapp.com/films')
-  const data = await response.json()
+  try {
+    const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json'
+    const response = await fetch('https://ghibliapi.herokuapp.com/films')
+    const data = await response.json()
+  } catch (err) {
+    console.log('Data request failed', err);
+    res.json({ message: 'Data request failed', error: err });
+  }
 }

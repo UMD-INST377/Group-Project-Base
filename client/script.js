e39c04bdc211f9ccfa0596f4a41a6f719d65a6e0
eniteCZ <f.radmanovic@gmail.com>
2022-11-21 22:22:31 -0500

    Finished preliminary API controler

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 const APIControler = (function(){
 
-    const clientId = '';
-    const clientSecret = '';
+    const clientId = 'd9827efb2c79463b92becb457a635a04';
+    const clientSecret = '6f2f0a36046f4f21980873a48c7bdab0';
 
     // Private methods
     const _getToken = async () =>  {
@@ -17,6 +17,42 @@ const APIControler = (function(){
 
         const data = await result.json();
         return data.access_token;
+    }
+
+    const _getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 10;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: {'Authorization' : 'Bearer' + token}
+        });
+
+        const data = result.json();
+        return data.items;
+    }
+
+    const _getTrack = async (token, tracksEndPoint) => {
+
+        const result = await fetch(`${tracksEndPoint}`, {
+            method: 'GET',
+            headers: {'Authorization' : 'Bearer' + token}
+        });
+
+        const data = result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return _getToken();
+        },
+        getTracks(token, tracksEndPoint) {
+            return _getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, tracksEndPoint) {
+            return _getTrack(token, tracksEndPoint);
+        }
     }
 
 })();

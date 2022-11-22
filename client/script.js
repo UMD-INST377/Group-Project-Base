44b0034830d250d8488fa71772f4cb377c18887f
eniteCZ <f.radmanovic@gmail.com>
2022-11-22 10:28:59 -0500

    Moved away from object oriented way of coding to simpler stuff

--- client/script.js
+++ client/script.js
@@ -1,58 +1,60 @@
-const APIControler = (function(){
+const clientId = 'd9827efb2c79463b92becb457a635a04';
+const clientSecret = '6f2f0a36046f4f21980873a48c7bdab0';
 
-    const clientId = 'd9827efb2c79463b92becb457a635a04';
-    const clientSecret = '6f2f0a36046f4f21980873a48c7bdab0';
+// API dialogue functions
 
-    // Private methods
-    const _getToken = async () =>  {
+async function getToken() {
 
-        const result = await fetch('https://accounts.spotify.com/api/token',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
-            },
-            body: 'grant_type=client_credentials'
-        });
-
-        const data = await result.json();
-        return data.access_token;
-    }
-
-    const _getTracks = async (token, tracksEndPoint) => {
-
-        const limit = 10;
-
-        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
-            method: 'GET',
-            headers: {'Authorization' : 'Bearer' + token}
-        });
-
-        const data = result.json();
-        return data.items;
-    }
-
-    const _getTrack = async (token, tracksEndPoint) => {
-
-        const result = await fetch(`${tracksEndPoint}`, {
-            method: 'GET',
-            headers: {'Authorization' : 'Bearer' + token}
-        });
-
-        const data = result.json();
-        return data;
-    }
-
-    return {
-        getToken() {
-            return _getToken();
+    const result = await fetch('https://accounts.spotify.com/api/token',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic ' + btoa(clientId + ':' + clientSecret)
         },
-        getTracks(token, tracksEndPoint) {
-            return _getTracks(token, tracksEndPoint);
-        },
-        getTrack(token, tracksEndPoint) {
-            return _getTrack(token, tracksEndPoint);
-        }
-    }
+        body: 'grant_type=client_credentials'
+    });
 
-})();
+    const data = await result.json();
+    return data.access_token;
+}
+
+async function getPlaylistsByGenre(token, genre, limit) {
+
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + token}
+    });
+
+    const data = await result.json();
+    return data.playlists.items;
+}
+
+async function getTracks(token, tracksEndPoint) {
+
+    const limit = 10;
+
+    const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+        method: 'GET',
+        headers: {'Authorization' : 'Bearer' + token}
+    });
+
+    const data = result.json();
+    return data.items;
+}
+
+async function getTrack(token, tracksEndPoint) {
+
+    const result = await fetch(`${tracksEndPoint}`, {
+        method: 'GET',
+        headers: {'Authorization' : 'Bearer' + token}
+    });
+
+    const data = result.json();
+    return data;
+}
+
+const token = getToken();
+const playlist = getPlaylistsByGenre(token, "Rock", 1);
+const tracks = [] ;
+
+const songs = "";

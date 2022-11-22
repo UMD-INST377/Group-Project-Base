e7fa24e632d49f479bbe9530bccd278e5038bb32
Michael Campbell <macbestcam@gmail.com>
2022-11-22 12:31:18 -0500

    working static base

--- pages/umd_folder/script.js
+++ pages/umd_folder/script.js
@@ -1,33 +1,66 @@
-/* This function will collect all UMD students data and display top songs based on like */
-/* Create two functions*/
-/* Create a function that displays top ten songs */
-/* Create a chart associated with it */
+/* Data Request to API */ 
+const getTracklist = async (req_term, req_token) => {
+  url = "https://umd-spotify-backend.herokuapp.com/tracklist?";
+  const response = await fetch(
+    url +
+      new URLSearchParams({
+        access_token: req_token,
+        term: req_term,
+      })
+  );
+  const data = await response.json();
+  console.log(data)
+  return data;
+};
+token = ''
+term = 'long_term'
+if (token !== null) {
+  localStorage.setItem("access_token", token);
+} else {
+  token = localStorage.getItem("access_token");
+}
 
-/*async keywork means that we can make API requests */ 
-/* fetch the data and display in console*/ 
+console.log("token");
+console.log(token);
 
-/* Router.get request */ 
+let res = getTracklist(term,token)
 
+/*async function getTracklist(req_term,req_token){
+  const response = await fetch('https://umd-spotify-backend.herokuapp.com/tracklist?', {
+    method: 'get',
+    access_token: req_token,
+    term:req_term,
+    headers: {
+      'Content-Type':'application/json'
+    },
+  });
+  const json = await response.json();
+  console.log(json);
+}
+getTracklist();
+*/
 
-// Sends the authentication token and time_frame - (short_term,medium_term,long_term)
-// Returns an array of songs
+/* Create a barchart with popularity scores of each track from the list*/ 
+function initChart(){
+const ctx = document.getElementById('myChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Track 1', 'Track 2', 'Track 3', 'Track 4', 'Track 5', 'Track 6'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
 
-
-/*const getTracklist = async (req_term,req_token) => {
-    const response = await fetch('https://umd-spotify-backend.herokuapp.com/tracklist?', {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer` + req_term + req_token,
-      }, 
-    });
-    return response.json(); 
-    
-  }*/
-
-
-
-
-
-
-
-
+initChart()

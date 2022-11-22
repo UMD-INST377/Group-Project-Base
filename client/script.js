4ba3d9d66f94b63ae404640ea2b8a75d14dd73ab
Michael Chang <Mchang18@terpmail.umd.edu>
2022-11-22 15:03:05 -0500

    chart.js

---
+++ client/script.js
@@ -0,0 +1,45 @@
+// API TMDB
+const apiKey = 'api_key=b5fa8ab3e139be5c0b6e60ab752e3d09';
+const base_url = 'https://api.themoviedb.org/3';
+const apiURL = `${base_url}/discover/movie?sort_by=popularity.desc&${apiKey}`;
+
+async function getMovies() {
+  const MovieURL = `${base_url}/discover/movie?sort_by=popularity.desc&${apiKey}`;
+  const response = await fetch(MovieURL);
+  const ChartData = await response.json();
+
+  const voteNumber = ChartData.results.map((x) => x.vote_average);
+  const titleName = ChartData.results.map((x) => x.title);
+
+  console.log(voteNumber, titleName);
+
+  vote_average = voteNumber;
+  title = titleName;
+}
+
+async function dummyChart() {
+  await getMovies();
+
+  const ctx = document.getElementById('myChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: title,
+      datasets: [{
+        label: '# of Votes',
+        data: vote_average,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+dummyChart();

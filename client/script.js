0550744bcf6e5c15ca50d1bc8e268954dadc31c0
kevJamer <102767072+kevJamer@users.noreply.github.com>
2022-11-22 11:51:39 -0500

    working on js

--- client/script.js
+++ client/script.js
@@ -1,1 +1,23 @@
-console.log("hello world")
+
+function createplaylsit(array, song) {
+    playlist = [];
+    array.forEach((element) => { // not sure if element is correct
+       // will have to do an if statement grabbing songs wthat have the same genre
+       // add them to a new playlist array
+    });
+    return playlist;
+}
+
+
+
+async function mainEvent() {
+    // map here or something
+     const results =await fetch()
+}
+
+ document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+/*
+
+An asynchronous data request to your API
+A processing request that uses array methods (.map, .filter, .find, .reduce) to change your data into the shape your chart, map, or other component needs for display
+*/

d22da52e5af65d20af8906210dbb7bc2c07a582e
Michael Campbell <macbestcam@gmail.com>
2022-11-22 12:26:43 -0500

    Tested and working

--- requests/getGenresfromArtist.js
+++ requests/getGenresfromArtist.js
@@ -1,17 +1,25 @@
-// Sends the authentication token and author ids
-// Returns an object with genre counts given the authors ids
-const getTracklist = async (id,req_token) => {
-  const response = await fetch('https://umd-spotify-backend.herokuapp.com/genreslist?', {
-    method: 'GET',
-    body: JSON.stringify({
-      ids: id,
-      access_token: req_token
-    }), 
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return response.json(); 
-  
-}
-
+/*
+  Fetch request to get genres count based on artist ids
+  Receives: Access token(obtained through login), artist ids(string of artist ids separated by commas)
+  and an object from previous iteration(optional)
+  Note: Short term may not return anything if you haven't listened to spotify recently.
+  Response Example: 
+  {
+    "alternative r&b": 4,
+    "indie jazz": 3,
+    "indie r&b": 4,
+    "indie soul": 3,
+  }
+*/
+const getGenresCount = async (artist_ids, req_token) => {
+  url = "https://umd-spotify-backend.herokuapp.com/genreslist?";
+  const response = await fetch(
+    url +
+      new URLSearchParams({
+        id_string: artist_ids,
+        access_token: req_token,
+      })
+  );
+  const data = await response.json();
+  return data;
+};

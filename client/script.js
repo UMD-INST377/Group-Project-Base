6cdea9d0b13eec6f12b458d45860e08722297efb
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-11-23 12:21:26 -0500

    Rebased branch

--- client/script.js
+++ client/script.js
@@ -1,8 +1,7 @@
-
 function shapeDataForLineChart(array) {
   return array.reduce((collection, item) => {
-    if(!collection[item.category]) {
-      collection[item.category] = [item]
+    if (!collection[item.category]) {
+      collection[item.category] = [item];
     } else {
       collection[item.category].push(item);
     }
@@ -11,30 +10,30 @@ function shapeDataForLineChart(array) {
 }
 
 function initChart(targetElement, dataObject) {
-    const labels = Object.keys(dataObject);
-    const info = Object.keys(dataObject).map((item) => dataObject[item].length);
-  
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: 'Restaurants By Category',
-        backgroundColor: 'rgb(255, 60, 255)',
-        borderColor: 'rgb(255, 255, 255)',
-        borderWidth: '3',
-        data: info
-      }]
-    };
-    const config = {
-      type: 'bar',
-      data: data,
-      options: {}
-    };
-  
-    return new Chart(
-      targetElement,
-      config
-    );
-  }
+  const labels = Object.keys(dataObject);
+  const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'Restaurants By Category',
+      backgroundColor: 'rgb(255, 60, 255)',
+      borderColor: 'rgb(255, 255, 255)',
+      borderWidth: '3',
+      data: info
+    }]
+  };
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+
+  return new Chart(
+    targetElement,
+    config
+  );
+}
 
 async function getData() {
   const url = 'https://api.coingecko.com/api/v3/coins/categories?order=name_asc'; // remote URL! you can test it in your browser
@@ -43,21 +42,20 @@ async function getData() {
   const reply = json.filter((item));
   console.log(reply);
   return reply;
-  }
+}
 
 async function mainEvent() {
   const form = document.querySelector('.main_form');
   const currentlist = [];
 
   form.addEventListener('submit', (submitEvent) => {
-    const cryptoData =  getData();
-    cryptoData.append(currentlist)
-    console.log(currentlist)
+    const cryptoData = getData();
+    cryptoData.append(currentlist);
+    console.log(currentlist);
   });
-  
+
   const chartData = await getData();
   const shapedData = shapeDataForLineChart(chartData);
   console.log(shapedData);
   const myChart = initChart(chartTarget, shapedData);
-
 }

8692af1f7737c878ea650bd29d5b5505423fa097
ZW975 <71403189+ZW975@users.noreply.github.com>
2022-11-23 00:00:34 -0500

    first time get some data

--- client/script.js
+++ client/script.js
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 /* leaving space here to add more functions */
 async function getData() {
-  var accessToken = "BQCwQEe5cq9Ncxlozh7QOdsEi9MAN7QEMfU";
+  var accessToken = "BQDTbois3Mm6vqnkBzrVhk4mNE1oTvmFYhX_b-7sezq2ZUchPAt7V6yn3hZ7iPiR3a8ZSdOINrlzCve2YW7jhoadfLWM8rEsD2yXNjGU_C4PBmiXKA37piRWtaZHjMtMBbyPd4Ip86vz4eQrVhbwq74CO7_LZPnXxPTyR0ZQckhR51t8I_hqRbN2uULeandN1ug";
   fetch("https://api.spotify.com/v1/albums/7jaSNQUBJbvfbZHLNFrV7P", {
     method: "GET",
     headers: {
@@ -9,26 +9,19 @@ async function getData() {
       "Content-Type": "application/json",
       Authorization: "Bearer " + accessToken,
     },
+  }).then((response) => {
+    console.log(
+      response.json().then((data) => {
+        console.log(data);
+      })
+    );
   });
-
-  const json = await data.json();
-  return json;
 }
 
 async function mainEvent() {
   const form = document.querySelector(".main_form");
 
   let albums = [];
-
-  form.addEventListener("input", (event) => {
-    nameList = [];
-    const albumName = getData(event.target.value);
-    console.log(albumName);
-    albumName.forEach((item, index) => {
-      const { name } = item.name;
-      nameList.push(name);
-      console.log(nameList);
-    });
-  });
+  getData();
 }
 document.addEventListener("DOMContentLoaded", async () => mainEvent());

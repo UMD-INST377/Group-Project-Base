abef8082c30908f31b6b55fceca91ff88f353af4
Biniam Zemedkun <ccobra230@gmail.com>
2022-11-22 22:59:43 -0500

    erased comment

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function processCameras(list) {
   console.log('speed cameras list');
   const range = [...Array(15).keys()];
-  // eslint-disable-next-line no-unused-vars
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];

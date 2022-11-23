c3abb3891216ea1d68db876db3091de04f4fa86f
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-11-22 20:42:03 -0500

    put API request in scripts.js

--- client/script.js
+++ client/script.js
@@ -1,13 +1,26 @@
 /* leaving space here to add more functions */
 /// test
+
+async function getData() {
+  const url ='https://api-nba-v1.p.rapidapi.com/players';
+  const options = {
+    headers: {
+      'X-RapidAPI-Key': '3264b40dd2mshd5149564bef5bf8p1bdbb3jsn1cb056c041c4',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+  }
+  const data = await fetch(url, options);
+  const json = await data.json();
+  return json;
+}
+
 async function mainEvent() {
   console.log('Hello World!');
-  const results = await fetch('/api/NBA');
-  const arrayFromJson = await results.json();
-
-  console.table(arrayFromJson.data);
-  console.log(arrayFromJson.data[0]);
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].id}`);
+  const results = await getData();
+  console.log(results)
+  console.table(results.data);
+  console.log(results.data[0]);
+  console.log(`${results.data[0].name} ${results.data[0].id}`);
 
   /// if (!arrayFromJson.data?.length) { return; }
 }

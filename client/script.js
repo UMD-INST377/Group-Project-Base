8ff3f1f7d9d30474a948f38d55a1a4e1c2eecc1f
JPHart98 <89784428+JPHart98@users.noreply.github.com>
2022-11-23 14:57:22 -0500

    Added async function and code to js file
    Removed the apiRoutes.js and foodService.js and added a get request and the code needed to run the request

--- client/script.js
+++ client/script.js
@@ -1,7 +1,22 @@
+<<<<<<< Updated upstream
 function injectHTML(list) {
   const target = document.querySelector('#song_list');
   target.innerHTML = '';
   
+=======
+/* eslint linebreak-style: ["error", "windows"] */
+
+function getRandomInclusive(min, max) {
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
+}
+function injectHTML(list) {
+  console.log('fired injectHTML');
+  const target = document.querySelector('#house_list');
+  target.innerHTML = '';
+
+>>>>>>> Stashed changes
   const listEl = document.createElement('ol');
   target.appendChild(listEl);
 
@@ -10,6 +25,7 @@ function injectHTML(list) {
     el.innerText = item.name;
     listEl.appendChild(el);
   });
+<<<<<<< Updated upstream
 
 }
 
@@ -38,6 +54,93 @@ async function mainEvent() {
       console.log(songList);
       injectHTML(songList)
     })
+=======
+}
+function filterList(list, filterInputValue) {
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = filterInputValue.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
+}
+function processHouse(list) {
+  const range = [...Array(10).keys()];
+  const newArray = range.map((item) => {
+    const index = getRandomInclusive(0, list.length);
+    return list[index];
+  });
+  return newArray;
+}
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+function markerPlace(array, map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/9hyf-46qb.json';
+  const data = await fetch(url);
+  const json = await data.json();
+  const reply = json.filter((item) => Boolean(item.location)).filter((item) => Boolean(item.inspection_id));
+  return reply;
+}
+async function mainEvent() {
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-house');
+  const loadAnimation = document.querySelector('.lds-ellipsis');
+  submit.style.display = 'none';
+  const pageMap = initMap();
+
+  const mapData = await getData();
+
+  // submit.style.display = 'none';
+
+  console.table(mapData);
+  console.log(mapData[0]);
+
+  console.log(
+    `${mapData[0].location} ${mapData[0].inspection_id}`
+  );
+
+  if (mapData.length > 0) {
+    submit.style.display = 'block';
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
+
+    let currentList = [];
+    form.addEventListener('input', (event) => {
+      console.log(event.target.value);
+      const newFilterList = filterList(currentList, event.target.value);
+      injectHTML(newFilterList);
+      markerPlace(newFilterList, pageMap);
+    });
+
+    form.addEventListener('submit', (SubmitEvent) => {
+      SubmitEvent.preventDefault();
+
+      currentList = processHouse(mapData);
+
+      injectHTML(currentList);
+      markerPlace(currentList, pageMap);
+    });
+>>>>>>> Stashed changes
   }
 }
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

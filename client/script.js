ec7e822afc98f361a7892b134980e81b3ccc4344
ZW975 <71403189+ZW975@users.noreply.github.com>
2022-11-22 21:11:47 -0500

    little update

--- client/script.js
+++ client/script.js
@@ -1,89 +1,95 @@
-async function getData() {
-    var SpotifyWebApi = require('spotify-web-api-node');
-const express = require('express')
+const { request } = require('express');
 
-const scopes = [
-    'ugc-image-upload',
-    'user-read-playback-state',
-    'user-modify-playback-state',
-    'user-read-currently-playing',
-    'streaming',
-    'app-remote-control',
-    'user-read-email',
-    'user-read-private',
-    'playlist-read-collaborative',
-    'playlist-modify-public',
-    'playlist-read-private',
-    'playlist-modify-private',
-    'user-library-modify',
-    'user-library-read',
-    'user-top-read',
-    'user-read-playback-position',
-    'user-read-recently-played',
-    'user-follow-read',
-    'user-follow-modify'
+function initChart() {
+
+}
+
+async function getSpotifyAPI() {
+  var SpotifyWebApi = require("spotify-web-api-node");
+  const express = require("express");
+
+  const scopes = [
+    "ugc-image-upload",
+    "user-read-playback-state",
+    "user-modify-playback-state",
+    "user-read-currently-playing",
+    "streaming",
+    "app-remote-control",
+    "user-read-email",
+    "user-read-private",
+    "playlist-read-collaborative",
+    "playlist-modify-public",
+    "playlist-read-private",
+    "playlist-modify-private",
+    "user-library-modify",
+    "user-library-read",
+    "user-top-read",
+    "user-read-playback-position",
+    "user-read-recently-played",
+    "user-follow-read",
+    "user-follow-modify",
   ];
-  
-// credentials are optional
-var spotifyApi = new SpotifyWebApi({
-    clientId: 'e187afb42b73476980c03329ce8256eb',
-    clientSecret: 'c8b47ada78f74dfbaffa9527f8d49ee1',
-    redirectUri: 'http://localhost:8888/callback'
+
+  // credentials are optional
+  var spotifyApi = new SpotifyWebApi({
+    clientId: "e187afb42b73476980c03329ce8256eb",
+    clientSecret: "c8b47ada78f74dfbaffa9527f8d49ee1",
+    redirectUri: "http://localhost:8888/callback",
   });
-  
+
   const app = express();
-  
-  app.get('/login', (req, res) => {
+
+  app.get("/login", (req, res) => {
     res.redirect(spotifyApi.createAuthorizeURL(scopes));
   });
-  
-  app.get('/callback', (req, res) => {
+
+  app.get("/callback", (req, res) => {
     const error = req.query.error;
     const code = req.query.code;
     const state = req.query.state;
-  
+
     if (error) {
-      console.error('Callback Error:', error);
+      console.error("Callback Error:", error);
       res.send(`Callback Error: ${error}`);
       return;
     }
-  
+
     spotifyApi
       .authorizationCodeGrant(code)
-      .then(data => {
-        const access_token = data.body['access_token'];
-        const refresh_token = data.body['refresh_token'];
-        const expires_in = data.body['expires_in'];
-  
+      .then((data) => {
+        const access_token = data.body["access_token"];
+        const refresh_token = data.body["refresh_token"];
+        const expires_in = data.body["expires_in"];
+
         spotifyApi.setAccessToken(access_token);
         spotifyApi.setRefreshToken(refresh_token);
-  
-        console.log('access_token:', access_token);
-        console.log('refresh_token:', refresh_token);
-  
+
+        console.log("access_token:", access_token);
+        console.log("refresh_token:", refresh_token);
+
         console.log(
           `Sucessfully retreived access token. Expires in ${expires_in} s.`
         );
-        res.send('Success! You can now close the window.');
-  
+        res.send("Success! You can now close the window.");
+
         setInterval(async () => {
           const data = await spotifyApi.refreshAccessToken();
-          const access_token = data.body['access_token'];
-  
-          console.log('The access token has been refreshed!');
-          console.log('access_token:', access_token);
+          const access_token = data.body["access_token"];
+
+          console.log("The access token has been refreshed!");
+          console.log("access_token:", access_token);
           spotifyApi.setAccessToken(access_token);
-        }, expires_in / 2 * 1000);
+        }, (expires_in / 2) * 1000);
       })
-      .catch(error => {
-        console.error('Error getting Tokens:', error);
+      .catch((error) => {
+        console.error("Error getting Tokens:", error);
         res.send(`Error getting Tokens: ${error}`);
       });
   });
-  
+
   app.listen(8888, () =>
     console.log(
-      'HTTP Server up. Now go to http://localhost:8888/login in your browser.'
+      "HTTP Server up. Now go to http://localhost:8888/login in your browser."
     )
   );
 }

885b8715b06b04ec2dd39c102c60d047b6b81647
kevJamer <102767072+kevJamer@users.noreply.github.com>
2022-11-26 17:54:07 -0500

    added working chart

--- client/script.js
+++ client/script.js
@@ -1,29 +1,61 @@
-function getRandomInclusing(min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
 
-function injectHTML(list) {
 
+function makeChart(x, y) {
+  const ctx = document.getElementById('myChart');
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: x, // json[0] 'year'
+      datasets: [{
+        label: 'Total Population of United States', // total population
+        data: y, // json[1] 'population data' --> might have to mess with it to make it readable (not in the millions)
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 }
+
+function getYear(array) { 
+
+  ret_arr = [];
+  array.forEach((element) => {
+    ret_arr.push(element.Year);
+  });
+  console.log(ret_arr);
+  return ret_arr;
+}
+
+function getPopulation(array) {
+  ret_arr = [];
+  array.forEach((element) => {
+    ret_arr.push(element.Population)
+  });
+  console.log(ret_arr);
+  return ret_arr;
+}
+
 
 async function mainEvent() {
-    // map here or something
-    const results = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population%27');
-    const arrayFromJson = await results.json(); 
-    console.log(arrayFromJson);
+  const data = await fetch('https://datausa.io/api/data?drilldowns=Nation&measures=Population');
+  const array_US = await data.json(); 
+  console.log(array_US);
 
+  x_labels = [];
+  populationData = [];
 
-     if(arrayFromJson.data?.length > 0) {     
-      //inject? and map shit
-     }
+  if (array_US.data?.length > 0) {
+   x_labels = getYear(array_US.data);
+   populationData = getPopulation(array_US.data);
+  }
 
+  makeChart(x_labels, populationData);
 }
- document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
 
-
- /*
-
-An asynchronous data request to your API 
-A processing request that uses array methods (.map, .filter, .find, .reduce) to change your data into the shape your chart, map, or other component needs for display
+mainEvent();

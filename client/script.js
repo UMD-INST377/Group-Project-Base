6c31959eba10f34cb3d0366f886715a86d08f6cb
Mattangent <112779807+Mattangent@users.noreply.github.com>
2022-11-26 16:24:29 -0500

    added fetch api

--- client/script.js
+++ client/script.js
@@ -1,49 +1,8 @@
-
-
-function getRandomInclusing(min,max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
-
-
-
-function injectHTML(list) {
-
+async function retrieveData() {
+  const url = 'https://datausa.io/api/data?drilldowns=Nation&measures=Population';
+  const data = await fetch(url);
+  const json = await data.json(); 
+  console.log(json);
+  // here is where we get the data from our request as JSON
 }
-
-function createplaylsit(array, song) {
-    playlist = []; 
-    array.forEach((element) => { // not sure if element is correct
-        // will have to do an if statement grabbing songs what have the same genre
-        // add them to a new playlist array
-     });
-    return playlist;
-}
-
-async function mainEvent() {
-    // map here or something
-     const results = await fetch()
-
-
-     if(arrayFromJson.data?.length > 0) {     
-
-        form.addEventListener('input', (event) =>{
-
-        });
-
-
-
-    } else {
-        console.log('Sorry, that is not a valid genre');
-    }
-
-
-}
-
- document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
-/*
-
-An asynchronous data request to your API 
-A processing request that uses array methods (.map, .filter, .find, .reduce) to change your data into the shape your chart, map, or other component needs for display
-*/
+retrieveData();

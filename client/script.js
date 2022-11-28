dc445443107c54fe9183cce38e8fba96a9e0cfb9
Mihiled <hsombat@umd.edu>
2022-11-28 11:00:57 -0500

    Updated Functions/Added Leaflet Map

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-import fetch from 'node-fetch';
+// import fetch from 'node-fetch';
 
 async function dataYear(year, info) {
   // info can be organization, type_cleanup, type_litter, number_bags
@@ -20,21 +20,35 @@ async function dataYear(year, info) {
 
 // dataYear(2022, 'organization'); // test to see if function works
 
-async function createArray(year, info) {
-  let dataList = [];
+async function createArray(year) {
+  const dataList = [];
   try {
     const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json?impl_comp_yr=';
     let obj;
     const res = await fetch(url + year);
     obj = await res.json();
     for (let i = 0; i < obj.length; i++) {
-      dataList.push(obj[i][info]);
+      dataList.push(obj[i]);
     }
-  console.log(dataList)
+    return dataList;
   } catch (err) {
     console.log('Data request failed', err);
     res.json({message: 'Data request failed', error: err});
   }
 }
 
-createArray(2022, 'organization'); // test to see if function works
+// const test = await(createArray(2022))
+
+// test.forEach((x, i) => console.log(x['latitude'] + ' ' + x['longitude']))
+
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9897, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+initMap();

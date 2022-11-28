345d7703635ad9f40178f9a6fe7115d94c04aefc
ZW975 <71403189+ZW975@users.noreply.github.com>
2022-11-28 17:46:56 -0500

    more attempts to read the json data

--- client/script.js
+++ client/script.js
@@ -3,7 +3,7 @@
 /* leaving space here to add more functions */
 async function getData() {
   var accessToken =
-    "BQDTbois3Mm6vqnkBzrVhk4mNE1oTvmFYhX_b-7sezq2ZUchPAt7V6yn3hZ7iPiR3a8ZSdOINrlzCve2YW7jhoadfLWM8rEsD2yXNjGU_C4PBmiXKA37piRWtaZHjMtMBbyPd4Ip86vz4eQrVhbwq74CO7_LZPnXxPTyR0ZQckhR51t8I_hqRbN2uULeandN1ug";
+    "BQDLhQu0lvVsd6TaHoc5n_kOzwIRrtOFvpkdhuekLdTMIxCbOilq1JYyiTlpGyr3orYsm-McgNr2DBss6T1Fl94R4-RPjK7ndbSKVV-sd9Dv-gQYU2m6MI0-fM5nyO9VfFfPYV4RzslTM2Z2Sp511um0a0VyAq1amdLdXpgyqBvYBKvgiCDLSSUxbBdzDKyBQZ4";
   const data = await fetch(
     "https://api.spotify.com/v1/albums/7jaSNQUBJbvfbZHLNFrV7P",
     {
@@ -19,18 +19,36 @@ async function getData() {
   return json;
 }
 
+function traverse_duration(obj){
+  lst = [];
+  for(var prop in obj){
+      if(typeof obj[prop]=="duration_ms"){
+          traverse_duration(obj[prop[i]]);
+      }else{
+          lst.push(obj[prop[i]]);
+      }
+  }
+  return lst;
+}
+
+function traverse_name(obj){
+  lst = [];
+  for(var prop in obj){
+      if(typeof obj[prop]=="name"){
+          traverse_name(obj[prop[i]]);
+      }else{
+          lst.push(obj[prop[i]]);
+      }
+  }
+  return lst;
+}
+
 async function mainEvent() {
   const form = document.querySelector(".main_form");
 
   const AlbumData = getData();
-  nameList = [];
-  durationList = [];
-  AlbumData.track.forEach((item, index) => {
-    const songName = item.name;
-    const duration = item.duration_ms;
-    nameList.push(songName);
-    durationList.push(duration);
-  });
+  nameList = traverse_duration(AlbumData);
+  durationList = traverse_name(AlbumData);
   console.log(nameList);
   console.log(durationList);
 }

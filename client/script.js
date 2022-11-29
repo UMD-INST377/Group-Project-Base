072c2be9357166efa5ea5cf37859707f01d059d1
na5im <72241606+na5im@users.noreply.github.com>
2022-11-29 18:51:08 -0500

    successfully connect to api

--- client/script.js
+++ client/script.js
@@ -1,8 +1,14 @@
+const form = document.querySelector('.main-form');
+const submit = document.querySelector('#get-location');
+// const data = getData();
+
+// console.log(data);
+
 async function getData() {
   const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json';
-  const data = await fetch(url);
-  const json = await data.json();
-  return json;
+  const apiData = await fetch(url);
+  const json = await apiData.json();
+  console.log(json);
+  return json
 }
-
-const data = await getData();
+getData()

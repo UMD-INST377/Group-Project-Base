5b0d6fb273a8bf697e9352ed86a42f2276392054
Segun Awe <oawe5@umd.edu>
2022-11-29 11:47:42 -0500

    displaying api data

--- teamScript.js
+++ teamScript.js
@@ -16,12 +16,14 @@ async function teamToID(teamName) {
   const res = json.response;
   let teamID = 0;
   let teamLogo = '';
+  let fullTeamName = '';
   res.forEach((item, index) => {
     teamID = item.id;
     teamLogo = item.logo;
+    fullTeamName = item.name;
   });
   console.log(` ${teamName}, ${teamID}`);
-  return [teamID, teamLogo];
+  return [teamID, teamLogo, fullTeamName];
 }
 
 async function getAnyTeam(teamName, year) {
@@ -31,12 +33,13 @@ async function getAnyTeam(teamName, year) {
   const data = await teamToID(teamName);
   const teamID = data[0];
   const teamLogo = data[1];
+  const fullTeamName = data[2];
 
   const url = `https://api-nba-v1.p.rapidapi.com/teams/statistics?id=${teamID}&season=${year}`;
   const apiData = await fetch(url, options);
   const json = await apiData.json();
   const res = json.response;
-  return [res, teamLogo];
+  return [res, teamLogo, fullTeamName];
 }
 
 async function mainEvent() {
@@ -46,12 +49,28 @@ async function mainEvent() {
   console.log(teamName);
   console.log(year);
 
-//   const data = await getAnyTeam(teamName, year);
-  const teamLogo = data[1]
-  const teamResponse = data[0]
+  const data = await getAnyTeam(teamName, year);
+  const teamLogo = data[1];
+  const teamResponse = data[0];
+  const teamFullName = data[2];
 
   console.log(teamLogo);
   console.log(teamResponse);
+
+  teamResponse.forEach((item, index) => {
+    document.getElementById('total-games').textContent = `Games played: ${item.games}`;
+    document.getElementById('FG-percent').textContent = `Team FG percentage: ${item.fgp}`;
+    document.getElementById('FT-percent').textContent = `Team Free Throw percentage: ${item.ftp}`;
+    document.getElementById('Three-percent').textContent = `Team Three Point percentage: ${item.tpp}`;
+    document.getElementById('total-points').textContent = `Total Points: ${item.points}`;
+    document.getElementById('total-rebound').textContent = `Total Rebounds: ${item.totReb}`;
+    document.getElementById('total-assists').textContent = `Total Assists: ${item.assists}`;
+    document.getElementById('total-steal').textContent = `Total Steals: ${item.steals}`;
+    document.getElementById('total-turn').textContent = `Total Turnovers: ${item.turnovers}`;
+    document.getElementById('total-blocks').textContent = `Total Blocks: ${item.blocks}`;
+  });
+
+  document.getElementById('team-header').textContent = `${teamFullName} ${year}`;
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

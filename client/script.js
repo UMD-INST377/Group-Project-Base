6e16ce9c26d5bac65e84906ad7a4642802a8cf66
Jsmurillo16 <89802043+Jsmurillo16@users.noreply.github.com>
2022-11-29 00:49:31 -0500

    modified:   client/index.html modified:   client/script.js modified:   client/styles.css

--- client/script.js
+++ client/script.js
@@ -1,39 +1,53 @@
-var xmlhttp = new XMLHttpRequest();
-var url = "'https://api.coindesk.com/v1/bpi/currentprice.json'";
-xmlhttp.open("GET",url,true);
-xmlhttp.send();
-xmlhttp.onreadystatechange = function(){
-    if(this.readyState == 4 && this.status == 200){
-        var data = JSON.parse(this.responseText);
-        //console.log(data)
-        date = data.date_population.map(function(elem){
-            return elem.date;
-        })
-        population = data.date_population.map(function(elem){
-            return elem.population;
-        })
-        //console.log(population)
-
-        const ctx = document.getElementById('canvas').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: date,
-                datasets: [{
-                    label: 'Population',
-                    data: population,
-                    backgroundColor: "#ff335e"
-                    
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
+/* eslint-disable no-console */
+function makeGraph(x, y) {
+    const fetching = document.getElementById('chartToDisplay');
+    new Chart(fetching, {
+      type: 'bar',
+      data: {
+        labels: x,
+        datasets: [{
+          label: 'Countries',
+          data: y,
+          borderwidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    });
+  }
+  
+  function getCountry(array) {
+    returnCountryArray = [];
+    array.forEach((element) => (returnCountryArray.push(element.Country)));
+    console.log(returnCountryArray);
+    return returnCountryArray;
+  }
+  
+  function getRate(array) {
+    returnRateArray = [];
+    array.forEach((element) => (returnRateArray.push(element.Rate)));
+    console.log(returnRateArray);
+    return returnRateArray;
+  }
+  
+  async function mainEvent() {
+    const data = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+    const arrayBTC = await data.json();
+    console.log(arrayBTC);
+  
+    xLabel = [];
+    rateData = [];
+  
+    if (arrayBTC.data?.length > 0) {
+      xLabel = getCountry(arrayBTC.data);
+      rateData = getRate(arrayBTC.data);
     }
-
-}
+    makeGraph(xLabel, rateData);
+  }
+  
+  mainEvent();

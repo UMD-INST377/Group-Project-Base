f898c14af51ab86a76ed77a592362b46e359f66a
itshue <hehizele@terpmail.umd.edu>
2022-11-30 00:34:41 -0500

    update script
    correct errors

--- client/script.js
+++ client/script.js
@@ -25,47 +25,48 @@ const data = getData();
 //               latitude: latitude,
 //               mobile: item.mobile,
 //               email: item.email
-//           });            
+//           });
 //       }
 //   }
 //   });
 
-console.log(data.keys)
+console.log(data.keys);
 
 function districtCount(data, district) {
   let count = 0;
-  data.forEach(obj => {
+  data.forEach((obj) => {
     Object.entries(obj).forEach(([key, value]) => {
-        obj.forEach((litter) => {
-          if (!litter.council_district) { return; }
-          if (litter.council_district === district) { count++; }
-    })
-});
+      obj.forEach((litter) => {
+        if (!litter.council_district) { return; }
+        if (litter.council_district === district) { count++; }
+      });
+    });
 
-  return count;
-})}
+    return count;
+  });
+}
 
-const districts = {1: 0,
-                   2: 0,
-                  3: 0,
-                  4: 0,
-                  5: 0,
-                  6: 0,
-                  7: 0,
-                  8: 0,
-                  9: 0};
+const districts = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0
+};
 
-
-function districtLitters(){
-
-  for (var i = 1; i <= 9; i++){
-    districts[i] = districtCount(data, i)
+function districtLitters() {
+  for (let i = 1; i <= 9; i++) {
+    districts[i] = districtCount(data, i);
   }
 }
 
 districtLitters();
 
-console.log(districts)
+console.log(districts);
 // function districtCount(district) {
 //  var count = 0;
 // for (var i = 0; i < data.length; i++) {
@@ -75,7 +76,6 @@ console.log(districts)
 //  }
 //  return count;
 // }
-
 
 function initMap() {
   console.log('initMap');

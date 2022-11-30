130ef66f9cb8c445f046ae5819e3168718699e95
eniteCZ <f.radmanovic@gmail.com>
2022-11-30 17:06:33 -0500

    Idea is to have 100 genres

--- client/script.js
+++ client/script.js
@@ -20,7 +20,7 @@ async function getToken() {
 
 async function getGenres(token) {
 
-    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+    const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US&limit=50`, {
         method: 'GET',
         headers: { 'Authorization' : 'Bearer ' + token}
     });

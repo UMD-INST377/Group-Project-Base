b67848651f6f178b433db68e614ed21e9d75af97
kbouda <102676776+kbouda@users.noreply.github.com>
2022-11-29 19:28:20 -0500

    added new api

--- client/script.js
+++ client/script.js
@@ -1,7 +1,20 @@
-async function getData() {
-  const url = 'https://movie-database-alternative.p.rapidapi.com/';
-  const data = await fetch(url);
-  const json = await data.json();
-  /* const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name)); */
-  return reply;
-}
+// async function getData() {
+//   const url = 'https://movie-database-alternative.p.rapidapi.com/';
+//   const data = await fetch(url);
+//   const json = await data.json();
+//   /* const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name)); */
+//   return reply;
+// }
+
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '79286f9b48msh455d85ddc2d4316p18f1ccjsnabaae7ed74b1',
+    'X-RapidAPI-Host': 'genius.p.rapidapi.com'
+  }
+};
+
+fetch('https://genius.p.rapidapi.com/artists/16775/songs', options)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));

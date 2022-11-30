d92a2e6395d352e05985213cb6b0cb8bea4703d9
na5im <72241606+na5im@users.noreply.github.com>
2022-11-29 19:09:18 -0500

    added map

--- client/script.js
+++ client/script.js
@@ -9,6 +9,34 @@ async function getData() {
   const apiData = await fetch(url);
   const json = await apiData.json();
   console.log(json);
-  return json
+  return json;
 }
-getData()
+getData();
+
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9849, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item, index) => {
+    const {coordinates} = item.Location;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
+
+const map = initMap();

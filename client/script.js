deb5bd0761b5c9cd77f8b3b06a27865f19dbd701
eniteCZ <f.radmanovic@gmail.com>
2022-11-30 17:35:36 -0500

    Added handling of playlists that could not be reached

--- client/script.js
+++ client/script.js
@@ -76,12 +76,13 @@ async function initSongs(){
     console.table(genres)
     genres.forEach(async genre => {
         console.log(`Getting tracks from: ${genre.name} has id: ${genre.id}`)
-        const playlists = getPlaylistsByGenre(token, genre.id, 10);
-        console.table(playlists);
+        const playlists = await getPlaylistsByGenre(token, genre.id, 10)
+        if(typeof playlists !== "undefined")
+            {console.table(playlists);} 
     })
     
     //const playlists = await getPlaylistsByGenre(token, "0JQ5DAqbMKFDXXwE9BDJAr", 10);
-    console.table(playlists);
+    //console.table(playlists);
     const plalylistSg = "https://api.spotify.com/v1/playlists/37i9dQZF1DXcF6B6QPhFDv/tracks"
     const tracks = await getTracks(token, plalylistSg);
     console.table(tracks.items)

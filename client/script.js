f26329dfbcf8898e35e6b4404d482aa1c945e3d2
Bo Wu <bw1@terpmail.umd.edu>
2022-11-30 19:55:46 -0500

    co

--- client/script.js
+++ client/script.js
@@ -1,8 +1,34 @@
+import { response } from "express";
+
 //This will get the data into console
-fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
+const results = fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(err => console.error(err));
+
+const arrayFromJson = res.json();
+
+
+function injectHTML(list) {
+  console.log('fired injectHTML');
+  const target = document.querySelector("#rlist");
+  target.innerHTML = '';
+
+  const listelmnt = document.createElement("ol");
+  target.appendChild(listelmnt);
+
+  list.forEach(i => {
+    const element = document.createElement("li");
+    element.innerText = i.name;
+    listelmnt.appendChild(element);
+  });
+}
+
+if (arrayFromJson.data?.length > 0){
+  injectHTML(arrayFromJson);
+}
+
+
 
 
 let slideIndex = 0;
@@ -12,10 +38,10 @@ function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
+    slides[i].style.display = "none";
   }
   slideIndex++;
   if (slideIndex > slides.length) {slideIndex = 1} 
   slides[slideIndex-1].style.display = "block"; 
-  setTimeout(showSlides, 7000); // Change image every 2 seconds
+  setTimeout(showSlides, 10000); // Change image every 2 seconds
 }

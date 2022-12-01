a5b5eacdd2ea4b54a4e0dbc0ecbb5b50a18e3028
govindnageswaran <govindnageswaran222@gmail.com>
2022-12-01 18:56:16 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,20 +1,22 @@
 async function mainEvent() {
   const options = {
-    method: "GET",
+    method: 'GET',
     headers: {
-      "X-RapidAPI-Key": "5dc02365d4mshd9eff5d73eae486p1c6bb0jsnd676be7e93c5",
-      "X-RapidAPI-Host": "api-nba-v1.p.rapidapi.com",
-    },
+      'X-RapidAPI-Key': '5dc02365d4mshd9eff5d73eae486p1c6bb0jsnd676be7e93c5',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
   };
   const dataGet = await fetch(
-    "https://api-nba-v1.p.rapidapi.com/players?team=41&season=2021",
+    'https://api-nba-v1.p.rapidapi.com/players?team=41&season=2021',
     options
   );
   const arrayFromJson = await dataGet.json();
-  const response_object = arrayFromJson["response"];
+  const response_object = arrayFromJson.response;
   console.log(response_object);
   weight1 = notNull(response_object);
-  console.log(weight1)
+  console.log(weight1);
+  const chartTarget = document.querySelector('#myChart');
+  initChart(chartTarget);
 }
 
 function notNull(array) {
@@ -24,4 +26,33 @@ function notNull(array) {
     }
   });
 }
-document.addEventListener("DOMContentLoaded", async () => mainEvent());
+function initChart(chart) {
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June'
+  ];
+
+  const data = {
+    label: labels,
+    dataset: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255,99,132)',
+      borderColor: 'rgb(255,99,132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+  };
+  const config = {
+    type: 'bar',
+    data: data,
+    options: {}
+  };
+  return new Chart(
+    chart,
+    config
+  );
+}
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

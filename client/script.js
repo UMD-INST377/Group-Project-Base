d2ab999ba6701f54e1d1d7ac078742558692802b
Sean Leahy <sfleahy2000@gmail.com>
2022-12-01 13:18:58 -0500

    mainEvent

---
+++ client/script.js
@@ -0,0 +1,9 @@
+function initMap() {
+    console.log('initMap');
+    const map = L.map('map').setView([38.9897, -76.9378], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return map;
+  }

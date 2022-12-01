8141bdeae9f5be44b5e991a75d2c30ff5061fd76
John L <102560209+Jklehner@users.noreply.github.com>
2022-11-30 21:43:04 -0500

    update to location test

--- js/script.js
+++ js/script.js
@@ -19,7 +19,7 @@ function injectHTML(list) {
   target.appendChild(listEl);
   list.forEach((item) => {
     const el = document.createElement('li');
-    el.innerText = item.street_address;
+    el.innerText = item.location;
     listEl.appendChild(el);
   });
 }

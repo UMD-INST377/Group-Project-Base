891db07f267f74031ab3e66818afcd7af233ef80
EricSalmeron <69407681+EricSalmeron@users.noreply.github.com>
2022-12-01 02:52:25 -0500

    updatre

--- client/script.js
+++ client/script.js
@@ -1,5 +1,3 @@
-
-
 const options = {
   method: 'GET',
   headers: {
@@ -18,28 +16,54 @@ function lowerCaseName(string) {
   return string.toLowerCase();
 }
 
+const ctx = document.getElementById('myChart');
+
+new  Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true
+      }
+    }
+  }
+});
+
 async function getPlayer(e) {
   try {
     const name = document.querySelector('#namePlayer').value;
     const playerName = lowerCaseName(name);
-    const newData = await fetch(`https://api-nba-v1.p.rapidapi.com/players?name=${playerName}`, options)
+    const newData = await fetch(`https://api-nba-v1.p.rapidapi.com/players?name=${playerName}`, options);
     const data = await newData.json();
+    console.log(data);
+
+    const height = data.dat.map((x) => x.response[0].weight.pounds);
+    console.log(height);
+
     const playerID = data.response[0].id;
-    document.querySelector(".playerBox").innerHTML = `
+    document.querySelector('.playerBox').innerHTML = `
     <div>
       </div>
       <div class="playerInfo">
         <h1>${capitalizeFirstLetter(
-          data.response[0].firstname + " " + data.response[0].lastname
-        )}</h3>
+    `${data.response[0].firstname} ${data.response[0].lastname}`
+  )}</h3>
         <p>ID: ${(ind = data.response[0].id)}</p>
         ${ind}
       </div>`;
-      fetch(`https://api-nba-v1.p.rapidapi.com/players/statistics?id=${playerID}&season=2021`, options)
-        .then((res) => res.json())
-        .then((info) => {
-          console.log(info);
-          document.querySelector('.nbaBox').innerHTML = `
+    fetch(`https://api-nba-v1.p.rapidapi.com/players/statistics?id=${playerID}&season=2021`, options)
+      .then((res) => res.json())
+      .then((info) => {
+        console.log(info);
+        document.querySelector('.nbaBox').innerHTML = `
             <div class = "nbaInfo">
                 <h1>${info.response[0].team.name}</h3>
             </div>

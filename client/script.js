94ee3d15599928b2b5a33cd07741fa3958190263
Jzheng-prog <zhengjohn017@gmail.com>
2022-11-30 19:08:37 -0500

    data
    loaded data to data page, but not in table format

--- client/script.js
+++ client/script.js
@@ -1,17 +1,44 @@
 
 //This will get the data into console
-const spendingData = fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
+// fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(err => console.error(err))
+
+fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
+.then(function(response){
+    return response.json();
+})
+.then(function(products){
+    let placeHolder = document.querySelector("#data-output");
+    let out = "";
+
+    for(let product of products) {
+        out += `
+        <tr> 
+        <td>${product.payee_name}</td>
+        <td>${product.agency}</td>
+        <td>${product.zip_code}</td>
+        <td>${product.amount}</td>
+        </tr>
+        `;
+    }
+
+    placeHolder.innerHTML = out;
+})
+
+
+
+
+
 
 //Need different functions to process data
-async function getData(){
-    const response = await fetch(spendingData);
-    const data = await response.json();
-    const {agency} = data;
-    console.log(agency);
-}
+// async function getData(){
+//     const response = await fetch(spendingData);
+//     const data = await response.json();
+//     const {agency} = data;
+//     console.log(agency);
+// }
 
 
 

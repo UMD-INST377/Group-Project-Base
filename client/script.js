68b05ea0420043ab6a2588427c0bd9befe32b6a2
Patrick Mohler <pdmohlergm@gmail.com>
2022-12-02 14:55:21 -0500

    updates

--- client/script.js
+++
@@ -1,7 +0,0 @@
-
-async function getData() {
-  const url = 'https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json'; // remote URL! you can test it in your browser
-  const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
-  const json = await data.json(); // the data isn't json until we access it using dot notation
-  return json;
-}

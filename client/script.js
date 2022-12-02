85be8a2d1528901dd43a3bc36e0a9d1cf8c8e330
Patrick Mohler <pdmohlergm@gmail.com>
2022-12-02 14:54:45 -0500

    data request

--- client/script.js
+++ client/script.js
@@ -1,7 +1,27 @@
+/*
+Regular Functions
+*/
 
+function processSpending() {
+
+}
+
+/*
+Asycronous Functions
+*/
+
+// Function to get the json data from the API
 async function getData() {
   const url = 'https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json'; // remote URL! you can test it in your browser
   const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
   const json = await data.json(); // the data isn't json until we access it using dot notation
   return json;
+}
+
+// Main function
+async function mainEvent() {
+
+// waits for the data to be gathered
+const spendingData = await getData();
+
 }

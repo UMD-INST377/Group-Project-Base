f25750e9f7bf77c61544b2fd14d1597cca5b7ef1
KiraFreedman <kira.ally@gmail.com>
2022-12-02 16:43:50 -0500

    trying different api

--- Food Data/script.js
+++ Food Data/script.js
@@ -1,22 +1,24 @@
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': 'e2a17786bamsh39f2855f9aaf40dp1b10c0jsnf0ccf57e91e0',
-		'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
-	}
-};
+//const options = {
+	//method: 'GET',
+	//headers: {
+	//	'X-RapidAPI-Key': 'e2a17786bamsh39f2855f9aaf40dp1b10c0jsnf0ccf57e91e0',
+	//	'X-RapidAPI-Host': 'calorieninjas.p.rapidapi.com'
+//	}
+// };
 
 async function getData(){
-  const url = 'https://calorieninjas.p.rapidapi.com/v1/nutrition?query=tomato'; 
+  const url = 'https://data.princegeorgescountymd.gov/resource/2qma-7ez9.json'; 
   const data = await fetch(url, options);
-	const json = data.json();
+  // console.log(data);
+	const json = await data.json();
+  console.log(json);
 
-  const name = json.items.map((x)=> x.sugar_g);
-  const name2 = json.items.map((x)=> x.fiber_g);
-  const name3 = json.items.map((x)=> x.sodium_mg);
-  console.log(name,name2,name3);
+  // const name = json.response.map((amount)=> amount.);
+  // console.log(name);
+  // tableData= name
 }
-getData()
+
+
 xlabels = [
   'sugar_g',
   'fiber_g',
@@ -29,18 +31,23 @@ xlabels = [
   'protein_g',
   'carbohydrates_total_g'
 ]
+const blockNumber = [];
+const area_land = [];
 
-tableData = 
-function makeChart() {
+async function makeChart() {
+  await getData();
+
+
+  console.log('hello')
   const ctx = document.getElementById('myChart');
 
   new Chart(ctx, {
     type: 'bar',
     data: {
-      labels: xlabels,
+      labels: blockNumber,
       datasets: [{
-        label: '# of ',
-        data: [12, 19, 3, 5, 2, 3],
+        label: '# of contents',
+        data: area_land,
         borderWidth: 1
       }]
     },
@@ -54,30 +61,6 @@ function makeChart() {
   });
 }
 
-// function changeChart(chart, dataObject) {
-  //const labels = Object.keys(dataObject);
-  //const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+makeChart();
 
-  //chart.data.labels = labels;
-  //chart.data.datasets.forEach((set) =>{
-    //set.data = info;
-    // return set;});
-  // chart.update();
-// } 
 
-async function mainEvent() {
-  console.log("hi1"); 
-  const form = document.querySelector('.main_form');
-
-  const submit = document.querySelector('#get-resto');
-  const resto = document.querySelector('#resto');
-  const chart = document.querySelector('#myChart');
-
-  let myChart = makeChart();
-  form.addEventListener('submit', (submitEvent) => {
-    // This is needed to stop our page from changing to a new URL even though it heard a GET request
-    submitEvent.preventDefault();
-  });
-}
-
-document.addEventListener('DOMContentLoaded', async () => mainEvent());

fc9a65eaa70b87583a8ed0f353641ae3b07213d4
seanl2 <76665604+seanl2@users.noreply.github.com>
2022-12-01 20:03:32 -0500

    updated

---
+++ client/script.js
@@ -0,0 +1,19 @@
+function initMap() {
+    console.log('initMap');
+    const map = L.map('map').setView([38.9897, -76.9378], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    return map;
+  }
+
+  
+
+async function mainEvent() {
+
+    const pageMap = initMap();
+    
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

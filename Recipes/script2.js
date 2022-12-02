b56fa1ec74d1a36e02031f577705f351ba17ac56
KiraFreedman <kira.ally@gmail.com>
2022-12-01 21:08:20 -0500

    updated things

--- Recipes/script2.js
+++ Recipes/script2.js
@@ -1,12 +1,11 @@
 let slidePosition = 0;
 
-// gather a reference to every slide we're using via the class name and querySelectorAll
+
 const slides = document.querySelectorAll('.carousel_item');
 
-// change that "NodeList" into a Javascript "array", to get access to "array methods"
 const slidesArray = Array.from(slides);
 
-// Figure out how many slides we have available
+
 const totalSlides = slidesArray.length;
 
 function updateSlidePosition() {
@@ -20,27 +19,15 @@ function updateSlidePosition() {
 }
 
 function moveToNextSlide() {
-  /*
-    add an if statement here that checks
-    if you're already at the max number of slides
-    and if so, sets your slidePosition to the first index of an array
-    if not, set the slidePosition to the current position plus one
-  */
+
   if (slidePosition === totalSlides - 1) {
     slidePosition = 0;
   } else {
     slidePosition += 1;
   }
-  updateSlidePosition(); // this is how you call a function within a function
+  updateSlidePosition(); 
 }
 function moveToPrevSlide() {
-  // add your code in here for when you click the "prev" button
-  /*
-    add an if statement here that checks
-    if you're already at the first index position for an array
-    and if so, sets your slidePosition to the last slide position in totalSlides
-    if not, set the slidePosition to the current position minus one
-  */
   if (slidePosition === 0) {
     slidePosition = totalSlides - 1;
   } else {
@@ -49,14 +36,10 @@ function moveToPrevSlide() {
   updateSlidePosition();
 }
 
-/*
-  These two functions have been assigned via "addEventListener"
-  to the elements accessed by the "querySelector" set to the class name on each
-*/
-document.querySelector('.next') // Get the appropriate element (<button class="next">)
-  .addEventListener('click', () => { // set an event listener on it - when it's clicked, do this callback function
-    console.log('clicked next'); // let's tell the client console we made it to this point in the script
-    moveToNextSlide(); // call the function above to handle this
+document.querySelector('.next') 
+  .addEventListener('click', () => {  
+    console.log('clicked next'); 
+    moveToNextSlide(); 
   });
 
 document.querySelector('.prev')

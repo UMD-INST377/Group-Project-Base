c71d9e39aeeed20603e1f38555d73f0b6100ec10
twalz624 <111401516+twalz624@users.noreply.github.com>
2022-12-02 17:11:43 -0500

    separated everything into separate functions for clarity

--- client/script.js
+++ client/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-new */
 const options = {
   method: 'GET',
   headers: {
@@ -5,9 +6,6 @@ const options = {
     'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
   }
 };
-
-
-document.querySelector('#search').addEventListener('click', getPlayer);
 
 const ctx = document.getElementById('myChart');
 
@@ -19,70 +17,85 @@ function lowerCaseName(string) {
   return string.toLowerCase();
 }
 
+function fillPlayerInfo(firstname, lastname, id) {
+  document.querySelector('.playerBox').innerHTML = `
+  <div>
+    </div>
+    <div class="playerInfo">
+      <h1>${capitalizeFirstLetter(
+    `${firstname} ${lastname}`
+  )}</h3>
+      <p>ID: ${(ind = id)}</p>
+      ${ind}
+    </div>`;
+}
+
+function fillNBAInfo (data) {
+  document.querySelector('.nbaBox').innerHTML = `
+  <div class = "nbaInfo">
+      <h1>${info.response[0].team.name}</h3>
+  </div>
+  <div>
+  <img
+  src ="${info.response[0].team.logo}"
+  alt = "${data.name}"
+  />
+  </div>
+  `;
+}
+
+function writeChart(data) {
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['weight'],
+      datasets: [{
+        label: [`${data.response[0].firstname} ${data.response[0].lastname}`],
+        data: [weight],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
 async function getPlayer() {
   try {
     const name = document.querySelector('#namePlayer').value;
     const playerName = lowerCaseName(name);
     const newData = await fetch(`https://api-nba-v1.p.rapidapi.com/players?name=${playerName}`, options);
-    const data = await newData.json()
-    console.log(data)
-    const weight = data.response[0].weight.pounds
-    console.log(weight)
-
-    const playerID = data.response[0].id;
-    document.querySelector('.playerBox').innerHTML = `
-    <div>
-      </div>
-      <div class="playerInfo">
-        <h1>${capitalizeFirstLetter(
-    `${data.response[0].firstname} ${data.response[0].lastname}`
-  )}</h3>
-        <p>ID: ${(ind = data.response[0].id)}</p>
-        ${ind}
-      </div>`;
-    fetch(`https://api-nba-v1.p.rapidapi.com/players/statistics?id=${playerID}&season=2021`, options)
-      .then((res) => res.json())
-      .then((info) => {
-        document.querySelector('.nbaBox').innerHTML = `
-            <div class = "nbaInfo">
-                <h1>${info.response[0].team.name}</h3>
-            </div>
-            <div>
-            <img
-            src ="${info.response[0].team.logo}"
-            alt = "${data.name}"
-            />
-            </div>
-            `;
-      });
-      new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['weight'],
-          datasets: [{
-            label: [data.response[0].firstname + " " + data.response[0].lastname],
-            data: [weight],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      });
-    labelsWeight = weight
-    console.log(weight)
+    const data = await newData.json();
+    return data.response;
   } catch (err) {
     console.log('Data Request Failed', err);
   }
 }
 
-
-async function gettingData() {
-  await getPlayer()
+async function getPlayerData() {
+  try {
+    const playerData = await fetch(`https://api-nba-v1.p.rapidapi.com/players/statistics?id=${playerID}&season=2021`, options)
+    const data = await playerData.json();
+    return data.response;
+  } catch (err) {
+    console.log('Data Request Failed', err);
+  }
 }
 
-gettingData();
+async function mainEvent() {
+  let playerList = [];
+  let team1List = [];
+  let team2List = [];
+  document.querySelector('#search').addEventListener('click', (event) => {
+    playerList = getPlayer(event.target.value);
+    console.log(playerList);
+  });
+
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); 

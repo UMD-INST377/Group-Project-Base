b1a266ba63efc29d9cb76b53fbe2b30525e61461
okennedyumd <okennedy@umd.edu>
2022-12-02 17:54:00 -0500

    Working on map and search

--- client/pages/script.js
+++ client/pages/script.js
@@ -1,4 +1,5 @@
 // Work in Progress
+
 function injectHTML(list) {
   console.log('fired injectHTML');
   const target = document.querySelector('#restaurant_list');
@@ -11,6 +12,99 @@ function injectHTML(list) {
     el.innerText = item.name;
     listEl.appendChild(el);
   });
-
-  document.addEventListener('DOMContentLoaded', async () => mainEvent());
 }
+
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.5324, -77.125], 10);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+function markerPlace(array, map) {
+  console.log('markerPlace', array);
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
+
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column_1;
+    console.log(item);
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 9)
+    }
+  });
+}
+
+async function mainEvent() {
+  const pageMap = initMap();
+  // the async keyword means we can make API requests
+  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
+  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const loadAnimation = document.querySelector('.lds-ellipsis');
+  submit.style.display = 'none'; // let your submit button disappear
+
+  /*
+        Let's get some data from the API - it will take a second or two to load
+        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
+        It's at about line 27 - go have a look and see what we're retrieving and sending back.
+       */
+  const results = await fetch('/api/foodServicePG');
+  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
+
+  /*
+        Below this comment, we log out a table of all the results using "dot notation"
+        An alternate notation would be "bracket notation" - arrayFromJson["data"]
+        Dot notation is preferred in JS unless you have a good reason to use brackets
+        The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
+      */
+  // console.table(arrayFromJson.data);
+
+  // in your browser console, try expanding this object to see what fields are available to work with
+  // for example: arrayFromJson.data[0].name, etc
+  // console.log(arrayFromJson.data[0]);
+
+  // this is called "string interpolation" and is how we build large text blocks with variables
+  // console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+
+  // This IF statement ensures we can't do anything if we don't have information yet
+  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+    loadAnimation.classList.remove('lds-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
+    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
+    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
+    let currentList = [];
+
+    form.addEventListener('input', (event) => {
+      console.log(event.target.value);
+      const newFilterList = filterList(currentList, event.target.value);
+      injectHTML(newFilterList);
+      markerPlace(newFilterList, pageMap);
+    });
+    form.addEventListener('submit', (submitEvent) => {
+      // This is needed to stop our page from changing to a new URL even though it heard a GET request
+      submitEvent.preventDefault();
+
+      // This constant will have the value of your 15-restaurant collection when it processes
+      currentList = processRestaurants(arrayFromJson.data);
+      console.log(currentList);
+
+      // And this function call will perform the "side effect" of injecting the HTML list for you
+      injectHTML(currentList);
+      markerPlace(currentList, pageMap);
+
+      // By separating the functions, we open the possibility of regenerating the list
+      // without having to retrieve fresh data every time
+      // We also have access to some form values, so we could filter the list based on name
+    });
+  }
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

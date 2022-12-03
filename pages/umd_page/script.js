e4270ba37c256f29af91b9cd6c5e08bc7b08aa00
Maham Sohail <92546069+MahamSohail@users.noreply.github.com>
2022-12-02 21:48:17 -0500

    lab_10 commit

--- pages/umd_page/script.js
+++ pages/umd_page/script.js
@@ -1,5 +1,5 @@
 /* Data Request to API */
-token = "BQBT4wBqclxU6HCkerH5j2ug-JU_lRZKZg3wfxzTjWvHuGxRIgI0SA1ntj3A4Tfm6k1JgwnKnZJQxwV4hdjj5XdoAz_NqOFJMm8VthxSgaUHMwbw7RV8ARFtPh_oO3k8FbjNbT9yBq8UDPjVcoMv8gc43-lwRr6mW9lSiUJv5MpKCdHLEjEtREI8cCzq3SbNED-ZBsjhVEIoSinGZ3EHCi0xtXROwJJq7knTY4n8aNPgfeANXP7v"
+token = "BQBKe21DUghMdZ3_P_HFTWfRKEmAMF_ydyRrOTJzlsUDn8N8BFVux8xlft4qx4z2ftsuS-ZpnV1zNgHIQrbVKmcONMsEtdghNZjuYDLIO-_lZewbwmMo9cDDviAV0cYmvadqSOREGQdgJpg3bKQKDZ8y6d6RbaYjj2z-1clJS5EIOVDSqgZqG1MkZ2qXHNbVeRgOM8SOtN0UxHP7JgCM05o-LFbmXBCSrTg3Zcm5sPNAPUEfWr_U"
 term = "long_term";
 artist_ids = "39cDMNnxwjrKJE1dyt47jh,1aBDI4nH6OfAkNyUX08O2V";
 album_id = "0TnOYISbd1XYRBk9myaseg";
@@ -66,15 +66,19 @@ const initChart = (chart,chartData) => {
 async function mainEvent() {
   const data_list = document.querySelector("#data");
   const submit = document.querySelector("#load_button_2");
-  const graph_submit = document.querySelector("#graph_load_2") 
+  const graph_submit = document.querySelector("#graph_load_2")
+  //const graph_form = document.querySelector('.graph-form') 
   const chart_target = document.querySelector("#myChart")
-  
+  const laodAnimation = document.querySelector('.lds-ellipsis')
+  graph_submit.style.display='none';
+
   const results = await fetch(
     `https://umd-spotify-backend.herokuapp.com/artist_albums?access_token=${token}&id=${album_id}`
   );
   const arrayFromJson = await results.json();
   //console.log(arrayFromJson)
-  
+
+
   submit.addEventListener("click", async (submitEvent) => {
     submitEvent.preventDefault();
     data_list.innerHTML = "";
@@ -83,6 +87,12 @@ async function mainEvent() {
       data_format(item,data_list)
     });
   });
+
+  if (!arrayFromJson.items?.length === 0){ return ;}
+  graph_submit.style.display = 'block'
+  laodAnimation.classList.remove('lds-ellipsis');
+  laodAnimation.classList.add('lds-ellipsis_hidden');
+  
   graph_submit.addEventListener("click", async (submitEvent) => {
     submitEvent.preventDefault();
     chart_target.innerHTML="";
@@ -90,13 +100,15 @@ async function mainEvent() {
       name_result:[],
       track_results:[]
     }
-    arrayFromJson["items"].forEach((item) => {
+    
+    arrayFromJson["items"].forEach((item) => {        
       const { name, total_tracks } = item
       total_result.name_result.push(name)
       total_result.track_results.push(total_tracks)
       });
       initChart(chart_target, total_result)
   });
+  
 }
 document.addEventListener("DOMContentLoaded", async () => mainEvent());
 

6de65ba98647ef99e747b5657647bab045e74310
Eunbi Kim <ekim0135@terpmail.umd.edu>
2022-12-02 23:51:15 -0500

    update api....

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import express from 'express';
 import fetch from 'node-fetch';
 
--- routes/test.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-dotenv.config();
-
-console.log("check::" + process.env.CLIENT_ID);
-console.log("check::" + process.env.CLIENT_SECRET);

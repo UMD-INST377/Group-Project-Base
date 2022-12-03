7a59431075ed6a8fafa4e2740afb4e40befb0cc8
Brice Tikum <btikum18@umd.edu>
2022-12-03 13:48:52 -0500

    few more changes

--- client/script.js
+++ client/script.js
@@ -1,142 +1,65 @@
-// const nbaRequest  = await fetch();
-// const chartArray = await.nbaRequest.json();
-
 /* eslint-disable max-len */
-
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
-
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-
+// fixing merge head
 function getRandomIntInclusive(min, max) {
   const newMin = Math.ceil(min);
   const newMax = Math.floor(max);
-  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (newMax - newMin + 1) + newMin);
 }
-
 function injectHTML(list) {
   console.log('fired injectHTML');
-  const target = document.querySelector('#restaurant_list');
+  const target = document.querySelector('#player_list');
   target.innerHTML = '';
 
   const listEl = document.createElement('ol');
   target.appendChild(listEl);
+
   list.forEach((item) => {
     const el = document.createElement('li');
-    el.innerText = item.name;
+    el.innerText = `${item.player.first_name} ${item.player.last_name} ${item.player.position}`; // maybe change this to lastname? however name should work
     listEl.appendChild(el);
   });
-  /*
-      ## JS and HTML Injection
-        There are a bunch of methods to inject text or HTML into a document using JS
-        Mainly, they're considered "unsafe" because they can spoof a page pretty easily
-        But they're useful for starting to understand how websites work
-        the usual ones are element.innerText and element.innerHTML
-        Here's an article on the differences if you want to know more:
-        https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-
-      ## What to do in this function
-        - Accept a list of restaurant objects
-        - using a .forEach method, inject a list element into your index.html for every element in the list
-        - Display the name of that restaurant and what category of food it is
-    */
 }
 
-function processRestaurants(list) {
-  console.log('fired restaurants list');
+function processPlayers(list) {
+  console.log('fired Players list');
   const range = [...Array(15).keys()];
   const newArray = range.map((item) => {
     const index = getRandomIntInclusive(0, list.length);
     return list[index];
   });
   return newArray;
-
-  /*
-        ## Process Data Separately From Injecting It
-          This function should accept your 1,000 records
-          then select 15 random records
-          and return an object containing only the restaurant's name, category, and geocoded location
-          So we can inject them using the HTML injection function
-
-          You can find the column names by carefully looking at your single returned record
-          https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-
-        ## What to do in this function:
-
-        - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
-        - using a .map function on that range,
-        - Make a list of 15 random restaurants from your list of 100 from your data request
-        - Return only their name, category, and location
-        - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
-      */
 }
 
 function filterList(array, filterInputValue) {
   return array.filter((item) => {
-    if (!item.name) { return; }
-    const lowerCaseName = item.name.toLowerCase();
+    if (!item.last_name) { return; }
+    const lowerCaseName = item.last_name.toLowerCase(); // name appears again maybe lastname
     const lowerCaseQuery = filterInputValue.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
   });
 }
 
-function initMap() {
-  console.log('initMap');
-  const map = L.map('map').setView([38.7849, -76.9378], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
-  return map;
-}
+function initChart(chart, object) {
+  const labels = Object.keys(object);
 
-function markerPlace(array, map) {
-  console.log('markerPlace', array);
-  // const marker = L.marker([51.5, -0.09]).addTo(map);
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item, index) => {
-    const {coordinates} = item.geocoded_column_1;
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
-    if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 10);
-    }
-  });
-}
-
-function initChart(chart) {
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'June'
-  ];
+  const info = Object.keys(object).map((item) => object[item].length);
 
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My first dataset',
+      label: 'NBA 3 pointers made in 2022 per player',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
+      data: info
     }]
   };
 
   const config = {
-    type: 'line',
+    type: 'bar',
     data: data,
-    options: {}
+    options: {
+      indexAxis: 'y'
+    }
   };
 
   return new Chart(
@@ -145,106 +68,84 @@ function initChart(chart) {
   );
 }
 
-async function getData() {
-  'Nothing yet';
+function changeChart(chart, dataObject) {
+  const labels = Object.keys(dataObject);
+  const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+
+  chart.data.labels = labels;
+  chart.data.datasets.forEach((set) => {
+    set.data = info;
+    return set;
+  });
+  chart.update();
+}
+
+function shapeDataForBarChart(array) {
+  return array.reduce((collection, item) => {
+    if (!collection[item.fg3m]) {
+      collection[item.fg3m] = [item];
+    } else {
+      collection[item.fg3m].push(item);
+    }
+    return collection;
+  }, {});
+}
+
+async function nbaData() {
+  const url = 'https://www.balldontlie.io/api/v1/stats?per_page=100&seasons[]=2022'; // Data goes here
+  const data = await fetch(url);
+  const json = await data.json();
+  console.log(json);
+  return json;
 }
 
 async function mainEvent() {
-  /*
-        ## Main Event
-          Separating your main programming from your side functions will help you organize your thoughts
-          When you're not working in a heavily-commented "learning" file, this also is more legible
-          If you separate your work, when one piece is complete, you can save it and trust it
-      */
-
-  // the async keyword means we can make API requests
-  // const pageMap = initMap();
-
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const form = document.querySelector('.main_form'); // get your main form
+  const submit = document.querySelector('#get-resto'); // reference tosubmit button
   const loadAnimation = document.querySelector('.lds-ellipsis');
   const chartTarget = document.querySelector('#myChart');
-  submit.style.display = 'none'; // let your submit button disappear
+  submit.style.display = 'none'; // submit button disappear
 
-  /*
-        Let's get some data from the API - it will take a second or two to load
-        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-        It's at about line 27 - go have a look and see what we're retrieving and sending back.
-       */
+  /* API data request await */
 
-  const options = {
-    method: 'GET',
-    headers: {
-      'X-RapidAPI-Key': '07db91be42msh07d9e004a5cf4fep13e4b3jsnafbe33e6107b',
-      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
-    }
-  };
+  const chartData = await nbaData();
+  console.log(chartData.data);
+  const shapedData = shapeDataForBarChart(chartData.data);
+  const myChart = initChart(chartTarget, shapedData);
 
-  const nbaRequest = await fetch('https://api-nba-v1.p.rapidapi.com/players?team=1&season=2021', options)
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-
-  // const chartArray = await.nbaRequest.json();
-  const arrayFromJson = await nbaRequest.json();
-  // const results = await fetch('/api/foodServicePG');
-  // const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
-  initChart(chartTarget);
-
-  /* 7
-        Below this comment, we log out a table of all the results using "dot notation"
-        An alternate notation would be "bracket notation" - arrayFromJson["data"]
-        Dot notation is preferred in JS unless you have a good reason to use brackets
-        The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-      */
-  // console.table(arrayFromJson.data);
-
-  // in your browser console, try expanding this object to see what fields are available to work with
-  // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
-
-  // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+  console.log(`${chartData.data[0].player.first_name} ${chartData.data[0].player.last_name}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
+  if (!chartData.data?.length) { return; } // Return if no data
 
-    loadAnimation.classList.remove('lds-ellipsis');
-    loadAnimation.classList.add('lds-ellipsis_hidden');
+  let currentList = [];
 
-    let currentList = [];
+  submit.style.display = 'block';
+  loadAnimation.classList.remove('lds-ellipsis');
+  loadAnimation.classList.add('lds-ellipsis_hidden');
 
-    form.addEventListener('input', (event) => {
-      console.log(event.target.value);
-      const newList = filterList(currentList, event.target.value);
-      injectHTML(newList);
-      markerPlace(newList, pageMap);
-    });
+  form.addEventListener('input', (event) => {
+    console.log('input', event.target.value);
+    const filteredList = filterList(currentList, event.target.value);
+    injectHTML(filteredList);
+    const localData = shapeDataForBarChart(chartData.data);
+    changeChart(myChart, localData);
+    // markerPlace(filteredList, pageMap);
+  });
 
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
-      submitEvent.preventDefault(currentList, pageMap);
+  form.addEventListener('submit', (submitEvent) => {
+    // This is needed to stop our page from changing to a new URL even though it heard a GET request
+    submitEvent.preventDefault();
 
-      // This constant will have the value of your 15-restaurant collection when it processes
-      currentList = processRestaurants(arrayFromJson.data);
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(currentList);
-      // markerPlace(currentList, pageMap);
+    // This constant will have the value of your 15-restaurant collection when it processes
+    currentList = processPlayers(chartData.data);
+    console.log(currentList);
 
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
-    });
-  }
+    // And this function call will perform the "side effect" of injecting the HTML list for you
+    injectHTML(currentList);
+    const localData = shapeDataForBarChart(chartData.data);
+    changeChart(myChart, localData);
+    // markerPlace(currentList, pageMap);
+  });
 }
-
-/*
-      This last line actually runs first!
-      It's calling the 'mainEvent' function at line 57
-      It runs first because the listener is set to when your HTML content has loaded
-    */
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests

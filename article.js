3cb094874b08ec64e94fc9dfde20b519e4192f11
tmzgod <tmzgod769@gmail.com>
2022-12-03 00:03:48 -0500

    add comment
    need fix bug and demo

--- article.js
+++ article.js
@@ -1,10 +1,10 @@
-
+/* clientId id */
 const clientId = 'e401920d65c744c0b6b85dd07d110aa3';
-
+/* Secret */
 const clientSecret = '52ed8f6740514deba983ada74ebd19bf';
-
+/* localuserdata */
 let localdata = {};
-
+/* get token everytime */
 fetch('https://accounts.spotify.com/api/token', {
   method: 'POST',
   headers: {
@@ -22,10 +22,10 @@ fetch('https://accounts.spotify.com/api/token', {
   })
 
 var maindata = '';
-
+//chart dom object
 var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = '';
-
+var myChart = ''; //this is chart entity object
+// here is reset button
 document.querySelector('#reset').addEventListener('click', function () {
   if (myChart != '') {
     myChart.destroy();
@@ -35,18 +35,18 @@ document.querySelector('#reset').addEventListener('click', function () {
   }
 
 })
-
+// draw the chart
 function showdata (maindata) {
   let data = maindata;
-
+// return name 
   var labels = data.artists.map((ele) => {
     return ele.name
   })
-
+// return  followers total number
   var followers = data.artists.map((ele) => {
     return ele.followers.total
   })
-
+// return background color 
   var backgroundColor = [];
   followers.forEach((ele, i) => {
     backgroundColor.push(choseRgb().toString())
@@ -69,15 +69,15 @@ function showdata (maindata) {
       plugins: {
         title: {
           display: true,
-          text: 'Artists followers',/* 标题 */
+          text: 'Artists followers',/* the name of the chart */
         }
       },
     }
   });
 }
-
+// click for submit
 document.querySelector('#submit').addEventListener('click', function () {
-  var q = document.querySelector('input').value;
+  var q = document.querySelector('input').value; // this is the user input
   if (q == '') {
     alert('pleace input text')
   } else {
@@ -88,12 +88,12 @@ document.querySelector('#submit').addEventListener('click', function () {
     fetch(`https://api.spotify.com/v1/artists?ids=${q}`, {
       method: 'GET',
       headers: { 'Authorization': 'Bearer ' + localdata.access_token }
-    }).then(response => response.json())
+    }).then(response => response.json())// resolve readable data
       .then(data => {
         maindata = data;
         showdata(maindata)
-      })
-      .catch(err => console.log("Oh, error", err))
+      })// resolve use then
+      .catch(err => console.log("Oh, error", err))// if not use catch 
   }
 })
 
@@ -108,30 +108,30 @@ document.querySelector('#submit').addEventListener('click', function () {
 
 function choseRgb () {
 
-
+// use math random to make chart color random 
   let r = Math.floor(Math.random() * 256);
 
   let g = Math.floor(Math.random() * 256);
 
   let b = Math.floor(Math.random() * 256);
 
-
+// return color veriable
   return `rgb(${r},${g},${b})`;
 }
 document.querySelector('#max').addEventListener('click', function () {
-
+// show the max number 
   console.log(maindata)
-
+ // find the max number
   let showFirst = maindata.artists.sort(dateData("followers", true))[0]
   console.log(showFirst)
   let result = { 'artists': [showFirst] };
   if (myChart != '') {
-
+ // reset
     myChart.destroy();
   }
   console.log(result);
   if (maindata != '') {
-
+ // re-render chart 
     showdata(result)
   }
 })

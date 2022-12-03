738b5339bf4f9ef24b5569aebd23f2e9240ccd44
Prince Okpoziakpo <princeokpoziakpo@gmail.com>
2022-12-03 01:25:16 -0500

    Added functions to access properties, and update the chart

--- client/script.js
+++ client/script.js
@@ -1,33 +1,38 @@
+/* eslint-disable no-plusplus */
+/* eslint-disable no-param-reassign */
 /* eslint-disable no-console */
 /* eslint-disable max-len */
 
-import {fs} from 'fs';
+async function getData() {
+  const url = 'https://api.coingecko.com/api/v3/coins/categories?order=name_asc'; // remote URL! you can test it in your browser
+  const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
+  const json = await data.json();
 
-// async function initChart(targetElement, jsonObject) {
+  return json;
+}
 
-function initChart(targetElement, jsonObject) {
+async function getProperty(json, property) {
+  return json.forEach((item) => item[property]);
+}
+
+async function rotateList(array, start, numOfElements) {
+  const retval = [];
+  if (start > array.length - 1) {
+    start = 0;
+  }
+  for (let i = start + numOfElements; i >= start; i--) {
+    retval.push(array[i]);
+  }
+  return retval;
+}
+
+async function initChart(initLabels, initMarketCapData, targetElement) {
   /*
   targetElement: The DOM object where we want to insert the visualization
   dataObject: The Object containing the information in a one-to-one key-value form
 
   returns Chart: A new Chart object that's configured with the data in dataObject
   */
-  const currencies = Object.keys(jsonObject); // Get the indexes of the cryptocurrency
-  console.log(currencies)
-
-  const currencyDetails = currencies.map((item) => jsonObject[item]); // Get the cryptocurrency data
-  console.log(currencyDetails)
-
-  const labels = currencies.map((currency) => { // Get the currency names
-    const info = currencyDetails[currency];
-    return info.name;
-  });
-  console.log(labels)
-
-  const marketCapData = currencies.map((currency) => { // Get the market cap for each cryptocurrency
-    const info = currencyDetails[currency];
-    return info.market_cap;
-  });
 
   const data = {
     labels: labels,
@@ -56,32 +61,39 @@ function initChart(targetElement, jsonObject) {
   );
 }
 
-function shappedDataforBarChart(json){}
-
-async function getData() {
-  const url = 'https://api.coingecko.com/api/v3/coins/categories?order=name_asc'; // remote URL! you can test it in your browser
-  const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
-  const json = await data.json();
-  console.log(json)
-
-  return json;
+async updateMarketCapChart(chart, labels, marketCap) {
+  let newLabels = []; 
 }
 
-async function mainEvent() {
-  const data = await getData(); // get the json data
-  const chartTarget = document.querySelector('#myChart');
-
-  console.log('started!');
-  fs.writeFile('crypto-data.txt', data, (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('success!');
-    }
+function addData(chart, label, data) {
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.push(data);
   });
+  chart.update();
+}
 
-  initChart(chartTarget, data);
+function removeData(chart) {
+  chart.data.labels.pop();
+  chart.data.datasets.forEach((dataset) => {
+      dataset.data.pop();
+  });
+  chart.update();
+}
 
+
+async function mainEvent() {
+  const data = getData();
+  const json = await data.json(); // get the json data
+  const labels = getProperty(json, 'name');
+  const marketCap = getProperty(json, 'market_cap');
+
+  // Visualizations
+  const chartTarget = document.querySelector('#myChart');
+  const marketCapChart = initChart(labels, marketCap);
+  const filterListButton = document.querySelector('myBtn', async () => {
+    
+  });
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

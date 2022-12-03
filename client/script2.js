1753e360398802d1590aa7d58096297519649d35
htran517 <htran517@terpmail.umd.edu>
2022-12-02 21:24:48 -0500

    html injection base

--- client/script2.js
+++ client/script2.js
@@ -1,29 +1,3 @@
-function initMap() {
-  console.log('initMap');
-  const map = L.map('map').setView([38.9897, -76.9374], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
-  return map;
-}
-
-function markerPlace(array, map) {
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item, index) => {
-    const coordinates = item.location;
-    L.marker([coordinates.latitude, coordinates.longitude]).addTo(map);
-    if (index === 0) {
-      map.setView([coordinates.latitude, coordinates.longitude], 10);
-    }
-  });
-}
-
 function filterList(list, filterInputValue) {
   return list.filter((item) => {
     if (!item.street_number.concat(' ', item.street_name).concat(' ', item.street_type).concat(', ', item.city).concat(', ', item.state).concat(' ', item.zip_code)) {
@@ -67,23 +41,15 @@ async function mainEvent() {
     `${data[0].street_number} ${data[0].street_name} ${data[0].street_type}, ${data[0].city}, ${data[0].state} ${data[0].zip_code}`
   );
 
-  const pageMap = initMap();
-  const mapForm = document.querySelector('.map_form');
-  const propMapInput = document.querySelector('#prop_map');
-  markerPlace(data, pageMap);
+  const propForm = document.querySelector('.property_form');
+  const propInfo = document.querySelector('#prop_info');
 
   if (data?.length > 0) {
-    mapForm.addEventListener('input', (event) => {
-      console.log(event.target.value);
-      const filteredList = filterList(data, event.target.value);
-      markerPlace(filteredList, pageMap);
-    });
-
-    mapForm.addEventListener('submit', (event) => {
+    propForm.addEventListener('submit', (event) => {
       event.preventDefault();
 
-      propMapInput.value = '';
-      markerPlace(data, pageMap);
+      const filteredList = filterList(data, propInfo.value);
+      injectHTML(filteredList);
     });
   }
 }

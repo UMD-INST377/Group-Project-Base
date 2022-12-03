d6192d88b1cd8de6809fb06e87cadf0afa3dc977
Travious46 <46880742+Travious46@users.noreply.github.com>
2022-12-02 22:32:38 -0500

    Added functions and html for chart.js

--- script.js
+++ script.js
@@ -1,4 +1,30 @@
-// 79286f9b48msh455d85ddc2d4316p18f1ccjsnabaae7ed74b1
+function initChart(chart) {
+  // const ctx = document.getElementById('myChart');
+
+  return new Chart(chart, {
+    type: 'bar',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
+}
+
+function processData(array) {
+  
+}
+
 const options = {
   method: 'GET',
   headers: {
@@ -8,14 +34,19 @@ const options = {
 };
 
 async function getData() {
-  const url = 'https://genius.p.rapidapi.com/artists/16775/songs';
+  const url = 'https://genius.p.rapidapi.com/search?q=Kendrick%20Lamar';
   const data = await fetch(url, options);
   const json = await data.json();
+  const reply = json.filter()
   return json;
 }
 async function mainEvent() {
   console.log('Script loaded');
   const data = await getData();
   console.log('Data recived', data);
+  const chartTarget = document.querySelector('#myChart');
+
+  const myChart = initChart(chartTarget);
+  const chartData = await getData();
 }
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

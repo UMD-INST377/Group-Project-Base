b1c073c773f825b6e732bf1ebb37c34ba201c160
na5im <72241606+na5im@users.noreply.github.com>
2022-12-04 15:48:47 -0500

    fix data retrieval

--- client/script.js
+++ client/script.js
@@ -1,18 +1,48 @@
 const form = document.querySelector('.main-form');
 const submit = document.querySelector('#get-location');
-// const data = getData();
 
-// console.log(data);
+function initMap() {
+  console.log('initMap');
+  const map = L.map('map').setView([38.9849, -76.9378], 13);
+  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(map);
+  return map;
+}
+
+// function markerPlace(array, map) {
+//   map.eachLayer((layer) => {
+//     if (layer instanceof L.Marker) {
+//       layer.remove();
+//     }
+//   });
+
+//   array.forEach((item, index) => {
+//     const {coordinates} = item.geocoded_column;
+//     L.marker([coordinates[1], coordinates[0]]).addTo(map);
+//     if (index === 0) {
+//       map.setView([coordinates[1], coordinates[0]], 10);
+//     }
+//   });
+// }
+
+const map = initMap();
 
 async function getData() {
   const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json';
   const apiData = await fetch(url);
   const json = await apiData.json();
-  console.log(json);
-  return json;
+  const reply = json.filter((item) => Boolean(item.geocoded_column))
+    .filter((item) => Boolean(item.council_district));
+  return reply;
 }
 
-const data = getData();
+async function main() {
+  const data = await getData();
+  console.log(data[0]);
+}
+main();
 
 // const data = []
 
@@ -25,47 +55,46 @@ const data = getData();
 //               latitude: latitude,
 //               mobile: item.mobile,
 //               email: item.email
-//           });            
+//           });
 //       }
 //   }
 //   });
 
-console.log(data.keys)
+// function districtCount(data, district) {
+//   let count = 0;
+//   data.forEach((obj) => {
+//     Object.entries(obj).forEach(([key, value]) => {
+//       obj.forEach((litter) => {
+//         if (!litter.council_district) { return; }
+//         if (litter.council_district === district) { count++; }
+//       });
+//     });
 
-function districtCount(data, district) {
-  let count = 0;
-  data.forEach(obj => {
-    Object.entries(obj).forEach(([key, value]) => {
-        obj.forEach((litter) => {
-          if (!litter.council_district) { return; }
-          if (litter.council_district === district) { count++; }
-    })
-});
+//     return count;
+//   });
+// }
 
-  return count;
-})}
+const districts = {
+  1: 0,
+  2: 0,
+  3: 0,
+  4: 0,
+  5: 0,
+  6: 0,
+  7: 0,
+  8: 0,
+  9: 0
+};
 
-const districts = {1: 0,
-                   2: 0,
-                  3: 0,
-                  4: 0,
-                  5: 0,
-                  6: 0,
-                  7: 0,
-                  8: 0,
-                  9: 0};
+// function districtLitters() {
+//   for (let i = 1; i <= 9; i++) {
+//     districts[i] = districtCount(data, i);
+//   }
+// }
 
+// districtLitters();
 
-function districtLitters(){
-
-  for (var i = 1; i <= 9; i++){
-    districts[i] = districtCount(data, i)
-  }
-}
-
-districtLitters();
-
-console.log(districts)
+console.log(districts);
 // function districtCount(district) {
 //  var count = 0;
 // for (var i = 0; i < data.length; i++) {
@@ -76,31 +105,3 @@ console.log(districts)
 //  return count;
 // }
 
-
-function initMap() {
-  console.log('initMap');
-  const map = L.map('map').setView([38.9849, -76.9378], 13);
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map);
-  return map;
-}
-
-function markerPlace(array, map) {
-  map.eachLayer((layer) => {
-    if (layer instanceof L.Marker) {
-      layer.remove();
-    }
-  });
-
-  array.forEach((item, index) => {
-    const {coordinates} = item.Location;
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
-    if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 10);
-    }
-  });
-}
-
-const map = initMap();

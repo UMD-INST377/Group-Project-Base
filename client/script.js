844b5cf8808758c30b64f084f6739e7b477b990a
na5im <72241606+na5im@users.noreply.github.com>
2022-12-04 18:46:27 -0500

    removed duplicates from district array

--- client/script.js
+++ client/script.js
@@ -22,21 +22,21 @@ function initMap() {
   return map;
 }
 
-// function markerPlace(array, map) {
-//   map.eachLayer((layer) => {
-//     if (layer instanceof L.Marker) {
-//       layer.remove();
-//     }
-//   });
+function markerPlace(array, map) {
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      layer.remove();
+    }
+  });
 
-//   array.forEach((item, index) => {
-//     const {coordinates} = item.geocoded_column;
-//     L.marker([coordinates[1], coordinates[0]]).addTo(map);
-//     if (index === 0) {
-//       map.setView([coordinates[1], coordinates[0]], 10);
-//     }
-//   });
-// }
+  array.forEach((item, index) => {
+    const {coordinates} = item.geocoded_column;
+    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    if (index === 0) {
+      map.setView([coordinates[1], coordinates[0]], 10);
+    }
+  });
+}
 
 // const districtCities = {
 //   1: ['Adelphi', 'Beltsville', 'Calverton', 'College Park', 'Laurel', 'Montpelier', 'South Laurel', 'Vansville', 'West Laurel'],
@@ -52,22 +52,20 @@ function initMap() {
 
 const districts = [
   [1, 'Adelphi', 'Beltsville', 'Calverton', 'College Park', 'Laurel', 'Montpelier', 'South Laurel', 'Vansville', 'West Laurel'],
-  [2, 'Adelphi', 'Avondale', 'Brentwood', 'Carole Highlands', 'Chillum', 'Green Meadows', 'Hyattsville', 'Langley Park', 'Lewisdale', 'Mount Rainier', 'North Brentwood'],
-  [3, 'Beacon Heights', 'Berwyn Heights', 'College Park', 'East Pines', 'Glenn Dale', 'Glenridge', 'Landover Hills', 'Lanham', 'New Carrollton', 'Riverdale Heights and Hills', 'Riverdale Park', 'Seabrook', 'Templeton Knolls', 'University Park', 'West Lanham Hills', 'Woodlawn'],
+  [2, 'Avondale', 'Brentwood', 'Carole Highlands', 'Chillum', 'Green Meadows', 'Hyattsville', 'Langley Park', 'Lewisdale', 'Mount Rainier', 'North Brentwood'],
+  [3, 'Beacon Heights', 'Berwyn Heights', 'East Pines', 'Glenridge', 'Lanham', 'New Carrollton', 'Riverdale Heights and Hills', 'Riverdale Park', 'Seabrook', 'Templeton Knolls', 'University Park', 'West Lanham Hills', 'Woodlawn'],
   [4, 'Bowie', 'Glenn Dale', 'Greenbelt', 'Westchester Park', 'Seabrook', 'Upper Marlboro'],
-  [5, 'Bladensburg', 'Cheverly', 'Edmonston', 'Fairmount Heights', 'Glenarden', 'Landover', 'Colmar Manor', 'Cottage City', 'Springdale', 'Bowie', 'Lanham', 'Hyattsville', 'Landover Hills'],
-  [6, 'South Bowie', 'Capitol Heights', 'District Heights', 'Forestville', 'Kettering', 'Largo', 'Mitchellville', 'Upper Marlboro'],
+  [5, 'Bladensburg', 'Cheverly', 'Edmonston', 'Fairmount Heights', 'Glenarden', 'Landover', 'Colmar Manor', 'Cottage City', 'Springdale', 'Landover Hills'],
+  [6, 'South Bowie', 'Forestville', 'Kettering', 'Largo', 'Mitchellville'],
   [7, 'District Heights', 'Bradbury', 'Boulevard Heights', 'Capitol Heights', 'Hillcrest Heights', 'Marlow Heights', 'Seat Pleasant', 'Suitland', 'Morningside'],
-  [8, 'Camp Springs', 'Andrews Air Force Base', 'Clinton', 'Forest Heights', 'Fort Washington', 'Glass Manor', 'Marlow Heights', 'Oxon Hill', 'Temple Hills'],
-  [9, 'Accokeek', 'Aquasco', 'Baden', 'Brandywine', 'Cheltenham', 'Clinton', 'Eagle Harbor', 'Fort Washington', 'Piscataway', 'Upper Marlboro']
+  [8, 'Camp Springs', 'Andrews Air Force Base', 'Clinton', 'Forest Heights', 'Glass Manor', 'Oxon Hill', 'Temple Hills'],
+  [9, 'Accokeek', 'Aquasco', 'Baden', 'Brandywine', 'Cheltenham', 'Eagle Harbor', 'Fort Washington', 'Piscataway']
 
 ];
 
+// This function finds the district a city belongs to.
 function findDistrict(districtArray, city) {
-  let match = [];
-  // for (const i of districtArray) {
-  //   if (i.includes(city)) return i[0];
-  // }
+  const match = [];
   districtArray.forEach((district) => {
     if (district.includes(city)) {
       match.push(district[0]);
@@ -75,7 +73,8 @@ function findDistrict(districtArray, city) {
   });
   return match.pop();
 }
-console.log(findDistrict(districts, 'Upper Marlboro'));
+
+console.log(findDistrict(districts, 'Laurel'));
 
 async function main() {
   const map = initMap();
@@ -134,7 +133,6 @@ main();
 
 // districtLitters();
 
-console.log(districts);
 // function districtCount(district) {
 //  var count = 0;
 // for (var i = 0; i < data.length; i++) {

c24f1f8186e6c79f3c621725f73605261dbd5b38
na5im <72241606+na5im@users.noreply.github.com>
2022-12-04 16:27:52 -0500

    fixed api request and organized file

--- client/script.js
+++ client/script.js
@@ -1,81 +1,5 @@
 const form = document.querySelector('.main-form');
 const submit = document.querySelector('#get-location');
-// const data = getData();
-
-// console.log(data);
-
-async function getData() {
-  const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json';
-  const apiData = await fetch(url);
-  const json = await apiData.json();
-  console.log(json);
-  return json;
-}
-
-const data = getData();
-
-// const data = []
-
-// $.getJSON(data, function (json) {
-//   for (var key in json) {
-//       if (json.hasOwnProperty(key)) {
-//           var item = json[key];
-//           data.push({
-//               district: council_district,
-//               latitude: latitude,
-//               mobile: item.mobile,
-//               email: item.email
-//           });            
-//       }
-//   }
-//   });
-
-console.log(data.keys)
-
-function districtCount(data, district) {
-  let count = 0;
-  data.forEach(obj => {
-    Object.entries(obj).forEach(([key, value]) => {
-        obj.forEach((litter) => {
-          if (!litter.council_district) { return; }
-          if (litter.council_district === district) { count++; }
-    })
-});
-
-  return count;
-})}
-
-const districts = {1: 0,
-                   2: 0,
-                  3: 0,
-                  4: 0,
-                  5: 0,
-                  6: 0,
-                  7: 0,
-                  8: 0,
-                  9: 0};
-
-
-function districtLitters(){
-
-  for (var i = 1; i <= 9; i++){
-    districts[i] = districtCount(data, i)
-  }
-}
-
-districtLitters();
-
-console.log(districts)
-// function districtCount(district) {
-//  var count = 0;
-// for (var i = 0; i < data.length; i++) {
-//      if (data[i].council_district == district) {
-//          count++;
-//      }
-//  }
-//  return count;
-// }
-
 
 function initMap() {
   console.log('initMap');
@@ -95,7 +19,7 @@ function markerPlace(array, map) {
   });
 
   array.forEach((item, index) => {
-    const {coordinates} = item.Location;
+    const {coordinates} = item.geocoded_column;
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
     if (index === 0) {
       map.setView([coordinates[1], coordinates[0]], 10);
@@ -103,4 +27,79 @@ function markerPlace(array, map) {
   });
 }
 
-const map = initMap();
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/9tsa-iner.json';
+  const apiData = await fetch(url);
+  const json = await apiData.json();
+  console.log(json);
+  return json;
+}
+
+async function main() {
+  const map = initMap();
+  const data = await getData();
+
+  console.log(data[0]);
+}
+main();
+
+// const data = []
+
+// $.getJSON(data, function (json) {
+//   for (var key in json) {
+//       if (json.hasOwnProperty(key)) {
+//           var item = json[key];
+//           data.push({
+//               district: council_district,
+//               latitude: latitude,
+//               mobile: item.mobile,
+//               email: item.email
+//           });
+//       }
+//   }
+//   });
+
+// function districtCount(data, district) {
+//   let count = 0;
+//   data.forEach((obj) => {
+//     Object.entries(obj).forEach(([key, value]) => {
+//       obj.forEach((litter) => {
+//         if (!litter.council_district) { return; }
+//         if (litter.council_district === district) { count++; }
+//       });
+//     });
+
+//     return count;
+//   });
+// }
+
+// const districts = {
+//   1: 0,
+//   2: 0,
+//   3: 0,
+//   4: 0,
+//   5: 0,
+//   6: 0,
+//   7: 0,
+//   8: 0,
+//   9: 0
+// };
+
+// function districtLitters() {
+//   for (let i = 1; i <= 9; i++) {
+//     districts[i] = districtCount(data, i);
+//   }
+// }
+
+// districtLitters();
+
+// console.log(districts);
+// function districtCount(district) {
+//  var count = 0;
+// for (var i = 0; i < data.length; i++) {
+//      if (data[i].council_district == district) {
+//          count++;
+//      }
+//  }
+//  return count;
+// }

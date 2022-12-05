3f3c15698011ffff23f852549411a9ffcbb8b6f0
Jzheng-prog <zhengjohn017@gmail.com>
2022-12-05 03:20:56 -0500

    minor changes
    move all script to scrip and comments

--- client/script.js
+++ client/script.js
@@ -29,3 +29,133 @@ fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
 
     placeHolder.innerHTML = out;
 })
+
+
+// Code for the buttons //
+async function fetchData() {
+  const url = "https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json";
+  const response = await fetch(url);
+
+  const dataPoints = await response.json();
+  console.log(dataPoints);
+  return dataPoints;
+};
+
+async function getPayee() {
+  fetchData().then(dataPoints => {
+    const payeeDat = dataPoints.map(function(index){
+      return index.payee_name
+    })
+    const unique = [...new Set(dataPoints.map(item => item.payee_name))];
+    const count = {};
+
+    for (const element of payeeDat) {
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+    myChart.config.data.labels = unique;
+    myChart.config.data.datasets[0].label = "Total";
+    myChart.config.data.datasets[0].data = Object.values(count);
+    myChart.update();
+  })
+
+};
+async function getAgency() {
+  fetchData().then(dataPoints => {
+    const agencyDat = dataPoints.map(function(index){
+      return index.agency
+    })
+
+    const unique = [...new Set(dataPoints.map(item => item.agency))];
+    const count = {};
+
+    for (const element of agencyDat) {
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+    myChart.config.data.labels = unique;
+    myChart.config.data.datasets[0].label = "Total";
+    myChart.config.data.datasets[0].data = Object.values(count);
+    myChart.update();
+  })
+};
+async function getZip() {
+  fetchData().then(dataPoints => {
+    const zipDat = dataPoints.map(function(index){
+      return index.zip_code
+    })
+    const unique = [...new Set(dataPoints.map(item => item.zip_code))];
+    const count = {};
+
+    for (const element of zipDat) {
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+    myChart.config.data.labels = unique;
+    myChart.config.data.datasets[0].label = "Total";
+    myChart.config.data.datasets[0].data = Object.values(count);
+    myChart.update();
+  })
+
+};
+
+
+
+//need to set this to get range instead of unique number
+async function getAmount() {
+  fetchData().then(dataPoints => {
+    const amountDat = dataPoints.map(function(index){
+      return index.amount
+    })
+    const unique = [...new Set(dataPoints.map(item => item.amount))];
+    const count = {};
+
+    for (const element of amountDat) {
+      if (count[element]) {
+        count[element] += 1;
+      } else {
+        count[element] = 1;
+      }
+    }
+    myChart.config.data.labels = unique;
+    myChart.config.data.datasets[0].label = "Total";
+    myChart.config.data.datasets[0].data = Object.values(count);
+    myChart.update();
+  })
+
+};
+
+async function BarChart(){
+  myChart.config.type = "bar";
+  myChart.update();
+}
+
+async function DoughnutChart(){
+  myChart.config.type = "doughnut";
+  myChart.update();
+}
+// Code for the buttons // top
+
+const ctx = document.getElementById('chart');
+
+const myChart = new Chart(ctx, {
+  type: 'doughnut',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [{
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      borderWidth: 1
+    }]
+  }
+});
+

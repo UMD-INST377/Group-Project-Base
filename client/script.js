eb224d5481fef3f9169875508c621ba4cc36ba42
Jaeeun Lee <chsezbet@gmail.com>
2022-12-04 21:33:33 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -82,7 +82,7 @@ function getRandomIntInclusive(min, max){
 
   function initMap(){
     console.log('initMap');
-    const map = L.map('map').setView([38.7849, -76.8721],9);
+    const map = L.map('map').setView([38.880540, -76.831386],16);
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'

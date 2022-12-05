e83548de37d21894634d621a1c3d1c492c362bde
Abdulrezak MOhamed <98439067+RezikEffort@users.noreply.github.com>
2022-12-05 13:29:12 -0500

    js

--- client/script.js
+++ client/script.js
@@ -1,12 +1,13 @@
-const { INTEGER } = require("sequelize");
+const form = document.getElementById("form")
 
-form.eventListener('submit', (Eve) => { 
-    Eve.preventDefault();
+form.eventListener('submit', (e) => { 
+    e.preventDefault();
+    // console.log("Form has been submitted!")
     const ticker = text.value;
 })
 
 async function getData(){
-    const url = 'https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=financialData'
+    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=financialData`
     const data = await fetch (url)
     const json = await data.json()
 }

e1002f70ac42f1a00e44a4e75fbf0e35b5d64cdb
Jason Kearney <76897730+Fictionfriction@users.noreply.github.com>
2022-12-05 11:00:05 -0500

    Merge branch 'main' of https://github.com/Fictionfriction/Group-14-Spotify-Project

--- client/script.js
+++ client/script.js
@@ -1,3 +1,19 @@
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '0e06a17926msh338e516eca5534bp1efaadjsnb8d059881c35',
+    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+  }
+};
+
+fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    return response;
+  })
+  .catch((err) => console.error(err));
+
 d3.selectAll('p').style('color', () => `hsl(${Math.random() * 360},100%,50%)`);
 /* Cuurent Date and Time */
 function date_time() {
@@ -9,19 +25,7 @@ function date_time() {
 setInterval(date_time, 100);
 
 
-
-
-
-
-
-
-
-
-
-
 // D3.js
 // import * as d3 from 'd3';
-
-
 
 // d3.selectAll('p').style('color', () => `hsl(${Math.random() * 360},100%,50%)`);

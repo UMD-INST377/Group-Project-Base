e4d276ed42155ab41a5eb3279a247d2819b9ebe5
Eunbi Kim <ekim0135@terpmail.umd.edu>
2022-12-05 19:18:02 -0500

    edit

--- client/api.js
+++ client/api.js
@@ -1,12 +1,27 @@
+// const options = {
+// 	method: 'GET',
+// 	headers: {
+// 		'X-RapidAPI-Key': '797e066927msh352227c1319737ap175081jsn4d6cedd2f90e',
+// 		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+// 	}
+// };
+
+// fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
+
 const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '797e066927msh352227c1319737ap175081jsn4d6cedd2f90e',
-		'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
+		'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'
 	}
 };
 
-fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
+fetch('https://ott-details.p.rapidapi.com/getParams?param=genre', options)
 	.then(response => response.json())
 	.then(response => console.log(response))
 	.catch(err => console.error(err));
+
+

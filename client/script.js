00aa49635a6c952e7cd29897e583d1146d5a6a37
Sean Leahy <sfleahy2000@gmail.com>
2022-12-05 19:28:50 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -8,57 +8,55 @@ function initMap() {
   return map;
 }
 
-function initChart(chart) {
-const labels = [
-  'CRIME 1',
-  'CRIME 1',
-  'CRIME 2',
-  'CRIME 3',
-  'May',
-  'June'
-];
-
-const data = {
-  labels: labels,
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [0, 10, 5, 2, 30, 45]
-  }]
-};
-const config = {
-  type: 'line',
-  data: data,
-  options: {}
-};
-
-return new Chart(
-  chart,
-  config
-);
-}
-/*   const ctx = document.getElementById('myChart');
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+function initChart(chart, object) {
+  const labels = Object.keys(object);
+  const info = Object.keys(object).map((item) => object[item].length);
+  
+  const data = {
+    labels: labels,
     datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
+      label: 'Restaurants by Category',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
     }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
+  };
+  
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+  
+  return new Chart(
+    chart,
+    config
+  );
   }
-});
-return ctx;
-} */
+  
+  function changeChart(chart, dataObject) {
+  const labels = Object.keys(dataObject);
+  const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+  chart.data.labels = labels;
+  chart.data.datasets.forEach((set) => {
+    set.data = info;
+    return set;
+  });
+  
+  chart.update();
+  }
+  
+  function shapeDataForLineChart(array) {
+  return array.reduce((collection, item) => {
+    if (!collection[item.category]) {
+      collection[item.category] = [item];
+    } else {
+      collection[item.category].push(item);
+    }
+    return collection;
+  }, {});
+  }
+
 
 async function mainEvent() {
 

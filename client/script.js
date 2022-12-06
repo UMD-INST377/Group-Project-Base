10dbcce27a022ced27c5d96c433909423d75177e
Mihiled <hsombat@umd.edu>
2022-12-06 15:52:49 -0500

    info html page text added

--- client/script.js
+++ client/script.js
@@ -1,5 +1,5 @@
 function initMap() {
-  const map = L.map('map').setView([38.9897, -76.9378], 10);
+  const map = L.map('map').setView([38.9897, -76.9378], 11);
   L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'

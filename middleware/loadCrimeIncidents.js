e2078ab3939978acefe123f7d90cde29aeda6748
Sean Leahy <sfleahy2000@gmail.com>
2022-12-06 12:13:03 -0500

    added api endpoint

--- middleware/loadCrimeIncidents.js
+++ middleware/loadCrimeIncidents.js
@@ -1,6 +1,6 @@
 export async function loadCrimeIncidentsData(req, res, next) {
   try {
-    const url = 'https://data.princegeorgescountymd.gov/Public-Safety/Crime-Incidents-February-2017-to-Present/wb4e-w4nf.json'; // remote URL! you can test it in your browser
+    const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json'; // remote URL! you can test it in your browser
     const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
     const json = await data.json(); // the data isn't json until we access it using dot notation
 

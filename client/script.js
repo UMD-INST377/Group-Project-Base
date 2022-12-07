8ec06d0d7b3bc6e113fb2555953236cac96d104c
govindnageswaran <govindnageswaran222@gmail.com>
2022-12-07 14:04:58 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,3 +1,26 @@
+function initChart(chart) {
+  return new Chart(document.getElementById('myChart'), {
+    type: 'bar',
+    data: {
+      labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
+      datasets: [
+        {
+          label: 'Population (millions)',
+          backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
+          data: [2478, 5267, 734, 784, 433]
+        }
+      ]
+    },
+    options: {
+      legend: { display: false },
+      title: {
+        display: true,
+        text: 'Predicted world population (millions) in 2050'
+      }
+    }
+  });
+}
+
 // eslint-disable-next-line no-unused-vars
 async function mainEvent() {
   const options = {
@@ -13,29 +36,6 @@ async function mainEvent() {
   );
   const arrayFromJson = await dataGet.json();
   const response_object = arrayFromJson.response;
-  console.log('hello');
+  console.log(response_object);
   document.addEventListener('DOMContentLoaded', async () => mainEvent());
 }
-
-// function initChart() {
-//   return new Chart(document.getElementById('myChart'), {
-//     type: 'bar',
-//     data: {
-//       labels: ['Africa', 'Asia', 'Europe', 'Latin America', 'North America'],
-//       datasets: [
-//         {
-//           label: 'Population (millions)',
-//           backgroundColor: ['#3e95cd', '#8e5ea2', '#3cba9f', '#e8c3b9', '#c45850'],
-//           data: [2478, 5267, 734, 784, 433]
-//         }
-//       ]
-//     },
-//     options: {
-//       legend: { display: false },
-//       title: {
-//         display: true,
-//         text: 'Predicted world population (millions) in 2050'
-//       }
-//     }
-//   });
-// }

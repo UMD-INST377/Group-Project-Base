55cecc9df85a4d077ede9628cfbf161da47d6007
Eunbi Kim <ekim0135@terpmail.umd.edu>
2022-12-07 15:43:13 -0500

    change

--- client/script.js
+++ client/script.js
@@ -1,4 +1,17 @@
-// get API
+
+/* Cuurent Date and Time */
+function date_time() {
+  const datetimeDisplay = document.getElementById('date-time');
+  const datetimeString = new Date().toLocaleString();
+  const formatString = datetimeString.replace(', ', ' - ');
+  datetimeDisplay.textContent = formatString;
+}
+setInterval(date_time, 100);
+
+
+
+
+/* get API image */
 function getComedy() {
   return fetch('https://api.tvmaze.com/shows')
     .then((response) => response.json())
@@ -21,44 +34,8 @@ positiveButton.addEventListener('click', () => {
 
 
 
-// const options = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '0e06a17926msh338e516eca5534bp1efaadjsnb8d059881c35',
-//     'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-//   }
-// };
 
-// fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', options)
-//   .then((response) => response.json())
-//   .then((response) => {
-//     console.log(response);
-//     return response;
-//   })
-//   .catch((err) => console.error(err));
 
-// const genres = {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': '0e06a17926msh338e516eca5534bp1efaadjsnb8d059881c35',
-//     'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-//   }
-// };
-
-// fetch('https://imdb8.p.rapidapi.com/title/v2/get-popular-movies-by-genre?genre=adventure&limit=100', genres)
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
-
-// d3.selectAll('p').style('color', () => `hsl(${Math.random() * 360},100%,50%)`);
-// /* Cuurent Date and Time */
-// function date_time() {
-//   const datetimeDisplay = document.getElementById('date-time');
-//   const datetimeString = new Date().toLocaleString();
-//   const formatString = datetimeString.replace(', ', ' - ');
-//   datetimeDisplay.textContent = formatString;
-// }
-// setInterval(date_time, 100);
 
 
 

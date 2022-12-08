5a53a90a429ba9ff2a89440b011dfad9c1e187d0
Daniel H Gonzalez <raptor3500@hotmail.com>
2022-12-08 18:15:52 -0500

    working on css and chart.js resizing

--- client/script.js
+++ client/script.js
@@ -44,7 +44,25 @@ async function initEcosystemMarketCapChart() {
     labels: labelSublist,
     datasets: [{
       label: 'Eco System Market Cap',
-      data: marketCapSublist
+      data: marketCapSublist,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
     }]
   };
 
@@ -183,6 +201,7 @@ async function initFallingCryptoTable() {
   nextThreeButton.addEventListener('click', async (submitEvent) => { // display the previous three cryptocurrencies
     console.log('next3');
   });
+  return table;
 }
 
 async function mainEvent() {

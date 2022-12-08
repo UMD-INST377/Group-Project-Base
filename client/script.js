a44c1a2f45b583da46390f7a18fd74030faf38c1
Abdulrezak MOhamed <98439067+RezikEffort@users.noreply.github.com>
2022-12-08 18:04:04 -0500

    js edit, troubleshooting api

--- client/script.js
+++ client/script.js
@@ -1,16 +1,27 @@
 const form = document.getElementById("form")
 
-form.eventListener('submit', (e) => { 
+form.addEventListener('submit', (e) => { 
     e.preventDefault();
     // console.log("Form has been submitted!")
-    const ticker = text.value;
+    const ticker = e.target.value;
 })
 
 async function getData(){
-    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=financialData`
+    // const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/${ticker}?modules=financialData`
+    const url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/aapl?modules=financialData`
     const data = await fetch (url)
     const json = await data.json()
+    console.log(json);
+    return json;
 }
+
+async function mainEvent(){
+    const test = await getData();
+    console.log(test);
+}
+
+document.addEventListener('DOMContentLoaded', async () => mainEvent());
+
 
 /* function filterData (json){
 

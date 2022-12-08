b4b0336e3d7d0cef705d0d2321f94186382555ec
mchan02 <90062401+mchan02@users.noreply.github.com>
2022-12-08 00:30:34 -0500

    chart.js

--- script.js
+++ script.js
@@ -29,18 +29,18 @@ function injectHTML(list) {
     listEl.appendChild(el);
   });
   /*
-      ## JS and HTML Injection
-        There are a bunch of methods to inject text or HTML into a document using JS
-        Mainly, they're considered "unsafe" because they can spoof a page pretty easily
-        But they're useful for starting to understand how websites work
-        the usual ones are element.innerText and element.innerHTML
-        Here's an article on the differences if you want to know more:
-        https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
-      ## What to do in this function
-        - Accept a list of restaurant objects
-        - using a .forEach method, inject a list element into your index.html for every element in the list
-        - Display the name of that restaurant and what category of food it is
-    */
+        ## JS and HTML Injection
+          There are a bunch of methods to inject text or HTML into a document using JS
+          Mainly, they're considered "unsafe" because they can spoof a page pretty easily
+          But they're useful for starting to understand how websites work
+          the usual ones are element.innerText and element.innerHTML
+          Here's an article on the differences if you want to know more:
+          https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
+        ## What to do in this function
+          - Accept a list of restaurant objects
+          - using a .forEach method, inject a list element into your index.html for every element in the list
+          - Display the name of that restaurant and what category of food it is
+      */
 }
 
 function processRestaurants(list) {
@@ -53,20 +53,20 @@ function processRestaurants(list) {
   return newArray;
 
   /*
-        ## Process Data Separately From Injecting It
-          This function should accept your 1,000 records
-          then select 15 random records
-          and return an object containing only the restaurant's name, category, and geocoded location
-          So we can inject them using the HTML injection function
-          You can find the column names by carefully looking at your single returned record
-          https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
-        ## What to do in this function:
-        - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
-        - using a .map function on that range,
-        - Make a list of 15 random restaurants from your list of 100 from your data request
-        - Return only their name, category, and location
-        - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
-      */
+          ## Process Data Separately From Injecting It
+            This function should accept your 1,000 records
+            then select 15 random records
+            and return an object containing only the restaurant's name, category, and geocoded location
+            So we can inject them using the HTML injection function
+            You can find the column names by carefully looking at your single returned record
+            https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
+          ## What to do in this function:
+          - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
+          - using a .map function on that range,
+          - Make a list of 15 random restaurants from your list of 100 from your data request
+          - Return only their name, category, and location
+          - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
+        */
 }
 
 function filterList(array, filterInputValue) {
@@ -91,7 +91,7 @@ function initMap() {
 function markerPlace(array, map) {
   console.log('markerPlace', array);
   map.eachLayer((layer) => {
-    if (layer instanceof L.marker) {
+    if (layer instanceof L.Marker) {
       layer.remove();
     }
   });
@@ -104,34 +104,77 @@ function markerPlace(array, map) {
   });
 }
 
+function initChart(chart) {
+  const labels = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June'
+  ];
+
+  const data = {
+    labels: labels,
+    datasets: [{
+      label: 'My First dataset',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [0, 10, 5, 2, 20, 30, 45]
+    }]
+  };
+
+  const config = {
+    type: 'line',
+    data: data,
+    options: {}
+  };
+
+  return new Chart(
+    document.getElementById('myChart'),
+    config
+  );
+}
+
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json';
+  const data = await fetch(url);
+  const json = await data.json();
+  const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+}
+
+
 async function mainEvent() {
   /*
-        ## Main Event
-          Separating your main programming from your side functions will help you organize your thoughts
-          When you're not working in a heavily-commented "learning" file, this also is more legible
-          If you separate your work, when one piece is complete, you can save it and trust it
-      */
-  const pageMap = initMap();
+          ## Main Event
+            Separating your main programming from your side functions will help you organize your thoughts
+            When you're not working in a heavily-commented "learning" file, this also is more legible
+            If you separate your work, when one piece is complete, you can save it and trust it
+        */
+  // const pageMap = initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
-  const loadAnimation = document.querySelector('.lds-ellipsis');
-  submit.style.display = 'none'; // let your submit button disappear
+  const chartTarget = document.querySelector('#myChart');
+  // const loadAnimation = document.querySelector('.lds-ellipsis');
+  // submit.style.display = 'none'; // let your submit button disappear
 
   /*
-        Let's get some data from the API - it will take a second or two to load
-        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-        It's at about line 27 - go have a look and see what we're retrieving and sending back.
-       */
+          Let's get some data from the API - it will take a second or two to load
+          This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
+          It's at about line 27 - go have a look and see what we're retrieving and sending back.
+         */
   const results = await fetch('/api/foodServicePG');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
+  initChart(chartTarget);
+  const chartData = await getData();
   /*
-        Below this comment, we log out a table of all the results using "dot notation"
-        An alternate notation would be "bracket notation" - arrayFromJson["data"]
-        Dot notation is preferred in JS unless you have a good reason to use brackets
-        The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-      */
+          Below this comment, we log out a table of all the results using "dot notation"
+          An alternate notation would be "bracket notation" - arrayFromJson["data"]
+          Dot notation is preferred in JS unless you have a good reason to use brackets
+          The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
+        */
   console.table(arrayFromJson.data);
 
   // in your browser console, try expanding this object to see what fields are available to work with
@@ -142,11 +185,8 @@ async function mainEvent() {
   console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+  if (chartData.length > 0) { // the question mark in this means "if this is set at all"
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
-
-    loadAnimation.classList.remove('lds-ellipsis');
-    loadAnimation.classList.add('lds-ellipsis_hidden');
 
     let currentList = [];
 
@@ -154,7 +194,7 @@ async function mainEvent() {
       console.log(event.target.value);
       const newArray = filterList(currentList, event.target.value);
       injectHTML(newArray);
-      markerPlace(newArray, pageMap);
+      // markerPlace(newArray, pageMap);
     });
     // And here's an eventListener! It's listening for a "submit" button specifically being clicked
     // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
@@ -167,7 +207,7 @@ async function mainEvent() {
 
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
-      markerPlace(currentList, pageMap);
+      // markerPlace(currentList, pageMap);
 
       // By separating the functions, we open the possibility of regenerating the list
       // without having to retrieve fresh data every time
@@ -176,8 +216,8 @@ async function mainEvent() {
   }
 }
 /*
-      This last line actually runs first!
-      It's calling the 'mainEvent' function at line 57
-      It runs first because the listener is set to when your HTML content has loaded
-    */
+        This last line actually runs first!
+        It's calling the 'mainEvent' function at line 57
+        It runs first because the listener is set to when your HTML content has loaded
+      */
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests

436af1435075bbdd870d32756b2d2137a319cf72
govindnageswaran <govindnageswaran222@gmail.com>
2022-12-08 18:49:49 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -42,21 +42,6 @@ async function mainEvent() {
   initChart();
 }
 
-const team_players = document.querySelector('.players');
-const player_list = Array.from(team_players);
-const totalPlayers = player_list.length;
-
-function show_players_team() {
-  team_players.classList.remove('player_hidden');
-  team_players.classList.add('player_visible');
-}
-document.querySelector('.btn')
-  .addEventListener('click', () => {
-    console.log('clicked vissible for players');
-    console.log(team_players);
-    show_players_team();
-  });
-
 function ShowDiv() {
   document.getElementById('myDiv').style.display = '';
 }

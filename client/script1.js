da0dacb52036bab846e81b26de3f03f01d964632
Michael Chang <Mchang18@terpmail.umd.edu>
2022-12-08 13:15:36 -0500

    update script1

--- client/script1.js
+++ client/script1.js
@@ -1,33 +1,30 @@
-const movie_URL = 'https://api.themoviedb.org/3/movie/now_playing?api_key=b5fa8ab3e139be5c0b6e60ab752e3d09&language=en-US&page=1';
-const image_movie = 'https://image.tmdb.org/t/p/w500';
-
 const form = document.getElementById('form');
 const main = document.getElementById('main');
 
-// Movies
-getMovies(movie_URL);
-async function getMovies(url) {
+// Gets one page (20 items) of now-playing movies
+NowPlayingMovies('https://api.themoviedb.org/3/movie/now_playing?api_key=b5fa8ab3e139be5c0b6e60ab752e3d09&language=en-US&page=1');
+async function NowPlayingMovies(url) {
   const res = await fetch(url);
   const data = await res.json();
   displayMovies(data.results);
   console.log(data.results);
 }
 
-// Displays Movies
-function displayMovies(getMovies) {
+// Displays movies on homepage
+function displayMovies(NowPlayingMovies) {
   main.innerHTML = '';
-  getMovies.forEach((movie) => {
+  NowPlayingMovies.forEach((movie) => {
     const {
       title, poster_path, vote_average, overview
     } = movie;
-    const moviesElement = document.createElement('div');
-    moviesElement.classList.add('movie');
-    moviesElement.innerHTML = `
-    <img src= "${image_movie + poster_path}" alt = "${title}" />
+    const movies_Element = document.createElement('div');
+    movies_Element.classList.add('movie');
+    movies_Element.innerHTML = `
+    <img src= "${'https://image.tmdb.org/t/p/w500' + poster_path}" alt = "${title}" />
     <div class = 'movie-info'>
     <h3>${title}</h3>
     </div>
     `;
-    main.appendChild(moviesElement);
+    main.appendChild(movies_Element);
   });
 }

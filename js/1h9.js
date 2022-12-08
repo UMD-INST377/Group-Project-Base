a8740ce8cc23ad8cada76f86353eb43ab19b3a09
JPHart98 <89784428+JPHart98@users.noreply.github.com>
2022-12-08 12:27:09 -0500

    Changed order and updated filter function
    Changed the order of html pages on index.html and added code into each js file to have filter function working

--- js/1h9.js
+++ js/1h9.js
@@ -28,7 +28,7 @@ function injectHTML(list) {
 // Function that fliters the list from the API data. //
 function filterList(list, filterInputValue) {
   return list.filter((item) => {
-    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseName = `${item.street_number} ${item.street_name} ${item.street_type} ${item.zip_code}`.toLowerCase();
     const lowerCaseQuery = filterInputValue.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
   });

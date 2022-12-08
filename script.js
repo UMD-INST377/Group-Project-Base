4678cfc18bd6defdb978f9d2a48dc69eb62f5e6b
seanl2 <76665604+seanl2@users.noreply.github.com>
2022-12-08 14:36:11 -0500

    chart shows

--- script.js
+++ script.js
@@ -8,25 +8,21 @@ function initMap() {
   return map;
 }
 
-function initChart(chart) {
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June'
-  ];
+function initChart(chart, object) {
+  const labels = Object.keys(object);
+  const info = Object.keys(object).map((item) => object[item].length);
 
   const data = {
     labels: labels,
     datasets: [{
-      label: 'My First dataset',
+      label: 'Crimes by Category',
       backgroundColor: 'rgb(255, 99, 132)',
       borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 30, 45]
+      data: info
     }]
-  };
+  }; 
+  console.log('initChart'); // Doesn't reach this
+
   const config = {
     type: 'line',
     data: data,
@@ -38,41 +34,74 @@ function initChart(chart) {
     config
   );
 }
-/*   const ctx = document.getElementById('myChart');
-new Chart(ctx, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
+
+function shapeDataForLineChart(array) {
+  return array.reduce((collection, item) => {
+    if (!collection[item.category]) {
+      collection[item.category] = [item];
+    } else {
+      collection[item.category].push(item);
     }
-  }
-});
-return ctx;
-} */
+    return collection;
+  }, {});
+}
+
+
+async function getData() {
+  const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json';
+  const data = await fetch(url);
+  const json = await data.json();
+  // eslint-disable-next-line max-len
+  const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+  return reply;
+}
+
+function changeChart(chart, dataObject) {
+  const labels = Object.keys(dataObject);
+  const info = Object.keys(dataObject).map((item) => dataObject[item].length);
+  chart.data.labels = labels;
+  chart.data.datasets.forEach((set) => {
+    set.data = info;
+    return set;
+  });
+
+  chart.update();
+}
+
 
 async function mainEvent() {
   const pageMap = initMap();
-
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
-  /* const loadAnimation = document.querySelector('.lds-ellipsis'); */
+  console.log('hey');
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-resto');
+  // const loadAnimation = document.querySelector('.lds-ellipsis');
   const restoName = document.querySelector('#resto');
   const chartTarget = document.querySelector('#myChart');
+  // submit.style.display = 'none';
+  
+  console.log('hii');
+  const chartData = await getData();
+  const shapedData = shapeDataForLineChart(chartData);
+  const myChart = initChart(chartTarget, shapedData);
+
+  if (chartData?.length > 0) {
+    submit.style.display = 'block';
+
+    loadAnimation.classList.remove('ldas-ellipsis');
+    loadAnimation.classList.add('lds-ellipsis_hidden');
+
+    let currentList = [];
+
+    form.addEventListener('input', (event) => {
+      console.log(event.target.value);
+    });
+
+
+  }
 
   const results = await fetch('/api/CrimeIncidentsPG');
-  const arrayFromJson = await results.json();
 
-  initChart(chartTarget);
+  const arrayFromJson = await results.json();
 }
 
 document.addEventListener('DOMContentLoaded', async () => mainEvent());

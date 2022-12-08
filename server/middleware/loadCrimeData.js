51857821dd1b82d7be440c6f968369d465954a20
mchan02 <90062401+mchan02@users.noreply.github.com>
2022-12-08 03:22:09 -0500

    crime data map

---
+++ server/middleware/loadCrimeData.js
@@ -0,0 +1,28 @@
+/* eslint-disable max-len */
+/* eslint-disable import/prefer-default-export */
+import fetch from 'node-fetch';
+
+/*
+  This function loads data from our third party API
+  It has been separated out so it will not be repeated in our "controllers"
+  This separation also simplifies our imports - note that 'fetch' is imported here and not elsewhere.
+*/
+
+// note "export" keyword here
+export async function importCrimeData(req, res, next) {
+  try {
+    const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json'; // remote URL! you can test it in your browser
+    const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
+    const json = await data.json(); // the data isn't json until we access it using dot notation
+
+    const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+
+    console.log('Results in crimeData middleware', json.length); // let's check that something's there before we return it
+    req.crimeData = reply; // and let's _attach_ the data to our request object here
+    next();
+  } catch (err) {
+    // and let's handle any errors by closing the request with a message
+    console.log('Data request failed', err);
+    res.json({ message: 'Data request failed', error: err });
+  }
+}

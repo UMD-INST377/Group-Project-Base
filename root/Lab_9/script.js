d4503f7b167988af98a25a73cd1cb0a8ca421a04
seanl2 <76665604+seanl2@users.noreply.github.com>
2022-12-08 20:54:06 -0500

    Update script.js

--- root/Lab_9/script.js
+++ root/Lab_9/script.js
@@ -1,53 +1,9 @@
-/* eslint-disable max-len */
-
-/*
-  Hook this script to index.html
-  by adding `<script src="script.js">` just before your closing `</body>` tag
-*/
-
-/*
-  ## Utility Functions
-    Under this comment place any utility functions you need - like an inclusive random number selector
-    https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-*/
-
+/* eslint-disable no-new-wrappers */
 function getRandomIntInclusive(min, max) {
-  newMin = Math.ceil(min);
-  newMax = Math.floor(max);
+  const newMin = Math.ceil(min);
+  const newMax = Math.floor(max);
+  // eslint-disable-next-line max-len
   return Math.floor(Math.random() * (newMax - newMin + 1) + newMin); // The maximum is inclusive and the minimum is inclusive
-}
-
-function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector('#restaurant_list');
-  target.innerHTML = '';
-
-  const listEl = document.createElement('ol');
-  target.appendChild(listEl);
-  list.forEach((item) => {
-    const el = document.createElement('li');
-    el.innerText = item.name;
-    listEl.appendChild(el);
-  });
-}
-
-function processRestaurants(list) {
-  console.log('fired restaurants list');
-  const range = [...Array(15).keys()]; // Special notation to create an array of 15 elements
-  const newArray = range.map((item) => {
-    const index = getRandomIntInclusive(0, list.length);
-    return list[index];
-  });
-  return newArray;
-}
-
-function filterList(list, filterInputValue) {
-  return list.filter((item) => {
-    if (!item.name) { return; }
-    const lowerCaseName = item.name.toLowerCase();
-    const lowerCaseQuery = filterInputValue.toLowerCase();
-    return lowerCaseName.includes(lowerCaseQuery);
-  });
 }
 
 function initMap() {
@@ -61,17 +17,21 @@ function initMap() {
 }
 
 function markerPlace(array, map) {
-  // const marker = L.marker([51.5, -0.09]).addTo(map);
+  console.log('markerPlace');
   map.eachLayer((layer) => {
     if (layer instanceof L.Marker) {
       layer.remove();
     }
   });
+
   array.forEach((item, index) => {
-    const { coordinates } = item.geocoded_column_1;
-    L.marker([coordinates[1], coordinates[0]]).addTo(map);
+    const lat = new Number(item.latitude);
+    const lng = new Number(item.longitude);
+    const newLatLng = L.latLng(lat, lng);
+
+    L.marker(newLatLng).addTo(map);
     if (index === 0) {
-      map.setView([coordinates[1], coordinates[0]], 10);
+      map.setView(newLatLng, 10);
     }
   });
 }
@@ -83,23 +43,68 @@ function initChart(chart, object) {
   const data = {
     labels: labels,
     datasets: [{
-      label: 'Restaurants by Category',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
+      label: 'My First Dataset',
+      data: info,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 205, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(201, 203, 207, 0.2)'
+      ],
+      borderColor: [
+        'rgb(255, 99, 132)',
+        'rgb(255, 159, 64)',
+        'rgb(255, 205, 86)',
+        'rgb(75, 192, 192)',
+        'rgb(54, 162, 235)',
+        'rgb(153, 102, 255)',
+        'rgb(201, 203, 207)'
+      ],
+      borderWidth: 1
     }]
   };
-
+  console.log('initChart');
   const config = {
-    type: 'line',
+    type: 'bar',
     data: data,
-    options: {}
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    },
   };
 
   return new Chart(
     chart,
     config
   );
+}
+
+function processCrime(list) {
+  console.log('fired processCrime');
+  const newArray = [];
+  list.forEach((item, index) => {
+    const date = new Number(item.date.substring(0, 4));
+    newArray.push(item);
+  });
+  console.log(newArray[0]);
+  return newArray;
+}
+
+function shapeDataForBarChart(array) {
+  return array.reduce((collection, item) => {
+    if (!collection[item.category]) {
+      collection[item.category] = [item];
+    } else {
+      collection[item.category].push(item);
+    }
+    return collection;
+  }, {});
 }
 
 function changeChart(chart, dataObject) {
@@ -114,112 +119,45 @@ function changeChart(chart, dataObject) {
   chart.update();
 }
 
-function shapeDataForLineChart(array) {
-  return array.reduce((collection, item) => {
-    if (!collection[item.category]) {
-      collection[item.category] = [item];
-    } else {
-      collection[item.category].push(item);
-    }
-    return collection;
-  }, {});
-}
-
 async function getData() {
-  const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json'; // remote URL! you can test it in your browser
-  const data = await fetch(url); // We're using a library that mimics a browser 'fetch' for simplicity
-  const json = await data.json(); // the data isn't json until we access it using dot notation
-  const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
-  return reply;
+  const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json';
+  const data = await fetch(url);
+  const json = await data.json();
+  // eslint-disable-next-line max-len
+  // const reply = json.filter((item) => Boolean(item.geocoded_column_1)).filter((item) => Boolean(item.name));
+  return json;
 }
 
 async function mainEvent() {
-  /*
-          ## Main Event
-            Separating your main programming from your side functions will help you organize your thoughts
-            When you're not working in a heavily-commented "learning" file, this also is more legible
-            If you separate your work, when one piece is complete, you can save it and trust it
-        */
+  const pageMap = initMap();
 
-  // const pageMap = initMap();
-  // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-resto');
   const loadAnimation = document.querySelector('.lds-ellipsis');
+  // const restoName = document.querySelector('#resto');
   const chartTarget = document.querySelector('#myChart');
-  submit.style.display = 'none'; // let your submit button disappear
+  // submit.style.display = 'none';
 
-  /*
-          Let's get some data from the API - it will take a second or two to load
-          This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-          It's at about line 27 - go have a look and see what we're retrieving and sending back.
-         */
-  const results = await fetch('/api/foodServicePG');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
-  const chartData = await getData();
-  const shapedData = shapeDataForLineChart(chartData);
-  console.log(shapedData);
+  const arrayFromJson = await getData();
+  const shapedData = shapeDataForBarChart(arrayFromJson);
   const myChart = initChart(chartTarget, shapedData);
 
-  /*
-          Below this comment, we log out a table of all the results using "dot notation"
-          An alternate notation would be "bracket notation" - arrayFromJson["data"]
-          Dot notation is preferred in JS unless you have a good reason to use brackets
-          The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-        */
-  console.table(arrayFromJson.data);
-
-  // in your browser console, try expanding this object to see what fields are available to work with
-  // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
-
-  // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
-
-  // This IF statement ensures we can't do anything if we don't have information yet
-  if (chartData.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
-
+  if (arrayFromJson?.length > 0) {
+    submit.style.display = 'block';
+    console.log('hii');
     loadAnimation.classList.remove('lds-ellipsis');
     loadAnimation.classList.add('lds-ellipsis_hidden');
 
     let currentList = [];
 
-    form.addEventListener('input', (event) => {
-      console.log(event.target.value);
-      const filteredList = filterList(currentList, event.target.value);
-      injectHTML(filteredList);
-      const localData = shapeDataForLineChart(filterList);
-      changeChart(myChart, localData);
-      // markerPlace(filteredList, pageMap);
-    });
-
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
+      console.log(submitEvent.target);
+      currentList = processCrime(arrayFromJson);
 
-      // This constant will have the value of your 15-restaurant collection when it processes
-      currentList = processRestaurants(arrayFromJson.data);
-
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(currentList);
-      const localData = shapeDataForLineChart(chartData);
-      changeChart(myChart, localData);
-      // markerPlace(currentList, pageMap);
-
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
+      markerPlace(currentList, pageMap);
     });
   }
 }
 
-/*
-      This last line actually runs first!
-      It's calling the 'mainEvent' function at line 57
-      It runs first because the listener is set to when your HTML content has loaded
-    */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

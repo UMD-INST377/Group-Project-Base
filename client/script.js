56be89a02bcd68d6174f928474f1732271f0cdf3
Abdulrezak MOhamed <98439067+RezikEffort@users.noreply.github.com>
2022-12-09 10:23:45 -0500

    js edit

--- client/script.js
+++ client/script.js
@@ -1,20 +1,28 @@
-const form = document.getElementById("form")
+/* const form = document.getElementById("form")
 
 form.addEventListener('submit', (e) => { 
     e.preventDefault();
     // console.log("Form has been submitted!")
     const ticker = e.target.value;
-})
+}) */
+
+/* var Fred = require('fred-api');
+
+apiKey = process.env.FRED_KEY;
+fred   = new Fred('04a0ff4303f5c02f2a434ad8543c1b1c');
+fred.getSeries({series_id: 'GNPCA'}, function(error, result) {
+    console.log(result)
+}); */
 
 async function getData(){
     url = `https://query1.finance.yahoo.com/v10/finance/quoteSummary/tsla?modules=financialData`
-    const data = await get(url,headers={
-
-        'User-Agent': 'My User Agent 1.0'
-        })
-    const json = await data.json()
-    console.log(json);
-    return json;
+    try{
+        const data = await fetch(url,headers={'User-Agent': 'My User Agent 1.0'})
+        return await data.json()
+    }
+    catch (error){
+        console.log("error",error)
+    }
 }
 
 async function mainEvent(){

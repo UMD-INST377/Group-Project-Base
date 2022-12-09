7c36a2e4df7c45bb37f6a746fa35a7eb4b28dad3
Eunbi Kim <ekim0135@terpmail.umd.edu>
2022-12-09 02:34:15 -0500

    asynchrouos data request API and array method

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import fetch from 'node-fetch';
 
-import foodService from './foodService.js';
 
 const router = express.Router();
 
@@ -9,9 +8,44 @@ router.get('/', (req, res) => {
   res.send('Here is TV shows API!');
 });
 
-// Generic API inclusion demonstration
-// Replace this with the group member's actual route
-// This leads to /api/member1
-router.use('/foodService', foodService);
 
-export default router;
+// fetch TV shows API 
+router.route('/shows') // actually localhost:3000/api/shows
+  .get(async (req, res) => {
+    try {
+      const url = 'https://api.tvmaze.com/shows';
+      const data = await fetch(url);
+      const json = await data.json();
+      console.log(json);
+
+      res.json({data: json});
+    } catch (error) {
+      console.log(error);
+      res.json({error: error});
+    }
+  })
+  .put((req, res) => {
+    try {
+      res.json({message: 'put TV shows endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })
+  .post((req, res) => {
+    try {
+      res.json({message: 'post TV shows endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  })
+  .delete((req, res) => {
+    try {
+      res.json({message: 'delete TV shows endpoint'});
+    } catch (error) {
+      console.log(error);
+      res.json({error: 'Something went wrong on the server'});
+    }
+  });
+  export default router; 

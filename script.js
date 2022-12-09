7e8429ee5ca5de5372f78839e55c13fb45197c11
mchan02 <90062401+mchan02@users.noreply.github.com>
2022-12-09 02:17:16 -0500

    delete chart

--- script.js
+++ script.js
@@ -111,58 +111,6 @@ function markerPlace(array, map) {
   });
 }
 
-function initChart(chart) {
-  const labels = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June'
-  ];
-
-  const data = {
-    labels: labels,
-    datasets: [{
-      label: 'My First dataset',
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgb(255, 99, 132)',
-      data: [0, 10, 5, 2, 20, 30, 45]
-    }]
-  };
-
-  const config = {
-    type: 'line',
-    data: data,
-    options: {}
-  };
-
-  return new Chart(
-    chart,
-    config
-  );
-}
-
-/* function shapeDataForLineChart(array) {
-    if (!array[item.category]) {
-      array[item.category] = [item]
-    } else {
-      array[item.category].push(item);
-    }
-    console.log(array)
-    return collection;
-  }, {}); 
-}
-
-async function getData() {
-  const url = 'https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json';
-  const data = await fetch(url);
-  const json = await data.json();
-  const reply = json.filter((item) => Boolean(item.location)).filter((item) => Boolean(item.street_address));
-  return reply;
-} */
-
-
 async function mainEvent() {
   /*
         ## Main Event
@@ -171,18 +119,11 @@ async function mainEvent() {
           If you separate your work, when one piece is complete, you can save it and trust it
       */
   const pageMap = initMap();
-  // the async keyword means we can make API requests
-  const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
-  const submit = document.querySelector('#get-resto'); // get a reference to your submit button
+  const form = document.querySelector('.main_form');
+  const submit = document.querySelector('#get-resto');
   const loadAnimation = document.querySelector('.lds-ellipsis');
-  const chartTarget = document.querySelector('#myChart');
   submit.style.display = 'none'; // let your submit button disappear
 
-  /*
-        Let's get some data from the API - it will take a second or two to load
-        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-        It's at about line 27 - go have a look and see what we're retrieving and sending back.
-       */
   const results = await fetch('https://data.princegeorgescountymd.gov/resource/wb4e-w4nf.json');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
 
@@ -201,10 +142,6 @@ async function mainEvent() {
   // this is called "string interpolation" and is how we build large text blocks with variables
   console.log(`${arrayFromJson[0].incident_case_id} ${arrayFromJson[0].clearance_code_inc_type}`);
 
-  initChart(chartTarget);
-  // const chartData = await getData();
-  // const shapedData = shapeDataForLineChart(chartData);
-  // const myChart = initChart(chartTarget, shapedData);
   // This IF statement ensures we can't do anything if we don't have information yet
   if (arrayFromJson?.length > 0) { // the question mark in this means "if this is set at all"
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
@@ -220,30 +157,15 @@ async function mainEvent() {
       injectHTML(newArray);
       markerPlace(newArray, pageMap);
     });
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
     form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
       submitEvent.preventDefault();
-
-      // This constant will have the value of your 15-restaurant collection when it processes
       currentList = processCrimes(arrayFromJson);
 
       console.log(currentList);
 
-      // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
       markerPlace(currentList, pageMap);
-
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
     });
   }
 }
-/*
-      This last line actually runs first!
-      It's calling the 'mainEvent' function at line 57
-      It runs first because the listener is set to when your HTML content has loaded
-    */
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+document.addEventListener('DOMContentLoaded', async () => mainEvent()); 

c379f5133f3863642b3a3413faa9b7dffeb02308
KiraFreedman <kira.ally@gmail.com>
2022-12-08 21:35:28 -0500

    resetting things

--- Food Data/script.js
+++ Food Data/script.js
@@ -57,46 +57,17 @@ async function makeChart() {
 }
 makeChart();
 
-function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector("#data_list");
-  target.innerHTML = '';
 
-  const listEL = document.createElement('ol');
-  target.appendChild(listEL);
-  list.forEach((item) => {
-    const el = document.createElement('li');
-    el.innerText = item.name;
-    listEL.appendChild(el);
-  });
-
-  function getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); 
-  }
-
-  async function sendData(list){
-    console.log('fired results');
-    const range = [...Array(3).keys()];
-    const newArray = range.map((item) => {
-      const index = getRandomIntInclusive(0, list.length);
-      return list[index];
-    })
-    return newArray;
-  }
 async function mainEvent() {
   const form = document.querySelector('.main_form');
 
   const submit = document.querySelector('#get-resto');
-  submit.style.display = 'none';
-  // const resto = document.querySelector('#resto');
+  const resto = document.querySelector('#resto');
   const chart = document.querySelector('#myChart');
 
-  const results = await fetch('/api/foodServicePG');
-  const arrayFromJson = await getData();
+  // const arrayFromJson = await getData();
 
-  console.table(arrayFromJson);
+  // console.table(arrayFromJson);
 
 
   const myChart = makeChart(chart);
@@ -104,8 +75,8 @@ async function mainEvent() {
   form.addEventListener('submit', (submitEvent) => {
     submitEvent.preventDefault();
 
-    const dataList = sendData(arrayFromJson.data);
-    console.log(dataList);
+    // const dataList = sendData(arrayFromJson.data);
+    // console.log(dataList);
   });
 }
 

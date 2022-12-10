8f4e4abbc4bafab34b91b62c329fccdb749475da
govindnageswaran <govindnageswaran222@gmail.com>
2022-12-09 20:40:31 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-use-before-define */
 async function mainEvent() {
+  // wizards data
   const options = {
     method: 'GET',
     headers: {
@@ -7,7 +8,7 @@ async function mainEvent() {
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
   };
-  const dataGet = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=41&season=2021', options);
+  // hawks data
   const options1 = {
     method: 'GET',
     headers: {
@@ -15,39 +16,55 @@ async function mainEvent() {
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
   };
+  // celtics data
+  const options2 = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '5dc02365d4mshd9eff5d73eae486p1c6bb0jsnd676be7e93c5',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
+  // heat data
+  const options3 = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '5dc02365d4mshd9eff5d73eae486p1c6bb0jsnd676be7e93c5',
+      'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+    }
+  };
+  const dataGet = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=41&season=2021', options);
   const dataGet1 = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=1&season=2021', options1);
+  const dataGet2 = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=2&season=2021', options2);
+  const dataGet3 = await fetch('https://api-nba-v1.p.rapidapi.com/teams/statistics?id=20&season=2021', options3);
 
-  // wizards data
+  // changing it to json format
   const arrayFromJson = await dataGet.json();
-  const response_object = arrayFromJson.response;
-  // hawks data
   const arrayFromJson2 = await dataGet1.json();
-  const response_object1 = arrayFromJson2.response;
-  // loaded the wizards data and it shows on console
-  console.log(response_object[0]);
-  // loaded the hawks data and it shows on console
-  console.log(response_object1[0]);
+  const arrayFromJson3 = await dataGet2.json();
+  const arrayFromJson4 = await dataGet3.json();
 
-  // changing the wizards data that are strings into float
-  const fgp = parseFloat(response_object[0].fgp);
-  const ftp = parseFloat(response_object[0].ftp);
-  const tpp = parseFloat(response_object[0].tpp);
+  const wizards_data = arrayFromJson.response;
+  const hawks_data = arrayFromJson2.response;
+  const celtics_data = arrayFromJson3.response;
+  const heat_data = arrayFromJson4.response;
 
-  // changing the hawks data that are strings into float
-  const fgp1 = parseFloat(response_object1[0].fgp);
-  const ftp1 = parseFloat(response_object1[0].ftp);
-  const tpp1 = parseFloat(response_object1[0].tpp);
+  console.log(wizards_data[0]);
+  console.log(hawks_data[0]);
+  console.log(celtics_data[0]);
+  console.log(heat_data[0]);
 
   function initChart() {
     const data = {
       labels: [
-        'Free Throw Percentage',
-        'Free Goal Percentage',
-        'Three Point Percentage'
+        'Wizards Total Games Played',
+        'Hawks Total Games Played',
+        'Celtics Total Games Played',
+        'Heat Total Games Played'
       ],
       datasets: [{
-        label: 'ATL Hawks Shot Percentage',
-        data: [ftp1, fgp1, tpp1],
+        label: '',
+        // eslint-disable-next-line max-len
+        data: [wizards_data[0].games, hawks_data[0].games, celtics_data[0].games, heat_data[0].games],
         backgroundColor: [
           'rgb(255, 99, 132)',
           'rgb(54, 162, 235)',
@@ -57,7 +74,7 @@ async function mainEvent() {
         padding: 20
       }]
     };
-    const config = {type: 'doughnut', data: data};
+    const config = {type: 'bar', data: data};
 
     return new Chart(document.querySelector('#myChart'), config);
   }
@@ -68,8 +85,8 @@ function ShowDiv() {
   document.getElementById('myDiv').style.display = '';
 }
 
-function imageClickedWizard() {
-  window.location = 'wizard_stats.html';
+function imageClickedTeams() {
+  window.location = 'at_stats.html';
   return window;
 }
 

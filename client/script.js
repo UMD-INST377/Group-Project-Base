e2b5f0fa2bc021f02acce53fe71fa42bbf1a98f4
Jzheng-prog <zhengjohn017@gmail.com>
2022-12-09 23:04:26 -0500

    Update script.js
    made the fetch into function. createTable function. mainEvent is being called with filter. Filter data is being returned and filter table is being created.

--- client/script.js
+++ client/script.js
@@ -1,36 +1,26 @@
 
-//This will get the data into console
-fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.error(err))
-
 //this will populate the data from json to the data html page
-fetch("https://data.princegeorgescountymd.gov/resource/jh2p-ym6a.json")
-.then(function(response){
-    return response.json();
-})
-.then(function(products){
-    let placeHolder = document.querySelector("#data-output");
-    let out = "";
 
-    let num = 1;
-    for(let product of products) {
-        out += `
-        <tr> 
-        <td class = "tableData">${num++}</td>
-        <td class = "tableData">${product.payee_name}</td>
-        <td class = "tableData">${product.agency}</td>
-        <td class = "tableData">${product.zip_code}</td>
-        <td class = "tableData">${product.amount}</td>
-        <td class = "tableData">${product.payment_description}</td>
-        </tr>
-        `;
-    }
+async function createTable(json) {
+  
+  let placeHolder = document.querySelector("#data-output");
+  let out = "";
 
-    placeHolder.innerHTML = out;
-})
-
+  let num = 1;
+  for (let x of json) {
+    out += `
+      <tr> 
+      <td class = "tableData">${num++}</td>
+      <td class = "tableData">${x.payee_name}</td>
+      <td class = "tableData">${x.agency}</td>
+      <td class = "tableData">${x.zip_code}</td>
+      <td class = "tableData">${x.amount}</td>
+      <td class = "tableData">${x.payment_description}</td>
+      </tr>
+      `;
+  }
+  placeHolder.innerHTML = out;
+}
 
 // Code for the buttons //
 async function fetchData() {
@@ -38,17 +28,16 @@ async function fetchData() {
   const response = await fetch(url);
 
   const dataPoints = await response.json();
-  console.log(dataPoints);
   return dataPoints;
-};
+}
 
 // Code for the fetch payee buttons //
 async function getPayee() {
-  fetchData().then(dataPoints => {
-    const payeeDat = dataPoints.map(function(index){
-      return index.payee_name
-    })
-    const unique = [...new Set(dataPoints.map(item => item.payee_name))];
+  fetchData().then((dataPoints) => {
+    const payeeDat = dataPoints.map(function (index) {
+      return index.payee_name;
+    });
+    const unique = [...new Set(dataPoints.map((item) => item.payee_name))];
     const count = {};
 
     for (const element of payeeDat) {
@@ -62,18 +51,17 @@ async function getPayee() {
     myChart.config.data.datasets[0].label = "Total";
     myChart.config.data.datasets[0].data = Object.values(count);
     myChart.update();
-  })
-
-};
+  });
+}
 
 // Code for the fetch agency buttons //
 async function getAgency() {
-  fetchData().then(dataPoints => {
-    const agencyDat = dataPoints.map(function(index){
-      return index.agency
-    })
+  fetchData().then((dataPoints) => {
+    const agencyDat = dataPoints.map(function (index) {
+      return index.agency;
+    });
 
-    const unique = [...new Set(dataPoints.map(item => item.agency))];
+    const unique = [...new Set(dataPoints.map((item) => item.agency))];
     const count = {};
 
     for (const element of agencyDat) {
@@ -87,16 +75,16 @@ async function getAgency() {
     myChart.config.data.datasets[0].label = "Total";
     myChart.config.data.datasets[0].data = Object.values(count);
     myChart.update();
-  })
-};
+  });
+}
 
 // Code for the fetch zipcode buttons //
 async function getZip() {
-  fetchData().then(dataPoints => {
-    const zipDat = dataPoints.map(function(index){
-      return index.zip_code
-    })
-    const unique = [...new Set(dataPoints.map(item => item.zip_code))];
+  fetchData().then((dataPoints) => {
+    const zipDat = dataPoints.map(function (index) {
+      return index.zip_code;
+    });
+    const unique = [...new Set(dataPoints.map((item) => item.zip_code))];
     const count = {};
 
     for (const element of zipDat) {
@@ -110,117 +98,132 @@ async function getZip() {
     myChart.config.data.datasets[0].label = "Total";
     myChart.config.data.datasets[0].data = Object.values(count);
     myChart.update();
-  })
-
-};
+  });
+}
 
 //need to set this to get range instead of unique number
 async function getAmount() {
-  fetchData().then(dataPoints => {
-    const amountDat = dataPoints.map(function(index){
-      return index.amount
-    })
+  fetchData().then((dataPoints) => {
+    const amountDat = dataPoints.map(function (index) {
+      return index.amount;
+    });
     const count = {};
 
     const range = [100, 200, 300, 400, 500, 600, 700, 800, 900, 1000];
     console.log(range);
 
-    for(const x of amountDat) {
-      for(const y of range) {
-
-         if(x < y && x > y - 100) {
-            if(count[y]) {
-              count[y] += 1;
-            }else{
-              count[y] = 1;
-            }
-         }
+    for (const x of amountDat) {
+      for (const y of range) {
+        if (x < y && x > y - 100) {
+          if (count[y]) {
+            count[y] += 1;
+          } else {
+            count[y] = 1;
+          }
+        }
       }
     }
 
-    const labels = ["0-99", "100-199", "200-299", "300-399", "400-499","500-599", "600-699", "700-799", "800-899", "900-999", "1000+"];
+    const labels = [
+      "0-99",
+      "100-199",
+      "200-299",
+      "300-399",
+      "400-499",
+      "500-599",
+      "600-699",
+      "700-799",
+      "800-899",
+      "900-999",
+      "1000+",
+    ];
 
     myChart.config.data.labels = labels;
     myChart.config.data.datasets[0].label = "Total";
     myChart.config.data.datasets[0].data = Object.values(count);
     myChart.update();
-  })
-
-};
-
+  });
+}
 
 //Change chart type
-async function BarChart(){
+async function BarChart() {
   myChart.config.type = "bar";
   myChart.update();
 }
 
-async function DoughnutChart(){
+async function DoughnutChart() {
   myChart.config.type = "doughnut";
   myChart.update();
 }
 // Code for the buttons // top
 
-
-
-function filterTable(array, filterItem){
+function filterTable(array, filterItem) {
   return array.filter((item) => {
-    const lowerCaseName = item.agency.toLowerCase();
+    const lowerCaseName = JSON.stringify(item).toLowerCase();
     const lowerCaseQuery = filterItem.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
-  })
+  });
 }
 
 function injectHTML(list) {
-  console.log('fired injectHTML');
-  const target = document.querySelector('#data-output');
-  target.innerHTML = '';
+  console.log("fired injectHTML");
+  const target = document.querySelector("#data-output");
+  target.innerHTML = "";
 
-  const listEl = document.createElement('ol');
+  const listEl = document.createElement("ol");
   target.appendChild(listEl);
 
   list.forEach((item) => {
-    const el = document.createElement('li');
+    const el = document.createElement("li");
     el.innerText = item.name;
     listEl.appendChild(el);
   });
 }
 
-async function mainEvent(){
+async function mainEvent() {
 
-  const form = document.querySelector('.main_form');
+  const form = document.querySelector(".main_form");
 
-  console.log("inside mainevent");
+  const response = await fetchData();
 
-  if(response.data?.length > 0){
+  createTable(response);
 
-    console.log("inside if");
+  if (response.length > 0) {
 
-    form.addEventListener('input', (event) => {
-      console.log(event.target.value);
+    form.addEventListener("input", (event) => {
 
+      const myFILData = filterTable(response, event.target.value);
+
+      createTable(myFILData);
+      console.log(myFILData);
     });
   }
-
 }
 
-
-
-const ctx = document.getElementById('chart');
+const ctx = document.getElementById("chart");
 
 const myChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange', 'Black', 'Grey', 'Pink'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  }
+    labels: [
+      "Red",
+      "Blue",
+      "Yellow",
+      "Green",
+      "Purple",
+      "Orange",
+      "Black",
+      "Grey",
+      "Pink",
+    ],
+    datasets: [
+      {
+        label: "# of Votes",
+        data: [12, 19, 3, 5, 2, 3],
+        borderWidth: 1,
+      },
+    ],
+  },
 });
 
-document.addEventListener('DomContentLoaded', async () => mainEvent());
-
-
-
+document.addEventListener("DOMContentLoaded", async () => mainEvent()); // the async keyword means we can make API requests

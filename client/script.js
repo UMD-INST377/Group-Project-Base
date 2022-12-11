43c7e073ab104ef6f5f6ad3e57aa772e028976a0
Bo Wu <bw1@terpmail.umd.edu>
2022-12-10 20:46:19 -0500

    commit 8

--- client/script.js
+++ client/script.js
@@ -1,11 +1,10 @@
 function injectHTML(list, htmlelm) {
   let target = document.querySelector(htmlelm);
   target.innerHTML = '';
-
-
+  
   // populate table head
   const head = document.createElement('tr');
-  for (const key of Object.keys(list['data'][0])) {
+  for (const key of Object.keys(list[0])) {
     const th = document.createElement('th');
     th.innerText = cap(key);
     head.appendChild(th);
@@ -13,7 +12,7 @@ function injectHTML(list, htmlelm) {
   target.appendChild(head);
 
   // populate table content
-  for (const [key, value] of Object.entries(list['data'])) {
+  for (const [key, value] of Object.entries(list)) {
     const tr = document.createElement('tr');
     const row = Object.values(value);
     row.forEach(element => {
@@ -30,20 +29,6 @@ function cap(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-function arrayToTable(data) {
-  const keys = [...data.reduce((all, obj) => {
-    Object.keys(obj).forEach(key => all.add(key));
-    return all;
-  }, new Set())];
-
-  const header = keys.map(key => `<th>${key}</th>`).join('')
-  const tbody = data.map(row => keys.map(key => `<td>${row[key]}</td>`).join('')).map(row => `<tr>${row}</tr>`)
-
-  return `<table>
-      <thead><tr>${header}</tr></thead>
-      <tbody>${tbody}</body>
-  </table>`;
-}
 
 function getRandInt(min, max) {
   min = Math.ceil(min)
@@ -51,32 +36,14 @@ function getRandInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function processRestaurants(list) {
-  console.log('fired restaurants list');
-  const range = [...Array(15).keys()];
-  const arr = range.map((i) => {
-    const index = getRandInt(0, list.length);
-    return list[index]
-  })
-  return arr;
-  /*
-    ## Process Data Separately From Injecting It
-      This function should accept your 1,000 records
-      then select 15 random records
-      and return an object containing only the restaurant's name, category, and geocoded location
-      So we can inject them using the HTML injection function
-      */
-}
-
-function filterlist(array, filterInputvalue) {
-  return array.filter((i) => {
-    if (!i.name) { return; }
-    const lowerCaseName = i.name.toLowerCase();
+function filterlist(list, filterInputvalue) {
+  return list.filter((i) => {
+    if (!i) { return; }
+    const lowerCaseName = JSON.stringify(i).toLowerCase();
     const lowerCaseQuery = filterInputvalue.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
   });
 }
-
 
 function addButt(htmlelm) {
   const form = document.querySelector(htmlelm);
@@ -108,79 +75,54 @@ async function fetchJson(yr) {
 }
 
 async function mainEvent() {
+  
+  // hide button
+  const submit = document.querySelector('#get');
+  submit.style.display = 'none';
+
+  // show the table 
+  let yrtitle = '2022';
+  let yr = document.querySelector('.yr_form');
+  let data = await fetchJson(2022);
+  injectHTML(data['data'], '#rlist');
+  let table = document.querySelector('#yrtitle');
+  let title = document.createElement('h2');
+  title.innerText = 'Data from ' + yrtitle;
+  table.appendChild(title);
 
   // click button to change the year
-  let yr = document.querySelector('.yr_form');
-  injectHTML(await fetchJson(2022), '#rlist');
   yr.addEventListener('input', async (event) => {
     event.preventDefault();
-    let data = await fetchJson(event.target.value);
-    console.log(data);
+    data = await fetchJson(event.target.value);
+    console.log(data.data.length);
     if (data.data.length > 0) {
       document.querySelector('#rlist')
-        .addEventListener('load', injectHTML(data, '#rlist'))
+        .addEventListener('load', injectHTML(data['data'], '#rlist'));
+      yrtitle = await event.target.value;
+      // add table title
+      title.innerText = 'Data from ' + yrtitle;
+      table.replaceChild(title);
     }
   });
 
-
-  /*
-    // the async keyword means we can make API requests
-    const form = document.querySelector('.main_form');
-    const submit = document.querySelector('#get'); // get a reference to your submit button
-    submit.style.display = 'none'; // let your submit button disappear
-  */
-  /*
-    Let's get some data from the API - it will take a second or two to load
-    This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-    It's at about line 27 - go have a look and see what we're retrieving and sending back.
-   */
-  const results = await fetch('/');
-  const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
-
-  console.table(arrayFromJson.data);
-
-  // in your browser console, try expanding this object to see what fields are available to work with
-  // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
-
-  // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
-
-  // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
-    submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
-    // And here's an eventListener! It's listening for a "submit" button specifically being clicked
-    // this is a synchronous event event, because we already did our async request above, and waited for it to resolve
-    let currentlist = [];
-
-    form.addEventListener('input', (event) => {
+  // search box
+  if (data.data.length > 0) {
+    let search = document.querySelector('.sr_form');
+    search.addEventListener('input', async (event) => {
       console.log(event.target.value)
-      const tlist = filterlist(currentlist, event.target.value);
-      injectHTML(tlist);
-    })
-
-    form.addEventListener('submit', (submitEvent) => {
-      // This is needed to stop our page from changing to a new URL even though it heard a GET request
-      submitEvent.preventDefault();
-
-      // This constant will have the value of your 15-restaurant collection when it processes
-      currentlist = processRestaurants(arrayFromJson.data);
-      console.log(currentlist);
-
-      // And this function call will perform the "side effect" of injecting the HTML list for you
-      injectHTML(currentlist);
-
-      // By separating the functions, we open the possibility of regenerating the list
-      // without having to retrieve fresh data every time
-      // We also have access to some form values, so we could filter the list based on name
+      const flist = await filterlist(data['data'], event.target.value);
+      injectHTML(flist, '#rlist');
+    });
+    submit.style.display = 'block';
+    search.addEventListener('submit', async (event) => {
+      event.preventDefault();
     });
   }
+
+  expo = data;
 }
 
-/*
-This last line actually runs first!
-It runs first because the listener is set to when your HTML content has loaded
-*/
-addButt('#butts');
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+// export the variable
+let expo;
+// run main
+document.addEventListener('DOMContentLoaded', async () => mainEvent());

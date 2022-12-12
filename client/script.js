a0511f2a70196cf071b17300846bca879f5e02ea
govindnageswaran <govindnageswaran222@gmail.com>
2022-12-12 14:01:53 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -9,7 +9,6 @@ async function mainEvent() {
       'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
     }
   };
-  
 
   const dataGet = await fetch('https://api-nba-v1.p.rapidapi.com/games?team=41&season=2021', options);
   const dataGet1 = await fetch('https://api-nba-v1.p.rapidapi.com/games?team=1&season=2021', options);
@@ -19,7 +18,6 @@ async function mainEvent() {
   const arrayFromJson2 = await dataGet1.json();
   const arrayFromJson3 = await dataGet2.json();
 
-
   const wizards_data = arrayFromJson.response;
   const hawks_data = arrayFromJson2.response;
   const celtics_data = arrayFromJson3.response;
@@ -28,95 +26,58 @@ async function mainEvent() {
   console.log(hawks_data);
   console.log(celtics_data);
 
+  const wizards_result = count_wins(wizards_data);
+  const wizards_points = get_total_points(wizards_data);
+  const hawks_result = count_wins(hawks_data);
+  const hawks_points = get_total_points(hawks_data);
+  const celtics_result = count_wins(celtics_data);
+  const celtics_points = get_total_points(celtics_data);
 
-  let wizards_result = count_wins(wizards_data);
-  let wizards_points = get_total_points(wizards_data);
-  let hawks_result = count_wins(hawks_data);
-  let hawks_points = get_total_points(hawks_data);
-  let celtics_result = count_wins(celtics_data)
-  let celtics_points = get_total_points(celtics_data);
+  const win_lost = initChart2();
+  const points_graph = initChart3();
 
-  let win_lost =initChart2();
-  let points_graph= initChart3();
-
-  
-
-
-  query_team_data.addEventListener('click',(event) => {
+  query_team_data.addEventListener('click', (event) => {
     removeData(win_lost);
     removeData(points_graph);
     const team_id = event.target.id;
-    
-    console.log(team_id)
 
-    if(team_id === '41' ){
-      console.log(team_id)
-      addData(win_lost,['Wins','Loss'], [wizards_result[0],wizards_result[1]])
-      addData(points_graph,wizards_points[0], wizards_points[1])
+    console.log(team_id);
+
+    if (team_id === '41') {
+      console.log(team_id);
+      addData(win_lost, ['Wins', 'Loss'], [wizards_result[0], wizards_result[1]]);
+      addData(points_graph, wizards_points[0], wizards_points[1]);
+    } else if (team_id === '1') {
+      console.log(team_id);
+      addData(win_lost, ['Wins', 'Loss'], [hawks_result[0], hawks_result[1]]);
+      addData(points_graph, hawks_points[0], hawks_points[1]);
+    } else if (team_id === '2') {
+      console.log(team_id);
+      addData(win_lost, ['Wins', 'Loss'], [celtics_result[0], celtics_result[1]]);
+      addData(points_graph, celtics_points[0], celtics_points[1]);
+    } else {
+      console.log('This is not working');
     }
-    else if(team_id === '1'){
-      console.log(team_id)
-      addData(win_lost,['Wins','Loss'], [hawks_result[0],hawks_result[1]])
-      addData(points_graph,hawks_points[0], hawks_points[1])
-    }else if(team_id === '2'){
-      console.log(team_id)
-      addData(win_lost,['Wins','Loss'], [celtics_result[0],celtics_result[1]])
-      addData(points_graph,celtics_points[0], celtics_points[1])
-    }
-    else{
-      console.log("This is not working")
-    }
-  
-    });
+  });
 
-
-
-
-
-
-
-
-
-
-
-  function count_wins(team_data){
+  function count_wins(team_data) {
     let team_home_wins = 0;
     let team_home_loss = 0;
-   /* console.log(typeof Object.values(wizards_data));
-    const new_wizarddata = Object.values(wizards_data);*/
-    team_data.forEach((item) =>{
-   team_home_wins += item.scores.home.series.win;
-   team_home_loss += item.scores.home.series.loss;
+    /* console.log(typeof Object.values(wizards_data));
+    const new_wizarddata = Object.values(wizards_data); */
+    team_data.forEach((item) => {
+      team_home_wins += item.scores.home.series.win;
+      team_home_loss += item.scores.home.series.loss;
+    });
+    return [team_home_wins, team_home_loss];
   }
-  );
-  return [team_home_wins,team_home_loss];
-}
 
-
-
-
-
-
-
-
-  function get_total_points(team_data){
-
-   const team_points =team_data.map((item)=>{
-      return item.scores.home.points;
-    })
-    const number_of_games =team_data.map((item , index)=>{
-      return index;
-    })
-    return [number_of_games,team_points];
+  function get_total_points(team_data) {
+    const team_points = team_data.map((item) => item.scores.home.points);
+    const number_of_games = team_data.map((item, index) => index);
+    return [number_of_games, team_points];
   }
-  
 
-
-
- 
-  
-
-  
   function initChart2() {
     const data = {
       labels: [
@@ -126,7 +87,7 @@ async function mainEvent() {
       datasets: [{
         label: '',
         // eslint-disable-next-line max-len
-        data: [wizards_result[0],wizards_result[1]],
+        data: [wizards_result[0], wizards_result[1]],
         backgroundColor: [
           'rgb(255, 99, 132)',
           'rgb(54, 162, 235)'
@@ -139,15 +100,10 @@ async function mainEvent() {
     return new Chart(document.querySelector('#myChart1'), config);
   }
 
-
-
-
-
   function initChart3() {
     const data = {
-      labels: 
-        wizards_points[0]
-      ,
+      labels:
+        wizards_points[0],
       datasets: [{
         label: 'Points',
         // eslint-disable-next-line max-len
@@ -161,12 +117,7 @@ async function mainEvent() {
     const config = {type: 'line', data: data};
 
     return new Chart(document.querySelector('#myChart2'), config);
-  
- 
   }
-
-
-
 }
 function ShowDiv() {
   document.getElementById('myDiv').style.display = '';
@@ -194,22 +145,15 @@ function imageClickedTeams() {
 //   }
 // }
 
-
-
-
-
-
-
 function addData(chart, label, data) {
-
- chart.data.labels = label;
-  chart.data.datasets[0].data=data;
+  chart.data.labels = label;
+  chart.data.datasets[0].data = data;
   chart.update();
 }
 
 function removeData(chart) {
   chart.data.labels = [];
-  chart.data.datasets[0].data=[];
+  chart.data.datasets[0].data = [];
   chart.update();
 }
 

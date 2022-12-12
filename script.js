4f907d21b1f1cb12efb7c44599c1244436c036b3
Jason Kearney <76897730+Fictionfriction@users.noreply.github.com>
2022-12-12 13:40:19 -0500

    add comment

--- script.js
+++ script.js
@@ -94,6 +94,8 @@ async function mainEvent() {
 // */
 document.addEventListener('DOMContentLoaded', async () => mainEvent());
 
+
+// call tv api and return tv show images
 function getComedy() {
   return fetch('https://api.tvmaze.com/shows')
     .then((response) => response.json())

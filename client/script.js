f66f17ab88341f0f59a7260cf4988d2d63f5b8c6
Rijul Newalkar <64926047+rijul-newalkar@users.noreply.github.com>
2022-12-11 19:23:34 -0500

    updates

--- client/script.js
+++ client/script.js
@@ -1,26 +1,31 @@
-const client = new PolygonAdapter({
-  apikey: 'InuXsEB9mOYV2ppfgbNkRhTnuE_rvC3Z',
-  realtimeEnabled: true // True(default) = Use websockets for updates.
+const nba = require('nba.js').default;
+
+nba.stats.allPlayers().then((res) => {
+  console.log(res);
 });
 
-const widget = new TradingView.Widget({
-  fullscreen: true,
-  symbol: 'AAPL',
-  interval: '1D',
-  timezone: 'America/New_York',
-  container_id: 'tv_chart_container', /* ID of the container element */
-  datafeed: client, /* Our Polygon.io Adapter */
-  library_path: '/charting_library/', /* Where your TV Library files reside */
-  locale: 'en',
-  disabled_features: ['use_localstorage_for_settings'],
-  enabled_features: ['study_templates'],
-  charts_storage_url: 'http://saveload.tradingview.com',
-  charts_storage_api_version: '1.1',
-  client_id: 'tradingview.com',
-  user_id: 'public_user_id',
-  theme: 'Dark' /* Light or Dark */
+nba.stats.allPlayers((err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(res);
 });
+
+nba.stats.allPlayers({ Season: '2014-15' }, (err, res) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+
+  console.log(res);
+});
+
+nba.stats.allPlayers({ IsOnlyCurrentSeason: 0 })
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
 
 function mainEvent() {
-  widget()
+
 }

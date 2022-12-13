224d545da56f8a8f843308906b510499560363c5
Rijul Newalkar <64926047+rijul-newalkar@users.noreply.github.com>
2022-12-12 21:22:42 -0500

    updated js file

--- client/script.js
+++ client/script.js
@@ -1,31 +1,49 @@
-const nba = require('nba.js').default;
-
-nba.stats.allPlayers().then((res) => {
-  console.log(res);
-});
-
-nba.stats.allPlayers((err, res) => {
-  if (err) {
-    console.error(err);
-    return;
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'e421dae6f5msh781141400772cc1p133bfdjsn7330a891d044',
+    'X-RapidAPI-Host': 'yahoo-finance15.p.rapidapi.com'
   }
+};
 
-  console.log(res);
-});
+async function getData() {
+  const userInput = document.getElementById('userInput').value;
+  // var test1 = await changeText2()
+  // console.log(test1)
+  url = `https://yahoo-finance15.p.rapidapi.com/api/yahoo/qu/quote/${userInput}/financial-data`;
+  data = await fetch(url, options);
+  res = await data.json();
+  // return res
+  result = res.financialData;
+  char_data = [result.totalRevenue.longFmt, result.grossProfits.longFmt];
+  data_filter = [`Current Price: ${result.currentPrice.raw}`, `Earnings Growth: ${result.earningsGrowth.fmt}`, `Total Revenue: ${result.totalRevenue.fmt}`,
+    `Gross Profits: ${result.grossProfits.fmt}`, `Profit Margins: ${result.profitMargins.fmt}`];
+  // console.log(data_filter)
 
-nba.stats.allPlayers({ Season: '2014-15' }, (err, res) => {
-  if (err) {
-    console.error(err);
-    return;
+  let new_data = '';
+  for (let i = 0; i < data_filter.length; i++) {
+    new_data += `<li>${data_filter[i]}</li>`;
   }
+  const ctx = document.getElementById('myChart');
 
-  console.log(res);
-});
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['Gross Profits', 'Total Revenue'],
+      datasets: [{
+        label: 'Profit Margin',
+        data: [parseInt(char_data[0]), parseInt(char_data[1])],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 
-nba.stats.allPlayers({ IsOnlyCurrentSeason: 0 })
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
-
-function mainEvent() {
-
+  document.getElementById('apiResponse').innerHTML = new_data;
 }

1d0d8782d4305a7bbb31f9125d5a36e8657fa601
sanaxamjad <98844696+sanaxamjad@users.noreply.github.com>
2023-05-02 13:13:03 -0400

    Initial Commit 12 Final Project

--- script.js
+++ script.js
@@ -51,6 +51,69 @@ getEarthquakeData().then(quakes => {
 
   // Add the marker clusters layer to the map
   map.addLayer(markers);
-});
 
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
+  // Add a text box filter to the earthquake data
+  const filterInput = document.createElement('input');
+  filterInput.setAttribute('type', 'text');
+  filterInput.setAttribute('placeholder', 'Filter earthquakes by location');
+  filterInput.addEventListener('input', (event) => {
+    const query = event.target.value.toLowerCase();
+    markers.clearLayers();
+    quakeMarkers.forEach(quake => {
+      if (quake.title.toLowerCase().includes(query)) {
+        const marker = L.marker(quake.latlng, {
+          title: quake.title,
+          icon: L.divIcon({
+            className: "marker",
+            html: "<span>" + quake.magnitude + "</span>"
+          })
+        });
+        marker.bindPopup("<h3>" + quake.title + "</h3><p><strong>Magnitude:</strong> " + quake.magnitude + "</p><p><strong>Depth:</strong> " + quake.depth + " km</p>");
+        markers.addLayer(marker);
+      }
+    });
+  });
+
+  // Add the filter input to the page
+  const searchBox = document.querySelector('.search-box');
+  searchBox.appendChild(filterInput);
+});
+const searchButton = document.querySelector('#search-btn');
+
+searchButton.addEventListener('click', () => {
+  const searchText = document.querySelector('#search-text').value;
+
+  // Perform the API request using fetch()
+  fetch(`https://api.example.com/search?q=${searchText}`)
+    .then(response => response.json())
+    .then(data => {
+      // Process the response data and display it on the page
+      const resultsContainer = document.querySelector('#search-results');
+      resultsContainer.innerHTML = '';
+
+      if (data.results.length === 0) {
+        resultsContainer.innerHTML = 'No results found.';
+      } else {
+        data.results.forEach(result => {
+          const resultDiv = document.createElement('div');
+          resultDiv.classList.add('result');
+
+          const title = document.createElement('h2');
+          title.innerText = result.title;
+
+          const description = document.createElement('p');
+          description.innerText = result.description;
+
+          resultDiv.appendChild(title);
+          resultDiv.appendChild(description);
+          resultsContainer.appendChild(resultDiv);
+        });
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      // Display an error message on the page
+      const resultsContainer = document.querySelector('#search-results');
+      resultsContainer.innerHTML = 'An error occurred. Please try again later.';
+    });
+});
